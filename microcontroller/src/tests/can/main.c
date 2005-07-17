0978735282a9c89f345da158aca97b8d355bea75
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-07-17 22:12:18 +0000

    CAN Kann

--- microcontroller/src/tests/can/main.c
+++ microcontroller/src/tests/can/main.c
@@ -1,26 +1,65 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/signal.h>
 #include <stdlib.h>
+
+#include "config.h"
+
 #include "util.h"
 #include "uart.h"
 #include "spi.h"
 #include "can.h"
-#include "console.h"
+
+// Timer Interrupt
+SIGNAL(SIG_OUTPUT_COMPARE0)
+{
+	static int count = 0;
+
+	if ( count++ > 0x40 ) {
+		count = 0;
+		PORTC ^= 0x08;
+
+		can_message *msg = can_buffer_get();
+		can_transmit(msg);
+	}
+}
+
+void timer0_on(){
+/* 	TCCR0: FOC0 WGM00 COM01 COM00 WGM01 CS02 CS01 CS00
+		CS02 CS01 CS00
+		 0    0    0	       stop
+		 0    0    1       clk
+		 0    1    0       clk/8
+		 0    1    1       clk/64
+		 1    0    0       clk/256
+		 1    0    1       clk/1024
+	
+*/
+	TCCR0 = 0x08 | 0x05 ;// CTC Mode, clk/1024
+	TCNT0 = 0;	// reset timer
+	OCR0  = 0xff;	// Compare with this value
+	TIMSK = 0x02;	// Compare match Interrupt on
+}
+
 
 int main(){
 	uart_init();
-	console_init();
 	spi_init();
 	can_init();
+	mcp_setmode(normal);
 
 	sei();
 	DDRC = 0xff;
 
 	uart_putc('*');
 
+	timer0_on();
+
 	while(1) {
-		console();
+		can_message *msg = can_get();
+		uart_putc('!');
 	}
+
 	return 0;
 }
