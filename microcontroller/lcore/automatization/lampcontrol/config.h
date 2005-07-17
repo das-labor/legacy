eac940896e7646a564defc7e35b494ccbe7bfe10
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-07-17 01:14:13 +0000

    moving stuff

---
+++ microcontroller/lcore/automatization/lampcontrol/config.h
@@ -0,0 +1,34 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// main.c defines
+#define NUM_LAMPE 4
+
+// util.[ch] defines
+// lap.[ch] defines
+
+// can.[ch] defines
+#undef CAN_INTERRUPT		//set this to enable interrupt driven and buffering version
+#define CAN_RX_BUFFER_SIZE 2	//only used for Interrupt
+#define CAN_TX_BUFFER_SIZE 2	//only used for Interrupt
+
+#define CAN_RXBUFSIZE 128
+
+// spi.[ch] defines
+#undef SPI_HARDWARE 
+#define SPI_PORT PORTB		//for slave select
+#define SPI_PIN PINB		//for slave select
+#define SPI_DDR DDRB
+
+#define SPI_PIN_MOSI PB5
+#define SPI_PIN_MISO PB6
+#define SPI_PIN_SCK PB7
+#define SPI_PIN_SS PB4		// for slave select
+
+//interrupt pin of MCP2515 for non interrupt drive can
+#define SPI_PIN_MCP_INT PD2
+	
+
+// 
+
+#endif 
