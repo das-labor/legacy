8e3a3e0c3dacf338318cafed6139a8ffc922d1a8
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-08-02 23:40:08 +0000

    Borg basis für alle Borgs

---
+++ microcontroller/src/borg/borg-base/main.c
@@ -0,0 +1,35 @@
+
+#include <avr/interrupt.h>
+#include "config.h"
+#include "scrolltext.h"
+#include "programm.h"
+#include "borg_hw.h"
+#include "pixel.h"
+
+
+
+int main (void){
+	clear_screen(0);
+	borg_hw_init();
+	sei();
+	
+	for(;;){
+		matrix1();
+		fadein();
+		joern1();
+		//test1();
+		snake();
+		schachbrett(20);
+		spirale(20);
+		labor_borg();
+		feuer();
+		
+
+//		testline();
+//		test1();
+//		draw_l();
+
+//		clear_screen;
+//     		wait(500);	
+	}
+}
