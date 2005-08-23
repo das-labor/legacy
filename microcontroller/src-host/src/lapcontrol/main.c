792a141b4c4abd9530118ff8717ea6e32dce17c5
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-08-23 16:19:25 +0000

    trying to get this lapcontrol out of the door

--- microcontroller/src-host/src/lapcontrol/main.c
+++ microcontroller/src-host/src/lapcontrol/main.c
@@ -32,14 +32,72 @@ struct option longopts[] =
   { NULL, 0, NULL, 0 }
 };
 
+/**
+ * Available Subcommands
+ */
+typedef struct {
+	void (*fkt)(int, char**);
+	char *cmd;
+	char *sig;
+	char *desc;
+} cmd_t;
+
+void fkt_ping(int argc, char *argv[]) 
+{
+	can_addr addr = atoi(argv[1]);
+	lap_ping(addr);
+};
+
+void fkt_reset(int argc, char *argv[]) 
+{
+};
+
+void fkt_dump(int argc, char *argv[]) 
+{
+	can_message *msg;
+
+	while(1) {
+		msg = can_get_nb();
+		
+		if (msg) {
+			printf( "%10d: %02x:%02x -> %02x:%02x\n", localtime(),
+					msg->addr_src, msg->port_src,
+					msg->addr_dst, msg->port_dst );
+		}
+		can_free(msg);
+	}
+};
+
+
+
+cmd_t cmds[] = {
+  { &fkt_dump,  "dump", "dump", "Packet dump from CAN bus" },
+  { &fkt_reset, "reset", "reset <addr>", "Send reset to <addr>" },
+  { &fkt_ping,  "ping",	"ping <addr>", "Send ping to <addr>" },
+//  { &ftk_lampe, "lampe", "lampe ...",  "Control lampe-device" }
+  { NULL, NULL, NULL, NULL }
+};
+
+
 void help()
 {
-	printf("\nUsage: %s [OPTIONS]\n", progname);
-	printf("\
+	cmd_t *ncmd;
+
+	printf("\nUsage: %s [OPTIONS] <COMMAND>\n", progname);
+	printf("\n\
+Options:\n\n\
    -h, --help              display this help and exit\n\
    -v, --verbose           be more verbose and display a CAN packet dump\n\
    -s, --server HOST       use specified server (default: localhost)\n\
-   -p, --port PORT         use specified TCP/IP port (default: 2342)\n\n" );
+   -p, --port PORT         use specified TCP/IP port (default: 2342)\n\n\
+Commands:\n\n" );
+
+	ncmd = cmds;
+	while(ncmd->fkt) {
+		printf( "   %-20s       %s\n", ncmd->sig, ncmd->desc );
+
+		ncmd++;
+	}
 }
 
 void print_packets(){
@@ -81,14 +139,27 @@ int main(int argc, char *argv[])
 		}
 	} // while
 
+
+	if (optind == argc) {
+		help();
+		exit(1);
+	}
+
 	can_init(server, tcpport);
 	debug(1, "CAN communication established" );
 
+	char *arg = argv[optind];
 
-	lap_ping( 0xff );
+	cmd_t *cmd = cmds;
+	while(cmd->fkt) {
+		if (strcmp(arg, cmd->cmd) == 0) {
+			(*(cmd->fkt))(argc-optind, &(argv[optind]));
+			goto done;
+		}
+		cmd++;
+	}
 
-//	can_message *msg = can_buffer_get();
-//	can_transmit(msg);
+done:
 	cann_close(0);
 
 	return 0;
