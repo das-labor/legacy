e6bdacf618e75b4efb1e5d20584ace0a64f43cc0
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-08-25 18:55:34 +0000

    generating packets seems to work // missing files

---
+++ microcontroller/src-host/src/lapcontrol/cmds-base.h
@@ -0,0 +1,14 @@
+#ifndef CMDSBASE_H
+#define CMDSBASE_H
+
+/**
+ * Some available commands  
+ */
+
+void fkt_ping(int argc, char *argv[]);
+void fkt_reset(int argc, char *argv[]);
+void fkt_dump(int argc, char *argv[]);
+void fkt_packet(int argc, char *argv[]);
+
+
+#endif // CMDSBASE_H
