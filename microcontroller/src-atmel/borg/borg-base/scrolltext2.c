98f72db7f4ea26f92f1e9e250e683ee35b154551
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-09-18 15:12:45 +0000

    added can to borg-ls

--- microcontroller/src-atmel/borg/borg-base/scrolltext2.c
+++ microcontroller/src-atmel/borg/borg-base/scrolltext2.c
@@ -21,8 +21,8 @@ struct FONT_DEF
 {
    unsigned char store_width;            /* glyph storage width in bytes */
    unsigned char glyph_height;  		 /* glyph height for storage */
-   unsigned char *glyph_table;      /* font table start address in memory */
-   unsigned char *width_table; 	 /* variable width table start adress */
+   const unsigned char * glyph_table;      /* font table start address in memory */
+   const unsigned char *width_table; 	 /* variable width table start adress */
    unsigned char glyph_beg;			 	 /* start ascii offset in table */
    unsigned char glyph_end;				 /* end ascii offset in table */
    unsigned char glyph_def;				 /* code for undefined glyph code */
