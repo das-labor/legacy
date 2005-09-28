10ccc6c0983bec20ff31fadf120434054c949236
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-09-28 21:05:04 +0000

    split scrolltext2 into fonts.c

---
+++ microcontroller/src-atmel/borg/borg-ls-orig/fonts.h
@@ -0,0 +1,17 @@
+#ifndef BORG_FONTS_H
+#define BORG_FONTS_H
+
+struct FONT_DEF 
+{
+   unsigned char store_width;            /* glyph storage width in bytes */
+   unsigned char glyph_height;           /* glyph height for storage */
+   const unsigned char *glyph_table;     /* font table start address in memory */
+   const unsigned char *width_table;     /* variable width table start adress */
+   unsigned char glyph_beg;              /* start ascii offset in table */
+   unsigned char glyph_end;              /* end ascii offset in table */
+   unsigned char glyph_def;              /* code for undefined glyph code */
+};
+
+extern struct FONT_DEF font[];
+
+#endif // BORG_FONTS_H
