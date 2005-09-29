d4f7d6af86c62bf235a1d61269926c86ead37d57
madex <madex@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-09-29 23:07:39 +0000

    neue tolle animationsengine in scrolltext3.c.

--- microcontroller/src-atmel/borg/borg-ls-apiSimulator/scrolltext3.c
+++ microcontroller/src-atmel/borg/borg-ls-apiSimulator/scrolltext3.c
@@ -14,11 +14,11 @@
 #include "font-uni53.h"
 
 #define MAX_FONTS 1
-font fonts[MAX_FONTS];
+font fonts[MAX_FONTS] = {font_uni53};
 
 
 void shift_in(unsigned char glyph, unsigned char fontNr, unsigned int delay){
-	unsigned char  y, s, len;
+	unsigned char  y, s, len, byte;
 	unsigned int i;
 	if (fontNr >= MAX_FONTS) 
 		fontNr = MAX_FONTS - 1;
@@ -26,11 +26,12 @@ void shift_in(unsigned char glyph, unsigned char fontNr, unsigned int delay){
             glyph = fonts[fontNr].glyph_def;
     } 
 	glyph -= fonts[fontNr].glyph_beg;
-	for (i = pgm_read_byte(fonts[fontNr].fontIndex+glyph); i < pgm_read_byte(fonts[fontNr].fontIndex+glyph+1); i++) {
+	for (i = pgm_read_word(fonts[fontNr].fontIndex+glyph); i < pgm_read_word(fonts[fontNr].fontIndex+glyph+1); i++) {
 		shift_pixmap_l();
 		for (s = 0; s < fonts[fontNr].storebytes; s++) {
+            byte = pgm_read_byte(fonts[fontNr].fontData+fonts[fontNr].storebytes*i+s);
 			for (y = 7; y < 8; y--) {
-				if (pgm_read_byte(fonts[fontNr].fontData+fonts[fontNr].storebytes*i+s) & (1 << y)) {	
+				if (byte & (1 << y)) {	
 						setpixel((pixel){NUM_COLS-1, y+(fonts[fontNr].storebytes-1-s)*8}, 3);
 				}
 			}
@@ -51,10 +52,66 @@ void shift_out(unsigned char cols, unsigned int delay){
 }
 
 void scrolltext(char *str, unsigned char fontNr, unsigned int delay) {
-	 fonts[0] = font_uni53;
-     char *tmp = str;
-     while (*tmp) {
-        shift_in(*tmp++, fontNr, delay);
+     unsigned char x, y;
+     //char *tmp = str;
+     //while (*tmp) {
+     //   shift_in(*tmp++, fontNr, delay);
+     //}
+     //shift_out(NUM_COLS+8, delay);
+     clear_screen(0);
+     draw_Text(str, 72, 1, 0, 1, 3);
+     wait(3000);
+     clear_screen(0);
+     for (x = 0; x < 80; x++) {
+         draw_Text(str, x, 0, 0, 1, x%2?1:3);
+         wait(100-x);
+         clear_screen(0);
      }
-     shift_out(NUM_COLS+8, delay);
+     draw_Text(str, 80, 3, 0, 1, 3);
+     wait(70);
+     clear_screen(0);
+     for (x = 80; x > 60; x--) {
+         draw_Text(str, x, 0, 0, 1, 3);
+         wait(70);
+         clear_screen(0);
+     }
+}
+
+               // Pos
+void draw_Text(char *str, char posx, char posy, unsigned char fontNr, unsigned char space, unsigned char color) {
+	unsigned char byte;
+    char x, y, glyph = *str;
+	unsigned int charC, charEnd;
+	if (fontNr >= MAX_FONTS) 
+		fontNr = MAX_FONTS - 1;
+	if ((glyph < fonts[fontNr].glyph_beg) || (glyph > fonts[fontNr].glyph_end)) {
+       glyph = fonts[fontNr].glyph_def;
+    } 
+	glyph -= fonts[fontNr].glyph_beg;
+	
+	charC = pgm_read_word(fonts[fontNr].fontIndex+glyph);
+	charEnd = pgm_read_word(fonts[fontNr].fontIndex+glyph+1);
+	if (fontNr >= MAX_FONTS) 
+		fontNr = MAX_FONTS - 1;
+	for (x = posx; x; x--) {
+        byte = pgm_read_byte(fonts[fontNr].fontData+fonts[fontNr].storebytes*charC);
+		for (y = posy; y < NUM_ROWS; y++) {
+			if (byte & (1 << (y-posy))) {	
+               setpixel((pixel){x, y}, color);
+			}	
+		}
+		if (charC < charEnd) {                  
+            charC++;
+        } 
+        if (charC >= charEnd) {
+            x -= space;
+            if (!(glyph = *++str)) return;      
+            if ((glyph < fonts[fontNr].glyph_beg) || (glyph > fonts[fontNr].glyph_end)) {      
+               glyph = fonts[fontNr].glyph_def;
+            } 
+            glyph -= fonts[fontNr].glyph_beg;   
+            charC = pgm_read_word(fonts[fontNr].fontIndex+glyph);
+            charEnd = pgm_read_word(fonts[fontNr].fontIndex+glyph+1);
+        }
+	}	
 }
