ecccbbcc7b7ea17ca0082f6f0027e67d0e438bb5
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-10-15 19:38:08 +0000

    mih

--- microcontroller/src-atmel/borg/borg-ls-apiSimulator/config.h
+++ microcontroller/src-atmel/borg/borg-ls-apiSimulator/config.h
@@ -2,7 +2,15 @@
 #define NUM_COLS 96
 #define NUM_PLANES 1
 #define NUMPLANE 3
-#define SCROLLTXT_DELAY 60
-#define STREAMER_NUM 30
+
 #define LINEBYTES (((NUM_COLS-1)/8)+1)
 
+#define SCROLLTEXT_STRING_SIZE 128
+#define STREAMER_NUM 100
+
+#define SNAKE_DELAY 50
+
+#define FEUER_N 5
+#define FEUER_S 30
+#define FEUER_DIV 47
+#define FEUER_DELAY 30
--- microcontroller/src-atmel/borg/borg-ls-apiSimulator/util.h
+++ microcontroller/src-atmel/borg/borg-ls-apiSimulator/util.h
@@ -2,7 +2,8 @@
 #define random myRandom
 #ifndef AVR
 extern unsigned char joystick;
-#      define PORTB joystick
+#	define PINB joystick
+#	define PORTB joystick
 #endif
 void myWait(unsigned int ms);
 
