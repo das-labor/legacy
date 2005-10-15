17509a12ed1cf2d08bd0ee9f525ef87494dedc98
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-10-15 19:38:08 +0000

    mih

--- microcontroller/src-atmel/borg/borg-ls-apiSimulator/scrolltext2.h
+++
@@ -1,2 +0,0 @@
-void scrolltext(char *str, unsigned char fontNr, unsigned int delay);
-void draw_Text(char *str, unsigned posx, char posy, unsigned char fontNr, unsigned char space, unsigned char color);
