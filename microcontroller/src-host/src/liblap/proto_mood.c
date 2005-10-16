5b4e6f680ff8f3831099ce647dd993c70109c6f1
joern <joern@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-10-16 03:41:49 +0000

    argh!

--- microcontroller/src-host/src/liblap/proto_mood.c
+++ microcontroller/src-host/src/liblap/proto_mood.c
@@ -2,6 +2,12 @@
 #include "lap.h"
 #include "proto_mood.h"
 
+struct moodvals{
+uint8_t v1;
+uint8_t v2;
+uint8_t v3;
+uint8_t v4;
+};
 unsigned char mood_set_mood( can_addr dst, unsigned char mood,unsigned char val1, unsigned char val2, unsigned char val3, unsigned char val4 )
 {	
 	pdo_message * msg  = (pdo_message *) can_buffer_get();
@@ -23,25 +29,21 @@ unsigned char mood_set_mood( can_addr dst, unsigned char mood,unsigned char val1
 }
 
 
-void mood_get_mood(can_addr dst,uint8_t mood,int * val1,int * val2,int * val3,int * val4){
+int mood_get_mood(can_addr dst,int mood,int moodled){
+	int mv;
 	pdo_message * msg  = (pdo_message *) can_buffer_get();
-       	
 	msg->port_dst = PORT_MOOD;
 	msg->port_src = PORT_MGT;
 	msg->addr_dst = dst;
 	msg->addr_src =	0x00;
-	msg->dlc      = 6;
+	msg->dlc      = 2;
 	msg->cmd      = FKT_MOOD_GET;
 	msg->data[0]  = mood;
 	can_transmit((can_message *)msg);
 	
 	pdo_message * rmsg =(pdo_message*) can_get();
 	while(rmsg->addr_src != dst && rmsg->addr_dst != 0x00 && rmsg->cmd != FKT_MOOD_GET){
-		*val1=msg->data[0];
-		*val2=msg->data[1];
-		*val3=msg->data[2];
-		*val4=msg->data[3];
-
+		mv=msg->data[moodled];
 	}
-	
+	return  mv;
 }
--- microcontroller/src-host/src/liblap/proto_mood.h
+++ microcontroller/src-host/src/liblap/proto_mood.h
@@ -4,6 +4,6 @@
 // functions
 unsigned char mood_set_mood( can_addr dst,unsigned char mood, unsigned char val1, unsigned char val2, unsigned char val3, unsigned char val4 );
 
-void mood_get_mood(can_addr dst,uint8_t mood,int * val1,int * val2,int * val3,int * val4);
+int mood_get_mood(can_addr dst,int mood,int moodled);
 
 #endif
