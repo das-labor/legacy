9e8d9b3adf0f7558661544a774c52b0615d18233
joern <joern@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-10-16 01:41:43 +0000

    na frisst mono das?

--- microcontroller/src-host/src/liblap/proto_mood.h
+++ microcontroller/src-host/src/liblap/proto_mood.h
@@ -4,6 +4,6 @@
 // functions
 unsigned char mood_set_mood( can_addr dst,unsigned char mood, unsigned char val1, unsigned char val2, unsigned char val3, unsigned char val4 );
 
-void mood_get_mood(can_addr dst,uint8_t mood,uint8_t * val1,uint8_t * val2,uint8_t * val3,uint8_t * val4);
+void mood_get_mood(can_addr dst,uint8_t mood,int * val1,int * val2,int * val3,int * val4);
 
 #endif
