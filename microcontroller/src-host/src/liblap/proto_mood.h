caf5bb94fcdd6f84b5c7b0de32110686d36ea87c
joern <joern@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-10-16 01:28:02 +0000

    lets give it a try

--- microcontroller/src-host/src/liblap/proto_mood.h
+++ microcontroller/src-host/src/liblap/proto_mood.h
@@ -2,7 +2,8 @@
 #define MOOD_H
 
 // functions
-unsigned char mood_set_mood( can_addr dst, unsigned char val1, unsigned char val2, unsigned char val3, unsigned char val4 );
+unsigned char mood_set_mood( can_addr dst,unsigned char mood, unsigned char val1, unsigned char val2, unsigned char val3, unsigned char val4 );
 
+void mood_get_mood(can_addr dst,uint8_t mood,uint8_t * val1,uint8_t * val2,uint8_t * val3,uint8_t * val4);
 
 #endif
