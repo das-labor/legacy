a4db80f4284d6658b843e32372824c496e6d2a9b
madex <madex@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-11-03 23:30:05 +0000

    aufgerumt und Makefile

--- microcontroller/src-mono/AllMacht/src/callbacks.h
+++
@@ -1,7 +0,0 @@
-#include <gtk/gtk.h>
-
-
-gboolean
-OnWindowDeleteEvent                    (GtkWidget       *widget,
-                                        GdkEvent        *event,
-                                        gpointer         user_data);
