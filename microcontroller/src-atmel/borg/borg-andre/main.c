bdb65c08cdb6b091f38776e266d3afd4407af1aa
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-11-27 00:14:56 +0000

    svn sucks!

---
+++ microcontroller/src-atmel/borg/borg-andre/main.c
@@ -0,0 +1,70 @@
+
+#include <setjmp.h>
+#include <avr/interrupt.h>
+
+#include "config.h"
+#include "scrolltext.h"
+#include "programm.h"
+#include "borg_hw.h"
+#include "pixel.h"
+#include "borg_can.h"
+
+jmp_buf newmode_jmpbuf;
+
+int main (void){
+	unsigned char mode;
+
+	clear_screen(0);
+	borg_hw_init();
+	bcan_init();	
+	sei();
+
+	mode = setjmp(newmode_jmpbuf);
+	
+	for(;;){
+		switch(mode++) {
+		case 1:
+			scrolltext(scrolltext_text);
+			break;
+		case 2:
+			spirale(5);
+			break;
+		case 3:
+			joern1();
+			break;
+		case 4:
+			snake();
+			break;
+		case 5:
+			schachbrett(20);
+			break;
+		case 6:
+			feuer();
+			break;
+		case 7:
+			matrix();
+			mode = 1;
+			break;
+		case 31:
+			test_level1();
+			break;
+		case 32:
+			test_level2();
+			break;
+		case 33:
+			test_level3();
+			break;
+		case 34:
+			fadein();
+			break;
+		case 35:
+			test1();
+			break;
+		case 0xFF:
+			off();
+			break;
+		default:
+			break;
+		}
+	}
+}
