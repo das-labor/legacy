b847ba53d31c328974ec2b9f35d50a1e323bd609
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2005-12-06 20:10:06 +0000

    blm support added

--- microcontroller/src-host/src/lapcontrol/cmds-borg.c
+++ microcontroller/src-host/src/lapcontrol/cmds-borg.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <errno.h>
 
 #include <can.h>
 #include <lap.h>
@@ -43,6 +44,143 @@ void cmd_borg_mode(int argc, char *argv[])
 argerror:
 	debug(0, "borg mode <addr> <number>");
 }
+
+void blm_frame_send(int (* bmp)[8] ){
+	
+	pdo_message *msg;
+	unsigned char x;
+	for(x=0;x<8;x+=2){
+		msg = (pdo_message *)can_buffer_get();
+
+		msg->addr_src = 0x00;
+		msg->addr_dst = 0x42;
+		msg->port_src = 0x23;
+		msg->port_dst = 0x23;
+		msg->dlc      = 8;
+		msg->cmd      = 4;
+
+		msg->data[0] = x;
+
+		(*bmp)[x] >>= 1;
+		(*bmp)[x+1] >>= 1;
+		msg->data[1] = (*bmp)[x] & 0xff;	
+		msg->data[2] = ((*bmp)[x]>>8) & 0xff;	
+		msg->data[3] = ((*bmp)[x]>>16) & 0xff;
+		msg->data[4] = ((*bmp)[x+1]) & 0xff;
+		msg->data[5] = ((*bmp)[x+1]>>8) & 0xff;
+		msg->data[6] = ((*bmp)[x+1]>>16) & 0xff;
+		
+		can_transmit((can_message*)msg);
+	}
+}
+
+
+
+void cmd_borg_blm(int argc, char *argv[]){
+	pdo_message *msg;
+	char * filename;
+	FILE * blm;
+	int lc, i, len, duration=0;
+	char buf[100];
+//	char *src, *dst;
+
+//	int addr;
+
+	if (argc != 2) goto argerror;
+
+	filename = argv[1];
+
+	if (strcmp (filename, "-") == 0)
+		blm = stdin;
+	else
+		blm = fopen (filename, "r");
+
+	if (!blm)
+		{
+			fprintf (stderr, "Can't open '%s': %s\n", filename, strerror (errno));
+			return;
+		}
+ 
+	lc = 1;
+	if (!bl_fgets (buf, sizeof (buf), blm) && lc++)
+		goto blerror;
+
+	if (buf[0] != '#')
+		goto blerror;
+
+	i = 1;
+	while (isspace (buf[i]))
+		i++;
+
+	if (strncasecmp (buf + i, "BlinkenLights Movie", 19) != 0)
+		goto blerror;
+	
+	unsigned int bmp[8];
+	unsigned int linecnt=0;	
+			
+	while (bl_fgets (buf, sizeof (buf), blm) && lc++)
+		
+	{
+			len = strlen (buf);
+
+			if (len == 0)
+				continue;
+
+			if (buf[0] == '#')
+				continue;
+
+			if (buf[0] == '@')
+				{
+					fflush (stdout);
+					blm_frame_send(bmp);
+					usleep (duration*1000);
+
+					if (sscanf (buf+1, "%d", &duration) != 1 || duration < 0)
+						duration = 0;
+
+					printf ("\033[2J\033[H");
+					linecnt = 0;
+				}
+			else
+				{
+					/* skip empty lines */
+					for (i = 0; i < len; i++)
+						if (!isspace (buf[i]))
+							break;
+					if (i == len)
+						continue;
+
+					/* special case last line */
+					if (feof (blm))
+						len++;
+
+					bmp[linecnt] = 0;
+					for (i = 0; i < len - 1; i++){
+						bmp[linecnt] <<= 1;
+						putchar (buf[i] == '0' ? ' ' : '#'); 
+						bmp[linecnt] |= (buf[i] == '0' ? 0:1);
+					}
+					
+					linecnt++;
+					putchar ('\n');
+				}
+		}
+						
+	fclose (blm);
+	blm_frame_send(bmp);
+	usleep (duration*1000);
+	return 0;
+
+ blerror:
+	fprintf (stderr, "Error parsing BlinkenLights movie '%s' (line %d).\n", 
+					 filename, lc);
+	fclose (blm);
+	return -1;	
+argerror:
+	debug(0, "borg mode <addr> <number>");
+
+}
+
 
 
 void cmd_borg_scroll(int argc, char *argv[]) 
@@ -116,6 +254,7 @@ typedef struct {
 cmd_t borg_cmds[] = {
   { &cmd_borg_mode, "mode", "mode", "switch bord to mode" },
   { &cmd_borg_scroll, "scroll", "scroll", "set scrolltext" },
+  { &cmd_borg_blm, "blm", "blm", "play Blinkenmovie" },
   { NULL, NULL, NULL, NULL }
 };
 
