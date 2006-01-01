45215362a8e2bd63560b72fd509e5edeeb948c99
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-01-01 19:08:32 +0000

    my own can.h

---
+++ microcontroller/src-atmel/automatization/can-gateway-m32-new/can.h
@@ -0,0 +1,73 @@
+#ifndef CAN_H
+#define CAN_H
+
+#include "config.h"
+
+/*****************************************************************************
+ * Simple CAN Library
+ *
+ * #define CAN_INTERRUPT 1	//set this to enable interrupt driven 
+ *                                and buffering version
+ * #define CAN_RX_BUFFER_SIZE 2	//only used for Interrupt
+ * #define CAN_TX_BUFFER_SIZE 2	//only used for Interrupt
+ */
+
+#include "config.h"
+
+/*****************************************************************************
+ * Types
+ */
+
+typedef struct{
+	unsigned long id;
+	bool          rtr;
+	unsigned char dlc;
+	unsigned char data[8];
+}can_message_t;
+
+typedef enum { normal, mode_sleep, loopback, listenonly, config } can_mode_t ;
+
+
+/*****************************************************************************
+ * Global variables
+ */
+#ifdef CAN_HANDLEERROR
+  unsigned char can_error;
+#endif
+
+
+/*****************************************************************************
+ * Management
+ */
+
+void can_init();
+void can_setfilter();
+void can_setmode(can_mode_t);
+void can_setled(unsigned char led, unsigned char state);
+
+/*****************************************************************************
+ * Sending
+ */
+
+can_message * can_buffer_get();
+void can_transmit( can_message *msg );
+
+
+/*****************************************************************************
+ * Receiving
+ */
+
+can_message *can_get();
+can_message *can_get_nb();
+
+// this is only needed for Interrupt driven Version
+#ifndef CAN_INTERRUPT
+// #  define can_free(m)
+  void can_free(can_message * msg);
+#else
+  void can_free(can_message * msg);
+#endif
+
+
+
+#endif
