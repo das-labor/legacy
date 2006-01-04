710d279ce72388967c12c13ce2738d0df8c6103c
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-01-04 04:45:43 +0000

    blink - blink

---
+++ microcontroller/src-atmel/borg/Panel-One/borg_hw.c
@@ -0,0 +1,131 @@
+
+#include <avr/signal.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include "borg_hw.h"
+
+// 16 Spalten insgesamt direkt gesteuert, dafür 2 Ports
+#define COLPORT  PORTA
+#define COLDDR   DDRA
+
+// Der andere Port übernimmt die Steuerung der Schieberegister
+#define CTRLPORT PORTC
+#define CTRLDDR   DDRC
+
+// PINs on CTRLPORT
+#define PIN_EO3  PC5
+#define PIN_CP4  PC4
+#define PIN_SHOW PC3
+#define PIN_CP1  PC2
+#define PIN_CP2  PC1
+#define PIN_CP3  PC0
+
+//Der Puffer, in dem das aktuelle Bild gespeichert wird
+unsigned char pixmap[NUMPLANE][NUM_ROWS][LINEBYTES];
+
+inline void busywait() {
+	unsigned char i;
+	for(i=0;i<20;i++){
+		asm volatile("nop");
+	}
+}
+
+
+//Eine Zeile anzeigen
+inline void rowshow(unsigned char row, unsigned char plane){
+	//Die Zustände von der vorherigen Zeile löschen
+	COLPORT  = pixmap[plane][row][0];
+	busywait();
+	CTRLPORT |=  (1<<PIN_CP1);
+	busywait();
+	CTRLPORT &= ~(1<<PIN_CP1);
+	busywait();
+
+	COLPORT  = pixmap[plane][row][1];
+	busywait();
+	CTRLPORT |=  (1<<PIN_CP2);
+	busywait();
+	CTRLPORT &= ~(1<<PIN_CP2);
+	busywait();
+
+//	COLPORT  = pixmap[plane][row][2];
+//	busywait();
+//	CTRLPORT |=  (1<<PIN_CP4);
+//	busywait();
+//	CTRLPORT &= ~(1<<PIN_CP4);
+//	busywait();
+
+
+	COLPORT  = row;
+	busywait();
+	CTRLPORT |=  (1<<PIN_SHOW);
+	busywait();
+	CTRLPORT &= ~(1<<PIN_SHOW);
+	busywait();
+}
+
+
+//Dieser Interrupt wird je nach Ebene mit 50kHz 31,25kHz oder 12,5kHz ausgeführt
+SIGNAL(SIG_OUTPUT_COMPARE0)
+{
+	static unsigned char plane = 0;
+	static unsigned char row = 0;
+	
+	//Watchdog zurücksetzen
+	wdt_reset();
+	
+	//Die aktuelle Zeile in der aktuellen Ebene ausgeben
+	rowshow(row, plane);
+	
+	//Zeile und Ebene inkrementieren
+	if(++row == NUM_ROWS){
+		row = 0;
+		if(++plane==NUMPLANE) plane=0;
+	}
+}
+
+
+void timer0_off(){
+	cli();
+
+	TCCR0 = 0x00;
+	sei();
+}
+
+
+// Den Timer, der denn Interrupt auslöst, initialisieren
+void timer0_on(){
+/* 	TCCR0: FOC0 WGM00 COM01 COM00 WGM01 CS02 CS01 CS00
+		CS02 CS01 CS00
+		 0    0    0	       stop
+		 0    0    1       clk
+		 0    1    0       clk/8
+		 0    1    1       clk/64
+		 1    0    0       clk/256
+		 1    0    1       clk/1024
+	
+*/
+	TCCR0 = 0x0C;	// CTC Mode, clk/64
+	TCNT0 = 0;	// reset timer
+	OCR0  = 20;	// Compare with this value
+	TIMSK = 0x02;	// Compare match Interrupt on
+}
+
+void borg_hw_init(){
+	//Pins am Zeilenport auf Ausgang
+	CTRLDDR  |= (1<<PIN_EO3) | (1<<PIN_CP4) | (1<<PIN_SHOW) | (1<<PIN_CP1) | (1<<PIN_CP2) | (1<<PIN_CP3);
+	CTRLPORT |= (1<<PIN_EO3);
+	
+	//Alle Spalten erstmal aus
+	//Spalten Ports auf Ausgang
+	COLDDR  = 0xFF;
+	COLPORT = 0x00;
+	
+	timer0_on();
+
+	//Watchdog Timer aktivieren
+	wdt_reset();
+	wdt_enable(0x00);	// 17ms Watchdog
+}
+
