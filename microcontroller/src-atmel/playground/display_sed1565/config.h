03874efd52c8f570925b13213631232f7827724e
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-02-05 22:00:34 +0000

    For Display from Pollin

---
+++ microcontroller/src-atmel/playground/display_sed1565/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//spi defines
+#define SPI_HARDWARE
+#define SPI_DDR DDRB
+#define SPI_PIN_SS PB4
+#define SPI_PORT PORTB
+#define SPI_PIN_MOSI PB5
+#define SPI_PIN_MISO PB6
+#define SPI_PIN_SCK PB7
+
+
+#endif
---
+++ microcontroller/src-atmel/playground/display_sed1565/mk_function_table.pl
@@ -0,0 +1,1 @@
+../../../make/mk_function_table.pl
