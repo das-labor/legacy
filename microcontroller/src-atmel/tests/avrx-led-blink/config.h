a852cba4b48b20b960255b060db841e3d7e259dc
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-02-12 03:52:01 +0000

    cleanup

--- microcontroller/src-atmel/tests/avrx-led-blink/config.h
+++ microcontroller/src-atmel/tests/avrx-led-blink/config.h
@@ -1,12 +1,8 @@
-#define TCNT0_INIT (0xFF-F_CPU/256/TICKRATE)
-
-#define TMC8_CK256 (1<<CS02)
 
 #define F_CPU 16000000L
 #define F_MCP F_CPU
 
-#define TICKRATE 2500
-
-#define LED 	PORTC           // Change this per your hardware and jumper setup
-#define LEDDDR 	DDRC
+#define TICKRATE   2500
+#define TCNT0_INIT (0xFF-F_CPU/256/TICKRATE)
+#define TMC8_CK256 (1<<CS02)
 
