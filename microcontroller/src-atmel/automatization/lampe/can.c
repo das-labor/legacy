e4081f2832d363cff00179376e8a1e6498db0df2
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-03-02 18:02:15 +0000

    added missing files

---
+++ microcontroller/src-atmel/automatization/lampe/can.c
@@ -0,0 +1,350 @@
+
+#include <avr/io.h>
+#include <avr/signal.h>
+#include <avr/pgmspace.h>
+#define asm asm volatile
+
+
+#include "config.h"
+#include "can.h"
+#include "spi.h"
+#include "lap.h"
+
+//#include "mcp2515.inc"
+
+//Registers
+#define RXF0SIDH 0x00
+#define RXF0SIDL 0x01
+#define RXF0EID8 0x02
+#define RXF0EID0 0x03
+#define RXF1SIDH 0x04
+#define RXF1SIDL 0x05
+#define RXF1EID8 0x06
+#define RXF1EID0 0x07
+#define RXF2SIDH 0x08
+#define RXF2SIDL 0x09
+#define RXF2EID8 0x0A
+#define RXF2EID0 0x0B
+#define TXRTSCTRL 0x0D
+#define CANSTAT 0x0E
+#define CANCTRL 0x0F
+	#define CLKEN 2
+#define RXF3SIDH 0x10
+#define RXF3SIDL 0x11
+#define RXF3EID8 0x12
+#define RXF3EID0 0x13
+#define RXF4SIDH 0x14
+#define RXF4SIDL 0x15
+#define RXF4EID8 0x16
+#define RXF4EID0 0x17
+#define RXF5SIDH 0x18
+#define RXF5SIDL 0x19
+#define RXF5EID8 0x1A
+#define RXF5EID0 0x1B
+#define TEC 0x1C
+#define REC 0x1D
+#define RXM0SIDH 0x20
+#define RXM0SIDL 0x21
+#define RXM0EID8 0x22
+#define RXM0EID0 0x23
+#define RXM1SIDH 0x24
+#define RXM1SIDL 0x25
+#define RXM1EID8 0x26
+#define RXM1EID0 0x27
+#define CNF3 0x28
+#define CNF2 0x29
+#define CNF1 0x2A
+#define CANINTE 0x2B
+	#define MERRE 7
+	#define WAKIE 6
+	#define ERRIE 5
+	#define TX2IE 4
+	#define TX1IE 3
+	#define TX0IE 2
+	#define RX1IE 1
+	#define RX0IE 0
+#define CANINTF 0x2C
+	#define MERRF 7
+	#define WAKIF 6
+	#define ERRIF 5
+	#define TX2IF 4
+	#define TX1IF 3
+	#define TX0IF 2
+	#define RX1IF 1
+	#define RX0IF 0
+#define EFLG 0x2D
+#define TXB0CTRL 0x30
+	#define TXREQ 3
+#define TXB0SIDH 0x31
+#define TXB0SIDL 0x32
+	#define EXIDE 3
+#define TXB0EID8 0x33
+#define TXB0EID0 0x34
+#define TXB0DLC 0x35
+#define TXB0D0 0x36 
+
+#define RXB0CTRL 0x60
+	#define RXM1 6
+	#define RXM0 5
+	#define RXRTR 3
+	// Bits 2:0 FILHIT2:0
+#define RXB0SIDH 0x61
+#define RXB0SIDL 0x62
+#define RXB0EID8 0x63
+#define RXB0EID0 0x64
+#define RXB0DLC 0x65
+#define RXB0D0 0x66 
+
+//Command Bytes
+#define RESET 0xC0
+#define READ 0x03
+#define READ_RX_BUFFER 0x90
+#define WRITE 0x02
+#define LOAD_TX_BUFFER 0x40
+#define RTS 0x80
+#define READ_STATUS 0xA0
+#define RX_STATUS 0xB0
+#define BIT_MODIFY 0x05
+
+#define spi_set_ss() PORTB |= (1<<SPI_PIN_SS)
+#define spi_clear_ss() PORTB &= ~(1<<SPI_PIN_SS)
+
+can_message Rx_msg, Tx_msg;
+
+/* MCP */
+void mcp_write(unsigned char reg, unsigned char data) ;
+void mcp_write_b(PGM_P stream) ;
+unsigned char mcp_read(unsigned char reg) ;
+unsigned char mcp_status()  ;
+
+void message_load() ;
+
+// Functions
+
+
+unsigned char mcp_status(){
+	unsigned char d;
+	spi_set_ss();
+	spi_data(READ_STATUS);
+	d = spi_data(0);
+	spi_clear_ss();
+	return d;
+}
+
+
+inline static void mcp_bitmod(unsigned char reg, unsigned char mask, unsigned char val){
+	spi_set_ss();
+	spi_data(BIT_MODIFY);
+	spi_data(reg);
+	spi_data(mask);
+	spi_data(val);
+	spi_clear_ss();
+}
+
+
+
+unsigned char mcp_txreq_str[] PROGMEM ={
+	2, TXB0CTRL, (1<<TXREQ), 0,0
+};
+
+//load a message to mcp2515 and start transmission
+void can_transmit(){
+	unsigned char x;
+	
+	spi_set_ss();
+	spi_data(WRITE);
+	spi_data(TXB0SIDH);
+
+	spi_data( ((unsigned char)(Tx_msg.port_src << 2)) | (Tx_msg.port_dst >> 4 ) );
+	spi_data( (unsigned char)((Tx_msg.port_dst & 0x0C) << 3) | (1<<EXIDE) | (Tx_msg.port_dst & 0x03) );
+	spi_data(Tx_msg.addr_src);
+	spi_data(Tx_msg.addr_dst);
+	spi_data(Tx_msg.dlc);
+	for(x=0;x<Tx_msg.dlc;x++){
+		spi_data(Tx_msg.data[x]);
+	}
+	spi_clear_ss();
+	
+	mcp_write_b(mcp_txreq_str);
+	
+	/*
+	spi_set_ss();
+	spi_data(WRITE);
+	spi_data(TXB0CTRL);
+	spi_data( (1<<TXREQ) );
+	spi_clear_ss();
+	*/
+}
+
+
+//get a message from mcp2515 and disable RX interrupt Condition
+static inline void message_fetch(){
+	unsigned char tmp1, tmp2, tmp3;
+	unsigned char x;
+
+	spi_set_ss();
+	spi_data(READ);
+	spi_data(RXB0SIDH);
+	tmp1 = spi_data(0);
+	Rx_msg.port_src = tmp1 >> 2;
+	tmp2 = spi_data(0);
+	tmp3 = (unsigned char)((unsigned char)(tmp2 >> 3) & 0x0C);
+	Rx_msg.port_dst = ((unsigned char)(tmp1 <<4 ) & 0x30) | tmp3 | (unsigned char)(tmp2 & 0x03);
+
+	Rx_msg.addr_src = spi_data(0);
+	Rx_msg.addr_dst = spi_data(0);
+	Rx_msg.dlc = spi_data(0) & 0x0F;	
+	for(x=0;x<Rx_msg.dlc;x++){
+		Rx_msg.data[x] = spi_data(0);
+	}
+	spi_clear_ss();
+	
+	mcp_bitmod(CANINTF, (1<<RX0IF), 0x00);
+}
+
+
+static inline void mcp_reset(){
+	spi_set_ss();
+	spi_data(RESET);
+	spi_clear_ss();
+}
+
+void mcp_write(unsigned char reg, unsigned char data){
+	spi_set_ss();
+	spi_data(WRITE);
+	spi_data(reg);
+	spi_data(data);
+	spi_clear_ss();
+}
+
+void mcp_write_b(PGM_P stream){
+	unsigned char len;
+	
+	while(( len = pgm_read_byte(stream++) )){
+		spi_set_ss();
+		spi_data(WRITE);
+		while(len--){
+			spi_data(pgm_read_byte(stream++));
+		}
+		spi_clear_ss();
+	}
+}
+
+
+
+unsigned char mcp_read(unsigned char reg){
+	unsigned char d;
+	spi_set_ss();
+	spi_data(READ);
+	spi_data(reg);
+	d = spi_data(0);
+	spi_clear_ss();
+	return d;
+}
+
+/* Management */
+/*
+void can_setmode( can_mode_t mode ) {
+	unsigned char val = mode << 5;  
+	val |= 0x04;  // CLKEN
+
+	mcp_write( CANCTRL, val );
+}
+*/
+
+extern unsigned char Station_id;
+
+
+#define FLT_PORT_SRC 0
+#define FLT_PORT_DST1 PORT_LAMPE
+#define FLT_PORT_DST2 PORT_MGT
+#define FLT_ADDR_SRC 0
+
+#define MSK_PORT_SRC 0
+#define MSK_PORT_DST 0x3F
+#define MSK_ADDR_SRC 0
+#define MSK_ADDR_DST 0xFF
+
+
+
+/*******************************************************************/
+static inline void delayloop(){
+	unsigned char x;
+	for(x=0;x<255;x++){		
+		asm ("nop");
+	}
+
+}
+
+
+// 0x01 : 125kbit/8MHz
+// 0x03 : 125kbit/16MHz
+// 0x04 : 125kbit/20MHz
+#if F_MCP == 16000000
+#define CNF1_T 0x03
+#elif F_MCP == 8000000
+#define CNF1_T 0x01
+#elif F_MCP == 20000000
+#define CNF1_T 0x04
+#else
+#error Can Baudrate is only defined for 8, 16 and 20 MHz
+#endif 
+
+
+unsigned char mcp_config_str1[] PROGMEM ={
+	2, BFPCTRL, 0x0C,		//RXBF Pins to Output
+	4, CNF3,
+		0x05,			//CNF3
+		0xf1,			//CNF2
+		0x40 | CNF1_T,		//CNF1
+		
+	2, RXB0CTRL,(0<<RXM1) | (0<<RXM0),
+	9, RXF0SIDH,
+		(FLT_PORT_SRC << 2) | (FLT_PORT_DST1 >> 4 ),
+		((FLT_PORT_DST1 & 0x0C) << 3) | (1<<EXIDE) | (FLT_PORT_DST1 & 0x03),
+		FLT_ADDR_SRC,
+		0x35,
+		(FLT_PORT_SRC << 2) | (FLT_PORT_DST2 >> 4 ),
+		((FLT_PORT_DST2 & 0x0C) << 3) | (1<<EXIDE) | (FLT_PORT_DST2 & 0x03),
+		FLT_ADDR_SRC,
+		0x35,
+	5, RXM0SIDH,
+		(MSK_PORT_SRC << 2) | (MSK_PORT_DST >> 4 ),
+		((MSK_PORT_DST & 0x0C) << 3) | (MSK_PORT_DST & 0x03),
+		MSK_ADDR_SRC,
+		MSK_ADDR_DST,
+	0
+};
+	
+	
+unsigned char mcp_config_str2[] PROGMEM ={
+	2, CANCTRL, 0|(1<<CLKEN),
+	2, CANINTE, (1<<RX0IE),
+	0
+};
+
+void can_init(){
+	mcp_reset();
+	
+	delayloop();
+	
+	mcp_write_b(mcp_config_str1);
+
+	unsigned char tmp = Station_id;
+	mcp_write(RXF0EID0, tmp );
+	mcp_write(RXF1EID0, tmp );
+
+	mcp_write_b(mcp_config_str2);	
+}
+
+
+unsigned char can_get_nb(){
+	//check the pin, that the MCP's Interrup output connects to
+	if((SPI_REG_PIN_MCP_INT & (1<<SPI_PIN_MCP_INT))){
+		return 0; //no message
+	}
+	
+	message_fetch();
+	return 1;
+}
+
