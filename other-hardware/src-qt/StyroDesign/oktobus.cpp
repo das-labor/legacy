be06c57c6db2b75e62318ddee7cc4e3a3d5472e6
madex <madex@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-03-22 22:32:46 +0000

    fixed bug in scale chainCode and its possible to disable PARPORT support in though uncomenting a line in  ac_cfg.h

--- other-hardware/src-qt/StyroDesign/oktobus.cpp
+++ other-hardware/src-qt/StyroDesign/oktobus.cpp
@@ -1,6 +1,7 @@
 
 #include "ppi.h"
 #include "oktobus.h"
+#include "ac_cfg.h"
 
 Oktobus::Oktobus(){
 	stepcount[0] = 0;
@@ -9,23 +10,27 @@ Oktobus::Oktobus(){
 }
 
 int Oktobus::init(QString pardev){
+#if HAVEPARPORT
 	fd = ppi_open(pardev.toAscii().data());
 	if (fd < 0)
 		return 1;
+
 	ppi_setall(fd, PPIDATA, 0);
+#endif 
 	return 0;
 }
 
 uint8_t Oktobus::readInputs(){
 	uint8_t tmp;
-	
+#if HAVEPARPORT
+
 	ppi_setall(fd, 15, PPICTRL);
-	tmp = ppi_getall(fd, PPISTATUS);
+	tmp = ppi_getall(fd, PPISTATUS);	
 	tmp >>=4;
 	
 	ppi_setall(fd, 15, PPICTRL);
 	tmp |= ppi_getall(fd, PPISTATUS) & 0xf0;
-	
+#endif	
 	tmp ^= 0x88;
 	
 	return tmp;
@@ -51,13 +56,17 @@ void Oktobus::motorStep(uint8_t motor, bool dir){
 			break;
 		
 	}
+#if HAVEPARPORT
 	ppi_setmask(fd, PPIDATA, 0x03 << (2*motor), (motor_sequence[(unsigned char)stepcount[motor]%4]) << (2*motor) );
+#endif
 }
 
 void Oktobus::setOutput(uint8_t output, bool value) {
+#if HAVEPARPORT
 	if(value){
 		ppi_set(fd, PPIDATA, (1<<output));
 	}else{
 		ppi_clr(fd, PPIDATA, (1<<output));
 	}
+#endif
 }
