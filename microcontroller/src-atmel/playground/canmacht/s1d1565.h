81a789efb6f87c2a03173db2fb7eea9bcfc29f0a
dr4go <dr4go@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-04-12 10:04:30 +0000

    basic support for ks0108

--- microcontroller/src-atmel/playground/canmacht/s1d1565.h
+++
@@ -1,8 +0,0 @@
-extern void dispWriteCommand(unsigned char byte);
-extern void dispWriteByte(unsigned char byte);
-extern void dispInit(void);         //Initialize LCD
-extern void dispClear(void);
-extern void dispGotoXY(unsigned char Spalte, unsigned char Zeile);
-extern void dispEnable(void);
-extern unsigned char dispReadByte(void);
-
