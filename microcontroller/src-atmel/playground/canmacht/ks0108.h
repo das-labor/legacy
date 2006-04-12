c700fd8bc078a6baba42c55479c80ef285b2dce3
dr4go <dr4go@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-04-12 10:10:38 +0000

    jetzt hab ichs aber

---
+++ microcontroller/src-atmel/playground/canmacht/ks0108.h
@@ -0,0 +1,15 @@
+#ifndef	KS0108_H
+#define KS0108_H
+
+#include <avr/io.h>
+
+extern void dispSetPix(uint8_t x, uint8_t y, uint8_t color);
+extern void dispInit();
+
+//todo
+extern void dispHLine(uint8_t x, uint8_t y, uint8_t x2, uint8_t color);
+extern void dispVLine(uint8_t x, uint8_t y, uint8_t y2);
+extern void dispFillRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color);
+extern void dispDrawRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color);
+
+#endif //KS0108_H
