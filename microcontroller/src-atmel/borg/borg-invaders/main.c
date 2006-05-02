4b9aad8eb64143e3d475a1c98e4ba97bf74cc5e5
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-05-02 20:51:08 +0000

    version mit breakpoint

--- microcontroller/src-atmel/borg/borg-invaders/main.c
+++ microcontroller/src-atmel/borg/borg-invaders/main.c
@@ -27,60 +27,34 @@ int main (void){
 	oldOldmode = oldMode;
 	waitForFire = 1;	
 
-	/*
-        while(1) {
-          if (JOYISFIRE) 
-		setpixel((pixel){0,1}, 3);
-	  else
-		setpixel((pixel){0,1}, 0);
-
-          if (JOYISLEFT) 
-		setpixel((pixel){0,2}, 3);
-	  else
-		setpixel((pixel){0,2}, 0);
-
-          if (JOYISRIGHT) 
-		setpixel((pixel){0,3}, 3);
-	  else
-		setpixel((pixel){0,3}, 0);
-
-          if (JOYISUP) 
-		setpixel((pixel){0,4}, 3);
-	  else
-		setpixel((pixel){0,4}, 0);
-
-          if (JOYISDOWN) 
-		setpixel((pixel){0,5}, 3);
-	  else
-		setpixel((pixel){0,5}, 0);
-	}
-	*/
-
-
-       // borg_invaders();
-
 	for(;;){
 		oldMode = mode;
 		switch(mode++) {
 		case 1:
-			scrolltext(scrolltext_text);
+			breakpoint();
 			break;
 		case 2:
-			spirale(5);
+			BProtationgPoints();
 			break;
 		case 3:
-			joern1();
+			scrolltext(scrolltext_text);
 			break;
 		case 4:
-			snake();
+			spirale(5);
 			break;
 		case 5:
-			schachbrett(20);
+			joern1();
 			break;
 		case 6:
-			feuer();
+			snake();
 			break;
 		case 7:
+			schachbrett(20);
+			break;
+		case 8:
+			feuer();
+			break;
+		case 9:
 			matrix();
 			mode = 1;
 			break;
@@ -99,12 +73,12 @@ int main (void){
 		case 35:
 			test1();
 			break;
-		case 0xFF:
-			off();
-			break;
 		case 43:
 			borg_invaders();
 			mode = oldOldmode;
+			break;
+		case 0xFF:
+			off();
 			break;
 		default:
 			break;
