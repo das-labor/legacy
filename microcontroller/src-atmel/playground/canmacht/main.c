666c54db6174e8c9ca404d963a8c1e3997eaff11
dr4go <dr4go@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-05-02 11:05:23 +0000

    malloc(world_t)

--- microcontroller/src-atmel/playground/canmacht/main.c
+++ microcontroller/src-atmel/playground/canmacht/main.c
@@ -9,48 +9,42 @@
 #include "config.h"
 #include "xcan.h"
 #include "xlap.h"
-#include "keys.h"
+//#include "keys.h"
 #include "menu.h"
-
 #include "grafiklcd.h"
 
 //AVRX_GCC_TASK(Monitor, 20, 0);          // External Task: Debug Monitor
 
 
-AVRX_SIGINT(SIG_OVERFLOW0)
-{
-    IntProlog();                // Save interrupted context, switch stacks
-    TCNT0 = TCNT0_INIT;			// Reload the timer counter
-    AvrXTimerHandler();         // Process Timer queue
-    Epilog();                   // Restore context of next running task
-};
+AVRX_SIGINT(SIG_OVERFLOW0) {
+	IntProlog();                // Save interrupted context, switch stacks
+	TCNT0 = TCNT0_INIT;			// Reload the timer counter
+	AvrXTimerHandler();         // Process Timer queue
+	Epilog();                   // Restore context of next running task
+}
 
-int main(void)
-{
-    AvrXSetKernelStack(0);
+int main(void) {
+	AvrXSetKernelStack(0);
+	MCUCR = 1<<SE;      	// Enable "sleep" mode (low power when idle)
+	TCNT0 = TCNT0_INIT;		// Load overflow counter of timer0
+	TCCR0 = TMC8_CK256;		// Set Timer0 to CPUCLK/256
+	TIMSK = 1<<TOIE0;		// Enable interrupt flag
 
-    MCUCR = 1<<SE;      	// Enable "sleep" mode (low power when idle)
-    TCNT0 = TCNT0_INIT;		// Load overflow counter of timer0
-    TCCR0 = TMC8_CK256;		// Set Timer0 to CPUCLK/256
-    TIMSK = 1<<TOIE0;		// Enable interrupt flag
 
-//	PORTD = 0xff;
 	PORTB = 0x0f; // Tastenport
-	DDRB = 0x00;
-    //InitSerialIO(UBRR_INIT);    // Initialize USART baud rate generator
+	DDRB  = 0x00;
+	//InitSerialIO(UBRR_INIT);    // Initialize USART baud rate generator
 	xlap_init();
 	dispInit(); // Display initialisierung
 
-
-	
 	AvrXRunTask(TCB(laptask));
 	//AvrXRunTask(TCB(keys));
 	AvrXRunTask(TCB(menu));
 
-    /* Needed for EEPROM access in monitor */
-	AvrXSetSemaphore(&EEPromMutex);
-	
+	/* Needed for EEPROM access in monitor */
+	//AvrXSetSemaphore(&EEPromMutex);
 
-    Epilog();                   // Switch from AvrX Stack to first task
-    while(1);
-};
+
+	Epilog();                   // Switch from AvrX Stack to first task
+	while(1);
+}
--- microcontroller/src-atmel/playground/canmacht/menu.c
+++ microcontroller/src-atmel/playground/canmacht/menu.c
@@ -8,8 +8,10 @@
 #include "xcan.h"
 #include "menu.h"
 #include "text.h"
-
+#include "lap.h"
 #include "grafiklcd.h"
+#include "stdlib.h"
+#include "string.h"
 
 struct {
 	int8_t x;
@@ -17,12 +19,34 @@ struct {
 	} pos;		// doto: position handeling
 
 extern menu_t root;
+int8_t blightstat;
 
+uint8_t scriptstat;
+
+void make_basemenu();
+menu_item_t *make_item(void *show, void *enter, menu_t *menu, uint8_t type) ;
+menu_t *make_menu(char *name);
+void menu_add_item(menu_t *menu, menu_item_t *entry, uint8_t pos) ;
+void switch_blight(void *data, uint8_t selected);
+void menu_handler(void *data);
+void show_menu(void *data, uint8_t selected);
+void menu_update(uint8_t act);
+void show_text(void* data, uint8_t selected);
+void show_mood(void *data, uint8_t selected);
+void switch_script();
+/*
 menu_item_t mi_i0 = {&show_menu, &menu_handler, &root}; // nur zum testen
 menu_item_t *bla[] = {&mi_i0};				// "
 
-//menu_item_t *its_setup[] = {&bla};
-menu_t msetup = {"setup", 1, bla};
+//menu_item_t *its_blight[] = {&};
+menu_t mblight = {"blight", 1, 0};//its_blight};
+menu_item_t iblight = {&show_menu, &switch_blight, &mblight};
+
+menu_t mscan = {"scan", 1, bla};
+menu_item_t iscan = {&show_menu, &menu_handler, &mscan};
+
+menu_item_t *its_setup[] = {&iblight, &iscan};
+menu_t msetup = {"setup", 2, its_setup};
 menu_item_t isetup = {&show_menu, &menu_handler, &msetup};
 
 //menu_item_t *its_all[] = {&iadr};
@@ -37,7 +61,11 @@ menu_item_t *its_info[] = {&itemp};
 menu_t minfo = {"wissen", 1, its_info};
 menu_item_t iinfo = {&show_menu, &menu_handler, &minfo};
 
-menu_t mmoodadr = {"alle", 1, bla};
+menu_t mdimm = {"dimm", 1, bla};
+menu_item_t idimm = {&show_mood, &menu_handler, &mdimm};
+
+menu_item_t *its_alle[] = {&idimm};
+menu_t mmoodadr = {"alle", 1, its_alle};
 menu_item_t imoodadr = {&show_menu, &menu_handler, &mmoodadr};
 
 menu_item_t *its_moodadr[] = {&imoodadr};
@@ -49,29 +77,49 @@ menu_t mctrl = {"macht", 1, its_ctrl};
 menu_item_t ictrl = {&show_menu, &menu_handler, &mctrl};
 
 menu_item_t *items_root[] = {&ictrl, &iinfo, &isetup};
-menu_t root = {"/", 3, items_root};
+//menu_t root = {"/", 3, items_root};
+
+*/
+
+
+menu_t root = {"/", 0, 0};
 
 TimerControlBlock switchtimer;
 
-AVRX_GCC_TASKDEF(menu, 100, 5) {
+AVRX_GCC_TASKDEF(menu, 100, 5)  {
+	DDRD |= (1 << PD7);
+  	TCCR2 = (1<<WGM21) | (1<<WGM20) | (1 << COM21) | (1 << COM20) | (1 << CS22) | (1 << CS21) | (0 << CS20);
+	OCR2 = 0;
+	make_basemenu();
 	menu_handler(&root);
-	while (1) {
-	}
+	while (1);
 }
-uint8_t getKey() {  // keyport in header
+
+void make_basemenu() {	
+	menu_add_item(&root, make_item(&show_menu, &menu_handler, make_menu("setup"), 0), 0);
+	menu_add_item(root.items[0]->data, make_item(&show_menu, &switch_blight, make_menu("bglight"), 1), 0);
+	menu_add_item(&root, make_item(&show_menu, &menu_handler, make_menu("ctrl"), 0), 1);
+	menu_add_item(root.items[1]->data, make_item(&show_menu, &menu_handler, make_menu("mood"), 0), 0);
+	menu_t * ctrl = root.items[1]->data;
+	menu_add_item(ctrl->items[0]->data, make_item(&show_menu, &switch_script, make_menu("script"), 1), 0);
+	menu_add_item(&root, make_item(&show_menu, &menu_handler, make_menu("info"), 0), 2);
 	
+}
+
+uint8_t getKey() {  // keyport in header
+	AvrXDelay(&switchtimer, 550);
 	while(1) {
-		AvrXDelay(&switchtimer, 550);
-		if(!(PINB & (1 << PB0))) {
+		
+		if(!(PINB & (1 << PB0))) { // up
 			return 0;
 		}
-		if(!(PINB & (1 << PB1))) {
+		if(!(PINB & (1 << PB1))) { // down
 			return 1;
 		}
-		if(!(PINB & (1 << PB2))) {
+		if(!(PINB & (1 << PB2))) { // ok
 			return 2;
 		}
-		if(!(PINB & (1 << PB3))) {
+		if(!(PINB & (1 << PB3))) { // back
 			return 3;
 		}
 	}
@@ -88,7 +136,7 @@ void menu_handler(void *data) {
 
 	for(i = 0; i < menu->size; i++) {
 		menu->items[i]->show(menu->items[i]->data, (sel==i));
-		pos.y += 9;		
+		pos.y += 9;
 		if(i >= 6)
 			break;
 	}
@@ -96,7 +144,8 @@ void menu_handler(void *data) {
 		key = getKey();
 		// enter
 		if(key == 2) {
-			dispFillRect(0, pos.x, 41, 10*menu->size, 0);
+			if(!menu->items[sel]->type&0x01)
+				dispFillRect(0, pos.x, 41, 10*menu->size, 0);
 			//pos.x += 20;
 			menu->items[sel]->enter(menu->items[sel]->data); // enter	
 			goto DRAW_MEN;
@@ -133,21 +182,104 @@ void show_menu(void *data, uint8_t selected) {
 	dispFillRect(pos.x+1, pos.y+1, 39, 8, selected);
 	draw_Text(menu->name, pos.x+2, pos.y+2, 0, 1, !selected);
 }
-
+/*
 void show_text(void *data, uint8_t selected) {
 	menu_t *menu = data;
 	
-}
-
+} */
+/*
 void show_mood(void *data, uint8_t selected) {
 	menu_t *menu = data;
 	dispDrawRect(50, pos.y, 65, 9, 1);
 	dispFillRect(pos.x+1, pos.y+1, 39, 8, selected);
 }
+*/
+void menu_add_item(menu_t *menu, menu_item_t *entry, uint8_t pos) {
+	uint8_t i;
+	//pos > size? pos=size
+	menu->size += 1;
+	menu->items = realloc(menu->items, (menu->size) * sizeof(menu_item_t*));
+	for(i = menu->size-1; i > pos; i--) {
+		menu->items[i] = menu->items[i-1];
+	}
+	menu->items[pos] = entry;
+	// elemente von hinten nach hinten aufrücken bis pos
+	// element einfügen
+}
 
+// make item
+menu_t *make_menu(char *name) { // name
+	menu_t *newmenu = malloc(sizeof(menu_t)) ;
+
+	newmenu->name = malloc(strlen(name)+1);
+	strcpy(newmenu->name, name);
+	newmenu->items = 0;
+	newmenu->size = 0;
+	return newmenu;
+}
+/*
+void menu_del_item (menu_t *menu, uint8_t pos) { // von wo  pos
+
+}
+*/
+menu_item_t *make_item(void *show, void *enter, menu_t *menu, uint8_t type) {
+	menu_item_t *menuitem = malloc(sizeof(menu_item_t));
+	menuitem->show = show;
+	menuitem->enter = enter;
+	menuitem->data = menu;
+	menuitem->type = type;
+	return menuitem;
+}
+
+void switch_blight(void *data, uint8_t selected) {
+	uint8_t key;
+	dispDrawRect(50, 15, 31, 4, 1);
+	dispFillRect(51, 16, blightstat*3, 3, 1);
+	while(1) {  // keyhandeling
+		key = getKey();
+		if(key == 3) {
+			dispFillRect(50, 15, 32, 5, 0);
+			return;
+		}
+	//	up down
+		if(key < 2) {
+			dispFillRect(51, 16, blightstat*3, 3, 0);
+			if(key == 1)
+				blightstat--;
+			if(key == 0)
+				blightstat++;
+			if(blightstat == 11)
+				blightstat = 0;
+			if(blightstat < 0)
+				blightstat = 10;
+			dispFillRect(51, 16, blightstat*3, 3, 1);
+			OCR2 = blightstat*15;
+		}
+	}
+}
+
+void switch_script() {
+	static can_message_t msg={0xa0, 0xa1, PORT_REMOTE, 0x06};
+
+	msg.dlc = 1;
+	msg.data[0] = scriptstat;
+	can_put(&msg);
+	if(scriptstat)
+		scriptstat=0;
+	else
+		scriptstat=1;
+}
+/*
 void ctrl_mood(void *data) {
 	menu_t *menu = data;
 	uint8_t i, key;
+	static can_message_t msg={0xa0, 0xa1, PORT_REMOTE, PORT_MOOD, 0};
+	//msg.addr_src = 0xa0;
+	
+	//msg.dlc = 1;
+	//msg.data[0] = FKT_MOOD_GET_B;
+	//can_put(&msg);
+
 	while(1) {  // keyhandeling
 		key = getKey();
 		// enter
@@ -155,7 +287,11 @@ void ctrl_mood(void *data) {
 			// dunkeler balken --
 		}
 		if(key == 1) {
-			// heller balken++
+			msg.dlc = 3;
+			msg.data[0] = FKT_MOOD_SET_B;
+			msg.data[1] = 0x00;
+			msg.data[2] = 0x45;
+			can_put(&msg);
 		}
 		if(key == 2) {
 			// sel balken?
@@ -163,9 +299,10 @@ void ctrl_mood(void *data) {
 		if(key == 3) {
 			// back
 		}
+		
 	}
 }
-
+*/
 /*
 setup
 scan
