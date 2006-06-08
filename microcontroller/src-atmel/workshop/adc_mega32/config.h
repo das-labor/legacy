c29d7ccad6507a3ca591d0a6ebaf049aec1a0c17
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-06-08 19:40:35 +0000

    Beispile zu Workshop hinzugefügt

---
+++ microcontroller/src-atmel/workshop/adc_mega32/config.h
@@ -0,0 +1,13 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+#include <avr/io.h>
+
+//c uart.[ch] defines
+#define UART_INTERRUPT 1
+#define UART_BAUD_RATE 4800
+#define UART_RXBUFSIZE 16
+#define UART_TXBUFSIZE 16
+#define UART_LINE_BUFFER_SIZE 40
+//#define UART_LEDS 1
+
+#endif
