7e160febb66a37aa900282b0638ae8e60d6244a0
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-06-29 02:10:47 +0000

    added

---
+++ microcontroller/src-c64/canboot/config.h
@@ -0,0 +1,28 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+// spi.[ch] defines
+#define SPI_DDR CIA2+CIA_DDRA	//for slave select
+#define SPI_PORT CIA2+CIA_PRA	//for slave select
+#define SPI_PIN_SS 2		//for slave select
+
+
+// can.[ch] defines
+#undef CAN_INTERRUPT		//set this to enable interrupt driven and buffering version
+#define CAN_RX_BUFFER_SIZE 2	//only used for Interrupt
+#define CAN_TX_BUFFER_SIZE 2	//only used for Interrupt
+
+#define F_MCP 20000000
+
+//interrupt pin of MCP2515 for non interrupt driven can
+//#define SPI_REG_PIN_MCP_INT ~REG(CIA2+CIA_ICR)
+//#define SPI_PIN_MCP_INT 4
+#define SPI_REG_PIN_MCP_INT REG(CIA2+CIA_PRB)
+#define SPI_PIN_MCP_INT 0
+
+
+// lap.[ch] defines
+
+
+#endif
