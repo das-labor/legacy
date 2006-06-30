ebf026c7e2b2f0dcb6ba653a70a966cc88174c66
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-06-30 22:08:40 +0000

    blah

---
+++ microcontroller/src-8051/ibmreader/rs232/Main.c
@@ -0,0 +1,66 @@
+#include "stdio.h"
+#include "ser.h"
+#include "80c451.h"
+
+
+enum media {serial, display, port};    // You expand the list to match you're purpose
+
+
+int media;
+
+
+void putchar(char);                    // Prototype declaration.
+// void disp_putc(char c);
+// void port_putc(char c);
+
+
+void main(void)
+{
+    ser_init();                        // Initial setup of the internal UART using the 
+                                       // library function "ser_init" from ser.h
+                                       // "ser_init" however dosn't enable all interrupts (EA) 
+                                       // only the serial interrupt (ES) so WE have to do this.
+    EA = 1;                            // Enable all interrupts including ES (serial irq)
+
+    media = serial;                    // Set standard IO to the internal UART
+    printf_small("Hello World\n");     // Well you know :-)
+    
+    media = display;                   // Set standard IO to a display device
+    printf_small("Hello World\n");
+    
+    media = port;                      // Set standard IO to port in this case port1
+    printf_small("0123456789ABCDEF");  // tricked you didn't I :-)
+}
+
+
+void putchar(char c)
+{
+    switch(media)
+    {
+        case serial  : ser_putc(c);    // Send the char to internal UART using the
+        break;                         // library function "ser_putc" from ser.h
+        
+        
+/*        case display : disp_putc(c);   // Send the char to the display using the
+        break;                         // function disp_putc
+        
+        
+        case port    : port_putc(c);   // Send the char to the port using the
+        break;                         // function port_putc
+*/        
+        default      : ser_putc(c);    // Let's use the Internal UART as default.
+    }
+}
+
+
+/*
+void disp_putc(char c)                 // Here YOU define how the char is send to the
+{                                      // display.
+}
+*/
+
+/*
+void port_putc(char c)                 // Here YOU define how the char is send to the
+{                                      // port.
+}
+*/
---
+++ microcontroller/src-8051/ibmreader/test/test.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "80c451.h"
+
+void main(void)
+{
+	while(1){
+		int x;
+		for(x=0;x<10000;x++){
+		
+		};
+		
+		P5 ^= 0xff;	
+	}
+}
