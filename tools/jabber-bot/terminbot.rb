d5372e3af68bc2396edc108a96bb7a8b3d13d74f
til <til@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-10-06 19:47:21 +0000

    you still cannot unsubscribe and the code is too much and way too ugly but it works!

---
+++ tools/jabber-bot/terminbot.rb
@@ -0,0 +1,181 @@
+#!/usr/bin/env ruby
+
+require 'xmpp4r'
+require 'xmpp4r/roster/helper/roster'
+require 'rexml/document'
+require 'date'
+
+
+XMLSOURCE=ARGV[0]+"/termine.xml";
+
+##xml parsen
+
+class Termin
+  attr_reader :day, :wday, :month, :year, :text, :link, :date, :datestr, :hour, :min;
+
+  def initialize(xml)
+    @day   = xml.elements["day"].text.to_i;
+    @wday  = xml.elements["wday"].text;
+    @month = xml.elements["month"].text.to_i;
+    @year  = xml.elements["year"].text.to_i;
+    @text  = xml.elements["text"].text;
+    @link  = xml.elements["link"].text;
+    @hour  = xml.elements["hour"].text;
+    @min   = xml.elements["min"].text;
+
+    @year += 2000 if @year < 2000;
+
+    @date  = Date.new( @year, @month, @day );
+    @month = @date.strftime("%m")
+    @day = @date.strftime("%d")
+    @datestr = "#{@day}.#{@month}.#{@year} (#{@wday})"
+  end
+
+  def to_s
+    return @datestr + " - " + @text;
+  end
+end
+
+doc = REXML::Document.new( File.new( XMLSOURCE ));
+at = Array.new
+
+# XML-Elemente ins Array
+doc.elements.each("/termine/termin") { |tx|
+    at.push(Termin.new(tx));
+}
+
+# Array nach Datum sortieren
+at.sort!{ |a,b| a.date <=> b.date }
+
+
+## Dieser Teil ist nicht schön, tut aber folgendes:
+
+# Gucken, ob morgen was ist, falls ja: zurückgeben; falls nein: Motivationstext
+def tomorrow(at)
+	morgen = String.new("\n")
+	at.each { |term|
+    if term.date == Date.today+1 then
+        morgen << "Morgen im LABOR: \n#{term}  #{term.link}";
+    end
+    }
+    if morgen == "\n"
+        morgen << "Morgen ist keine Veranstaltung im LABOR. Komm' vorbei und mach etwas eigenes! Das LABOR wartet auf Dich."
+    end
+	return morgen
+end
+
+# Gucken, ob heute was ist, falls ja: zurückgeben
+def today(at)
+	heute = String.new("\n")
+    at.each { |term|
+    if term.date == Date.today then
+        heute << "Heute im LABOR: \n#{term}  #{term.link}";
+    end
+    }
+	if heute == "\n"
+		heute << "Heute ist keine Veranstaltung im LABOR. Komm' vorbei und mach etwas eigenes! Das LABOR wartet auf Dich."
+	end
+    return heute
+end
+
+# Alle zukünftigen Termine zurückgeben
+def listall(at)
+	alles = String.new("\n")
+	at.each { |term|
+	if term.date > Date.today-1 then
+		alles << "#{term} \n#{term.link} \n\n"
+	end
+	}
+	return alles
+end
+
+
+#puts(listall(at))
+
+# Mit Jabber-Server verbinden
+client = Jabber::Client.new(Jabber::JID.new('terminbot@das-labor.org/LABOR-Termine'))
+client.connect
+client.auth('foobar23')
+
+client.send(Jabber::Presence.new.set_show(:chat).set_status('LABOR-Termine'))
+#client.send(Jabber::Presence.new(:chat, 'LABOR-Termine'))
+
+
+# So, eigentlich macht das hier einen Roster, und acceptet alle Subscriptions
+# tut's aber nicht.
+roster = Jabber::Roster::Helper.new(client)
+
+roster.add_subscription_request_callback{ |pres|
+     my_roster.accept_subscription(pres.from)
+	 puts("Subscription request from #{name}")
+}
+
+
+# Items aus dem Roster lesen und Empfängerliste erstellen
+def recipients 
+	jids = String.new("")
+	roster.groups.each { |group|
+		roster.find_by_group(group).each { |item|
+    		jids << "#{item.jid} "
+		}
+	}
+	return jids
+end
+
+# Terminerinnerung an alle User im Roster
+def reminder(at)
+	morgen = String.new("Morgen im LABOR: \n")
+	at.each { |term|
+	if term.date == Date.today+1 then
+        morgen << "\n#{term}  #{term.link}";
+    end
+    }
+	if morgen != "Morgen im LABOR: \n"
+		msg = Jabber::Message.new(jids)
+		msg.type = :chat
+		msg.set_body(morgen)
+		client.send(msg)
+	end
+end
+
+
+client.add_message_callback { |m|
+#	puts(m.body())
+#	puts Date.today
+	if m.body == "tomorrow" then
+		if(tomorrow(at))
+			answer = Jabber::Message.new(m.from)
+			answer.type = :chat
+			answer.set_body(tomorrow(at))
+			client.send(answer)
+		end
+    elsif m.body == "today" then
+		if(today(at))
+	        answer = Jabber::Message.new(m.from)
+    	    answer.type = :chat
+        	answer.set_body(today(at))
+	        client.send(answer)
+		end
+    elsif m.body == "all" then
+        if(listall(at))
+            answer = Jabber::Message.new(m.from)
+            answer.type = :chat
+            answer.set_body(listall(at))
+            client.send(answer)
+        end
+	elsif
+		answer = Jabber::Message.new(m.from)
+		answer.type = :chat
+        answer.set_body("
+Command not known!
+I know the following commands:
+all			Shows all events
+today		Shows todays events
+tomorrow	Shows tomorrows events
+")
+
+        client.send(answer)
+	end
+}
+
+Thread.stop
