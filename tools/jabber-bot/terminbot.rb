842d0e68272005fa8a39c5422cf7aa8cdb3947ca
til <til@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-10-08 11:04:10 +0000

    a bit of cleanup. you now have a config section

--- tools/jabber-bot/terminbot.rb
+++ tools/jabber-bot/terminbot.rb
@@ -24,13 +24,46 @@ require 'xmpp4r/version/iq/version'
 require 'rexml/document'
 require 'date'
 
-#keep-alive interval
-@interval=30
+# config options. adjust to your needs
+######################################
+
+@interval=30	#keep-alive interval
+@account = 'terminbot@das-labor.org/LABOR-Termine'	# the bots account / resource
+@password = 'foobar23'	# password (ah, no! just kidding! password must have a different meaning)
+@status = 'waiting for your requests'	# the bots status message
+
+# The answer, if there is no event today
+@notoday = "Heute ist keine Veranstaltung im LABOR. Komm' vorbei und mach etwas eigenes! Das LABOR wartet auf Dich."
+# The answer, if there is no event tomorrow
+@notomorrow = "Morgen ist keine Veranstaltung im LABOR. Komm' vorbei und mach etwas eigenes! Das LABOR wartet auf Dich."
+# The leading text for today's events
+@introtoday = "Heute im LABOR:"
+@introtomorrow = "Morgen im LABOR:"
+
+# the text a user gets on subscription
+@instructiontext = "Welcome to the event reminder service!\n
+For the bot to work properly (including being able to unsubscribe from the bot!), you have to grant your authorization.
+The bot will remind you one day, and again 4h before any event. Furthermore it knows the following commands: today, tomorrow, all"
+
+# help message
+@helpmessage = "
+Command not known!
+I know the following commands:
+all         Shows all events
+today       Shows todays events
+tomorrow    shows tomorrows events
+
+To unsubscribe from my service, just revoke my authorization or delete me from your roster."
+
+# Tim requested an annoyance feature
+@timsjid = "opti ät amessage dot de" # for the bot to work correctly, enter Tim's correct jid here
+@annoytext = "hoho!"
+
+######################################
 
 XMLSOURCE=ARGV[0]+"/termine.xml";
 
-##xml parsen
-
+# xml parsen
 class Termin
   attr_reader :day, :wday, :month, :year, :text, :link, :date, :datestr, :hour, :min;
 
@@ -57,48 +90,47 @@ class Termin
   end
 end
 
+# doc bauen, relevante Elemente in Array schubs, Array sortieren
 doc = REXML::Document.new( File.new( XMLSOURCE ));
-at = Array.new
+@at = Array.new
 
 # XML-Elemente ins Array
 doc.elements.each("/termine/termin") { |tx|
-    at.push(Termin.new(tx));
+   	@at.push(Termin.new(tx));
 }
 
 # Array nach Datum sortieren
-at.sort!{ |a,b| a.date <=> b.date }
-
+@at.sort!{ |a,b| a.date <=> b.date }
 
 ## Dieser Teil ist nicht schön, tut aber folgendes:
-
 # Gucken, ob morgen was ist, falls ja: zurückgeben; falls nein: Motivationstext
 def tomorrow(at)
 	morgen = String.new("\n")
 	at.each { |term|
     if term.date == Date.today+1 then
-        morgen << "Morgen im LABOR: \n#{term}  #{term.link}";
+		morgen << @introtomorrow
+        morgen << " \n#{term}  #{term.link}";
     end
     }
     if morgen == "\n"
-        morgen << "Morgen ist keine Veranstaltung im LABOR. Komm' vorbei und mach etwas eigenes! Das LABOR wartet auf Dich."
+        morgen << @notomorrow
     end
 	return morgen
 end
-
 # Gucken, ob heute was ist, falls ja: zurückgeben
 def today(at)
 	heute = String.new("\n")
     at.each { |term|
     if term.date == Date.today then
-        heute << "Heute im LABOR: \n#{term}  #{term.link}";
+		heute << @introtoday
+        heute << " \n#{term}  #{term.link}";
     end
     }
 	if heute == "\n"
-		heute << "Heute ist keine Veranstaltung im LABOR. Komm' vorbei und mach etwas eigenes! Das LABOR wartet auf Dich."
+		heute << @notoday
 	end
     return heute
 end
-
 # Alle zukünftigen Termine zurückgeben
 def listall(at)
 	alles = String.new("\n")
@@ -113,83 +145,91 @@ end
 
 #puts(listall(at))
 
-# Mit Jabber-Server verbinden
-@client = Jabber::Client.new(Jabber::JID.new('terminbot@das-labor.org/LABOR-Veranstaltungen'))
-@client.connect
-@client.auth('foobar23')
+def setup_connection
+	# Mit Jabber-Server verbinden
+	@client = Jabber::Client.new(Jabber::JID.new(@account))
+	@client.connect
+	@client.auth(@password)
+end
+
+def setup_roster
+	# Roster bauen
+	@roster = Jabber::Roster::Helper.new(@client)
 
 
-# Roster bauen
-@roster = Jabber::Roster::Helper.new(@client)
+	# Alle subscription requests akzeptieren und ebenfalls subscription requesten
+	@roster.add_subscription_request_callback { |item, presence|
+		if presence.type == :subscribe
+	    	@roster.accept_subscription(presence.from)
+			# puts("subscribed")
+			item.subscribe()
+			# Dem User erklären, warum er den Bot authorisieren muss und was der Bot kann
+			instruction = Jabber::Message.new(item.jid)
+			instruction.type = :chat
+			instruction.set_body(@instructiontext)
+			@client.send(instruction)
+		end
+	}
 
+	# Wenn ein User unsubscribet, also die Authorisation zurückzieht,
+	# entferne diesen User aus dem Roster des Bots
+	@roster.add_subscription_callback { |item, presence|
+	    if presence.type == :unsubscribed
+			# puts("unsubscribed")
+	        item.remove()
+	    end
+	}
+end
 
-# Alle subscription requests akzeptieren und ebenfalls subscription requesten
-@roster.add_subscription_request_callback { |item, presence|
-	if presence.type == :subscribe
-    	@roster.accept_subscription(presence.from)
-#		puts("subscribed")
-		item.subscribe()
-# Dem User erklären, warum er den Bot authorisieren muss und was der Bot kann
-		instruction = Jabber::Message.new(item.jid)
-		instruction.type = :chat
-		instruction.set_body("Welcome to the event reminder service!\n
-For the bot to work properly (including being able to unsubscribe from the bot!), you have to grant your authorization. 
-The bot will remind you one day, and again 4h before any event. Furthermore it knows the following commands: today, tomorrow, all")
-		@client.send(instruction)
-	end
-}
+def connect
+	@client.send(Jabber::Presence.new.set_show(:chat).set_status(@status))
+	#@client.send(Jabber::Presence.new(:chat, 'LABOR-Termine'))
+end
 
-# Wenn ein User unsubscribet, also die Authorisation zurückzieht,
-# entferne diesen User aus dem Roster des Bots
-@roster.add_subscription_callback { |item, presence|
-    if presence.type == :unsubscribed
-#        puts("unsubscribed")
-        item.remove()
-    end
-}
-
-@client.send(Jabber::Presence.new.set_show(:chat).set_status('waiting for your requests'))
-#@client.send(Jabber::Presence.new(:chat, 'LABOR-Termine'))
-
-
+def setup_messagehandler
 @client.add_message_callback { |m|
 #	puts(m.body())
 #	puts Date.today
 	if m.body == "tomorrow" then
 		answer = Jabber::Message.new(m.from)
 		answer.type = :chat
-		answer.set_body(tomorrow(at))
+		answer.set_body(tomorrow(@at))
 		@client.send(answer)
     elsif m.body == "today" then
         answer = Jabber::Message.new(m.from)
    	    answer.type = :chat
-       	answer.set_body(today(at))
+       	answer.set_body(today(@at))
         @client.send(answer)
+#    elsif m.body == "next7" then
+#		answer = Jabber::Message.new(m.from)
+#        answer.type = :chat
+#        answer.set_body(next7(@at))
+#        @client.send(answer)
     elsif m.body == "all" then
-        if(listall(at))
+        if(listall(@at))
             answer = Jabber::Message.new(m.from)
             answer.type = :chat
-            answer.set_body(listall(at))
+            answer.set_body(listall(@at))
             @client.send(answer)
         end
-    elsif m.body == "unsubscribe" then
-		remove_jid(m.from)
+# as tim requested an annoyance feature for the bot, there it is
+	elsif m.body == "annoytim" then
+		answer = Jabber::Message.new(m.from)
+        answer.type = :chat
+        answer.set_body("You just annoyed Tim")
+		annoy = Jabber::Message.new(@timsjid)
+		annoy.type = :chat
+		annoy.set_body(@annoytext)
+		@client.send(annoy)
+        @client.send(answer)
 	elsif
 		answer = Jabber::Message.new(m.from)
 		answer.type = :chat
-        answer.set_body("
-Command not known!
-I know the following commands:
-all			Shows all events
-today		Shows todays events
-tomorrow	shows tomorrows events
-
-To unsubscribe from my service, just revoke my authorization or delete me from your roster.
-")
-
+        answer.set_body(@helpmessage)
         @client.send(answer)
 	end
 }
+end
 
 # Die Verbindung bricht ständig weg.
 # Wir brauchen etwas, das wenig, aber regelmäßig Traffic macht
@@ -203,7 +243,16 @@ def keepalive
 	}
 end
 
-keepalive()
+# und alles ausführen (in der richtigen Reihenfolge)
+setup_connection()
+setup_roster()
+connect()
+setup_messagehandler()
+
 Thread.stop
+
+Thread.new {
+keepalive()
+}
 
 @client.close
