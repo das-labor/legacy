584bd22a2265bbb67885cd806117c70757e3abb2
joern <joern@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-11-01 18:29:41 +0000

    AAAAAAAAAAAAAARGH!

--- microcontroller/src-atmel/borg/borg-base/main.c
+++ microcontroller/src-atmel/borg/borg-base/main.c
@@ -1,22 +1,60 @@
+
 #include <setjmp.h>
 #include <avr/interrupt.h>
-#include <avr/pgmspace.h>
+
 #include "config.h"
+#include "programm.h"
 #include "borg_hw.h"
 #include "pixel.h"
-#include "scrolltext.h"
+#include "borg_can.h"
 
 jmp_buf newmode_jmpbuf;
-uint8_t text[] PROGMEM = "IT'S COLDER THAN BEFORE\n";
 
 int main (void){
 	unsigned char mode = 0;
+
 	clear_screen(0);
 	borg_hw_init();
+	bcan_init();	
 	sei();
 
 	mode = setjmp(newmode_jmpbuf);
+
 	for(;;){
-		alice_putstr(text);
+		switch(mode++) {
+		case 0:
+			off();
+			break;
+		case 1:
+			schachbrett(20);
+			break;
+		case 2:
+			spirale(20);
+			break;
+		case 3:
+			fadein();
+			break;
+		case 4:
+			joern1();
+			break;
+		case 5:
+			test1();
+			break;
+		case 6:
+			snake();
+			break;
+		case 7:
+			break;
+		case 8:
+			feuer();
+			break;
+/*
+		case 9:
+			//matrix();
+*/
+			break;
+		default:
+			mode = 1;
+		}
 	}
 }
