0950f4964a4a03fd75dceaaf7c47aba31b1c6309
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-11-16 15:36:16 +0000

    upps, old version

--- vhdl/Tests-S3E/UartTest/uart_rx.vhd
+++ vhdl/Tests-S3E/UartTest/uart_rx.vhd
@@ -12,7 +12,7 @@ entity uart_rx is
 		reset    : in  std_logic;
 		--
 		dout     : out std_logic_vector(7 downto 0);
-		full     : out std_logic;
+		avail    : out std_logic;
 		error    : out std_logic;
 		clear    : in  std_logic;
 		--
@@ -27,6 +27,7 @@ architecture rtl of uart_rx is
 signal bitcount  : integer range 0 to 10;
 signal count     : integer range 0 to FULLBIT;
 signal shiftreg  : std_logic_vector(7 downto 0);
+signal rxd2      : std_logic;
 
 begin
 
@@ -35,22 +36,24 @@ begin
 	if reset='1' then
 		bitcount <= 0;
 		count    <= 0;
+		error    <= '0';
+		avail    <= '0';
 	elsif clk'event and clk='1' then
 		if clear='1' then 
 			error <= '0';
-			full  <= '0';
+			avail <= '0';
 		end if;
 	
 		if count/=0 then 
 			count <= count - 1;
 		else
 			if bitcount=0 then     -- wait for startbit
-				if rxd='0' then     -- FOUND
+				if rxd2='0' then     -- FOUND
 					count    <= HALFBIT;
 					bitcount <= bitcount + 1;						
 				end if;
 			elsif bitcount=1 then  -- sample mid of startbit
-				if rxd='0' then     -- OK
+				if rxd2='0' then     -- OK
 					count    <= FULLBIT;
 					bitcount <= bitcount + 1;
 					shiftreg <= "00000000";
@@ -59,11 +62,11 @@ begin
 					bitcount <= 0;
 				end if;
 			elsif bitcount=10 then -- stopbit
-				if rxd='1' then     -- OK
+				if rxd2='1' then     -- OK
 					count    <= 0;
 					bitcount <= 0;
-					dout <= shiftreg;
-					full <= '1';
+					dout     <= shiftreg;
+					avail    <= '1';
 				else                -- ERROR
 					count    <= FULLBIT;
 					bitcount <= 0;
@@ -71,11 +74,22 @@ begin
 				end if;
 			else
 				shiftreg(6 downto 0) <= shiftreg(7 downto 1);
-				shiftreg(7) <= rxd;
+				shiftreg(7) <= rxd2;
 				count    <= FULLBIT;
 				bitcount <= bitcount + 1;
 			end if;
 		end if;
+	end if;
+end process;
+
+-----------------------------------------------------------------------------
+-- Sync incoming RXD (anti metastable) --------------------------------------
+syncproc: process(reset, clk) is
+begin
+	if reset='1' then
+		rxd2 <= '1';
+	elsif clk'event and clk='1' then
+		rxd2 <= rxd;
 	end if;
 end process;
 					
