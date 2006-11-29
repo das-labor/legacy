df5e46e4b0556147bdaa80fb0ea4071bec0e4fda
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2006-11-29 01:10:29 +0000

    added detection and ctl for PNP plane drivers.

--- microcontroller/src-atmel/borg/borg-3d/borg_hw.h
+++ microcontroller/src-atmel/borg/borg-3d/borg_hw.h
@@ -5,22 +5,6 @@
 
 //#define LINEBYTES (((NUM_COLS-1)/8)+1)
 
-#define CTRLPORT  PORTA
-#define CTRLDDR   DDRA
-
-#define DATAPORT  PORTC
-#define DATADDR   DDRC
-
-#define PIN_OE_DATA    	 PA0
-#define PIN_SHIFT_DATA   PA1
-#define PIN_CLEAR_DATA 	 PA2
-#define PIN_CLK_DATA 	 PA3
-#define PIN_SHIFT_PLANE  PA4
-#define PIN_CLK_PLANE    PA5
-#define PIN_CLEAR_PLANE  PA6
-
-
-
 extern unsigned char pixmap[NUM_LEVELS][NUM_PLANES][PLANEBYTES];
 
 void timer0_off();
