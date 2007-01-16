53e8bd28848ac6d25901b2bcba72c0b27e19e01f
til <til@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-01-16 18:15:55 +0000

    statusbot client and server

---
+++ tools/statusbot/server/config.rb
@@ -0,0 +1,23 @@
+# config options. adjust to your needs
+######################################
+
+@debug = 1		# set 1 to enable
+@interval=30    # keep-alive interval
+@account = 'statusdaemon@das-labor.org/status'  # the bots account / resource
+@password = 'foobar23'  # password (ah, no! just kidding! password must have a different meaning)
+@status = 'no status available, please wait...'   # the bots status message
+
+@setaccount = "statsetter@das-labor.org/status" # the account that may set the status
+
+# labor status icon variables
+@staticon = 'status.png'
+@iconon = 'statusON.png'
+@iconoff = 'statusOFF.png'
+@iconnone = 'statusNONE.png'
+
+# the text a user gets on subscription
+@instructiontext = "This is the LABOR status daemon!\n Go away!
+"
+
+######################################
+
