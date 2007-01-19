cc8149d317793cd49287d81673a6aa4f684f25c1
til <til@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-01-19 01:35:10 +0000

    timeout variable added

--- tools/statusbot/server/config.rb
+++ tools/statusbot/server/config.rb
@@ -3,6 +3,7 @@
 
 @debug = 1		# set 1 to enable
 @interval=30    # keep-alive interval
+@statustimeout=4	# how many keep-alive intervals to wait before setting status to NONE (20 might be a good value)
 @account = 'statusdaemon@das-labor.org/status'  # the bots account / resource
 @password = 'foobar23'  # password (ah, no! just kidding! password must have a different meaning)
 @status = 'no status available, please wait...'   # the bots status message
