35f4f63125209c61c70aed8c512229e0954f409c
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-02-06 01:43:43 +0000

    blah

--- microcontroller-2/LaborAccess/Panel/channel.h
+++ microcontroller-2/LaborAccess/Panel/channel.h
@@ -1,5 +1,6 @@
 
 #include "avrx.h"
+#include <inttypes.h>
 
 
 // Interface with using task
@@ -8,4 +9,4 @@ uint8_t channel_read(uint8_t num, uint8_t * buffer, uint8_t size);
 uint8_t channel_write(uint8_t num, uint8_t * buffer, uint8_t size);
 
 //Interface with receiving task
-void packet_received(uint8_t * data, uint16_t size);
+void packet_received(uint8_t id, uint8_t * data, uint16_t size);
--- microcontroller-2/LaborAccess/Panel/client.c
+++ microcontroller-2/LaborAccess/Panel/client.c
@@ -11,6 +11,8 @@
 #include "asn1.h"
 #include "channel.h"
 
+#include "protocol.h"
+
 ReaderMsg_t msg;
 
 uint8_t buf[256];
@@ -23,6 +25,8 @@ void putbuf(uint8_t * b, uint8_t s){
 
 }
 
+//uint8_t muh[]={0xde,0xad,0xbe,0xef, 0x69, 0x0e ,0x80, 0x02, 0x01, 0x00, 0x81, 0x08, 0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88};
+
 
 AVRX_GCC_TASKDEF(client, 90, 4){
 	while(1){
@@ -32,18 +36,32 @@ AVRX_GCC_TASKDEF(client, 90, 4){
 	
 		CARD_POWER_ON();
 
-		if(i2cEeDetect()){
-			uint8_t buf[30];
+		//i2cEeWrite(0,sizeof(muh), muh);
 		
-			asn1_obj_t root_obj = {4,251};
-			asn1_obj_t obj;
+		if(! i2cEeDetect())
+			goto error;
+		
+		request_cred_t request;
+		
+		asn1_obj_t root_obj = {4,251};
+		asn1_obj_t obj;
 			
-			asn1_get(&obj, &root_obj, 0x60);
+		if (asn1_get(&obj, &root_obj, 0x69))
+			goto error;
 			
-			asn1_read(&obj, 0x80, buf, 30);
+		request.type = REQUEST_OPEN_CRED;
 			
-			putbuf(buf, 32);
-		}
+		if ( asn1_read(&obj, 0x80, (uint8_t *) &request.credentials.index, 2) != 2)
+			goto error;
+			
+		if ( asn1_read(&obj, 0x81, (uint8_t *) &request.credentials.key, 8) != 8 )
+			goto error;
+		
+		channel_write(0, (uint8_t *)&request, sizeof(request));
+		
+		//channel_read(0, 
+		
+		error:
 		
 		CARD_POWER_OFF();
 
