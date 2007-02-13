a4d891520222187059f9e11bf48441282c4f2066
chris <chris@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-02-13 14:59:22 +0000

    Weiter gehen, hier gibt's nichts zu sehen...

--- microcontroller/src-atmel/borg/borg-apiSimulator/tetris/playfield.c
+++ microcontroller/src-atmel/borg/borg-apiSimulator/tetris/playfield.c
@@ -1,25 +1,66 @@
 #include <stdlib.h>
-#include <string.h>
 #include "playfield.h"
 
 /*****************************
  *  construction/destruction *
  *****************************/
-tetris_playfield_t* tetris_playfield_construct() {
-	tetris_playfield_t* p_playfield = (tetris_playfield_t*) malloc(sizeof(tetris_playfield_t));
-	tetris_playfield_reset(p_playfield);
-	return p_playfield;
+tetris_playfield_t* tetris_playfield_construct(uint8_t nWidth, uint8_t nHeight) {
+	if (nWidth <= 16) {
+		tetris_playfield_t* p_playfield = (tetris_playfield_t*) malloc(sizeof(tetris_playfield_t));
+		if (p_playfield != NULL) {
+			p_playfield->contents = (uint16_t*) malloc(sizeof(uint16_t) * nHeight);
+			if (p_playfield->contents != NULL) {				
+				p_playfield->nWidth = nWidth;
+				p_playfield->nHeight = nHeight;
+				tetris_playfield_reset(p_playfield);
+				
+				return p_playfield;
+			} else {
+				free(p_playfield);
+				p_playfield = NULL;
+			} 
+		}
+	}
+	return NULL;
 }
 
 void tetris_playfield_destruct(tetris_playfield_t* p_pl) {
-	if (p_pl != 0)
+	if (p_pl != NULL) {
+		if (p_pl->contents != NULL) {
+			free(p_pl->contents);
+		}
 		free(p_pl);
+	}
 }
 
 
 /********************************
  *  playfield related functions *
  ********************************/
+
+/* Function:           tetris_playfield_reset
+ * Description:        Resets playfield to begin a new game;
+ * Argument p_pl:      The playfield to perform action on
+ * Return value:       void
+ */
+void tetris_playfield_reset(tetris_playfield_t* p_pl) {
+	if (p_pl != NULL) {
+		p_pl->nFillHeight = 0;
+		p_pl->p_piece = NULL;
+		p_pl->status = TETRIS_PFS_DOCKED;
+		p_pl->nColumn = 0;
+		p_pl->nRow = 0;
+		
+		/* clear contents */
+		if (p_pl->contents != NULL) {
+			int i;
+			for (i = 0; i < p_pl->nHeight; ++i) {
+				p_pl->contents[i] = 0;
+			}
+		}
+	}
+}
+
 
 /* Function:           tetris_playfield_insertPiece
  * Description:        Inserts a new piece
@@ -34,12 +75,12 @@ uint8_t tetris_playfield_insertPiece(tetris_playfield_t* p_pl, tetris_piece_t* p
 		p_pl->p_piece = p_piece;
 		
 		/* set start position (in the middle of the top line) */
-		p_pl->x = (TETRIS_PF_WIDTH - 2) / 2;
+		p_pl->nRow = (p_pl->nWidth - 2) / 2;
 		int8_t row = tetris_piece_lastSolidMatterRow(p_piece);
 		if (row > -1) {
-			p_pl->y = -row;
+			p_pl->nRow = -row;
 		} else {
-			p_pl->y = 0;
+			p_pl->nRow = 0;
 		}
 		
 		/* OK */
@@ -51,13 +92,43 @@ uint8_t tetris_playfield_insertPiece(tetris_playfield_t* p_pl, tetris_piece_t* p
 }
 
 
-/* Function:           tetris_playfield_rotatePiece
- * Description:        rotates falling piece to the given direction
+/* Function:           collisionDetect
+ * Description:        
  * Argument p_pl:      The playfield to perform action on
- * Argument r:         The direction of the rotation (see also tetris_piece_rotation_t in piece.h)
- * Return value:       0 corresponds to false, anything other to true
+ * Argument p_pc:      The piece which shoud be checked
+ * Argument nRow:      The row where the piece should be moved
+ * Argument nColumn:   The column where the piece should be moved
+ * Return value:       1 for collision, 0 otherwise
  */
-uint8_t tetris_playfield_rotatePiece(tetris_playfield_t* p_pl, enum tetris_piece_rotation_t r) {
+uint16_t tetris_playfield_collisionDetect(tetris_playfield_t* p_pl, tetris_piece_t* p_pc, int8_t nRow, int8_t nColumn) {
+	/* only allow coordinates which are within sane ranges */
+	if ((nRow >= p_pl->nHeight) || (nColumn >= p_pl->nHeight)) {
+		
+		uint16_t piece = tetris_piece_getBitfield(p_pc);
+		uint16_t nPlayfieldRow;
+		uint16_t nPieceRow;
+		int8_t y;		
+
+		for (y = (nRow < 0) ? -nRow : 0; (y < 4) && ((y + nRow) < p_pl->nHeight); ++y) {
+			
+			/* negative shifting values are bad, so we have to work around them */
+			if (nColumn < 0) {
+				nPlayfieldRow = ((p_pl->contents[y + nRow] & (0x000F >> -nColumn)) << -nColumn) | (0x000F >> (4 + nColumn));
+			/* if piece passes right border, we insert some matter in playfield compare row */
+			} else if ((nColumn + 3) >= p_pl->nWidth) {
+				nPlayfieldRow = (p_pl->contents[y + nRow] >> nColumn) | (((0x000F << (nColumn + 4 - p_pl->nWidth))) & 0x000F);
+			} else {
+				nPlayfieldRow = (p_pl->contents[y + nRow] & (0x000F << nColumn)) >> nColumn;
+			}
+			
+			nPieceRow = (piece & (0x000F << (y * 4))) >> (0x000F << (y * 4)); 
+
+			if ((nPlayfieldRow & nPieceRow) != 0) {
+				return 1;
+			}
+		}
+	}
+	
 	return 0;
 }
 
@@ -69,17 +140,6 @@ uint8_t tetris_playfield_rotatePiece(tetris_playfield_t* p_pl, enum tetris_piece
  */
 enum tetris_playfield_status_t tetris_playfield_advancePiece(tetris_playfield_t* p_pl) {
 	return TETRIS_PFS_DOCKED;
-}
-
-
-/* Function:           tetris_playfield_isAllowedDirection
- * Description:        Informs if piece can be moved to given direction
- * Argument p_pl:      The playfield to perform action on
- * Argument direction: Direction (see tetris_playfield_direction_t)
- * Return value:       0 corresponds to false, anything other to true
- */
-uint8_t tetris_playfield_isAllowedDirection(tetris_playfield_t* p_pl, enum tetris_playfield_direction_t direction) {
-	return 0;
 }
 
 
@@ -110,16 +170,4 @@ enum tetris_playfield_status_t tetris_playfield_dropPiece(tetris_playfield_t* p_
  */
 uint8_t tetris_playfield_removeCompleteLines(tetris_playfield_t* p_pl) {
 	return 0;
-}
-
-
-/* Function:           tetris_playfield_reset
- * Description:        Resets playfield to begin a new game;
- * Argument p_pl:      The playfield to perform action on
- * Return value:       void
- */
-void tetris_playfield_reset(tetris_playfield_t* p_pl) {
-	if (p_pl != NULL) {
-		memset(p_pl, 0, sizeof(tetris_playfield_t));
-	}
 }
