b9a4cf92b926bf6ca0cbe9b52322146895638aa2
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-03-05 01:05:48 +0000

    wer weiss ob das man geht....

--- microcontroller/src-atmel/automatization/torcontrol/radio.c
+++ microcontroller/src-atmel/automatization/torcontrol/radio.c
@@ -3,12 +3,19 @@
 #include <avr/io.h>
 #include <string.h>
 
-#include "radiocontrol.h"
+#include "radio.h"
 
+/*
 void radio_init ( void )
 {
 	cli();
 	radio_txcount = 0;
+
+	TCNT1 = 100;			// Load overflow counter of timer0
+	TCCR1 = (1<<CS02);		// Set Timer1 to CPUCLK/256
+	TIMSK |= 1<<TOIE1;		// Enable interrupt flag
+
+//   Soerens (funktionierender) code
 //	TCCR0B |= (1<<CS01);// | (1<<CS00);  // clk / 64
 //	TCCR0A |= (1<<WGM01); // ctc mode
 //	TIMSK  |= (1<<OCIE0A);
@@ -16,17 +23,18 @@ void radio_init ( void )
 //	OCR0A = RADIO_SEND_TIME;
 	sei();
 }
+*/
 
 const uint8_t sendcode[5] = { 0x65, 0xb2, 0xcb, 0x2c, 0xb0 };
 
 uint8_t radio_txcount;
 
-ISR(TIMER0_COMPA_vect)
+// AVRX_SIGINT(SIG_INTERRUPT1)
+void radio_tick()
 {
 	static uint8_t bytecounter = 0, bitmask = 0x80;
 
-	
-
+//	IntProlog();             // Switch to kernel stack/context
 
 	if (bytecounter == 0)
 	{
@@ -59,4 +67,6 @@ ISR(TIMER0_COMPA_vect)
 		bitmask = 0x80;
 		bytecounter++;
 	}
+
+//	Epilog();
 }
