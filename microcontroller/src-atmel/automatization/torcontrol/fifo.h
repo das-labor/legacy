2f525320e0d93bfa3902a3a419f7080d2446a336
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-03-05 00:46:52 +0000

    torcontrol von soeren

---
+++ microcontroller/src-atmel/automatization/torcontrol/fifo.h
@@ -0,0 +1,51 @@
+#ifndef _AvrXFifo_h_
+#define _AvrXFifo_h_
+#include "avrx.h"
+/*
+    Buffered BYTE oriented FIFO
+
+	Uses providers/consumer model.  When an interrupt
+	drives the consumer/provider side use the interrupt calls
+
+	The fifo needs to be both declared and initialized.
+*/
+
+typedef struct AvrXFifo
+{
+	Mutex	Producer;
+	Mutex	Consumer;
+	uint8_t in;
+	uint8_t out;
+	uint8_t size;
+	uint32_t buf[];
+}
+AvrXFifo, *pAvrXFifo;
+
+enum
+{
+	FIFO_OK,
+	FIFO_ERR = -1	// Same as stdio EOF
+};
+
+#define AVRX_DECL_FIFO(Name, Size)					\
+uint8_t Name##Fifo[ Size * 4 + sizeof(AvrXFifo)];	\
+const pAvrXFifo Name = (pAvrXFifo)Name##Fifo;		\
+static const uint8_t Name##FifoSz = Size;
+
+#define AVRX_INIT_FIFO(Name)		\
+	AvrXFlushFifo(Name);			\
+	Name->size = Name##FifoSz
+
+#define AVRX_EXT_FIFO(Name)			\
+	extern uint8_t Name##Fifo[];	\
+	static const pAvrXFifo Name = (pAvrXFifo)Name##Fifo
+
+int16_t AvrXPutFifo(pAvrXFifo, uint32_t);
+int16_t AvrXPullFifo(pAvrXFifo);
+void AvrXWaitPutFifo(pAvrXFifo, uint32_t);
+uint32_t AvrXWaitPullFifo(pAvrXFifo);
+int16_t AvrXPeekFifo(pAvrXFifo);
+int16_t AvrXStatFifo(pAvrXFifo);
+void AvrXFlushFifo(pAvrXFifo);
+
+#endif	// _AvrXFifo_h_
---
+++ microcontroller/src-atmel/automatization/torcontrol/rf.h
@@ -0,0 +1,39 @@
+#ifndef RADIOCONTROL_H_
+#define RADIOCONTROL_H_
+#ifndef RADIO_QUEUESIZE
+	// size of queue in bytes.
+	#define RADIO_QUEUESIZE 8
+#endif
+
+#ifndef RADIO_TX_PORT
+	#define RADIO_TX_PORT PORTD
+#endif
+#ifndef RADIO_TX_BIT
+	#define RADIO_TX_BIT _BV(PD6)
+#endif
+
+// value for output compare match. see ISR in radiocontrol.c
+#ifndef RADIO_SEND_TIME
+	#define RADIO_SEND_TIME 50  /* one third of real send time */
+//	#define RADIO_SEND_TIME 0x20  /* one third of real send time */
+#endif
+// how many ms * 10 to wait before radio_tx returns an error
+#ifndef RADIO_TIMEOUT
+	#define RADIO_TIMEOUT 15
+#endif
+#define RADIO_TIMER_ENABLE() 
+
+
+/* ___ END OF CONFIG ___ */
+#if ( RADIO_QUEUESIZE > 32 )
+	#warning RADIO_QUESIZE _must not_ exceed 32 bytes -- set to 32.
+	#define RADIO_QUEUESIZE 32
+#endif
+
+void radio_init ( void );
+uint8_t radio_tx ( uint8_t *in_str );
+
+extern uint8_t radio_txcount;
+
+
+#endif
