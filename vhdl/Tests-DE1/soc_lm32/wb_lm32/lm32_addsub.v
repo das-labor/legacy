c2f14d2699898a833afee3d69e38bfd777ad3bc6
madex <madex@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-03-12 18:55:57 +0000

    Added a DE1 folder for all quartus DE1 bases projects

---
+++ vhdl/Tests-DE1/soc_lm32/wb_lm32/lm32_addsub.v
@@ -0,0 +1,90 @@
+// =============================================================================
+//                           COPYRIGHT NOTICE
+// Copyright 2006 (c) Lattice Semiconductor Corporation
+// ALL RIGHTS RESERVED
+// This confidential and proprietary software may be used only as authorised by
+// a licensing agreement from Lattice Semiconductor Corporation.
+// The entire notice above must be reproduced on all authorized copies and
+// copies may only be made to the extent permitted by a licensing agreement from
+// Lattice Semiconductor Corporation.
+//
+// Lattice Semiconductor Corporation        TEL : 1-800-Lattice (USA and Canada)
+// 5555 NE Moore Court                            408-826-6000 (other locations)
+// Hillsboro, OR 97124                     web  : http://www.latticesemi.com/
+// U.S.A                                   email: techsupport@latticesemi.com
+// =============================================================================/
+//                         FILE DETAILS
+// Project          : LatticeMico32
+// File             : lm32_addsub.v
+// Title            : PMI adder/subtractor.
+// Version          : 6.0.13
+// =============================================================================
+
+/////////////////////////////////////////////////////
+// Module interface
+/////////////////////////////////////////////////////
+
+module lm32_addsub (
+    // ----- Inputs -------
+    DataA, 
+    DataB, 
+    Cin, 
+    Add_Sub, 
+    // ----- Outputs -------
+    Result, 
+    Cout
+    );
+
+/////////////////////////////////////////////////////
+// Inputs
+/////////////////////////////////////////////////////
+
+input [31:0] DataA;
+input [31:0] DataB;
+input Cin;
+input Add_Sub;
+
+/////////////////////////////////////////////////////
+// Outputs
+/////////////////////////////////////////////////////
+
+output [31:0] Result;
+wire   [31:0] Result;
+output Cout;
+wire   Cout;
+
+/////////////////////////////////////////////////////
+// Instantiations
+///////////////////////////////////////////////////// 
+
+//pmi_addsub #(
+//    // ----- Parameters -------
+//    .pmi_data_width     (32),
+//    .pmi_result_width   (32),
+//    .pmi_sign           ("off"),
+//    .pmi_family         ("ECP"),
+//    .module_type        ("pmi_addsub")
+//  ) addsub (
+    // ----- Inputs -------
+//    .DataA              (DataA),
+//    .DataB              (DataB),
+//    .Cin                (Cin),
+//    .Add_Sub            (Add_Sub),
+    // ----- Outputs -------
+//    .Result             (Result),
+//    .Cout               (Cout),
+//    .Overflow           ()
+//    );
+
+assign Result = DataA + DataB;
+
+//my_addsub(
+//	.A	(DataA),
+//	.B	(DataB),
+//	.C_IN	(Cin),
+//	.C_OUT	(Cout),
+//	.ADD	(Add_Sub),
+//	.S	(Result)
+//	);
+
+endmodule
---
+++ vhdl/Tests-DE1/soc_lm32/wb_lm32/system_conf.v
@@ -0,0 +1,25 @@
+`ifndef SYSTEM_CONF
+`define SYSTEM_CONF
+`timescale 1ns / 100 ps
+`define ADDRESS_LOCK
+`define ebrEBR_DATA_WIDTH 32
+`define ebrINIT_FILE_NAME "C:/LatticeMico32/micosystem/platforms/PlatformA/PlatformA.mem"
+`define ebrINIT_FILE_FORMAT "hex"
+`define ADDRESS_LOCK
+`define LEDBOTH_INPUT_AND_OUTPUT
+`define LEDDATA_WIDTH 32'h8
+`define LEDINPUT_WIDTH 32'h1
+`define LEDOUTPUT_WIDTH 32'h8
+`define LEDPOSE_EDGE_IRQ
+`define CFG_EBA_RESET 32'h0
+`define CFG_SIGN_EXTEND_ENABLED
+`define INCLUDE_LM32
+`define ADDRESS_LOCK
+// `define CFG_ICACHE_ENABLED
+// `define CFG_ICACHE_BASE_ADDRESS 0
+// `define CFG_ICACHE_LIMIT 2048
+// `define CFG_ICACHE_SETS 256
+// `define CFG_ICACHE_BYTES_PER_LINE 8
+// `define CFG_ICACHE_ASSOCIATIVITY 1
+
+`endif // SYSTEM_CONF
