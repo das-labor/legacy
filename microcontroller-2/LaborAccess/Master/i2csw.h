6c648ccaf0d35d9b8c4a0f42203ccde7b078c1df
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-03-15 14:39:43 +0000

    blah

---
+++ microcontroller-2/LaborAccess/Master/i2csw.h
@@ -0,0 +1,35 @@
+#ifndef I2CSW_H
+#define I2CSW_H
+
+#include "types.h"
+
+// include project-dependent settings
+#include "i2cswconf.h"
+
+// functions
+
+void i2cStart();
+
+void i2cStop();
+
+
+u08 i2cPutbyte(u08 b);
+
+u08 i2cGetbyte(u08 last);
+
+
+void i2cInit(void);
+
+//write to i2c eeprom in multibyte write mode
+s08 i2cEeWrite(u16 address, u16 len, u08 *data);
+
+//read bytes from eeprom to buffer
+void i2cEeRead(u16 address, u16 len, u08 *data);
+
+//detect if there is a valid i2c eeprom card inserted
+//returns card size in sectors of 256 bytes
+//a return value of zero means no valid card
+u08 i2cEeDetect();
+
+
+#endif
