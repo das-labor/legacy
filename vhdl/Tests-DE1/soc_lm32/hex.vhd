a782e8d3820a5c6c44498c5372e5f325baf89ce1
madex <madex@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-03-16 23:41:16 +0000

    added support for switching between the high and the low word of the programmcounter in the 7 segment display

--- vhdl/Tests-DE1/soc_lm32/hex.vhd
+++ vhdl/Tests-DE1/soc_lm32/hex.vhd
@@ -7,31 +7,38 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity hex is
+entity nibble7Seg is
    port (
-      digit    : in  std_logic_vector(3 downto 0);
+      nibble0  : in  std_logic_vector(3 downto 0);
+	  nibble1  : in  std_logic_vector(3 downto 0);
+	  sel      : in  std_logic;
 	  hex      : out std_logic_vector(6 downto 0)	
         );
-end hex;
+end nibble7Seg;
 
-architecture rtl of hex is
+architecture rtl of nibble7Seg is
+signal dig  : std_logic_vector(3 downto 0);
 begin
-hex <= "1000000" when digit="0000" else
-	   "1111001" when digit="0001" else
-	   "0100100" when digit="0010" else
-	   "0110000" when digit="0011" else
-	   "0011001" when digit="0100" else
-	   "0010010" when digit="0101" else
-	   "0000010" when digit="0110" else
-	   "1111000" when digit="0111" else
-	   "0000000" when digit="1000" else
-	   "0010000" when digit="1001" else
-	   "0001000" when digit="1010" else
-	   "0000011" when digit="1011" else
-	   "1000110" when digit="1100" else
-	   "0100001" when digit="1101" else
-	   "0000110" when digit="1110" else
-	   "0001110" when digit="1111";
+
+dig <= nibble0 when sel='0' else
+       nibble1;
+
+hex <= "1000000" when dig="0000" else
+	   "1111001" when dig="0001" else
+	   "0100100" when dig="0010" else
+	   "0110000" when dig="0011" else
+	   "0011001" when dig="0100" else
+	   "0010010" when dig="0101" else
+	   "0000010" when dig="0110" else
+	   "1111000" when dig="0111" else
+	   "0000000" when dig="1000" else
+	   "0010000" when dig="1001" else
+	   "0001000" when dig="1010" else
+	   "0000011" when dig="1011" else
+	   "1000110" when dig="1100" else
+	   "0100001" when dig="1101" else
+	   "0000110" when dig="1110" else
+	   "0001110" when dig="1111";
 
 --process is
 --begin
@@ -56,3 +63,4 @@ hex <= "1000000" when digit="0000" else
 --end process;
 
 end rtl;
+
