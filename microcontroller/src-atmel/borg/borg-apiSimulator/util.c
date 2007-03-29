bf15ee210ece71e30ee954cb96070ee38be545cc
chris <chris@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-03-29 00:48:49 +0000

    added waitForFire to the wait function and modes to the display loop

--- microcontroller/src-atmel/borg/borg-apiSimulator/util.c
+++ microcontroller/src-atmel/borg/borg-apiSimulator/util.c
@@ -2,12 +2,22 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <setjmp.h>
+#include "joystick.h"
 
 #ifdef _WIN32
 #	include <windows.h>
 #endif
 
+extern jmp_buf newmode_jmpbuf;
+
 void myWait(unsigned int ms) {
+	if (waitForFire) {
+		if (JOYISFIRE) {
+			longjmp(newmode_jmpbuf, 43);
+		}
+	}
+	
 #ifdef _WIN32
 	Sleep(ms);
 #else
