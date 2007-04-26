b50f1f721e9730d91e986965fd4c1f6a36a49da2
jul <jul@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-04-26 15:12:04 +0000

    kurz aufgeräumt und verwirrendes zeug gelöscht..

--- ruby/sandbox/base64.rb
+++
@@ -1,3 +0,0 @@
-require 'base64'
-
-p Base64.encode64('das ist ja ganz gut und schoen so...')
--- ruby/sandbox/cocoa/bluetooth.rb
+++ ruby/sandbox/cocoa/bluetooth.rb
@@ -3,7 +3,7 @@ require 'osx/cocoa'
 OSX::NSBundle.bundleWithPath("/System/Library/Frameworks/IOBluetoothUI.framework").load
 OSX.ns_import :IOBluetoothDeviceSelectorController
 OSX.ns_import :IOBluetoothDeviceInquiry
-OSX.ns_import :IOBluetoothDevice
+#OSX.ns_import :IOBluetoothDevice
 
 bluedevices = OSX::IOBluetoothDeviceSelectorController.deviceSelector
 # bluedevices = OSX::IOBluetoothDeviceInquiry #OSX::IOBluetoothDeviceSelectorController.deviceSelector
