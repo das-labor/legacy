5df54223797715f0e1e7365f266ad9e8a09869f7
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-05-24 21:56:30 +0000

    Testing

---
+++ microcontroller-2/LaborAccess/LabCardCreator/CTapiTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using CTapi;
+
+[TestFixture]
+public class CtTests
+{
+	[Test]
+	public void ResetTest()
+	{
+		// CTapi ct = new CTapi(0);
+		// ct.Reset();
+		Assert.Fail();
+	}
+}
