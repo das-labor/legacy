cb5f4106cd24bd580f642bd33d646e4624db6f68
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-06-15 23:02:43 +0000

    weiter genakkat

--- microcontroller-2/LaborAccess/Master/server1.c
+++ microcontroller-2/LaborAccess/Master/server1.c
@@ -14,9 +14,15 @@
 
 uint8_t doorstate;
 
+
+void send_simple_reply(uint8_t result){
+	channel_write(CHANNEL_SERVER1, (uint8_t *) &result, 1);
+}
+
+
 AVRX_GCC_TASKDEF(server1, 200, 4)
 {
-	uint8_t auth_mode = 0;
+	uint8_t authenticated_session = 0;
 	printf("Debug\r");
 	
 	while(1){
@@ -30,48 +36,86 @@ AVRX_GCC_TASKDEF(server1, 200, 4)
 		if(req.type < 0x80){//requests below 0x80 don't need credentials
 			switch(req.type){
 				case REQUEST_GET_DOORSTATE:
-					channel_write(CHANNEL_SERVER1, (uint8_t *) &doorstate, 1);
+					send_simple_reply(doorstate);
 					break;
 				case REQUEST_LOCK_DOWNSTAIRS:
 				{
-					uint8_t result = RESULT_OK;
 					doorstate &= ~(STATE_DOOR_DOWNSTAIRS);
-					channel_write(CHANNEL_SERVER1, (uint8_t *) &result, 1);
+					send_simple_reply(RESULT_OK);
 					break;
 				}
 				case REQUEST_UNLOCK_DOWNSTAIRS:
 				{
-					uint8_t result = RESULT_OK;
 					doorstate |= (STATE_DOOR_DOWNSTAIRS);
-					channel_write(CHANNEL_SERVER1, (uint8_t *) &result, 1);
+					send_simple_reply(RESULT_OK);
 					break;
 				}
-				default:
-					break;
-			}
-		}else{
-		
-			switch (req.type){
 				case REQUEST_AUTH:{
 					reply_auth_t reply;
 					request_cred_t * req_cred = (request_cred_t*) &req;
+					uint8_t result;
+					uint16_t id = req_cred->card_id;
 					
-					if(database_check(req_cred->card_id, req_cred->token) == 0){
-						reply.result = RESULT_OK;
-						database_get_nickname(req_cred->card_id, reply.nickname);
+					authenticated_session = 0;
+					reply.flags = 0;
+					
+					result = database_check(id, req_cred->token);
+					reply.result = result;
+					
+					if(result == RESULT_OK){
 						generate_token(reply.token);
-						database_store_token(req_cred->card_id, reply.token);
+						database_store_token(id, reply.token);
+						
+						reply.flags = database_get_flags(id);
+						database_get_nickname(id, reply.nickname);
+						
+						authenticated_session = 1;
+					}else if(result == RESULT_DEACTIVATED){
+						uint16_t did;
+						generate_token(reply.token);
+						database_store_token(id, reply.token);
+						
+						//if the card was deactivated, return the nickname of the
+						//one deactivating it.
+						did = database_get_deactivation_id(id);
+						database_get_nickname(did, reply.nickname);
 					}else{
-						reply.result = RESULT_DENIED;	
+						//result == RESULT_DENIED
 					}
 					
-					reply.privileges = 0;
-					
+					//return the reply to the client.
 					channel_write(CHANNEL_SERVER1, (uint8_t *) &reply, sizeof(reply_auth_t));
 					printf("reply\r");
-		
+					break;
+				}	
+				case REQUEST_RESET:
+					authenticated_session = 0;
+					send_simple_reply(RESULT_OK);
+					break;
+				default:
+					send_simple_reply(RESULT_DENIED);
+					break;
+			}
+			
+		}else{
+			if(authenticated_session != 1){
+				send_simple_reply(RESULT_DENIED);
+			}else{
+				switch (req.type){
+					case REQUEST_UNLOCK_UPSTAIRS:
+						doorstate |= STATE_DOOR_UPSTAIRS;
+						send_simple_reply(RESULT_OK);
+						break;
+					case REQUEST_LOCK_UPSTAIRS:
+						doorstate &= ~STATE_DOOR_UPSTAIRS;
+						send_simple_reply(RESULT_OK);
+						break;
+					
+					default:
+						send_simple_reply(RESULT_DENIED);
+						break;
+					
 				}
-				break;
 			}
 		}
 		
