78963d8387ce7c3d1a270a08906169b6fd2b7e51
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-06-18 03:10:12 +0000

    ready for public...

--- microcontroller/src-atmel/lib/rfmxx/main.c
+++ microcontroller/src-atmel/lib/rfmxx/main.c
@@ -8,11 +8,35 @@
 #include "config.h"
 #include "uart.h"
 
-//#define F_CPU 10000000UL
 #include <util/delay.h>
 
 void send(uint8_t i);
 void receive(void);
+
+
+void dump_registers (void)
+{
+	uint8_t i, k;
+	uint16_t rxbuf;
+
+	printf("\r\n");
+	for (i=0;i<0xff;i++)
+	{
+		rxbuf = rf12_trans((i));
+		if (rxbuf != 0x809c)
+		{
+			printf("%2x = %x\t", i, rxbuf);
+			if (k == 7)
+			{
+				printf("\r\n");
+			}
+			k = (k == 7) ? 0 : k+1;
+		}
+	}
+	rxbuf = rf12_trans(0x0000 | i);
+	printf("%i = %x\t", i, rxbuf);
+	printf("\r\n");
+}
 
 int main(void)
 {
@@ -27,25 +51,15 @@ int main(void)
 	uart_putstr("Hallo\r\n");
 	fdevopen(uart_putc, 0);
 
-//	rf12_init();					// ein paar Register setzen (z.B. CLK auf 10MHz)
-//	rf12_init();					// ein paar Register setzen (z.B. CLK auf 10MHz)
 	rf12_init();					// ein paar Register setzen (z.B. CLK auf 10MHz)
-	//rf12_setfreq(RF12FREQ(433.92));	// Sende/Empfangsfrequenz auf 433,92MHz einstellen
 	rf12_setfreq(RF12FREQ(434.0));	// Sende/Empfangsfrequenz auf 433,92MHz einstellen
-	rf12_setbandwidth(4, 1, 4);		// 200kHz Bandbreite, -6dB Verstärkung, DRSSI threshold: -79dBm 
+	rf12_setbandwidth(4, 1, 4);	// 200kHz Bandbreite, -6dB Verstärkung, DRSSI threshold: -79dBm 
 	rf12_setbaud(19200);			// 19200 baud
 	rf12_setpower(0, 6);			// 1mW Ausgangangsleistung, 120kHz Frequenzshift
 	PORTC = 0xff;
-//	while (1) asm volatile ("nop");
 
 	for (;;)
 	{
-	//	if (!(PINB & _BV(PB0)))
-		{
-			send(tx);
-			_delay_ms(100);
-		}
-
 		if (k<7)
 			tx <<= 1;
 		else
@@ -53,18 +67,23 @@ int main(void)
 		k++;
 		if (k==14) k=0;
 		
+		receive();
+		if (!(PINB & _BV(PB0)))
 		{
-			receive();
+			send(tx);
+			_delay_ms(100);
 		}
+		rxbuf = rf12_trans(0xCA83);
 	}
 }
 
 void receive(void)
 {
 	uint16_t moo = 0x00;
-	moo = rf12_rxdata_nb();
-	if (moo > 0xff)
-		PORTC = (moo & 0x00ff);
+//	moo = rf12_rxdata();
+	moo = rf12_rxdata_nb(0x5440);
+	if (moo > 0x00ff)
+		PORTC = (uint8_t) (moo & 0x00ff);
 }
 
 void send(uint8_t i)
