4f847074f75bfffdace0f50165d8bc7107fbee20
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-06-18 03:06:30 +0000

    increased code size by adding implementation of stuff

--- microcontroller-2/LaborAccess/Master/database.c
+++ microcontroller-2/LaborAccess/Master/database.c
@@ -1,40 +1,45 @@
 
 #include <stdint.h>
+#include <string.h>
 #include "i2csw.h"
+#include "protocol.h"
+
+#define DB_SIZE 1024
 
 #define DB_STATE_EMPTY 0xff
-#define DB_STATE_VALID 0x23
-
-#define FLAG_DEACTIVATED 
+#define DB_STATE_VALID 0x42
+#define DB_STATE_DEACTIVATED 0x23
 
 //One Database entry is 64 Byte in Size.
 typedef struct{
 	uint8_t state;
-	uint8_t flags;
+	uint8_t permissions;
 	uint16_t deactivation_id;
+	char reserved[12];
 	uint8_t token[8];
 	char nickname[8];
 	char realname[32];
-	char reserved[12];
 }db_entry_t;
 
 #define db ((db_entry_t *)0)
 
 uint8_t database_check(uint16_t id, uint8_t * token){
-	uint8_t state, flags;
+	uint8_t state;
 	uint8_t tmpbuf[8];
 	
+	if(id >= DB_SIZE)
+		return RESULT_DENIED;
+		
 	i2cEeRead(&state, &db[id].state, 1);
-	i2cEeRead(&flags, &db[id].flags, 1);
 	
-	if(state != DB_STATE_VALID)
+	if((state != DB_STATE_VALID) && (state != DB_STATE_DEACTIVATED)){
 		return RESULT_DENIED;
 		
 	i2cEeRead(tmpbuf, db[id].token, 8);
 	if( memcmp(tmpbuf, token, 8) != 0)
 		return RESULT_DENIED;
 	
-	if(flags & FLAG_DEACTIVATED)
+	if(state == DB_STATE_DEACTIVATED)
 		return RESULT_DEACTIVATED;
 	
 	return RESULT_OK;
@@ -45,11 +50,44 @@ void database_store_token(uint16_t id, uint8_t * token){
 }
 
 void database_get_nickname(uint16_t id, char * nickname){
-	i2cEeRead((uint8_t*)nickname, db[id].nickname, 8);
+	i2cEeRead((uint8_t*)nickname, (uint8_t*)db[id].nickname, 8);
+}
+
+uint8_t database_get_permissions(uint16_t id){
+	uint8_t permissions;
+	i2cEeRead(&permissions, &db[id].permissions, 1);
+	return permissions;
+}
+
+void database_set_permissions(uint16_t id, uint8_t permissions){
+	i2cEeWrite(&db[id].permissions, &permissions, 1);
 }
 
 uint16_t database_get_deactivation_id(uint16_t id){
 	uint16_t di;
-	i2cEeRead(&di, &db[id].deactivation_id, 1);
+	i2cEeRead((uint8_t*)&di, (uint8_t*)&db[id].deactivation_id, 2);
 	return di;
+}
+
+uint16_t database_new_entry(uint8_t permissions, char * nickname, char * realname){
+	uint16_t id;
+	uint8_t state;
+	for(id = 0; id<DB_SIZE; id++){
+		i2cEeRead(&state, &db[id].state, 1);
+		if(state != DB_STATE_EMPTY)
+			continue;
+		
+		state = DB_STATE_VALID;
+		i2cEeWrite(&db[id].state, &state, 1);
+		
+		//don't make new Admin cards here
+		permissions &= ~PERM_ADMIN;
+		i2cEeWrite(&db[id].permissions, &permissions, 1);
+		
+		i2cEeWrite(db[id].nickname, nickname, 8);
+		i2cEeWrite(db[id].realname, realname, 32);
+				
+		return id;
+	}
+	return 0xffff;
 }
--- microcontroller-2/LaborAccess/Master/server1.c
+++ microcontroller-2/LaborAccess/Master/server1.c
@@ -9,6 +9,7 @@
 #include "channels.h"
 #include "channel.h"
 #include "random.h"
+#include "database.h"
 
 #include <stdio.h>
 
@@ -22,7 +23,8 @@ void send_simple_reply(uint8_t result){
 
 AVRX_GCC_TASKDEF(server1, 200, 4)
 {
-	uint8_t authenticated_session = 0;
+	uint8_t admin_num = 0;
+	uint8_t session_perm = 0;
 	printf("Debug\r");
 	
 	while(1){
@@ -33,92 +35,149 @@ AVRX_GCC_TASKDEF(server1, 200, 4)
 		
 		if(size==0) goto handle_error;
 		
-		if(req.type < 0x80){//requests below 0x80 don't need credentials
-			switch(req.type){
-				case REQUEST_GET_DOORSTATE:
-					send_simple_reply(doorstate);
-					break;
-				case REQUEST_LOCK_DOWNSTAIRS:
-				{
-					doorstate &= ~(STATE_DOOR_DOWNSTAIRS);
-					send_simple_reply(RESULT_OK);
-					break;
+		switch(req.type){
+			case REQUEST_GET_DOORSTATE:
+				send_simple_reply(doorstate);
+				break;
+			case REQUEST_LOCK_DOWNSTAIRS:
+			{
+				doorstate &= ~(STATE_DOOR_DOWNSTAIRS);
+				send_simple_reply(RESULT_OK);
+				break;
+			}
+			case REQUEST_UNLOCK_DOWNSTAIRS:
+			{
+				doorstate |= (STATE_DOOR_DOWNSTAIRS);
+				send_simple_reply(RESULT_OK);
+				break;
+			}
+			case REQUEST_AUTH:{
+				reply_auth_t reply;
+				request_cred_t * req_cred = (request_cred_t*) &req;
+				uint8_t result;
+				uint16_t id = req_cred->card_id;
+				
+				reply.permissions = 0;
+				
+				result = database_check(id, req_cred->token);
+				reply.result = result;
+				
+				if(result == RESULT_OK){
+					generate_token(reply.token);
+					database_store_token(id, reply.token);
+					
+					session_perm = database_get_permissions(id);
+					reply.permissions = session_perm;
+					
+					if(session_perm & PERM_ADMIN){
+						admin_num++;	
+					}
+					
+					database_get_nickname(id, reply.nickname);
+				}else if(result == RESULT_DEACTIVATED){
+					uint16_t did;
+					generate_token(reply.token);
+					database_store_token(id, reply.token);
+					
+					//if the card was deactivated, return the nickname of the
+					//one deactivating it.
+					did = database_get_deactivation_id(id);
+					database_get_nickname(did, reply.nickname);
+				}else{
+					//result == RESULT_DENIED
 				}
-				case REQUEST_UNLOCK_DOWNSTAIRS:
-				{
-					doorstate |= (STATE_DOOR_DOWNSTAIRS);
+				
+				//return the reply to the client.
+				channel_write(CHANNEL_SERVER1, (uint8_t *) &reply, sizeof(reply_auth_t));
+				printf("reply\r");
+				break;
+			}	
+			case REQUEST_RESET:
+				admin_num = 0
+				session_perm = 0;
+				send_simple_reply(RESULT_OK);
+				break;
+			case REQUEST_UNLOCK_UPSTAIRS:
+				if(session_perm & PERM_DOOR_UPSTAIRS){
+					doorstate |= STATE_DOOR_UPSTAIRS;
 					send_simple_reply(RESULT_OK);
-					break;
+				}else{
+					send_simple_reply(RESULT_DENIED);
 				}
-				case REQUEST_AUTH:{
-					reply_auth_t reply;
-					request_cred_t * req_cred = (request_cred_t*) &req;
+				break;
+			case REQUEST_LOCK_UPSTAIRS:
+				if(session_perm & PERM_DOOR_UPSTAIRS){
+					doorstate &= ~STATE_DOOR_UPSTAIRS;
+					send_simple_reply(RESULT_OK);
+				}else{
+					send_simple_reply(RESULT_DENIED);
+				}	
+				break;
+			case REQUEST_NEW_CARD:
+				if(session_perm & PERM_ADMIN){
+					uint16_t id;
+					reply_new_card_t reply;
+					request_new_card_t * req_nc = (request_new_card_t*) &req;
+					id = database_new_entry(req_nc->permissions, req_nc->nickname, req_nc->realname);
+					if(id == 0xffff){
+						send_simple_reply(RESULT_DENIED);
+					}else{
+						reply.result = RESULT_OK;
+						reply.id = id;
+						generate_token(reply.token);
+						database_store_token(id, reply.token);
+						channel_write(CHANNEL_SERVER1, (uint8_t *) &reply, sizeof(reply));
+					}
+				}else{
+					send_simple_reply(RESULT_DENIED);
+				}
+				break;
+			case REQUEST_MAKE_ADMIN:
+				if(admin_num >= 2){
+					reply_make_admin_t reply;
+					request_make_admin_t * req_ma = (request_make_admin_t*) &req;
 					uint8_t result;
-					uint16_t id = req_cred->card_id;
+					uint16_t id = req_ma->card_id;
 					
-					authenticated_session = 0;
-					reply.flags = 0;
-					
-					result = database_check(id, req_cred->token);
+					result = database_check(id, req_ma->token);
 					reply.result = result;
 					
 					if(result == RESULT_OK){
+						uint8_t permissions;
 						generate_token(reply.token);
 						database_store_token(id, reply.token);
 						
-						reply.flags = database_get_flags(id);
-						database_get_nickname(id, reply.nickname);
+						permissions = database_get_permissions(id);
+						permissions |= PERM_ADMIN;
+						database_set_permissions(id, permissions);
 						
-						authenticated_session = 1;
 					}else if(result == RESULT_DEACTIVATED){
-						uint16_t did;
 						generate_token(reply.token);
 						database_store_token(id, reply.token);
 						
-						//if the card was deactivated, return the nickname of the
-						//one deactivating it.
-						did = database_get_deactivation_id(id);
-						database_get_nickname(did, reply.nickname);
 					}else{
 						//result == RESULT_DENIED
 					}
 					
 					//return the reply to the client.
-					channel_write(CHANNEL_SERVER1, (uint8_t *) &reply, sizeof(reply_auth_t));
-					printf("reply\r");
-					break;
-				}	
-				case REQUEST_RESET:
-					authenticated_session = 0;
-					send_simple_reply(RESULT_OK);
-					break;
-				default:
+					channel_write(CHANNEL_SERVER1, (uint8_t *) &reply, sizeof(reply));
+				}else{
 					send_simple_reply(RESULT_DENIED);
-					break;
-			}
-			
-		}else{
-			if(authenticated_session != 1){
-				send_simple_reply(RESULT_DENIED);
-			}else{
-				switch (req.type){
-					case REQUEST_UNLOCK_UPSTAIRS:
-						doorstate |= STATE_DOOR_UPSTAIRS;
-						send_simple_reply(RESULT_OK);
-						break;
-					case REQUEST_LOCK_UPSTAIRS:
-						doorstate &= ~STATE_DOOR_UPSTAIRS;
-						send_simple_reply(RESULT_OK);
-						break;
-					
-					default:
-						send_simple_reply(RESULT_DENIED);
-						break;
-					
 				}
-			}
+				break;
+			case REQUEST_DELETE_ADMIN:
+				if(session_perm & PERM_ADMIN){
+					
+					
+				}else{
+					send_simple_reply(RESULT_DENIED);
+				}
+				break;
+			default:
+				send_simple_reply(RESULT_DENIED);
+				break;
+					
 		}
-		
 		continue;
 		
 		
