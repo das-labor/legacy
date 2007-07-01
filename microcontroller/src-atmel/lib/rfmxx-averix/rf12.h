60f5a31d8170d06cd00e99562ce16628d8f3e0a4
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-07-01 13:13:28 +0000

    rfmlib + averix support

---
+++ microcontroller/src-atmel/lib/rfmxx-averix/rf12.h
@@ -0,0 +1,21 @@
+extern unsigned short rf12_trans(unsigned short wert);					// transfer 1 word to/from module
+extern void rf12_init(void);											// initialize module
+extern void rf12_setfreq(unsigned short freq);							// set center frequency
+extern void rf12_setbaud(unsigned short baud);							// set baudrate
+extern void rf12_setpower(unsigned char power, unsigned char mod);		// set transmission settings
+extern void rf12_setbandwidth(unsigned char bandwidth, unsigned char gain, unsigned char drssi);	// set receiver settings
+extern void rf12_txdata(uint8_t data);
+
+
+extern uint8_t rf12_rxbyte();
+uint8_t rf12_ready_timeout (uint8_t in_waitms);
+extern uint8_t rf12_rxarray ( uint8_t *target, uint8_t maxlen );
+
+#define RF12FREQ(freq)	((freq-430.0)/0.0025)							// macro for calculating frequency value out of frequency in MHz
+
+#ifndef cbi
+#define cbi(sfr, bit)     (_SFR_BYTE(sfr) &= ~_BV(bit)) 
+#endif
+#ifndef sbi
+#define sbi(sfr, bit)     (_SFR_BYTE(sfr) |= _BV(bit))  
+#endif
