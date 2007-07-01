52cda0ab0e102fedbf72ae96ee52bdcc09295cb8
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-07-01 13:14:35 +0000

    renamed

--- microcontroller/src-atmel/lib/rfmxx/main.c
+++ microcontroller/src-atmel/lib/rfmxx/main.c
@@ -3,7 +3,6 @@
 #include <avr/pgmspace.h>
 #include <avr/eeprom.h>
 #include <stdlib.h>
-#include "global.h"
 #include "rf12.h"     
 #include "config.h"
 #include "uart.h"
@@ -25,7 +24,7 @@ void dump_registers (void)
 		rxbuf = rf12_trans((i));
 		if (rxbuf != 0x809c)
 		{
-			printf("%2x = %x\t", i, rxbuf);
+			printf("%2x: %4x     ", i, rxbuf);
 			if (k == 7)
 			{
 				printf("\r\n");
@@ -42,14 +41,18 @@ int main(void)
 {
 	unsigned char i, tx=0x01, k=0;
 	uint16_t rxbuf = 0x0000;
+	char inputchar;
+
+	inputchar = malloc(sizeof(char));
+
 	DDRC = 0xff;
 	DDRD &= ~(_BV(PD2));
 	DDRB &= ~(_BV(PB0));
 	PORTB |= _BV(PB0);
 
 	uart_init();
-	uart_putstr("Hallo\r\n");
 	fdevopen(uart_putc, 0);
+	uart_putstr("Hallo\r\n");
 
 	rf12_init();					// ein paar Register setzen (z.B. CLK auf 10MHz)
 	rf12_setfreq(RF12FREQ(434.0));	// Sende/Empfangsfrequenz auf 433,92MHz einstellen
@@ -60,35 +63,53 @@ int main(void)
 
 	for (;;)
 	{
-		if (k<7)
-			tx <<= 1;
-		else
-			tx >>= 1;
-		k++;
-		if (k==14) k=0;
-		
-		receive();
-		if (!(PINB & _BV(PB0)))
+		//receive();
+		if (uart_getc_nb(&inputchar))
 		{
-			send(tx);
-			_delay_ms(100);
+			switch (inputchar)
+			{
+				case 's':
+//					rxbuf = rf12_trans(0x0000);
+//					printf("status register: %4x\r\n", rxbuf);
+					dump_registers();
+				break;
+				case 't':
+					uart_putc(inputchar);
+					send(inputchar);
+				break;
+				case 'r':
+					printf("receive()\r\n");
+				break;
+			}
 		}
-		rxbuf = rf12_trans(0xCA83);
+					receive();
+//		rxbuf = rf12_trans(0xCA83);
+	
 	}
 }
 
 void receive(void)
 {
 	uint16_t moo = 0x00;
+	char tmp[16];
 //	moo = rf12_rxdata();
-	moo = rf12_rxdata_nb(0x5440);
-	if (moo > 0x00ff)
-		PORTC = (uint8_t) (moo & 0x00ff);
+	moo = rf12_rxdata_nb(0x4000);
+//	rf12_rxarray(&tmp);
+	if (moo > 0xff)
+	{
+		printf ("char: %2x\r\n", (moo & 0x00ff));
+//		printf ("array: %s\r\n", tmp);
+//		printf("status register: %x\r\n", rf12_trans(0x0000));
+//		PORTC = (uint8_t) (moo & 0x00ff);
+	}
 }
 
 void send(uint8_t i)
 {
+	char test[] = "hallo nakkatest\r\n";
 //	PORTC = i;
-	rf12_txdata(i,1);
+//	rf12_txdata(i,1);
+	rf12_txarray(test, strlen(test));
+	rf12_txdata (i);
 }
 
--- microcontroller/src-atmel/lib/rfmxx/rf12.h
+++ microcontroller/src-atmel/lib/rfmxx/rf12.h
@@ -1,26 +1,19 @@
-#define RF_PORT	PORTD
-#define RF_DDR	DDRD
-#define RF_PIN	PIND
-#define F_CPU 16000000L
-
-#define SDI		7
-#define SCK		6
-#define CS		5
-#define SDO		3
-
-#define RFM_PIN_NIRQ	(_BV(PD3))
-#define RFM_PORT_NIRQ	PIND
-#define RFM_DDR_NIRQ	DDRD
-
 extern unsigned short rf12_trans(unsigned short wert);					// transfer 1 word to/from module
 extern void rf12_init(void);											// initialize module
 extern void rf12_setfreq(unsigned short freq);							// set center frequency
 extern void rf12_setbaud(unsigned short baud);							// set baudrate
 extern void rf12_setpower(unsigned char power, unsigned char mod);		// set transmission settings
 extern void rf12_setbandwidth(unsigned char bandwidth, unsigned char gain, unsigned char drssi);	// set receiver settings
-extern void rf12_txdata(uint8_t data, unsigned char number);		// transmit number of bytes from array
-extern uint8_t rf12_rxdata(void);		// receive number of bytes into array
+extern void rf12_txdata(uint8_t data);
+extern uint8_t rf12_rxdata(void);
+extern uint16_t rf12_rxdata_nb(uint16_t in_waitcycles);
 extern void rf12_ready(void);											// wait until FIFO ready (to transmit/read data)
 
 #define RF12FREQ(freq)	((freq-430.0)/0.0025)							// macro for calculating frequency value out of frequency in MHz
 
+#ifndef cbi
+#define cbi(sfr, bit)     (_SFR_BYTE(sfr) &= ~_BV(bit)) 
+#endif
+#ifndef sbi
+#define sbi(sfr, bit)     (_SFR_BYTE(sfr) |= _BV(bit))  
+#endif
