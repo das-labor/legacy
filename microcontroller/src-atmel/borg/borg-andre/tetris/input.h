8f7095d523fad1115960608d04a65811a5ebb797
chris <chris@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-07-19 06:20:56 +0000

    added chatter protection

--- microcontroller/src-atmel/borg/borg-andre/tetris/input.h
+++ microcontroller/src-atmel/borg/borg-andre/tetris/input.h
@@ -18,32 +18,51 @@
 
 typedef enum tetris_input_command_t
 {
-	TETRIS_INCMD_NONE,    // idle
 	TETRIS_INCMD_ROT_CW,  // rotate clockwise
 	TETRIS_INCMD_ROT_CCW, // rotate counter clockwise
 	TETRIS_INCMD_LEFT,    // move piece left
 	TETRIS_INCMD_RIGHT,   // move piece right
 	TETRIS_INCMD_DOWN,    // lower piece by one row
 	TETRIS_INCMD_DROP,    // move piece to the ground immediately  
-	TETRIS_INCMD_GRAVITY  // piece gets pulled by gravity 
+	TETRIS_INCMD_GRAVITY, // piece gets pulled by gravity
+	TETRIS_INCMD_IGNORE,  // padding byte, because avr-gcc can't handle uneven
+	                      // array sizes in PROGMEM
+	TETRIS_INCMD_NONE     // idle (must alway be the last one)
 }
 tetris_input_command_t;
 
 
 typedef struct tetris_input_t
 {
-	uint8_t nLevel;                 // current level
+	// current level (determines falling speed)
+	uint8_t nLevel;
 
-	uint8_t nLoopCycles;            // tracks how many loop cycles have been
-	                                // done since last forced piece movement
-
-	int8_t nRepeatCount;            // tracks how many loop cycles have been
-	                                // done since last reset of repeat counter
-
-	tetris_input_command_t cmdLast; // last command (important for key repeat)
+	// This counter tracks how many loop cycles have been done since the last
+	// forced piece movement. It gets reset if it either reaches a well
+	// defined value (causing a gravity command to be issued) or the player has
+	// moved down the piece herself/himself.
+	uint8_t nLoopCycles;
+	
+	// Amount of loop cycles in which the same command has been issued
+	// consecutively. It gets reset if either the current command differs from
+	// the last one or a well-defined value has been reached (thereby
+	// regulating the pace of the key repeat as commands are only processed
+	// if that value is reached).
+	int8_t nRepeatCount;
+	
+	// last command (important for key repeat)
+	tetris_input_command_t cmdLast;
+	
+	// Every command has its own counter. A command is ignored as long as its
+	// counter is unequal to 0. A counter gets set to a command specific
+	// value (or 0) if its corresponding command was issued directly before the
+	// player has released the button. All counters get decremented every loop
+	// cycle until they are zero. This is used to work against the chattering
+	// of some joysticks. Look at the function tetris_input_chatterCheck() in
+	// input.c for the default initial values of these counters.
+	uint8_t nIgnoreCmdCounter[TETRIS_INCMD_NONE];
 }
 tetris_input_t;
-
 
 /****************************
  * construction/destruction *
