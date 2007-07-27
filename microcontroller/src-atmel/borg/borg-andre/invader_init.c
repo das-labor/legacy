9f5ac0de3f94c5ccf8f5c12d984758255e762147
plopp <plopp@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-07-27 19:55:43 +0000

    invader level changed

--- microcontroller/src-atmel/borg/borg-andre/invader_init.c
+++ microcontroller/src-atmel/borg/borg-andre/invader_init.c
@@ -12,14 +12,14 @@ unsigned char peter[8][11] = {
 };
 
 unsigned char hans[8][11] = {
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-  {1, 2, 1, 1, 2, 2, 2, 1, 2, 2, 1},
-  {1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 2},
-  {1, 2, 1, 1, 2, 2, 2, 1, 2, 2, 1},
-  {1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 2},
-  {1, 2, 2, 1, 2, 1, 2, 1, 2, 2, 2},
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+  {0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0},
+  {0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0},
+  {0, 0, 3, 3, 0, 3, 0, 3, 3, 0, 0},
+  {0, 0, 3, 3, 3, 3, 3, 3, 3, 0, 0},
+  {0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0},
+  {0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0},
+  {0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0},
 };
 
 
@@ -83,7 +83,7 @@ initInvaders (Invaders * iv, unsigned char lv)
 	  iv->map[x][1] = 3;
 	  iv->map[x][2] = 2;
 	  iv->map[x][3] = 2;
-	  iv->map[x][4] = 1;
+	  //  iv->map[x][4] = 1;
 	  //}     
 	}
 
@@ -95,22 +95,23 @@ initInvaders (Invaders * iv, unsigned char lv)
       break;
 
     case 2:
-      for (x = 0; x < 8; ++x)
+      for (x = 0; x < 11; ++x)
 	{
-	  //for(y = 0; y < MAX_INVADER_HEIGHT; ++y) {
-	  iv->map[x][0] = 3;
-	  iv->map[x][1] = 3;
-	  iv->map[x][2] = 3;
-	  iv->map[x][3] = 2;
-	  iv->map[x][4] = 2;
-	  //}     
+	  for (y = 0; y < 8; ++y)
+	    {
+	      if (peter[y][x] != 0)
+		{
+		  iv->map[x][y] = 3;
+		}
+	    }
 	}
 
-      iv->pos.x = 4;
+      iv->pos.x = 3;
       iv->pos.y = SPACESHIP_LINE + 1;
-      iv->speed = MIN_SPEED - 1;
 
+      iv->speed = MIN_SPEED - 4;
       iv->direction = 1;
+
       break;
       
 
@@ -119,10 +120,7 @@ initInvaders (Invaders * iv, unsigned char lv)
 	{
 	  for (y = 0; y < 8; ++y)
 	    {
-	      if (hans[y][x] != 0)
-		{
-		  iv->map[x][y] = 2;
-		}
+		  iv->map[x][y] = hans[x][y];
 	    }
 	}
 
@@ -132,8 +130,6 @@ initInvaders (Invaders * iv, unsigned char lv)
       iv->speed = MIN_SPEED - 1;
       iv->direction = 1;
 
-      break;
-     
       break;
 
     case 4:
@@ -143,7 +139,7 @@ initInvaders (Invaders * iv, unsigned char lv)
 	    {
 	      if (peter[y][x] != 0)
 		{
-		  iv->map[x][y] = 2;
+		  iv->map[x][y] = 3;
 		}
 	    }
 	}
@@ -151,7 +147,7 @@ initInvaders (Invaders * iv, unsigned char lv)
       iv->pos.x = 3;
       iv->pos.y = SPACESHIP_LINE + 1;
 
-      iv->speed = MIN_SPEED - 1;
+      iv->speed = MIN_SPEED - 4;
       iv->direction = 1;
 
       break;
