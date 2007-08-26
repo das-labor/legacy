2c0cc44b95d01fdf489b5e4b6748d9bfa10d189a
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-08-26 11:39:42 +0000

    fooblah

--- other-hardware/src-qt/vclick/MainWindow.cpp
+++ other-hardware/src-qt/vclick/MainWindow.cpp
@@ -1,11 +1,26 @@
 
 #include "MainWindow.h"
 
+#include <QGraphicsRectItem>
+#include <QGraphicsTextItem>
+
 MainWindow::MainWindow(){
 	ui.setupUi(this);
 
-
-
-
+	scene = new QGraphicsScene;
+	
+	QGraphicsRectItem *r = new QGraphicsRectItem(10, 10, 30, 40);
+	
+	r->setFlag(QGraphicsItem::ItemIsSelectable);
+	
+	ui.graphicsView->setScene(scene);
+	
+	QGraphicsTextItem *t = scene->addText("Hello, world!");
+	
+	t->setFlag(QGraphicsItem::ItemIsSelectable);
+	
+	
+	scene->addItem(r);
+	
 
 }
