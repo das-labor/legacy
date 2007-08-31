274540f72b9b94fe830e80a34f0622ca2d21a6dc
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-08-31 11:24:42 +0000

    now can move rect around

--- other-hardware/src-qt/vclick/MainWindow.cpp
+++ other-hardware/src-qt/vclick/MainWindow.cpp
@@ -3,13 +3,31 @@
 
 #include <QGraphicsRectItem>
 #include <QGraphicsTextItem>
+#include <QGraphicsSceneMouseEvent>
+
+
+
+
+class block : public QGraphicsRectItem{
+	public:
+	block(qreal x, qreal y, qreal width, qreal height) : QGraphicsRectItem(x, y, width, height,0,0){
+		
+	}
+	void mouseMoveEvent ( QGraphicsSceneMouseEvent * event ){
+		setPos(event->scenePos());
+		
+	}
+	
+	
+};
+
 
 MainWindow::MainWindow(){
 	ui.setupUi(this);
 
 	scene = new QGraphicsScene;
 	
-	QGraphicsRectItem *r = new QGraphicsRectItem(10, 10, 30, 40);
+	QGraphicsRectItem *r = new block(10, 10, 30, 40);
 	
 	r->setFlag(QGraphicsItem::ItemIsSelectable);
 	
