f77196426b6d3cbdff4dfd2c603652f9db2b22d3
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-09-12 22:12:31 +0000

    tmp commit - grade kein Bock mehr.

--- microcontroller/src-atmel/lib/rfm12/rfm12.h
+++ microcontroller/src-atmel/lib/rfm12/rfm12.h
@@ -6,18 +6,18 @@ void rfm12_data(uint16_t d);
 void rfm12_tx(uint8_t tape, uint8_t length, uint8_t *data);
 uint16_t rfm12_read(uint16_t d);
 
-static inline uint8_t rfm12_rx_status();
 static inline uint8_t rfm12_tx_status();
+
 static inline uint8_t *rfm12_rx_buffer();
-static inline uint8_t rfm12_rx_protocol();
 static inline void rfm12_rx_clear();
-static inline uint8_t rfm12_rx_buffer_len();
 
 
 //size of transmit buffer
 #define RFM12_TX_BUFFER_SIZE 30
 
 //rx buffer size
+//there are going to be 2 Buffers of this size
+//(double_buffering)
 #define RFM12_RX_BUFFER_SIZE 30
 
 #define STATUS_FREE 0
@@ -27,6 +27,7 @@ static inline uint8_t rfm12_rx_buffer_len();
 
 
 /* Private structs needed for inline functions */
+
 
 
 typedef struct{
@@ -42,17 +43,30 @@ typedef struct{
 	uint8_t buffer[RFM12_TX_BUFFER_SIZE];//buffer for the raw bytes to be transmitted
 }rf_tx_buffer_t;
 
-typedef struct{
+
+//for storing the received bytes.
+typedef_struct{
 	uint8_t status; 		//is the buffer ready or occupied?
-	uint8_t num_bytes; 		//number of bytes to be received
-	uint8_t bytecount;   //received bytes counter
-	
-	//rx protocol fields
 	uint8_t len;			//length byte - number of bytes in buffer
 	uint8_t type;			//type field for airlab
 	uint8_t checksum;		//rx checksum
 	uint8_t buffer[RFM12_RX_BUFFER_SIZE]; //receive buffer
+}rf_buffer_t;
+
+
+typedef struct{
+	uint8_t num_bytes;		//number of bytes to be received
+	uint8_t bytecount;		//received bytes counter
+	
+	//points to the rf_buffer in rf_buffers that will be filled next
+	rf_buffer_t * rf_buffer_in;
+	//points to the rf_buffer in rf_buffers that will be read next
+	rf_buffer_t * rf_buffer_out;
+	
 }rf_rx_buffer_t;
+
+
+extern volatile rf_buffer_t rf_buffers[2];
 
 //Buffer and status for the message to be transmitted
 extern volatile rf_tx_buffer_t rf_tx_buffer;
@@ -66,32 +80,15 @@ static inline uint8_t rfm12_rx_status()
 	return rf_rx_buffer.status;
 }
 
-//inline function to return tx buffer status byte
-static inline uint8_t rfm12_tx_status()
-{
-	return rf_tx_buffer.status;
-}
-
-//inline function to retrieve current buffer pointer
+//inline function to retrieve current rf_buffer struct pointer
 static inline uint8_t *rfm12_rx_buffer()
 {
-	return rf_rx_buffer.buffer;
-}
-
-//inline function to get protocol type byte
-static inline uint8_t rfm12_rx_protocol()
-{
-	return rf_rx_buffer.type;
-}
-
-//inline function to get buffer byte count
-static inline uint8_t rfm12_rx_buffer_len()
-{
-	return rf_rx_buffer.len;
+	return rf_rx_buffer.rf_buffer_out;
 }
 
 //inline function to clear buffer complete/occupied status
 static inline void rfm12_rx_clear()
 {
-	rf_rx_buffer.status = STATUS_FREE;
+	rf_rx_buffer.rf_buffer_out->status = STATUS_FREE;
+	rf_rx_buffer.rf_buffer_out =
 }
