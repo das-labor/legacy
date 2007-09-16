e6eb07a123ee045423e91fee7d65b7081cf3780b
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-09-16 02:48:59 +0000

    +testing enviroment

---
+++ microcontroller-2/lib/PersistentCounter/test/config.h
@@ -0,0 +1,24 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+#include <avr/io.h>
+
+#define F_CPU 16000000         /* Oszillator-Frequenz in Hz */
+
+#define DEBUG uart
+
+#define INIT_EEPROM
+/*c uart.[ch] defines */
+#define UART_HOOK
+#define UART_INTERRUPT 1
+#define UART_BAUD_RATE 9600
+#define UART_RXBUFSIZE 16
+#define UART_TXBUFSIZE 16
+#define UART_LINE_BUFFER_SIZE 40
+#define UART_XON_XOFF
+#define UART_XON_XOFF_THRESHOLD_1 (UART_RXBUFSIZE - 4)
+#define UART_XON_XOFF_THRESHOLD_2 (UART_RXBUFSIZE - 12)
+
+#undef UART_LEDS
+
+#endif
+
