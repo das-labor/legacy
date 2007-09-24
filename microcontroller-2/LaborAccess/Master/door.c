d3e9a3feb19850154a529c8be1f0af0079d9ac72
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2007-09-24 07:23:45 +0000

    improved door opener, timeouts implemented & adjusted

--- microcontroller-2/LaborAccess/Master/door.c
+++ microcontroller-2/LaborAccess/Master/door.c
@@ -3,16 +3,81 @@
 
 #include "door.h"
 
-/* @brief actually turns step motor for door */
 
-void door_init ()
+/* @brief shut down motor the hard way.
+ */
+inline void door_motor_off(void)
 {
-	DOOR_DDR |= (DOOR_PIN0 | DOOR_PIN1 | DOOR_PIN2 | DOOR_PIN3);
+	DOOR_PORT &= ~(DOOR_PIN0 | DOOR_PIN1 | DOOR_PIN2 | DOOR_PIN3);
 }
 
+/* @brief motor watchdog function to turn motor automatically off.
+ */
+void door_motor_wd (uint8_t in_kick)
+{
+	static uint8_t mykicks = 0;
+	if (in_kick > 0)
+	{
+		mykicks = in_kick;
+	} else
+	{
+		if (mykicks > 0)
+			mykicks--;
+		else
+			door_motor_off();
+	}
+}
+
+
+
+/* @brief get adc values from infrared sensor on door
+ */
+uint16_t door_get_adcval ( void )
+{
+	uint16_t retval;
+	uint8_t i;
+
+	for(i=0; i<10; i++)
+	{
+		ADCSRA |= (1<<ADIF); // Reset interrupt flag
+		ADCSRA |= (1<<ADSC); // start conversion
+		while( (ADCSRA & (1<<ADIF)) == 0  ) asm volatile("nop");
+		retval = ADC;
+	}
+	door_motor_wd (10);
+	return retval;
+}
+
+/* @brief 0 = sensor is at mark point, 1 = sensor receives reflection
+ */
+uint8_t door_get_sensorstate ( void )
+{
+	if (door_get_adcval() < DOOR_SENSORTRESHOLD)
+		return 1;
+	return 0;
+}
+
+/* @brief set up ddrs for adc and motor port
+ */
+void door_init ()
+{
+	ADMUX = 0xc1; // see datasheet page 215.
+	ACSR = 0x00;
+	DOOR_ADCDDR &= ~(DOOR_ADCPIN);
+	DOOR_ADCPORT &= ~(DOOR_ADCPIN);
+	ADCSRA = (1<<ADEN) | 7; //ADC aktivieren, Takt/128
+	DOOR_DDR |= (DOOR_PIN0 | DOOR_PIN1 | DOOR_PIN2 | DOOR_PIN3);
+	DDRA |= (_BV(PA7));
+	PORTA |= (_BV(PA7));
+	door_motor_off();
+}
+
+/* @brief actually turns step motor for door
+ */
 void door_turn (uint8_t in_direction)
 {
-	static uint8_t mystate = 0x01, oldstate;
+	static uint8_t mystate = 0x01;
+	uint8_t oldstate;
 	
 	oldstate = mystate;
 
@@ -47,14 +112,55 @@ void door_turn (uint8_t in_direction)
 
 }
 
-
 AVRX_GCC_TASKDEF(door, 200, 5)
 {
+	uint8_t mystate = DOOR_STATE_INIT;
+	uint16_t optotimeout = DOOR_OPTOTIMEOUT;
+	uint8_t spincycles = 0;
+#if (DOOR_SPINTICKS>0xff)
+	uint16_t spinticks = 0;
+#else
+	uint8_t  spinticks = 0;
+#endif
+
+#if (DOOR_MAXTICKS > 0xff)
+	uint16_t totalticks = 0;
+#else
+	uint8_t  totalticks = 0;
+#endif
 	TimerControlBlock doortimer;
+
 	door_init();
+
+	// wait 500ms for adc & stuff to calm
+	AvrXDelay (&doortimer, 500);
+	mystate = DOOR_DIR_LEFT;
 	while (1)
 	{
-		door_turn (DOOR_DIR_RIGHT);
 		AvrXDelay (&doortimer, DOOR_WAITCYCLES);
+//		door_motor_wd(0);
+		if (!door_get_sensorstate()) // mark hit
+		{
+			if (spinticks > 0)
+			{
+				spinticks--;
+			} else
+			{
+				spinticks = DOOR_SPINTICKS;
+				spincycles++;
+			}
+			/* reset optotimeout */
+			optotimeout = DOOR_OPTOTIMEOUT;
+			door_turn (mystate & 0xF0);
+		} else if (optotimeout > DOOR_WAITCYCLES) // spin around and count down
+		{
+			optotimeout -= DOOR_WAITCYCLES;
+			door_turn (mystate & 0xF0);
+		} else // timeout hit!
+		{
+			door_motor_off();
+		}
+		totalticks++;
+		if (totalticks >= DOOR_MAXTICKS) door_motor_off();
 	}
 }
--- microcontroller-2/LaborAccess/Master/door.h
+++ microcontroller-2/LaborAccess/Master/door.h
@@ -12,14 +12,40 @@ AVRX_EXTERNTASK(door);
 #define DOOR_PIN2 (_BV(PC4))
 #define DOOR_PIN3 (_BV(PC5))
 
+#define DOOR_ADCPORT PORTA
+#define DOOR_ADCDDR DDRA
+#define DOOR_ADCPIN (_BV(PA1))
+#define DOOR_SENSORTRESHOLD 850
+
 // counterclockwise
-#define DOOR_DIR_LEFT  0x00
+#define DOOR_DIR_LEFT  0x10
 
 // clockwise
-#define DOOR_DIR_RIGHT 0x01
+#define DOOR_DIR_RIGHT 0x20
 
 /* time in ms to wait between steps */
-#define DOOR_WAITCYCLES 10
+#define DOOR_WAITCYCLES 2
+
+#define DOOR_STATE_MESSED     0x00
+#define DOOR_STATE_CLOSED     0x01
+#define DOOR_STATE_OPEN       0x02
+#define DOOR_STATE_TURN_RIGHT 0x04
+#define DOOR_STATE_TURN_LEFT  0x05
+#define DOOR_STATE_INIT DOOR_STATE_MESSED
+
+/* time in ms to wait before timing out the optical sensor */
+#define DOOR_OPTOTIMEOUT 2200
+
+/* ticks for mark-landing (should be greater than actual amount of ticks)  */
+#define DOOR_SPINTICKS 150
+
+/* total number of ticks before door routine stops
+ * regardless of state and current position.
+ * needed in cases where the mark is over the sensor and blocking or in other
+ * (yet unknown) cases
+ * */
+#define DOOR_MAXTICKS 2600
+
 
 void door_turn (uint8_t in_direction);
 #endif
