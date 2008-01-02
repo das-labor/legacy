3749e56ac190c9f43118cfa23fe3abf2432eafd3
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-01-02 18:37:26 +0000

    added files

---
+++ microcontroller-2/LabCopter/flightctrl/init.c
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *            init.c
+ *
+ *  Wed Jan  2 19:33:52 2008
+ *  Copyright  2008  Hansi
+ *  <gast@<host>>
+ ****************************************************************************/
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+//Initialize ADC
+void ADC_Init(void)
+{ 
+    ADMUX = 0;//Referenz ist extern
+    ADCSRA=(1<<ADEN)|(1<<ADSC)|(1<<ADATE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADIE);
+    //Free Running Mode, Division Factor 128, Interrupt on
+}
