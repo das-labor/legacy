f97888eac29f268604ac497ca2fbcc79c0f8ad16
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-03-07 02:45:54 +0000

    sync muting added, key combos added

--- microcontroller/src-atmel/playground/gauselmann/main.c
+++ microcontroller/src-atmel/playground/gauselmann/main.c
@@ -1,8 +1,11 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdlib.h>
+#include <util/delay.h>
 #include "ps2.h"
 #include "scancodes.inc"
+#include "uart.h"
 
 /*
 	PB0 SYNC_SENSE
@@ -35,6 +38,10 @@
 #define DDR_SYNC_ENABLE DDRD
 #define BIT_SYNC_ENABLE PD7
 
+#define SYNC_OFF() PORT_SYNC_ENABLE &= ~(1<<BIT_SYNC_ENABLE)
+#define SYNC_ON() PORT_SYNC_ENABLE |= (1<<BIT_SYNC_ENABLE)
+
+
 #define PORT_LED PORTD
 #define DDR_LED DDRD
 #define BIT_LED PD1
@@ -50,43 +57,93 @@
 #define KEY_1P_RIGHT	PS2_KEY_CRSR_RIGHT
 #define KEY_1P_B1		PS2_KEY_LCTRL
 #define KEY_1P_B2		PS2_KEY_LALT
-#define KEY_1P_B3		PS2_KEY_ENTER
-#define KEY_2P_START	PS2_KEY_2
-#define KEY_2P_UP		PS2_KEY_W
-#define KEY_2P_DOWN		PS2_KEY_S
-#define KEY_2P_LEFT		PS2_KEY_A
-#define KEY_2P_RIGHT	PS2_KEY_D
-#define KEY_2P_B1		PS2_KEY_Y
-#define KEY_2P_B2		PS2_KEY_X
-#define KEY_2P_B3		PS2_KEY_C
+#define KEY_1P_B3		PS2_KEY_SPACE
 
+#define KEY_2P_START	PS2_KEY_2
+#define KEY_2P_UP		PS2_KEY_R
+#define KEY_2P_DOWN		PS2_KEY_F
+#define KEY_2P_LEFT		PS2_KEY_D
+#define KEY_2P_RIGHT	PS2_KEY_G
+#define KEY_2P_B1		PS2_KEY_A
+#define KEY_2P_B2		PS2_KEY_S
+#define KEY_2P_B3		PS2_KEY_Q
+
+#define KEY_COIN_SLOT1	PS2_KEY_5
+#define KEY_COIN_SLOT2	PS2_KEY_6
+#define KEY_ESCAPE		PS2_KEY_ESC
 
 typedef struct{
 	volatile uint8_t *port;
 	uint8_t mask;
 	uint8_t state;
+	uint16_t release_key;	//key to be released(for key combinations)
 	uint16_t key;
 }keymap_t;
 
+
+//the first two keynums are offsets into keymappings table
+typedef struct{
+	uint8_t modifier_keynum;
+	uint8_t action_keynum;
+	uint16_t result_key;
+}keycomb_t;
+
+
 keymap_t keymappings[] = {
-	{&PINB, (1<<1), 0, KEY_1P_B2},
-	{&PINB, (1<<2), 0, KEY_1P_START},
-	{&PINB, (1<<3), 0, KEY_2P_LEFT},
-	{&PINB, (1<<4), 0, KEY_2P_DOWN},
-	{&PINB, (1<<5), 0, KEY_2P_RIGHT},
-	{&PINB, (1<<6), 0, KEY_2P_UP},
-	{&PINB, (1<<7), 0, KEY_1P_RIGHT},
-	{&PINC, (1<<0), 0, KEY_1P_B1},
-	{&PINC, (1<<1), 0, KEY_2P_START},
-	{&PINC, (1<<2), 0, KEY_2P_B1},
-	{&PINC, (1<<3), 0, KEY_2P_B2},
-	{&PINC, (1<<4), 0, KEY_1P_B3},
-	{&PINC, (1<<5), 0, KEY_2P_B3},
-	{&PIND, (1<<4), 0, KEY_1P_UP},
-	{&PIND, (1<<5), 0, KEY_1P_DOWN},
-	{&PIND, (1<<6), 0, KEY_1P_LEFT},
+	{&PINB, (1<<2), 0, 0, KEY_1P_START},//0
+	{&PIND, (1<<4), 0, 0, KEY_1P_UP},	//1
+	{&PIND, (1<<5), 0, 0, KEY_1P_DOWN},	//2
+	{&PIND, (1<<6), 0, 0, KEY_1P_LEFT},	//3
+	{&PINB, (1<<7), 0, 0, KEY_1P_RIGHT},//4
+	{&PINC, (1<<0), 0, 0, KEY_1P_B1},	//5
+	{&PINB, (1<<1), 0, 0, KEY_1P_B2},	//6
+	{&PINC, (1<<4), 0, 0, KEY_1P_B3},	//7
+	{&PINC, (1<<1), 0, 0, KEY_2P_START},//8
+	{&PINB, (1<<6), 0, 0, KEY_2P_UP},	//9
+	{&PINB, (1<<4), 0, 0, KEY_2P_DOWN},	//10
+	{&PINB, (1<<3), 0, 0, KEY_2P_LEFT},	//11
+	{&PINB, (1<<5), 0, 0, KEY_2P_RIGHT},//12
+	{&PINC, (1<<2), 0, 0, KEY_2P_B1},	//13
+	{&PINC, (1<<3), 0, 0, KEY_2P_B2},	//14
+	{&PINC, (1<<5), 0, 0, KEY_2P_B3},	//15
 };
 
+keycomb_t keycombinations[] = {
+	{1, 0, KEY_COIN_SLOT1},
+	{1, 8, KEY_COIN_SLOT2},
+	{0, 8, KEY_ESCAPE},
+};
+
+
+void wait(uint16_t ms){
+	for(;ms>0;ms--){
+		_delay_ms(1);
+	}
+}
+
+volatile uint16_t line_time;
+
+ISR(TIMER1_CAPT_vect){
+	static uint16_t last_capture, this_capture;
+	uint16_t line_t;
+	this_capture = ICR1;
+	line_t = this_capture-last_capture;
+	line_time = line_t;
+	last_capture = this_capture;
+	
+	if( (line_time > 520) || (line_time < 504) ){
+		LED_ON();
+		SYNC_OFF();
+	}
+
+}
+
+void init_timer1(){
+	//input capture noise canceler, falling edge
+	TCCR1B = (1<<ICNC1) | (0<<ICES1) | 1;
+	
+	TIMSK |= (1<<TICIE1);
+}
 
 int main(){
 	
@@ -98,48 +155,99 @@ int main(){
 	PORTD = 0x70;
 	
 	DDR_SYNC_ENABLE |= (1<<BIT_SYNC_ENABLE);
-	PORT_SYNC_ENABLE |= (1<<BIT_SYNC_ENABLE);
-	
 	DDR_LED |= (1<<BIT_LED);
 	
+	init_timer1();
 	init_ps2();
+	//uart_init();
+	
 	
 	sei();
-	
-	volatile unsigned int delay;
-	for(delay=0;delay<1000;delay++);
-	
-	ps2_put(1, (uint8_t[]) {0xaa} );
-	
-	
+
+	wait(500);
+
+	key_make(0xaa); //bat passed
 	
 	while(1){
+		
+		cli();
+		if( (line_time <= 520) && (line_time >= 504) ){
+			SYNC_ON();
+			LED_OFF();
+		}
+		sei();
+	
+
+//		char buf[10];
+//		itoa(line_time, buf, 10);
+//		uart_putstr(buf);
+//		uart_putstr("\r\n");
+
 		
 		
 		unsigned char data;
 		if(ps2_get_nb(&data)){
-			ps2_put(1, (uint8_t[]) {0xfa} );
+			//static uint8_t id = 0xab;
+			switch(data){
+				case 0xee: //echo
+					key_make(0xee);
+					break;
+				case 0xff: //reset
+					wait(500);
+					key_make(0xaa);
+					break;
+				case 0xf2:  //id
+					key_make(0xfa);
+					key_make(0xab);
+					key_make(0x83);
+					//id ^= (0xab ^ 0x83);
+					break;
+				default: //just acknowledge anything else the keyboard might be telling us...
+					key_make(0xfa);
+					break;
+			
+			}
+				
 			//char buf[10];
 			//itoa(data, buf, 16);
 			//uart_putstr(buf);
+			//uart_putstr("\r\n");
 		}
 		
-		uint8_t x;
+		uint8_t x, y;
 		for(x=0; x<(sizeof(keymappings)/sizeof(keymap_t)); x++){
 			uint8_t aktstate;
 			keymap_t *km = &keymappings[x];
-			aktstate = (~(*km->port)) & km->mask;
-			if(aktstate != km->state){
-				km->state = aktstate;
+			aktstate = ((*km->port) & km->mask)?0:1;
+			if( (!!aktstate) == (! km->state) ){
 				if(aktstate){
+					uint8_t comb_taken = 0;
+					
 					LED_ON();
-					key_make_b(km->key);
+					for(y=0; y<(sizeof(keycombinations)/sizeof(keycomb_t)); y++){
+						if(	(keycombinations[y].action_keynum == x) && 
+						   (keymappings[keycombinations[y].modifier_keynum].state) ){
+							comb_taken = 1;
+							key_make_b(keycombinations[y].result_key);
+							km->state = 2; //remember keypress triggered combination for release
+							km->release_key = keycombinations[y].result_key;
+						}
+					}
+					if(!comb_taken){
+						key_make_b(km->key);
+						km->state = 1;
+					}
 					LED_OFF();
 				}else{
 					LED_ON();
-					key_break_b(km->key);
+						
+					if(km->state != 2){
+						key_break_b(km->key);
+					}else{
+						key_break_b(km->release_key);
+					}
 					LED_OFF();
-
+					km->state = 0;
 				}
 			}
 		}
