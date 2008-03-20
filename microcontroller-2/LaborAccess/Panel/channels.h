a8709561c8fb8727a24dc9c80a5a64cf79bb1302
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-03-20 00:58:52 +0000

    defining channels externally now

---
+++ microcontroller-2/LaborAccess/Panel/channels.h
@@ -0,0 +1,12 @@
+/****** Main Buffer **************/
+#define BUFFERSIZE 42
+
+/******     Our channels     ***************/
+
+#define NUM_CHANNELS 1
+
+#define CLIENT_CHANNEL 0
+	//channelnumber of our client channel
+
+extern channel_t Channels[NUM_CHANNELS];
+
