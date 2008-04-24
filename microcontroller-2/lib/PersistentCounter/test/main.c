19a1fd7617f5904917a2d1b23c1a562c77ce43f5
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-04-24 15:13:34 +0000

    some small changes to the testprogramm

--- microcontroller-2/lib/PersistentCounter/test/main.c
+++ microcontroller-2/lib/PersistentCounter/test/main.c
@@ -14,7 +14,7 @@
 #include <string.h> /* memset */
  #include <avr/eeprom.h> 
 #include "uart.h"
-#include "persistentCounter.h"
+#include "percnt2.h"
 
 
 uint8_t EEMEM do_not_use=0x24;
@@ -24,24 +24,20 @@ uint8_t EEMEM do_not_use=0x24;
 /******************************************************************************/
 
 void wait(int ms){
-	TCCR2 = 0x0C;				/* CTC Mode, clk/64 */
-	OCR2 = 125;					/* 1000Hz */
-	for(;ms>0;ms--){
-		while(!(TIFR&0x80));	/* wait for compare matzch flag */
-		TIFR=0x80;				/* reset flag */
-	}
+	while(ms--)
+		_delay_ms(1);
 }
 
 /******************************************************************************/
 
 void dump_counter(){
-	uint8_t buffer[170];
+	uint8_t buffer[176];
 	uint8_t i;
-	eeprom_read_block(buffer, (void*)OFFSET, 170);
+	eeprom_read_block(buffer, (void*)OFFSET, 176);
 	uart_putstr("\r\nBuffer: ");
-	for (i=0; i<170; i+=16){
+	for (i=0; i<176; i+=16){
 		uart_putstr("\r\n\t");
-		uart_hexdump(buffer+i, (i+16<=170)?16:170-i);
+		uart_hexdump(buffer+i, (i+16<=176)?16:176-i);
 	}
 	
 }
@@ -53,25 +49,23 @@ int main(){
 	DDRB = 0x00;
 	PORTB = 0XFF;
 	
-	eeprom_write_word((void*)(0+OFFSET), 0xFFFF);
-	eeprom_write_word((void*)(2+OFFSET), 0xFFFF);
-	
 	uart_init();
-	percnt_init();
+	percnt_init(0);
 	
-	uint32_t i;
+	uint32_t i,c;
+	
+	c = percnt_get(0);
 	
 	for(i=0; i<1024; ++i){
-			uart_putstr("\r\ni = ");
-			uart_hexdump(&i, 4);
-		if(i!=percnt_get()){
-		//	uart_putstr("\r\ni = ");
-		//	uart_hexdump(&i, 4);
+			uart_putstr("\r\nc = ");
+			uart_hexdump(&c, 4);
+		if(c!=percnt_get(0)){
 			dump_counter();
 			while(1)
 				;
 		}
-		percnt_inc();
+		++c;
+		percnt_inc(0);
 	}
 
 	while(1)
