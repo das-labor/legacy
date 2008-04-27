526694a8e0d0de0638117df0c59575cea087bde2
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-04-27 12:07:30 +0000

    Basic structure for saftschubser

--- tools/saftschubser/saftschubser/MainWindow.cs
+++ tools/saftschubser/saftschubser/MainWindow.cs
@@ -1,39 +1,103 @@
-// MainWindow.cs created with MonoDevelop
-// User: mati at 00:52 26.04.2008
-//
-// To change standard headers go to Edit->Preferences->Coding->Standard Headers
-//
 using System;
 using Gtk;
 
 namespace saftschubser
 {
 
-public partial class MainWindow: Gtk.Window
-{	
-	Gtk.Widget kaufenScreen; 
-			
-	public MainWindow (): base (Gtk.WindowType.Toplevel)
-	{
-		Build ();
-		kaufenScreen = new Kauf();
-		//bestandScreen = new Bestand();
-		//labCtrlScreen = new LabCtrl();
-	}
-	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
-	{
-		Application.Quit ();
-		a.RetVal = true;
-	}
+	public partial class MainWindow: Gtk.Window
+	{	
+		enum ScreenMode {
+			Kaufen, Kasse, Bestand, LabCtrl, Admin
+		};
 
-	protected virtual void kaufenClicked (object sender, System.EventArgs e)
-	{
-		//vbox1.Add( new Kauf());
-		vbox1.Remove(kaufenScreen);
-		vbox1.Add( kaufenScreen);
-		vbox1.ShowAll();
+		// Members
+		Gtk.Widget kaufenScreen;
+		Gtk.Widget kasseScreen;
+		Gtk.Widget bestandScreen;
+		Gtk.Widget labCtrlScreen;
+		Gtk.Widget adminScreen;
+		
+		ScreenMode curMode;
+		bool       ignoreModeSwitchEvents;
+			
+		public MainWindow (): base (Gtk.WindowType.Toplevel)
+		{
+			Build ();
+			
+			curMode = ScreenMode.Kaufen;
+			ignoreModeSwitchEvents = false;
+
+			// alle möglichen Screens erzeugen und einhängen
+			kaufenScreen  = new Kauf();
+			kasseScreen   = new KasseScreen();
+			bestandScreen = new BestandScreen();
+			labCtrlScreen = new LabCtrlScreen();
+			adminScreen   = new AdminScreen();
+			
+			vbox1.Add( kaufenScreen );
+			vbox1.Add( kasseScreen );
+			vbox1.Add( bestandScreen );
+			vbox1.Add( labCtrlScreen );
+			vbox1.Add( adminScreen );
+			
+			// Darstellung aktualisieren
+			UpdateScreen();
+			//Fullscreen();
+		}
+		
+		protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+		{
+			Application.Quit ();
+			a.RetVal = true;
+		}
+
+		/// <summary>
+		///  Update screen appereance according to curMode
+		/// </summary>
+		protected void UpdateScreen()
+		{
+			ignoreModeSwitchEvents = true;
+			
+			// ToggleButtons aktualisieren 
+			kaufenBtn.Active  = (curMode == ScreenMode.Kaufen);
+			kasseBtn.Active   = (curMode == ScreenMode.Kasse);
+			bestandBtn.Active = (curMode == ScreenMode.Bestand);
+			labCtrlBtn.Active = (curMode == ScreenMode.LabCtrl);
+			adminBtn.Active   = (curMode == ScreenMode.Admin);
+				
+			// MainArea
+			kaufenScreen.Visible  = (curMode == ScreenMode.Kaufen);
+			kasseScreen.Visible   = (curMode == ScreenMode.Kasse);			
+			bestandScreen.Visible = (curMode == ScreenMode.Bestand);
+			labCtrlScreen.Visible = (curMode == ScreenMode.LabCtrl);
+			adminScreen.Visible   = (curMode == ScreenMode.Admin);
+			
+			// vbox1.ShowAll();
+			
+			ignoreModeSwitchEvents = false;
+		}
+
+		protected virtual void OnModeBtnClicked (object sender, System.EventArgs e)
+		{
+			if (ignoreModeSwitchEvents)	return;
+			
+			if (sender == kaufenBtn)
+				curMode = ScreenMode.Kaufen;
+			else if (sender == kasseBtn)
+				curMode = ScreenMode.Kasse;
+			else if (sender == bestandBtn)
+				curMode = ScreenMode.Bestand;
+			else if (sender == labCtrlBtn)
+				curMode = ScreenMode.LabCtrl;
+			else if (sender == adminBtn)
+				curMode = ScreenMode.Admin;
+			else 
+				System.Console.WriteLine( "Unknown OnModeBtnClicked sender!" );
+			
+			UpdateScreen();
+		}
+			
 			
 	}
 }
 	
-}
---
+++ tools/saftschubser/saftschubser/Screens/AdminScreen.cs
@@ -0,0 +1,21 @@
+// AdminScreen.cs created with MonoDevelop
+// User: joerg at 13:42 27.04.2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace saftschubser
+{
+	
+	
+	public partial class AdminScreen : Gtk.Bin
+	{
+		
+		public AdminScreen()
+		{
+			this.Build();
+		}
+	}
+}
