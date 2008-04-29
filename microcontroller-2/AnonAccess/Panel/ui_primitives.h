e3449adf5abdc55df1d20969e34ed5672c5259e8
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-04-29 00:39:41 +0000

    +ui_textwindow

--- microcontroller-2/AnonAccess/Panel/ui_primitives.h
+++ microcontroller-2/AnonAccess/Panel/ui_primitives.h
@@ -16,6 +16,24 @@ typedef struct menu_st{
 } menu_t;
 
 
+#define LCD_LABORLOGO     7
+#define LCD_RADIOSELECT   5
+#define LCD_CHECKSELECT   6
+#define LCD_RADIONOSELECT 'o'
+#define LCD_CHECKNOSELECT 0xDB
+
+#define LCD_FULLCHAR  255
+#define LCD_EMPTYCHAR  ' '
+#define LCD_RARROW 0x7E
+#define LCD_LARROW 0x7F
+
+#define NO_KEY     ' '
+#define ENTER_KEY  'F'
+#define SELECT_KEY 'E'
+#define UP_KEY     '0'
+#define DOWN_KEY   'C'
+
+
 void ui_primitives_init(void);
 void print_progressbar(double percent, uint8_t xpos, uint8_t ypos, uint8_t width);
 uint8_t radioselect(char* opts);
@@ -25,5 +43,7 @@ void checkselect_P(PGM_P opts, uint8_t* config);
 void menuexec(menu_t* menu);
 void draw_frame(uint8_t posx, uint8_t posy, uint8_t width, uint8_t height, char framechar);
 void ui_hexdump(const void* data, uint16_t length);
+void ui_textwindow(uint8_t posx, uint8_t posy, uint8_t width, uint8_t height, char* text);
+
 
 #endif /*UI_PRIMITIVES_H_*/
