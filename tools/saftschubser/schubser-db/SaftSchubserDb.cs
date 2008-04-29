de612fa6a950035ff18f6e6c115047811202336c
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-04-29 10:01:05 +0000

    rename directories -- intermediate commit

---
+++ tools/saftschubser/schubser-db/SaftSchubserDb.cs
@@ -0,0 +1,21 @@
+using System;
+using Db4objects.Db4o;
+
+namespace SaftSchubserDb
+{
+}
+	
+
+/*
+	enum TransaktionsTyp {
+		Kaufe, Aufladen, Administrativ
+	}
+	
+	class Transaktion 
+	{
+		string          _text;
+		TransaktionsTyp _typ;
+		
+		
+	}
+*/
