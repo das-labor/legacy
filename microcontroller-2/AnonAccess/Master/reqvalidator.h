6977d478efa75989c78757f86d26ea0fd8cb87fe
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-06-02 09:16:26 +0000

    finished handling for actions

--- microcontroller-2/AnonAccess/Master/reqvalidator.h
+++ microcontroller-2/AnonAccess/Master/reqvalidator.h
@@ -2,6 +2,7 @@
 #define REQVALIDATOR_H_
 
 bool check_permissions(uint8_t users, uint8_t admins, action_t action);
+bool final_action(action_t action);
 bool check_pin(authblock_t * ab, sha256_hash_t pinhash);
 void change_pin(authblock_t * ab, sha256_hash_t pinhash);
 
@@ -9,7 +10,9 @@ authcredvalid_state_t check_authblock(authblock_t * ab);
 void new_account(authblock_t * ab, char* nickname, sha256_hash_t pinhash,uint8_t anon, uint8_t pinflags);
 
 void modify_account(char * nickname, userflags_t setflags, userflags_t clearflags);
-uint8_t pin_required(uid_t uid, uint8_t admin);
+void modify_account_byuid(uid_t uid, userflags_t setflags, userflags_t clearflags);
 
+uint8_t pin_required(uid_t uid, uint8_t admin);
+bool final_action(action_t action);
 
 #endif /*REQVALIDATOR_H_*/
