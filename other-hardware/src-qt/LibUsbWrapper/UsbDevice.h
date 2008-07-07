30f00a8aaf9b6ee00612fc3f21a4e5845bbd60b3
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-07-07 20:53:18 +0000

    * initial import

---
+++ other-hardware/src-qt/LibUsbWrapper/UsbDevice.h
@@ -0,0 +1,19 @@
+class UsbDevice
+public:
+	UsbDevice(int vid, int pid);
+	~UsbDevice();
+
+	bool connect();
+	bool disconnect();
+	bool isConnected();
+
+	int reqeuestRead(int requesttype, int request, int value, int index, char *bytes, int size, int timeout);
+	//not implemented yet
+	int requestWrite(int requesttype, int request, int value, int index, char *bytes, int size, int timeout);
+
+	int fetchInterrupt(int ep, char *bytes, int size, int timeout);
+
+private:
+	usb_dev_handle	*handle;
+	usb_device		*device;
+};
