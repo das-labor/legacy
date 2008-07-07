f124a8921ff338b8158533a91b889be87fcccd7a
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-07-07 20:52:15 +0000

    * initial import

---
+++ other-hardware/src-qt/LibUsbWrapper.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <usb.h>
+
+class LibUsbWrapper
+{
+public:
+	//singleton instance method
+	static LibUsbWrapper& Instance()
+	{
+		static LibUsbWrapper _instance;
+		return _instance;
+	}
+
+	static void FindBusses();
+	static void FindDevices();
+	static usb_bus* GetBusses();
+
+private:
+	LibUsbWrapper();
+	~LibUsbWrapper();	
+};
