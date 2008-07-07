7e248d16a87d34c9532f59b33ef04ae5bf382410
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-07-07 20:49:36 +0000

    * initial commit of UsbEventDevice

---
+++ other-hardware/src-qt/labcopter-tool/UsbEventDevice.cpp
@@ -0,0 +1,72 @@
+#include "UsbEventDevice.h"
+
+UsbEventDevice::UsbEventDevice(int vid, int pid) : UsbDevice(int vid, int pid)
+{
+	running = false;
+}
+
+
+UsbEventDevice::~UsbEventDevice()
+{
+	if(!this.finished())
+	{
+		stopEventThread();
+		while(!this.finished());
+	}
+}
+
+
+void UsbEventDevice::startEventThread(int endPoint, unsigned int bufSize, unsigned int readTimeout)
+{
+	running = true;
+	_endPoint = endPoint;
+	_bufSize = bufSize;
+	_readTimeout = readTimeout;
+
+	this.start();
+}
+
+
+void UsbEventDevice::stopEventThread()
+{
+	running = false;
+}
+
+
+void UsbEventDevice::run()
+{
+	EventThread(_endPoint, _bufSize, _readTimeout);
+}
+
+
+void UsbEventDevice::EventThread(int endPoint, unsigned int bufSize, unsigned int eventTimeout)
+{
+	QByteArray ba(bufSize);
+	int recSize;
+
+	while(running)
+	{
+		recSize = fetchInterrupt(0, ba.constData(), bufSize, 0);
+
+		if(recSize > 0)
+		{
+			emit interruptReceived(new QByteArray(ba.constData(), recSize));
+		}
+	}
+
+	QThread::exit();
+}
+
+
+//not necessary
+/*bool UsbEventDevice::reqeuestRead(int requesttype, int request, int value, int index, QByteArray &bytes, int size, int timeout)
+{
+	bytes.resize(size+1);
+
+	requestRead();
+}
+
+bool UsbEventDevice::reqeuestWrite(int requesttype, int request, int value, int index, QByteArray &bytes, int size, int timeout)
+{
+}
+*/
---
+++ other-hardware/src-qt/labcopter-tool/UsbEventDevice.h
@@ -0,0 +1,30 @@
+#include <QObject>
+#include "UsbDevice.h"
+
+class UsbEventDevice : public QThread, UsbDevice
+{
+public:
+	UsbEventDevice(int vid, int pid);
+	~UsbEventDevice();
+
+	void startEventThread(int endPoint, unsigned int bufSize, unsigned int readTimeout);
+	void stopEventThread();
+
+	//not necessary
+	//bool reqeuestRead(int requesttype, int request, int value, int index, QByteArray bytes, int size, int timeout);
+	//bool reqeuestWrite(int requesttype, int request, int value, int index, QByteArray bytes, int size, int timeout);
+
+public signals:
+	void interruptReceived(QByteArray &bytes);
+	void connectChanged(bool state);
+
+private:
+	void eventThread();
+	virtual void run();
+
+	bool running;
+
+	int _endPoint;
+	unsigned int _bufSize;
+	unsigned int _readTimeout;
+};
