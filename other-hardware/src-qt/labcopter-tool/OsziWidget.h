ef9f14524a4ffc2f2fa1b509c1ad4da34df7b41d
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-07-21 07:49:11 +0000

    weitergem8 - Diese und die folgenden Zeilen werden ignoriert --
    M    labcopter-tool/Graph.cpp
    M    labcopter-tool/labcopter-tool.pro
    A    labcopter-tool/Graph.h
    M    labcopter-tool/OsziWidget.cpp
    M    labcopter-tool/MainWindow.cpp
    M    labcopter-tool/Makefile
    M    labcopter-tool/OsziWidget.h

--- other-hardware/src-qt/labcopter-tool/OsziWidget.h
+++ other-hardware/src-qt/labcopter-tool/OsziWidget.h
@@ -3,17 +3,21 @@
 #include <QGraphicsView>
 
 #include "ui_OsziWidget.h"
+#include "Graph.h"
 
 class OsziWidget : public QWidget{
 	Q_OBJECT;
 	
 	public:
 		OsziWidget(QWidget * parent);
+		void addGraph(Graph *);
+		void update();
 	
 	private:
 		Ui::OsziWidget ui;
 
 		QGraphicsScene * scene;
+		QList<Graph *> graphs;
 	
 	
 };
