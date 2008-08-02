044fcad6d519f73f97670127e296dec8dc29314d
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-08-02 00:25:04 +0000

    Bildschirmtastatur funzt

--- tools/Jeton/Jeton/Dialogs/TextualInputDialog.cs
+++ tools/Jeton/Jeton/Dialogs/TextualInputDialog.cs
@@ -13,27 +13,36 @@ namespace Jeton
 	
 	public partial class TextualInputDialog : Gtk.Dialog
 	{
-		Button _shiftButton;
+		ToggleButton _shiftButton;
 		public string text = "";
-					
-		public TextualInputDialog(string info)
-		{
-			this.Build();
-			infoLabel.Markup = info;
-			BuildKeyboard();
-		}
-		
-		
-		void BuildKeyboard(){
-			string keys =
+		public int maxLength = 50;
+		string keys_lowercase =
 				    "1234567890ß<backspace>\n" +
 					"<#25>qwertzuiopü\n" +
 					"<#40>asdfghjklöä\n" +
 					"<shift>yxcvbnm,.-\n" +
 					"<#0><space><#0>\n";
 
+		string keys_uppercase =
+				    "!\"§$%&/()=?<backspace>\n" +
+					"<#25>QWERTZUIOPÜ\n" +
+					"<#40>ASDFGHJKLÖÄ\n" +
+					"<shift>YXCVBNM;:_\n" +
+					"<#0><space><#0>\n";
+
+		public TextualInputDialog(string info)
+		{
+			this.Build();
+			infoLabel.Markup = info;
+			BuildKeyboard();
+			Update();
+		}
+		
+		
+		void BuildKeyboard(){
+			string keys = keys_lowercase;
 			string key;
-			uint row=0, col=0;
+			//uint row=0, col=0;
 			
 			HBox hbox = new HBox();
 			Button btn;
@@ -65,11 +74,12 @@ namespace Jeton
 						break;
 					case "shift":{
 						Image img = new Image( "gtk-go-up",  IconSize.Button);								
-						btn = new ToggleButton();
-						btn.Image = img;
-						btn.SetSizeRequest(65,50);
-						hbox.PackStart(btn, false, false, 0);
-						_shiftButton = btn;
+						ToggleButton tbtn = new ToggleButton();
+						tbtn.Image = img;
+						tbtn.SetSizeRequest(65,50);
+						tbtn.Toggled += new  EventHandler(OnShiftToggled);
+						hbox.PackStart(tbtn, false, false, 0);
+						_shiftButton = tbtn;
 						break;
 					}
 					case "space":
@@ -97,23 +107,83 @@ namespace Jeton
 						break;
 					}
 				}
-				//Console.WriteLine(key);
 			}
+		}
+		
+		void SetKeys(string keys){
+			string key;
+			uint row=0, col=0;
+			
+			HBox hbox = keyboardVBox.Children[0] as HBox;
+			Button btn;
+			
+			while(keys.Length != 0){
+				if(keys.StartsWith("<")){
+					key = keys.Substring(1, keys.IndexOf(">")-1);
+					keys = keys.Remove(0,keys.IndexOf(">")+1);
+				}else{
+					key = keys.Substring(0,1);
+					keys = keys.Remove(0,1);
+				}
+								
+				if(key.StartsWith("#")){
+					
+				}else{
+					switch(key){
+					case "\n":
+						col=0;
+						row++;
+						if(row < 5){
+							hbox = keyboardVBox.Children[row] as HBox;
+						}
+						continue;
+					case "shift":
+						break;
+					case "space":
+						break;
+					case "backspace":
+						break;	
+					default:
+						if( col < hbox.Children.GetLength( 0)  ){
+							btn = hbox.Children[col] as Button;
+							btn.Name = key;
+							btn.Label = key;
+							
+						}						
+						break;
+					}
+					
+				}				
+				col++;			
+			}
+
 		}
 		
 		void OnKeyClicked(object sender, EventArgs a){
 			Button btn = sender as Button;
-			text += btn.Name;
+			if(text.Length < maxLength){
+				text += btn.Name;
+			}
+			_shiftButton.Active = false;
 			Update();
 		}
 		
 		void OnBackspaceClicked(object sender, EventArgs a){
+			Console.WriteLine("foobar");
 			if(text.Length > 0){
 				text = text.Remove(text.Length-1,1);
 				Update();
 			}
 		}
-				
+		
+		void OnShiftToggled(object sender,  EventArgs a){
+			ToggleButton btn = sender as ToggleButton;
+			if(btn.Active){
+				SetKeys(keys_uppercase);
+			}else{
+				SetKeys(keys_lowercase);	
+			}
+		}
 		
 		void Update(){
 			textLabel.Markup = "<span size=\"xx-large\"><b>" + text + "</b></span>";		
