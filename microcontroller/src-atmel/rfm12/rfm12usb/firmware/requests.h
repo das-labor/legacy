c893ee4cd10fc1279ed340120b774a95954fe9f0
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-08-10 04:59:14 +0000

    implemented usb control transfer data receive function implemented multibyte (up to rfm tx buffer size) rfm12 raw packet transfersmajor upgrade of device firmware, implemented buffering techniques, implemented rudimentary error handling, reworked defines, created new header files, code cleanup

--- microcontroller/src-atmel/rfm12/rfm12usb/firmware/requests.h
+++ microcontroller/src-atmel/rfm12/rfm12usb/firmware/requests.h
@@ -20,17 +20,15 @@
 /* Request that the device sends back wValue and wIndex. This is used with
  * random data to test the reliability of the communication.
  */
-#define CUSTOM_RQ_GET_DATA    1
-/* Set the LED status. Control-OUT.
- * The requested status is passed in the "wValue" field of the control
- * transfer. No OUT data is sent. Bit 0 of the low byte of wValue controls
- * the LED.
- */
 
-#define CUSTOM_RQ_PUT_DATA    2
-/* Get the current LED status. Control-IN.
- * This control transfer involves a 1 byte data phase where the device sends
- * the current status to the host. The status is in bit 0 of the byte.
- */
+ //put data into the rfm12 transmit buffer
+#define RFMUSB_RQ_RFM12_PUT			0x01
+
+//poll for rfm12 received data
+#define RFMUSB_RQ_RFM12_GET			0x02
+
+//poll for notifications
+#define RFMUSB_RQ_GETNOTIFICATION	0x03
+
 
 #endif /* __REQUESTS_H_INCLUDED__ */
