cd023d02a4f6206bacb3392c6f9dfe9739746386
chris <chris@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-09-02 20:28:39 +0000

    ext

--- microcontroller/src-atmel/borg/borg-16/tetris/input.c
+++ microcontroller/src-atmel/borg/borg-16/tetris/input.c
@@ -29,7 +29,7 @@
 
 // amount of allowed loop cycles while in pause mode so that the game
 // automatically continues after a minute
-#define TETRIS_INPUT_PAUSE_CYCLES 1200
+#define TETRIS_INPUT_PAUSE_CYCLES 12000
 
 // minimum of cycles in gliding mode
 #define TETRIS_INPUT_GLIDE_CYCLES 75
