b94b4c7fdc6270a3261a1652f077cbdae0464b56
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-11-16 02:34:53 +0000

    working now

--- microcontroller/src-atmel/borg/borg-3d-mini/main.c
+++ microcontroller/src-atmel/borg/borg-3d-mini/main.c
@@ -27,7 +27,15 @@ PC2  x=2 y=2
 
  */
 
-#define SET3BIT(var, PORTB_0, B_0, PORTB_1, B_1, PORTB_2, B_2)  \
+
+#define SETOFF(PORTB_0, B_0, PORTB_1, B_1, PORTB_2, B_2)  \
+				PORTB_0 &= ~(1 << B_0); \
+				PORTB_1 &= ~(1 << B_1); \
+				PORTB_2 &= ~(1 << B_2);
+	
+
+
+#define SETPBIT(var, PORTB_0, B_0, PORTB_1, B_1, PORTB_2, B_2)  \
 		switch (var) { \
 			case 0: \
 				PORTB_0 |=  (1 << B_0); \
@@ -46,6 +54,56 @@ PC2  x=2 y=2
 				break;	\
 		}
 
+
+#define SET3BIT(var, PORTB_0, B_0, PORTB_1, B_1, PORTB_2, B_2)  \
+		switch (var) { \
+			case 0: \
+				PORTB_0 &= ~(1 << B_0); \
+				PORTB_1 &= ~(1 << B_1); \
+				PORTB_2 &= ~(1 << B_2); \
+				break; \
+			case 1: \
+				PORTB_0 |=  (1 << B_0); \
+				PORTB_1 &= ~(1 << B_1); \
+				PORTB_2 &= ~(1 << B_2); \
+				break; \
+			case 2: \
+				PORTB_0 &= ~(1 << B_0); \
+				PORTB_1 |=  (1 << B_1); \
+				PORTB_2 &= ~(1 << B_2); \
+				break; \
+			case 3: \
+				PORTB_0 |=  (1 << B_0); \
+				PORTB_1 |=  (1 << B_1); \
+				PORTB_2 &= ~(1 << B_2); \
+				break;	\
+			case 4: \
+				PORTB_0 &= ~(1 << B_0); \
+				PORTB_1 &= ~(1 << B_1); \
+				PORTB_2 |=  (1 << B_2); \
+				break; \
+			case 5: \
+				PORTB_0 |=  (1 << B_0); \
+				PORTB_1 &= ~(1 << B_1); \
+				PORTB_2 |=  (1 << B_2); \
+				break; \
+			case 6: \
+				PORTB_0 &= ~(1 << B_0); \
+				PORTB_1 |=  (1 << B_1); \
+				PORTB_2 |=  (1 << B_2); \
+				break; \
+			case 7: \
+				PORTB_0 |=  (1 << B_0); \
+				PORTB_1 |=  (1 << B_1); \
+				PORTB_2 |=  (1 << B_2); \
+				break;	\
+		}
+
+
+
+
+
+
 unsigned char pixmap[MAXBRITH][MAX_Z][MAX_Y];
 
 static inline void planeShow(unsigned char plane, unsigned char brith) {	
@@ -61,21 +119,29 @@ static inline void planeShow(unsigned char plane, unsigned char brith) {
 				OCR2 = 20;
 		}
 	}
-	SET3BIT(plane, PORTD, 3, PORTD, 4, PORTB, 0);
+	
+//	SETOFF(PORTD, 3, PORTD, 4, PORTB, 0);
+	
+	PORTD &= ~(1<<PD3);
+	PORTD &= ~(1<<PD4);
+	PORTB &= ~(1<<PB0);
+
 	SET3BIT(pixmap[brith][plane][0], PORTD, 0, PORTD, 1, PORTD, 2);
 	SET3BIT(pixmap[brith][plane][1], PORTC, 3, PORTC, 4, PORTC, 5);
-	SET3BIT(pixmap[brith][plane][2], PORTD, 0, PORTD, 1, PORTD, 2);
+	SET3BIT(pixmap[brith][plane][2], PORTC, 0, PORTC, 1, PORTC, 2);
+
+	SETPBIT(plane, PORTD, 3, PORTD, 4, PORTB, 0);
 }
 
-SIGNAL(SIG_OVERFLOW2)
+SIGNAL(TIMER2_COMP_vect)
 {
-	static unsigned char plane = 3;
+	static unsigned char plane = 2;
 	static unsigned char brith = 0;	
 
 	planeShow(plane, brith);
-	
+
 	if (plane-- == 0) {
-		plane = MAX_Z;
+		plane = MAX_Z-1;
 		if (++brith == MAXBRITH) 
 			brith = 0;
 	}
@@ -92,7 +158,7 @@ void timer0_on() {
 		 1    0    1       clk/1024
 	
 */
-	TCCR2 = 0x0B;	// CTC Mode, clk/8
+	TCCR2 = 0x0D;	// CTC Mode, clk/8
 	TCNT2 = 0;		// reset timer
 	OCR2  = 0x10;	// Compare with this value
 	TIMSK = (1 << OCIE2);	// Compare match Interrupt on
@@ -119,8 +185,22 @@ int main(void) {
 	init_Ports();
 	timer0_on();
 	sei();
-	uart_init();
-  	uart_putstr("starting \n");
+	//uart_init();
+  	//uart_putstr("starting \n");
+
+
+	uint8_t x,y,z;
+	for(x=0;x<3;x++){
+		for(y=0;y<3;y++){
+			for(z=0;z<3;z++){
+				setpixel3d((pixel3d){x,y,z}, 3);
+				wait(200);
+				setpixel3d((pixel3d){x,y,z}, 0);
+			}
+		}
+	}
+
+
 	while (1) {
 		snake();
 		schachBlinking2();
