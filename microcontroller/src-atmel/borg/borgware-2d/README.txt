4de4b15b803e2653d32e8891fd51228fc542a8ce
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-12-03 06:10:03 +0000

    need to commit changes again after renaming for some reason. svn broken?

--- microcontroller/src-atmel/borg/borgware-2d/README.txt
+++ microcontroller/src-atmel/borg/borgware-2d/README.txt
@@ -1,5 +1,8 @@
 
 Hier liegt der Versuch, eine universelle Codebasis für 2d-borgs zu bauen, die
-durch 'make menuconfig' konfigurierbar ist. Das ganze ist grade nochnicht
-besonders weit, und funktioniert noch garnicht. Work is in progress...
-
+durch 'make menuconfig' konfigurierbar ist.
+Zur Zeit ist es bereits möglich, gewisse Dinge in menuconfig einzustellen.
+Einfach mal ansehen! Compiliert werden kann das Ganze auch scon. Es fehlt noch
+Support für das Spiele-Auswahlmenü und die Spiele.
+Außerdem wird grade versucht, das Ganze mit dem Borg-api-Simulator zu
+integrieren.
--- microcontroller/src-atmel/borg/borgware-2d/rules.mk
+++ microcontroller/src-atmel/borg/borgware-2d/rules.mk
@@ -1,21 +1,33 @@
 
-OBJECTS += $(patsubst %.c,%.o,${SRC})
-OBJECTS += $(patsubst %.S,%.o,${ASRC})
+OBJECTS += $(patsubst %.c,obj_avr/%.o,${SRC})
+OBJECTS += $(patsubst %.S,obj_avr/%.o,${ASRC})
 
-%.a: $(OBJECTS)
-	$(RM) $@
-	$(AR) qcv $@ $^
+#./obj_avr/%.a: $(OBJECTS)
+#	$(AR) qcv $@ $^
 #	$(STRIP) --strip-unneeded $@
 
-%.o: %.S
-	$(CC) -o $@ $(CPPFLAGS) $(ASFLAGS) -c $<
+./obj_avr/%.o: %.S
+	@ if [ ! -d obj_avr ]; then mkdir obj_avr ; fi
+	@ echo "assembling $<"
+	@ $(CC) -o $@ $(CPPFLAGS) $(ASFLAGS) -c $<
+
+./obj_avr/%.o: %.c
+	@ if [ ! -d obj_avr ]; then mkdir obj_avr ; fi
+	@ echo "compiling $<"
+	@ $(CC) -o $@ $(CPPFLAGS) $(CFLAGS) $(LDFLAGS) -c $<
 
 clean-common:
 	$(RM) $(TARGET) *.[odasE] *.d.new *~
+	$(RM) -r ./obj_avr
 
 clean: clean-common
 
 all:
 	make -C $(TOPDIR) all
+	
+objects_avr: $(OBJECTS)
+	@ echo "writing object ineventory"
+	@ echo $(OBJECTS) > obj_avr/.objects
+	
 
 include $(TOPDIR)/depend.mk
