6496d73d87c2886dd73503cdea544f026629f210
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-01-25 00:22:44 +0000

    removed unnessecery

--- microcontroller/src-atmel/playground/rfid_reader/config.h
+++ microcontroller/src-atmel/playground/rfid_reader/config.h
@@ -10,19 +10,5 @@
 #define UART_RXBUFSIZE 16
 #define UART_TXBUFSIZE 16
 #define UART_LINE_BUFFER_SIZE 40
-//#define UART_LEDS 1
-
-
-//c at-kbd.[ch] defines
-
-#define KBD_DATA_DDR		DDRA
-#define KBD_DATA_OUT_PORT	PORTA
-#define KBD_DATA_IN_PORT	PINA
-#define KBD_DATA_BIT		0
-
-#define KBD_CLOCK_DDR		DDRB
-#define KBD_CLOCK_OUT_PORT 	PORTB
-#define KBD_CLOCK_IN_PORT 	PINB
-#define KBD_CLOCK_BIT		2
 
 #endif
