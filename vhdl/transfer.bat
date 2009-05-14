93f075b25137458053b562c2607c8b96ba466eb6
sauron <sauron@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-05-14 22:01:56 +0000

    Im Falschen Verzeichnis hochgeladen

--- vhdl/transfer.bat
+++
@@ -1,6 +0,0 @@
-mem2hex picoblaze.mem picoblaze.hex
-hex2svf picoblaze.hex picoblaze.svf
-svf2xsvf -d -i picoblaze.svf -o picoblaze.xsvf
-copy picoblaze.xsvf d:\borg_sender\sender
-c:\Xilinx92i\bin\nt\impact.exe -batch impact_batch.txt
-
