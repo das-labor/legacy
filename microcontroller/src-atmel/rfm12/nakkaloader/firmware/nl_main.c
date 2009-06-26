d15464143972b78ed2e26d62564492289b227911
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-06-26 12:15:51 +0000

    works. don't touch it.

--- microcontroller/src-atmel/rfm12/nakkaloader/firmware/nl_main.c
+++ microcontroller/src-atmel/rfm12/nakkaloader/firmware/nl_main.c
@@ -5,7 +5,7 @@
 #include <avr/pgmspace.h>
 #include <stdint.h>
 #include <string.h>
-// #include <util/crc16.h>
+#include <util/crc16.h>
 
 
 #include "rfm12_config.h"
@@ -23,7 +23,7 @@ void rxclear(void)
 void (*app_ptr)(void) = (void *)0x0000;
 uint8_t myaddress[NL_ADDRESSSIZE];
 
-void boot_program_page (uint16_t page, uint8_t *buf)
+void boot_program_page (uint32_t page, uint8_t *buf)
 {
 	uint16_t i;
 	uint8_t sreg;
@@ -70,7 +70,8 @@ uint8_t nl_match_packet (uint8_t *in_packet)
 
 void nl_tx_packet (uint8_t in_type, uint8_t in_len, uint8_t *in_payload)
 {
-	uint8_t txpacket[NL_ADDRESSSIZE + 1 + sizeof(nl_config)];
+	uint8_t txpacket[NL_ADDRESSSIZE + 1 + NL_PACKETSIZE];
+	uint8_t i = NL_ADDRESSSIZE + 1, k = 2; 
 
 //	txpacket[1] = myaddress[0];
 	txpacket[1] = 0xff;
@@ -83,10 +84,15 @@ void nl_tx_packet (uint8_t in_type, uint8_t in_len, uint8_t *in_payload)
 
 	if (in_len)
 	{
-		memcpy(txpacket + NL_ADDRESSSIZE + 1, in_payload, in_len);
+		for (;k<in_len;k++) 
+		{ 
+			txpacket[i] = *(in_payload + k); 
+			i++; 
+		} 
+		//memcpy(txpacket + NL_ADDRESSSIZE + 1, in_payload, in_len);
 	}
 	
-	rfm12_tx (in_len, NL_PACKETTYPE, txpacket);
+	rfm12_tx (i, NL_PACKETTYPE, txpacket);
 }
 
 void nl_boot_app ( void )
@@ -101,28 +107,30 @@ void nl_boot_app ( void )
 	//move interrupts back (also disables rfm12 int)
 	GICR = (1 << IVCE);
 	GICR = 0;
+
 	
 	app_ptr();
 }
 
-int __attribute__ ((naked)) main (void)
+int main (void)
+//int __attribute__ ((naked)) main (void)
 {
-	uint16_t i = 1;
+	uint32_t i = 1;
 	uint8_t k, mystate = NLPROTO_SLAVE_CONFIG;
 	uint8_t *rxbuf;
 	uint8_t mypage[SPM_PAGESIZE];
-	uint16_t pagenum = 0;
+	uint32_t pagenum = 0;
 	uint16_t crcsum = 0;
 	nl_config myconfig = {SPM_PAGESIZE, RFM12_RX_BUFFER_SIZE, NL_VERSION};
 	nl_flashcmd *mycmd;
 
-	DDRD |= (_BV(PD7) | _BV(PD5) | _BV(PD6));
-	PORTD |= _BV(PD7);
+	DDRD |= (_BV(PD7));
+	PORTD |= _BV(PD7); /* FSK for pollin board - uncomment */
 #if 0
 	/* fill config variables */
-	myconfig.pagesize = SPM_PAGESIZE;
-	myconfig.rxbufsize = RFM12_RX_BUFFER_SIZE;
-//	myconfig.version = NL_VERSION;
+	myconfig.pagesize = (uint16_t) SPM_PAGESIZE;
+	myconfig.rxbufsize = (uint8_t) RFM12_RX_BUFFER_SIZE;
+	myconfig.version = NL_VERSION;
 
 
 	/* read address */
@@ -146,9 +154,8 @@ int __attribute__ ((naked)) main (void)
 //	for (i=0;i < NL_MAXFAILS || NL_MAXFAILS == 0;i++)
 	while (2)
 	{
-		i++;
 
-		if (i == 0xffff) break;
+		if (i == 0x1ffff) break;
 
 		if (rfm12_rx_status() == STATUS_COMPLETE)
 		{
@@ -161,18 +168,6 @@ int __attribute__ ((naked)) main (void)
 			}
 
 			mystate = rxbuf[0];
-			
-			switch (rxbuf[0])
-			{
-				case NLPROTO_MASTER_EHLO:
-					rxclear();
-					break;
-
-				case NLPROTO_BOOT:
-					i = 0;
-					break;
-
-			}
 
 			i=0; /* somebody cares about us. reset error counter */
 
@@ -180,22 +175,24 @@ int __attribute__ ((naked)) main (void)
 
 
 		/* (re)transmit our configuration if master hasn't responded yet. */
-		if ((i & 0x7fff) == 0)
+		if ((i & 0x3fff) == 0)
 		{
 			switch (mystate)
 			{
 				case NLPROTO_SLAVE_CONFIG:
+					rxclear();
 					nl_tx_packet (NLPROTO_SLAVE_CONFIG, sizeof(myconfig), (uint8_t *) &myconfig);
 					break;
 
 				case NLPROTO_MASTER_EHLO:
+					rxclear();
 					nl_tx_packet (NLPROTO_MASTER_EHLO, 0, mypage);
 					break;
 
 				/* master is ready to flash */
 				case NLPROTO_PAGE_FILL:
-					mycmd = (nl_flashcmd *) (rxbuf + NL_ADDRESSSIZE + 1);
-#if 0
+					mycmd = (nl_flashcmd *) (rxbuf + NL_ADDRESSSIZE +1);
+#if 1
 					/* check boundaries */
 					if (mycmd->addr_start + (mycmd->addr_end - mycmd->addr_start) > SPM_PAGESIZE)
 					{
@@ -208,29 +205,37 @@ int __attribute__ ((naked)) main (void)
 						nl_tx_packet (NLPROTO_ERROR, 0, mypage);
 						#endif
 						rxclear();
+						nl_tx_packet (NLPROTO_ERROR, 0, mypage);
 						break;
 					}
 #endif
 					memcpy (mypage + mycmd->addr_start,
 						rxbuf + NL_ADDRESSSIZE + 1 + sizeof(nl_flashcmd),
 						mycmd->addr_end - mycmd->addr_start);
+					
+/*					memcpy (mypage + *((uint16_t *) (rxbuf + 6)),
+						rxbuf + NL_ADDRESSSIZE + 1 + sizeof(nl_flashcmd),
+						mycmd->addr_end - mycmd->addr_start);
 
-	/*				for (k=mycmd.addr_start;k<mycmd.addr_end;k++)
+*/
+/*					for (k=mycmd->addr_start;k<mycmd->addr_end;k++)
 						crcsum = _crc16_update (crcsum,
 							*(mypage + k)); */
+					crcsum = mycmd->addr_start;
 
-					rxclear();
+//					rxclear();
 
 					mystate = NLPROTO_PAGE_CHKSUM;
 					/* no break for retransmission purposes */
 				
 				case NLPROTO_PAGE_CHKSUM:
+					rxclear();
 					nl_tx_packet (NLPROTO_PAGE_CHKSUM, 2, (uint8_t *) &crcsum);
 					break;
 
 				/* commit page write */
 				case NLPROTO_PAGE_COMMIT:
-					pagenum = (uint16_t) (rxbuf[NL_ADDRESSSIZE +1]);
+					memcpy(&pagenum, rxbuf + NL_ADDRESSSIZE +1, sizeof(pagenum));
 
 					rxclear();
 
@@ -240,12 +245,19 @@ int __attribute__ ((naked)) main (void)
 					/* intentionally no break statement here to retransmit the commit ack */
 
 				case NLPROTO_PAGE_COMMITED:
-					nl_tx_packet (NLPROTO_PAGE_COMMITED, 4, (uint8_t *) &pagenum);
+					rxclear();
+					nl_tx_packet (NLPROTO_PAGE_COMMITED, sizeof(pagenum), (uint8_t *) &pagenum);
+					break;
+				
+				case NLPROTO_BOOT:
+					rxclear();
+					nl_boot_app();
 					break;
 
 			}
 		}
 		
+		i++;
 		rfm12_tick();
 	}
 
