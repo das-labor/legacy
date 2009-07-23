29c99ab587f189e789e16a4cb29257a4d72e492f
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-23 23:26:32 +0000

    hansi+soeren: started to rewrite / organize lib

---
+++ microcontroller/src-atmel/lib/rfm12/dev/rfm12_ctrl.c
@@ -0,0 +1,86 @@
+#include "rfm12_hw.h"
+#include "rfm12.h"
+
+void rfm12_init()
+{
+	SS_RELEASE();
+	DDR_SS |= (1<<BIT_SS);
+	
+	spi_init();
+
+	#ifdef RFM12_RAW_TX
+	rfm12_raw_tx = 0;
+	#endif
+
+	rf_tx_buffer.sync[0] = SYNC_MSB;
+	rf_tx_buffer.sync[1] = SYNC_LSB;
+	
+	rf_rx_buffer.rf_buffer_out = &rf_rx_buffer.rf_buffers[0];
+	rf_rx_buffer.rf_buffer_in  = &rf_rx_buffer.rf_buffers[0];
+	
+	//disable all power
+	//this was no good idea, because when one writes the PWRMGT register
+	//two times in a short time, the second write seems to be delayed.
+	//the PWRMGT register is written at the end of this function.
+	rfm12_data(RFM12_CMD_PWRMGT | PWRMGT_DEFAULT);
+
+	//enable internal data register and fifo
+	rfm12_data(RFM12_CMD_CFG | RFM12_CFG_EL | RFM12_CFG_EF | RFM12_BAND_433 | RFM12_XTAL_12PF);
+	
+	//automatic clock lock control(AL), digital Filter(!S), Data quality
+	//detector value 3
+	rfm12_data(RFM12_CMD_DATAFILTER | RFM12_DATAFILTER_AL | 3);
+	
+	//2 Byte Sync Pattern, Start fifo fill when sychron pattern received,
+	//disable sensitive reset, Fifo filled interrupt at 8 bits
+	rfm12_data(RFM12_CMD_FIFORESET | RFM12_FIFORESET_DR | (8<<4));
+
+	//set AFC to automatic, (+4 or -3)*2.5kHz Limit, fine mode, active and enabled
+	rfm12_data(RFM12_CMD_AFC | RFM12_AFC_AUTO_KEEP | RFM12_AFC_LIMIT_4
+				| RFM12_AFC_FI | RFM12_AFC_OE | RFM12_AFC_EN);
+	
+	//set frequency
+	rfm12_data(RFM12_CMD_FREQUENCY | RFM12_FREQUENCY_CALC_433(FREQ) );
+
+	//set datarate
+	rfm12_data(RFM12_CMD_DATARATE | DATARATE_VALUE );
+
+	//set rx parameters: int-in/vdi-out pin is vdi-out,
+	//Bandwith, LNA, RSSI
+	rfm12_data(RFM12_CMD_RXCTRL | RFM12_RXCTRL_P16_VDI 
+			| RFM12_RXCTRL_VDI_FAST | RFM12_RXCTRL_BW_400 | RFM12_RXCTRL_LNA_6 
+			| RFM12_RXCTRL_RSSI_79 );
+	
+	//set TX Power to -0dB, frequency shift = +-125kHz
+	rfm12_data(RFM12_CMD_TXCONF | RFM12_TXCONF_POWER_0 | RFM12_TXCONF_FS_CALC(125000) );
+	
+	//enable receiver
+	rfm12_data(RFM12_CMD_PWRMGT | PWRMGT_DEFAULT | RFM12_PWRMGT_ER);
+		
+	//init receiver fifo, we now begin receiving.
+	rfm12_data(CLEAR_FIFO);
+	rfm12_data(ACCEPT_DATA);
+
+	//setup interrupt for falling edge trigger
+	RFM12_INT_SETUP();
+	
+	//activate the interrupt
+	RFM12_INT_ON();
+
+#ifdef RFM12_RECEIVE_CW
+	adc_init();
+#endif
+}
+
+
+#if RFM12_LIVECTRL
+inline void rfm12_set_rate (uint16_t in_datarate)
+{
+	rfm12_data(RFM12_CMD_DATARATE | DATARATE_VALUE );
+}
+
+inline void rfm12_set_frequency (uint16_t in_freq)
+{
+	rfm12_data(RFM12_CMD_FREQUENCY | in_freq );
+}
+#endif
---
+++ microcontroller/src-atmel/lib/rfm12/dev/rfm12_ctrl.h
@@ -0,0 +1,35 @@
+/**** RFM 12 library for Atmel AVR Microcontrollers *******
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ *
+ * @author Peter Fuhrmann, Hans-Gert Dahme, Soeren Heisrath
+ */
+ 
+ #pragma once
+
+/******************************************************
+ *                                                    *
+ *    NO  C O N F I G U R A T I O N  IN THIS FILE     *
+ *                                                    *
+ *      ( thou shalt not change lines below )         *
+ *                                                    *
+ ******************************************************/
+
+/******************************************************
+* 	function protoypes						 *
+ ******************************************************/
+ 
+void rfm12_init();
