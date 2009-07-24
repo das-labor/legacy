b2c1b4c4a814e07a4e917343c3cc9194b98cffe7
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-24 17:15:09 +0000

    hansi: made headers one-time-include-able, fixed licenses

--- microcontroller/src-atmel/lib/rfm12/dev/include/rfm12_ctrl.c
+++ microcontroller/src-atmel/lib/rfm12/dev/include/rfm12_ctrl.c
@@ -4,13 +4,13 @@
  ******************************************************/
 
 
-#if RFM12_LIVECTRL
-inline void rfm12_set_rate (uint16_t in_datarate)
+#if RFM12_LIVECTRL > 0
+void rfm12_set_rate (uint16_t in_datarate)
 {
 	rfm12_data(RFM12_CMD_DATARATE | DATARATE_VALUE );
 }
 
-inline void rfm12_set_frequency (uint16_t in_freq)
+void rfm12_set_frequency (uint16_t in_freq)
 {
 	rfm12_data(RFM12_CMD_FREQUENCY | in_freq );
 }
