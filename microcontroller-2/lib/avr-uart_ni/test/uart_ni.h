c30411599924f2711326c821e7b750f1b1f81777
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-24 13:45:49 +0000

    some more comments and License file

--- microcontroller-2/lib/avr-uart_ni/test/uart_ni.h
+++ microcontroller-2/lib/avr-uart_ni/test/uart_ni.h
@@ -16,6 +16,16 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+/**
+ * \file     uart_ni.h
+ * \email    daniel.otte@rub.de
+ * \author   Daniel Otte 
+ * \date     2009-07-24
+ * \license  GPLv3 or later
+ * \ingroup  uart_ni
+ * \brief    declaration for non-interrupt uart
+ */
+
 #ifndef UART_NI_H_
 #define UART_NI_H_
 
@@ -24,19 +34,71 @@
 
 #if UART0
 
-void     uart0_init(void);
-void     uart0_putc(uint16_t c);
+/** \fn uart0_init(void)
+ * \brief initialize uart0.
+ * This function initializes the first uart according to the parameter specifyed
+ * in config.h .
+ */
+void uart0_init(void);
+
+/** \fn uart0_putc(uint16_t)
+ * \brief send data through uart0.
+ * This function sends data through the first uart 
+ * (the data size is debfined in config.h).
+ * \param c data to send
+ */
+void uart0_putc(uint16_t c);
+
+/** \fn uart0_getc(void)
+ * \brief read data from uart0.
+ * This function reads data from the first uart 
+ * (the data size is debfined in config.h).
+ * \return data recived by uart0
+ */
 uint16_t uart0_getc(void);
-uint8_t  uart0_dataavail(void)
+
+/** \fn uart0_dataavail(void)
+ * \brief checks if data is available.
+ * 
+ * This function checks the state of the input buffer of uart0 and
+ * returns if data is available or not.
+ * \return zero if no data is available else a value different from zero is returned
+ */
+uint8_t uart0_dataavail(void)
 
 #endif
 
 #if UART1
+/** \fn uart1_init(void)
+ * \brief initialize uart1.
+ * This function initializes the second uart according to the parameter specifyed
+ * in config.h .
+ */
+void uart1_init(void);
 
-void     uart1_init(void);
-void     uart1_putc(uint16_t c);
+/** \fn uart1_putc(uint16_t)
+ * \brief send data through uart1.
+ * This function sends data through the second uart 
+ * (the data size is debfined in config.h).
+ * \param c data to send
+ */
+void uart1_putc(uint16_t c);
+
+/** \fn uart1_getc(void)
+ * \brief read data from uart1.
+ * This function reads data from the second uart 
+ * (the data size is debfined in config.h).
+ * \return data recived by uart1
+ */
 uint16_t uart1_getc(void);
-uint8_t  uart1_dataavail(void)
+
+/** \fn uart1_dataavail(void)
+ * \brief checks if data is available.
+ * This function checks the state of the input buffer of uart1 and
+ * returns if data is available or not.
+ * \return zero if no data is available else a value different from zero is returned
+ */
+uint8_t uart1_dataavail(void)
 
 #endif
 
