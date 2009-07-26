ba6e7b8d6ffa037f5801c7c60780ec11ec628db7
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-26 00:37:43 +0000

    updated config

--- microcontroller/src-atmel/rfm12/rfm12usb/firmware/rfm12_config.h
+++ microcontroller/src-atmel/rfm12/rfm12usb/firmware/rfm12_config.h
@@ -95,15 +95,28 @@
  */
 #define RFM12_UART_DEBUG 0
 
-/**** INTERRUPT VECTOR
+/************************
+ * INTERRUPT VECTOR
  * set the name for the interrupt vector here
  */
+ 
+//the interrupt vector
 #define RFM12_INT_VECT (INT1_vect)
-#define RFM12_INT_BIT (INT1)
-//interrupt mask register
+
+//the interrupt mask register
 #define RFM12_INT_MSK (GICR)
-//setup Interrupt for falling egde trigger
-#define RFM12_INT_SETUP() (MCUCR |= (1<<ISC11))
+
+//the interrupt bit in the mask register
+#define RFM12_INT_BIT (INT1)
+
+//the interrupt flag register
+#define RFM12_INT_FLAG (GIFR)
+
+//the interrupt bit in the flag register
+#define RFM12_FLAG_BIT (INTF1)
+
+//setup the interrupt to trigger on negative edge
+#define RFM12_INT_SETUP()   (MCUCR |= (1<<ISC11))
 
 #define RFM12_CONFIG_H
 #endif
