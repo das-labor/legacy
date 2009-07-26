4d1daa80dba0094f83abc74a1fe2903e7f6850c2
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-26 00:38:31 +0000

    updated config

--- microcontroller/src-atmel/rfm12/nakkaloader/firmware/rfm12_config.h
+++ microcontroller/src-atmel/rfm12/nakkaloader/firmware/rfm12_config.h
@@ -113,7 +113,7 @@
 #define RFM12_FLAG_BIT (INTF1)
 
 //setup the interrupt to trigger on negative edge
-#define RFM12_INT_SETUP()   MCUCR |= (1<<ISC01)
+#define RFM12_INT_SETUP()   MCUCR |= (1<<ISC11)
 
 
 /************************
