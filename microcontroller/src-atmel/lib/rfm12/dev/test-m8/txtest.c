c33c8cd737effcc87f9be950acd9f216736f3861
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-29 12:40:44 +0000

    changed test to wakeup mode test

--- microcontroller/src-atmel/lib/rfm12/dev/test-m8/txtest.c
+++ microcontroller/src-atmel/lib/rfm12/dev/test-m8/txtest.c
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <string.h>
+#include <avr/sleep.h>
 
 #include "rfm12.h"
 #include "uart.h"
@@ -11,20 +12,20 @@ int main ( void )
 {
 	uint8_t i = 0;
 	uint16_t ticker = 0;
-
 	
 	uart_init();
+	
 	DDRD |= (_BV(PD7));
 	PORTD |= _BV(PD7);
 
 	_delay_ms(250);
 	_delay_ms(250);
 	_delay_ms(250);
-	
 	rfm12_init();
-	
+	rfm12_set_wakeup_timer(10);	
+			
 	sei();
-
+	
 	uart_putstr ("\r\n");
 	uart_putstr ("Hello\r\n");
 	
@@ -41,12 +42,14 @@ int main ( void )
 		{
 			ticker = 0;
 			
-			uart_putstr ("tx ");			
+			//uart_putstr ("tx ");
 			rfm12_tx (1, 0x23, &i);
 			
 			i++;
 		}
 
 		rfm12_tick();
+		
+		sleep_mode();
 	}
 }
