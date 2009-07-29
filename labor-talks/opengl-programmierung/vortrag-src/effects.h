538313e899e2c3b11aa36bc60bbdd53aeb7c1f48
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-29 19:10:01 +0000

    license fix

--- labor-talks/opengl-programmierung/vortrag-src/effects.h
+++ labor-talks/opengl-programmierung/vortrag-src/effects.h
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2009 by Soeren Heisrath <forename@surename dot org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to:
+ *  Free Software Foundation
+ *  51 Franklin Street, Fifth Floor
+ *  Boston, MA  02111-1301  USA
+ */
+
 #include <stdlib.h>
 #include <GL/gl.h>
 #pragma once
