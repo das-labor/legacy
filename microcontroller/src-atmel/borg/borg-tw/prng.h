ed4a2ebe42a389cc088eaf458c664e8907c8501c
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-08-02 15:11:25 +0000

    cleanup

--- microcontroller/src-atmel/borg/borg-tw/prng.h
+++ microcontroller/src-atmel/borg/borg-tw/prng.h
@@ -1,18 +1,1 @@
-/**
- * \author	Daniel Otte
- * \date	2007-02-10
- * \par \License GPL
- * \brief unsecure low weigth pseudo random number generator
- * \description implementation of a simple LFSR
- * 
- */
-
-#ifndef PRNG_H_
-#define PRNG_H_
-
-#include <stdint.h>
-
-uint8_t random8(void);
-void srandom(uint32_t seed);
-
-#endif /* PRNG_H_*/
+#include "lib/prng_simp.h"
