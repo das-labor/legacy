d2113d83fdc1bee8c9a1810f5d814e40cba840b5
madex <madex@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-08-19 21:49:04 +0000

    string mode now darker. added GPLV3. guitar half step down insted of the useless Custom. Note it runs only on old compilers.

--- microcontroller/src-atmel/playground/stimmmopped/main.c
+++ microcontroller/src-atmel/playground/stimmmopped/main.c
@@ -1,3 +1,24 @@
+/*
+    stimmmopped - optical guitar tuner
+    main.c
+    Copyright (C) 2007  Martin Ongsiek (madex@das-labor.org)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Version 0.20 */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
@@ -91,50 +112,54 @@ typedef enum {
 	TONE_A,
 	TONE_A_SHARP,
 	TONE_B,
+	TONE_B_SHARP,
+	EMPTY_SCREEN,
 	ENDMARK 
 } chars_t;
 
 chars_t display_tone = TONE_C;
 chars_t display_octave = NUM_0;
 
-display_t display[] = {
-{1, 1, 1, 1, 1, 1, 0, 0},  // NUM_0,
-{0, 1, 1, 0, 0, 0, 0, 0},  // NUM_1,
-{1, 1, 0, 1, 1, 0, 1, 0},  // NUM_2,
-{1, 1, 1, 1, 0, 0, 1, 0},  // NUM_3,
-{0, 1, 1, 0, 0, 1, 1, 0},  // NUM_4,
-{1, 0, 1, 1, 0, 1, 1, 0},  // NUM_5,
-{1, 0, 1, 1, 1, 1, 1, 0},  // NUM_6,
-{1, 1, 1, 0, 0, 0, 0, 0},  // NUM_7,
-{1, 1, 1, 1, 1, 1, 1, 0},  // NUM_8,
-{1, 1, 1, 1, 0, 1, 1, 0},  // NUM_9,
-{1, 0, 0, 1, 1, 1, 0, 0},  // TONE_C,
-{1, 0, 0, 1, 1, 1, 0, 1},  // TONE_C_SHARP,
-{0, 1, 1, 1, 1, 0, 1, 0},  // TONE_D,
-{0, 1, 1, 1, 1, 0, 1, 1},  // TONE_D_SHARP,
-{1, 0, 0, 1, 1, 1, 1, 0},  // TONE_E,
-{1, 0, 0, 0, 1, 1, 1, 0},  // TONE_F,
-{1, 0, 0, 0, 1, 1, 1, 1},  // TONE_F_SHARP,
-{1, 0, 1, 1, 1, 1, 0, 0},  // TONE_G,
-{1, 0, 1, 1, 1, 1, 0, 1},  // TONE_G_SHARP,
-{1, 1, 1, 0, 1, 1, 1, 0},  // TONE_A,
-{1, 1, 1, 0, 1, 1, 1, 1},  // TONE_A_SHARP,
-{0, 0, 1, 1, 1, 1, 1, 0},  // TONE_B,
+display_t display[ENDMARK] = {
+	{1, 1, 1, 1, 1, 1, 0, 0},  // NUM_0,
+	{0, 1, 1, 0, 0, 0, 0, 0},  // NUM_1,
+	{1, 1, 0, 1, 1, 0, 1, 0},  // NUM_2,
+	{1, 1, 1, 1, 0, 0, 1, 0},  // NUM_3,
+	{0, 1, 1, 0, 0, 1, 1, 0},  // NUM_4,
+	{1, 0, 1, 1, 0, 1, 1, 0},  // NUM_5,
+	{1, 0, 1, 1, 1, 1, 1, 0},  // NUM_6,
+	{1, 1, 1, 0, 0, 0, 0, 0},  // NUM_7,
+	{1, 1, 1, 1, 1, 1, 1, 0},  // NUM_8,
+	{1, 1, 1, 1, 0, 1, 1, 0},  // NUM_9,
+	{1, 0, 0, 1, 1, 1, 0, 0},  // TONE_C,
+	{1, 0, 0, 1, 1, 1, 0, 1},  // TONE_C_SHARP,
+	{0, 1, 1, 1, 1, 0, 1, 0},  // TONE_D,
+	{0, 1, 1, 1, 1, 0, 1, 1},  // TONE_D_SHARP,
+	{1, 0, 0, 1, 1, 1, 1, 0},  // TONE_E,
+	{1, 0, 0, 0, 1, 1, 1, 0},  // TONE_F,
+	{1, 0, 0, 0, 1, 1, 1, 1},  // TONE_F_SHARP,
+	{1, 0, 1, 1, 1, 1, 0, 0},  // TONE_G,
+	{1, 0, 1, 1, 1, 1, 0, 1},  // TONE_G_SHARP,
+	{1, 1, 1, 0, 1, 1, 1, 0},  // TONE_A,
+	{1, 1, 1, 0, 1, 1, 1, 1},  // TONE_A_SHARP,
+	{0, 0, 1, 1, 1, 1, 1, 0},  // TONE_B,
+	{0, 0, 1, 1, 1, 1, 1, 1},  // TONE_B_SHARP,
+	{0, 0, 0, 0, 0, 0, 0, 0},  // EMPTY_SCREEN,
 };
 
 unsigned long midi_freq_tab[108] = {
-	489249, 461789, 435871, 411408, 388317, 366522, 345951, 326534, 308207,
-	290909, 274582, 259171, 244624, 230895, 217936, 205704, 194159, 183261,
+    489249, 461789, 435871, 411408, 388317, 366522, 345951, 326534, 308207,
+    290909, 274582, 259171, 244624, 230895, 217936, 205704, 194159, 183261,
     172976, 163267, 154104, 145455, 137291, 129585, 122312, 115447, 108968,
-	102852,  97079,  91631,  86488,  81634,  77052,  72727,  68645,  64793,
-	 61156,  57724,  54484,  51426,  48540,  45815,  43244,  40817,  38526,
+    102852,  97079,  91631,  86488,  81634,  77052,  72727,  68645,  64793,
+     61156,  57724,  54484,  51426,  48540,  45815,  43244,  40817,  38526,
      36364,  34323,  32396,  30578,  28862,  27242,  25713,  24270,  22908,
-	 21622,  20408,  19263,  18182,  17161,  16198,  15289,  14431,  13621,
+     21622,  20408,  19263,  18182,  17161,  16198,  15289,  14431,  13621,
      12856,  12135,  11454,  10811,  10204,   9631,   9091,   8581,   8099,
       7645,   7215,   6810,   6428,   6067,   5727,   5405,   5102,   4816,
-	  4545,   4290,   4050,   3822,   3608,   3405,   3214,   3034,   2863,
-	  2703,   2551,   2408,   2273,   2145,   2025,   1911,   1804,   1703,
-	  1607,   1517,   1432,   1351,   1276,   1204,   1136,   1073,   1012
+      4545,   4290,   4050,   3822,   3608,   3405,   3214,   3034,   2863, 
+      2703,   2551,   2408,   2273,   2145,   2025,   1911,   1804,   1703,
+      1607,   1517,   1432,   1351,   1276,   1204,   1136,   1073,   1012
 };
 
 
@@ -176,7 +201,7 @@ ISR(INT1_vect) {
 }
 
 void calcFreqAndNotename(unsigned char note) {
-	unsigned long freq;
+	unsigned long freq = 1012;
 	//freq = (float) F_CPU/(440.*pow(WURZEL12_2, note-69.));
 	//freq = (float) F_CPU/(440.f*pow(2.f, (note-69.f)/12.f));
 	if (note >= 12 && note < 120)
@@ -185,7 +210,7 @@ void calcFreqAndNotename(unsigned char note) {
 	timer1_corrcet = freq % 10;
 	displayState = 0;
 	displayTimer = TIME_NOTE;
-	display_tone   = (note % 12) + TONE_C;
+	display_tone   =  (note % 12) + TONE_C;
 	display_octave = ((note - 12) / 12) + NUM_0; 
 }
 
@@ -209,6 +234,7 @@ void sleepAndWakeUpControl() {
 
 static unsigned char timer_pwr_dwn;
 static unsigned char taste1, taste2;
+
 void tastenEntprellen() {
 	unsigned char input;
 	input = (PIN_TASTEN & (1 << TASTE1)) == 0;
@@ -259,7 +285,6 @@ void tastenEntprellen() {
 		sleepAndWakeUpControl();
 		timeInaktive = TIME_INAKIVE;
 	}
-	
 }
 
 typedef enum {
@@ -269,19 +294,19 @@ typedef enum {
 
 typedef enum {
 	MR_BASS,
-	MR_BASS_DSHARP,
+	MR_BASS_HALF_DOWN,
 	MR_GUITAR,
-	MR_CUSTOM
+	MR_GUITAR_HALF_DOWN
 } menu_root_t;
 
 void setDisplay(chars_t ch) { 
 	display_t dis = display[ch];
 	PORT_SEG_GFABCP = (!dis.seg_a << SEG_A) | 
 	                  (!dis.seg_b << SEG_B) |
-					  (!dis.seg_c << SEG_C) | 
-					  (!dis.seg_f << SEG_F) |
-					  (!dis.seg_g << SEG_G) | 
-					  (!dis.seg_p << SEG_P);
+	                  (!dis.seg_c << SEG_C) |
+	                  (!dis.seg_f << SEG_F) |
+	                  (!dis.seg_g << SEG_G) |
+	                  (!dis.seg_p << SEG_P);
 	if (!dis.seg_e)
 		PORT_SEC_xxxxDE |=  (1 << SEG_E);
 	else
@@ -294,8 +319,11 @@ void setDisplay(chars_t ch) {
 
 void showCurrentNote() {
 	switch (displayState) {
-		case 0:
-			setDisplay(display_tone);
+		case 0:  // Saitenmodus dunkler darstellen.
+			if (displayTimer & 1)
+				setDisplay(display_tone);
+			else
+				setDisplay(EMPTY_SCREEN);	
 			if (displayTimer) {
  				displayTimer--;
 			} else { 
@@ -305,7 +333,10 @@ void showCurrentNote() {
 			break;
 			
 		case 1:
-			setDisplay(display_octave);
+			if (displayTimer & 1)
+				setDisplay(display_octave);
+			else
+				setDisplay(EMPTY_SCREEN);
 			if (displayTimer) {
  				displayTimer--;
 			} else { 
@@ -316,26 +347,21 @@ void showCurrentNote() {
 	}
 }
 
-
 // 0 markiert das ende
 unsigned char midi_notes_bass[] = {
 	23, 26, 28, 33, 38, 43, 0
 };
 
-unsigned char midi_notes_bass_dsharp[] = {
+unsigned char midi_notes_bass_half_down[] = {
 	22, 27, 32, 37, 42, 0
 };
-
 
 unsigned char midi_notes_guitar[] = {
 	38, 40, 45, 50, 55, 59, 64, 0
 };
 
-unsigned char midi_notes_custom[] = {
-	2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
-	22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-	38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
-	54, 55, 56, 57, 58, 59, 60, 61, 0
+unsigned char midi_notes_guitar_half_down[] = {
+	37, 39, 44, 49, 54, 58, 63, 0
 };
 
 menu_level_t  menu_level = ML_MAIN;
@@ -353,12 +379,12 @@ void stateMachineMenu() {
 					setDisplay(TONE_B);
 					note_ptr = midi_notes_bass;
 					if (taste1_sf)
-						menu_root = MR_BASS_DSHARP;
+						menu_root = MR_BASS_HALF_DOWN;
 					break;
 					
-				case MR_BASS_DSHARP: // Bass D-Sharp
-					setDisplay(TONE_D_SHARP);
-					note_ptr = midi_notes_bass_dsharp;
+				case MR_BASS_HALF_DOWN: // Bass 1/2 step down
+					setDisplay(TONE_B_SHARP);
+					note_ptr = midi_notes_bass_half_down;
 					if (taste1_sf)
 						menu_root = MR_GUITAR;
 					break;
@@ -367,12 +393,12 @@ void stateMachineMenu() {
 					setDisplay(TONE_G);
 					note_ptr = midi_notes_guitar;
 					if (taste1_sf)
-						menu_root = MR_CUSTOM;
+						menu_root = MR_GUITAR_HALF_DOWN;
 					break;
 				
-				case MR_CUSTOM: // Custom
-					setDisplay(TONE_C);
-					note_ptr = midi_notes_custom;
+				case MR_GUITAR_HALF_DOWN: // Guitar 1/2 step down
+					setDisplay(TONE_G_SHARP);
+					note_ptr = midi_notes_guitar_half_down;
 					if (taste1_sf) 
 						menu_root = MR_BASS;
 					break;
