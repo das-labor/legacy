6229722e00a2e373e42657ca219d64ade0eff1b1
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-09-21 16:23:22 +0000

    first rewrite

--- microcontroller/src-atmel/playground/PowerCommander/branche/rewrite-2009-09-20/avr_controll/switch.c
+++ microcontroller/src-atmel/playground/PowerCommander/branche/rewrite-2009-09-20/avr_controll/switch.c
@@ -3,120 +3,45 @@
 
 //#include <avr/io.h>
 
-
-
-void switch_on(struct t_status * data){
-	switch ((*data).data){
-	case switch00:
-		PORTC |= _BV(PC3);
-		break;
-	case switch01:
-		PORTC |= _BV(PC2);
-		break;
-	case switch02:
-		PORTC |= _BV(PC1);
-		break;
-	case switch03:
-		PORTC |= _BV(PC0);
-		break;
-	case switch04:
-		PORTB |= _BV(PB5);
-		break;
-	case switch05:
-			PORTB |= _BV(PB4);
-		break;
-	case switch06:
-		PORTB |= _BV(PB0);
-		break;
-	case switch07:
-		PORTD |= _BV(PD7);
-		break;
-	case optokopp00:
-		PORTD |= _BV(PD0);
-		break;
-	case optokopp01:
-		PORTD |= _BV(PD1);
-		break;
-	default:
-		break;
-	}
-	(*data).write_data = 0;
+inline void switch_fkt(struct t_switch_parameter *sw_para, struct t_busdata *data)	{
+	switch( (*data).in_data[1]) 
+		{
+		case A_SW_OFF:
+			{
+				(*sw_para).write_port &= ~_BV((*sw_para).pin);
+				(*data).write_data = HASNDATA;
+			}
+			break;
+		case A_SW_ON:
+			{
+				(*sw_para).write_port |= _BV((*sw_para).pin);
+				(*data).write_data = HASNDATA;
+			}
+			break;
+		case A_SW_STATUS:
+			{
+				(*data).out_data=((*sw_para).read_port >> (*sw_para).pin) & 1;
+				(*data).write_data = HASDATA;
+			}
+			break;
+		default:
+			break;
+		}
 }
 
-void switch_off(struct t_status * data){
-	switch ((*data).data){
-	case switch00:
-		PORTC &= ~_BV(PC3);
-		break;
-	case switch01:
-		PORTC &= ~_BV(PC2);
-		break;
-	case switch02:
-		PORTC &= ~_BV(PC1);
-		break;
-	case switch03:
-		PORTC &= ~_BV(PC0);
-		break;
-	case switch04:
-		PORTB &= ~_BV(PB5);
-		break;
-	case switch05:
-		PORTB &= ~_BV(PB4);
-		break;
-	case switch06:
-		PORTB &= ~_BV(PB0);
-		break;
-	case switch07:
-		PORTD &= ~_BV(PD7);
-		break;
-	case optokopp00:
-		PORTD &= ~_BV(PD0);
-		break;
-	case optokopp01:
-		PORTD &= ~_BV(PD1);
-		break;
-	default:
-		break;
-	}
-	(*data).write_data = 0;
-}
-
-void switch_status(struct t_status * data){
-	switch ((*data).data){
-	case switch00:
-		(*data).data=(PORTC >> PC3) & 1;
-		break;
-	case switch01:
-		(*data).data=(PORTC >> PC2) & 1;
-		break;
-	case switch02:
-		(*data).data=(PORTC >> PC1) & 1;
-		break;
-	case switch03:
-		(*data).data=(PORTC >> PC0) & 1;
-		break;
-	case switch04:
-		(*data).data=(PORTB >> PB5) & 1;
-		break;
-	case switch05:
-		(*data).data=(PORTB >> PB4) & 1;
-		break;
-	case switch06:
-		(*data).data=(PORTB >> PB0) & 1;
-		break;
-	case switch07:
-		(*data).data=(PORTD >> PD7) & 1;
-		break;
-	case optokopp00:
-		(*data).data=(PORTD >> PD0) & 1;
-		break;
-	case optokopp01:
-		(*data).data=(PORTD >> PD1) & 1;
-		break;
-	default:
-		break;
-	}
-	(*data).write_data = 1;
-}
 		
+// readonly switch
 
+inline void switch_ro_fkt(struct t_switch_parameter_ro *sw_para, struct t_busdata *data)	{
+	switch( (*data).in_data[1]) 
+		{
+		case A_SW_STATUS:
+			{
+				(*data).out_data=((*sw_para).read_port >> (*sw_para).pin) & 1;
+				(*data).write_data = HASDATA;
+			}
+			break;
+		default:
+			break;
+		}
+}
