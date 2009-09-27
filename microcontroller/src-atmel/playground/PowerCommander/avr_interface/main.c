f235e7a6f28820635c3a27a680ddcbfe52289442
dr4go <dr4go@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-09-27 19:59:26 +0000

    build on 644p

--- microcontroller/src-atmel/playground/PowerCommander/avr_interface/main.c
+++ microcontroller/src-atmel/playground/PowerCommander/avr_interface/main.c
@@ -33,10 +33,12 @@ int main(void)
 
     MCUCR = 1<<SE;      	// Enable "sleep" mode (low power when idle)
     TCNT0 = TCNT0_INIT;		// Load overflow counter of timer0
-    TCCR0 = TMC8_CK256;		// Set Timer0 to CPUCLK/256
-    TIMSK = 1<<TOIE0;		// Enable interrupt flag
+    //TCCR0 = TMC8_CK256;		// Set Timer0 to CPUCLK/256
+    TCCR0B = TMC8_CK256;		// set timer0 atmega644p
+//    TIMSK = 1<<TOIE0;		// Enable interrupt flag
+    TIMSK0 = _BV(TOIE0);		// Enable interrupt flag atmega644p
 
-DDRA |= _BV(PA7) | _BV(PA6);
+	DDRA |= _BV(PA7) | _BV(PA6); // Debug LED
 
 //	_delay_ms(1000);
 
