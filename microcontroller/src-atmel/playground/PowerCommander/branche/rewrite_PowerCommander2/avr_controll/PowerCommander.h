cb4561940a175e85380efe0166bf6ce81a41a08d
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-09-27 19:26:32 +0000

    controller fuer powercommander

--- microcontroller/src-atmel/playground/PowerCommander/branche/rewrite_PowerCommander2/avr_controll/PowerCommander.h
+++ microcontroller/src-atmel/playground/PowerCommander/branche/rewrite_PowerCommander2/avr_controll/PowerCommander.h
@@ -5,253 +5,151 @@
 #include <inttypes.h>
 #include <avr/io.h>
 
-#define MACHDUNKEL 0
-#define MACHHELL 1
-
-// pwm ... daher hier invers
-#define MAXHELL 20
-#define MAXDUNKEL 250
-#define BRIGHT_NO_CHANGE 255
-
+#define I2CADRESSE 15 // I2C Adresse
 
 #define HASDATA (0x01)
 #define HASNDATA (0x00)
 
 // der schalter generiert $Anzahl von events pro sekunde
 
-
 // immer wieder der gleiche scheiss!
 #define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
 
-#define I2CADRESSE 15 // I2C Adresse
-
-
-/*
-  userinterface Taster
- */
-#define U_VORTRAG (0x00)
-#define U_LOUNGE (0x01)
-#define U_COUNT (0x02)
 /* 
    Actionen als defines
 */
-#define A_SW_OFF      (0x00)
-#define A_SW_ON       (0x01)
-#define A_SW_STATUS   (0x02)
-#define A_SW_COUNT    (0x03)
-#define A_SW_NOCHANGE (0x04)
+#define F_SW_OFF      (0x00)
+#define F_SW_ON       (0x01)
+#define F_SW_STATUS   (0x02)
 
-// nur intern fuer queue notwendig
-// daher nicht in der count
-#define A_SW_NOCHANGE (0x04) 
+#define F_PWM_SET     (0x00)
+#define F_PWM_GET     (0x01)
 
-#define A_BR_SET     (0x00)
-#define A_BR_GET     (0x01)
-#define A_BR_COUNT   (0x02)
+
+#define F_NDEF (0xFF)
+
+#define D_NDEF (0x00)
 
 /*
-  Objekte als defines
+	funktionsklassen - switch oder PWMs
 */
-#define O_SW_FIRST (0x00)
-// kuechenlicht
+#define C_SW  (0x00)
+#define C_PWM (0x01)
+#define C_NDEF (0xFF)
+
+/*
+  schalter Objekte als defines
+*/
 #define O_SW00 (0x00)
-// beamer
 #define O_SW01 (0x01)
-// licht vortrag
 #define O_SW02 (0x02)
-// licht lounge
 #define O_SW03 (0x03)
-// tischsteckdose
 #define O_SW04 (0x04)
-// klo
 #define O_SW05 (0x05)
-// hauptschuetz
 #define O_SW06 (0x06)
-// frei
 #define O_SW07 (0x07)
-// licht+serverschrank-schuetz
 #define O_SW08 (0x08)
-// alle steckdosen (herd) schuetz
 #define O_SW09 (0x09)
+#define O_SW10 (0x0A)
+#define O_SW11 (0x0B)
+#define O_SW12 (0x0C)
 #define O_OPTO00 (O_SW08)
 #define O_OPTO01 (O_SW09)
-#define O_SW_LAST (0x09)
+#define O_OPTO02 (O_SW0A)
+#define O_OPTO03 (O_SW0B)
+#define O_OPTO04 (O_SW0C)
+#define O_OPTO05 (O_SW00)
+#define O_NDEF (0xFF)
 
-#define O_LAMP_FIRST   (0x0A)
-#define O_LAMP_TAFEL   (0x0A)
-#define O_LAMP_BEAMER  (0x0B)
-#define O_LAMP_SCHRANK (0x0C)
-#define O_LAMP_FLIPPER (0x0D)
-#define O_LAMP_FREE    (0x0E)
-#define O_LAMP_LOUNGE  (0x0F)
-#define O_LAMP_LAST    (0x0F)
+// switch lampe
+#define SWL_TAFEL       (O_OPTO02)
+#define SWL_BEAMER      (O_OPTO03)
+#define SWL_SCHRANK     (O_OPTO04)
+#define SWL_FLIPPER     (O_OPTO05)
+#define SWL_LOUNGE      (O_SW03)
+#define SWL_KLO         (O_SW05)
+#define SWL_KUECHE      (O_SW00)
 
-#define O_TASTER_FIRST (0x10)
-#define O_TASTER_LOUNGE (0x10)
-#define O_TASTER_VORTRAG (0x11)
-#define O_TASTER_MAIN (0x12)
-#define O_TASTER_LAST (0x12)
-
-#define O_SW_COUNT (0x0A)
-#define O_LAMP_COUNT (0x06)
-#define O_TASTER_COUNT (0x03)
-#define O_COUNT   (0x13)
-
-// ist 0.005sekunden * DOUBLECLICK
-#define T_CLICKDELAY 50
-#define T_CLICKTHRESHOLD 5
-
-// bei Timer/counter = 64*256 auf 16MHz
-// etwa 0.000512 sekunden
-// gibt es hier leider nicht ... ist aber auch nicht so das problem
-// #define TICKS_A0 (0x0020)  
-// etwa 0.002048
-#define TICKS_A1 (0x0002)
-// etwa 0.005120
-#define TICKS_A2 (0x0005)
-// etwa 0.01024
-#define TICKS_A3 (0x000A)
-// etwa 0.04096
-#define TICKS_A4 (0x0028)
-// etwa 0.125 sekunden
-#define TICKS_A5 (0x007A)
-// etwa 0.25 sekunden
-#define TICKS_A6 (0x00F4)
-// etwa 0.5 sekunden
-#define TICKS_A7 (0x01E8)
-// etwa 1.0 sekunden
-#define TICKS_A8 (0x03D1)
-// etwa 5.0 sekunden
-#define TICKS_A9 (0x1313)
+// switch andere
+// Hauptschuetz
+#define SWA_HS          (O_SW06)
+// beamer anaus
+#define SWA_BEAMER      (O_SW01)
+// freier schalter
+#define SWA_FREE        (O_SW07)
+// licht+serverschrank-schuetz
+#define SWA_LSSS        (O_SW08)
+// alle steckdosen (herd) schuetz
+#define SWA_HERD        (O_SW09)
+// tischsteckdose
+#define SWA_STECKDOSEN  (O_SW04)
 
 
-#define RINGBUFFERSIZE (3)
-#define QUEUE_SIZE (35)
+// PWMs als Objekte
+#define O_PWM00 (0x00)
+#define O_PWM01 (0x01)
+#define O_PWM02 (0x02)
+#define O_PWM03 (0x03)
+#define O_PWM04 (0x04)
+#define O_PWM05 (0x05)
+#define O_PWM_NDEF (0xFF)
 
-#define QUEUE_FREE (0x00)
-#define QUEUE_A0   (0x01)
-#define QUEUE_A1   (0x02)
-#define QUEUE_A2   (0x03)
-#define QUEUE_A3   (0x04)
-#define QUEUE_A4   (0x05)
-#define QUEUE_A5   (0x06)
-#define QUEUE_A6   (0x07)
-#define QUEUE_A7   (0x08)
-#define QUEUE_A8   (0x09)
-#define QUEUE_A9   (0x0A)
+#define PWM_TAFEL    (O_PWM00)
+#define PWM_BEAMER   (O_PWM01)
+#define PWM_SCHRANK  (O_PWM02)
+#define PWM_FLIPPER  (O_PWM03)
+#define PWM_FREE     (O_PWM04)
+#define PWM_LOUNGE   (O_PWM05)
 
-// struktur die daten enthaelt und das FLAG wenn daten
-// auf den I2C geschrieben werden sollen
-struct t_busdata {
-	uint8_t in_data[3];
+// ports - addressen
+#define O_SW00_PORT (&(PORTC))
+#define O_SW01_PORT (&(PORTC))
+#define O_SW02_PORT (&(PORTC))
+#define O_SW03_PORT (&(PORTC))
+#define O_SW04_PORT (&(PORTB))
+#define O_SW05_PORT (&(PORTB))
+#define O_SW06_PORT (&(PORTB))
+#define O_SW07_PORT (&(PORTD))
+#define O_SW08_PORT (&(PORTD))
+#define O_SW09_PORT (&(PORTD))
+#define O_SW0A_PORT (&(PORTD))
+#define O_SW0B_PORT (&(PORTB))
+#define O_SW0C_PORT (&(PORTD))
+
+#define O_PWM00_PORT (&(OCR0A))
+#define O_PWM01_PORT (&(OCR0B))
+#define O_PWM02_PORT (&(OCR1AL))
+#define O_PWM03_PORT (&(OCR1BL))
+#define O_PWM04_PORT (&(OCR2A))
+#define O_PWM05_PORT (&(OCR2B))
+
+// PINs - uin8_t
+#define O_SW00_PIN (PC3)
+#define O_SW01_PIN (PC2)
+#define O_SW02_PIN (PC1)
+#define O_SW03_PIN (PC0)
+#define O_SW04_PIN (PB5)
+#define O_SW05_PIN (PB4)
+#define O_SW06_PIN (PB0)
+#define O_SW07_PIN (PD7)
+#define O_SW08_PIN (PD0)
+#define O_SW09_PIN (PD1)
+#define O_SW0A_PIN (PD2)
+#define O_SW0B_PIN (PB7)
+#define O_SW0C_PIN (PD4)
+
+// Anordnen von Schaltern in einer Matrix
+// so, dass wird die mittels i++ durchgehen koennen
+
+
+struct t_i2cproto {
+	uint8_t class;
+	uint8_t object;
+	uint8_t fkt;
+	uint8_t has_in_data;
+	uint8_t has_out_data;
+	uint8_t in_data;
 	uint8_t out_data;
-	uint8_t write_data;
-};
-
-static struct t_switch_parameter{
-	volatile uint8_t *write_port;
-	volatile uint8_t *read_port;
-	uint8_t pin;
-}swp_matrix[]={
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 },
-	{ 0 , 0 , 0 } 
-};
-
-volatile uint8_t* lp_matrix[] = { 0, 0, 0, 0, 0, 0 };
-
-static struct t_ringbuffer{
-	uint16_t tastercounter; // mitzaehlen
-	uint16_t tastercounter_last; // was war der wert, als wir ihn zuletzt getestet haben
-	uint8_t clickpos; 
-	uint16_t clickstate[RINGBUFFERSIZE]; 
-	uint16_t clickstate_free[RINGBUFFERSIZE];
-}rb[]={
-	{ 0, 0, 0, { 0,0,0}, {0,0,0}},
-	{ 0, 0, 0, { 0,0,0}, {0,0,0}}
-};
-
-// den status nur fuer den Vortrag
-struct t_state_vortrag {
-  uint8_t bright_tafel;
-  uint8_t bright_beamer;
-  uint8_t bright_schraenke;
-  uint8_t bright_flipper;
-  uint8_t dimDirection;
-  uint8_t onoff;
-};
-
-// den status nur fuer die lounge
-struct t_state_lounge {
-  uint8_t bright_lounge;
-  uint8_t bright_free;
-  uint8_t dimDirection;
-  uint8_t onoff;
-};
-
-struct t_autoqueue_pos {
-  uint8_t queuefill;
-  uint8_t queuepos;
-};
-
-// unsere Queue lang genug um .... what ever!
-// XXX dies muss angepasst werden wenn sich die Anzahl der Lampen/Switche aendert
-static struct t_queue_object {
-  uint8_t _state;
-	uint8_t bright[O_LAMP_COUNT];
-	uint8_t switch_relai[O_SW_COUNT];
-}queue[]={
-	{ QUEUE_FREE, 
-		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-		{A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE}},
-	{ QUEUE_FREE, 
-		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-		{A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE}},
-	{ QUEUE_FREE, 
-		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-		{A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE}},
-	{ QUEUE_FREE, 
-		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-		{A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE}},
-	{ QUEUE_FREE, 
-		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-		{A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE}},
-	{ QUEUE_FREE, 
-		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-		{A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE}},
-	{ QUEUE_FREE, 
-		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-		{A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE,A_SW_NOCHANGE}}
-};
-
-
-
-/*   { PORTC, PORTC, PC3 }, // relai 00 - kuechenlicht */
-/*   { PORTC, PORTC, PC2 }, // 01  - beamer */
-/*   { PORTC, PORTC, PC1 }, // 02  - licht vortrag */
-/*   { PORTC, PORTC, PC0 }, // 03  - licht lounge */
-/*   { PORTB, PORTB, PB5 }, // 04 - tischsteckdose*/
-/*   { PORTB, PORTB, PB4 }, // 05 - klo */
-/*   { PORTB, PORTB, PB0 }, // 06 - hauptschuetze */
-/*   { PORTD, PORTD, PD7 }, // 07 - frei */
-/*   { PORTD, PORTD, PD0 }, // opto 00 - licht+serverschrank-schuetz*/
-/*   { PORTD, PORTD, PD1 }  // opto 01 - alle steckdosen (herd) schuetz */
-
-
-static struct t_switch_parameter_ro {
-  volatile uint8_t *read_port;
-  uint8_t pin;
-}tp_matrix[]={ 
-	{ 0, 0},
-	{ 0, 0},
-	{ 0, 0}
 };
 
 
--- microcontroller/src-atmel/playground/PowerCommander/branche/rewrite_PowerCommander2/avr_controll/main.c
+++ microcontroller/src-atmel/playground/PowerCommander/branche/rewrite_PowerCommander2/avr_controll/main.c
@@ -1,276 +1,34 @@
 /* -*- Mode: C; tab-width: 2 -*- */
 /*
  * PowerCommander - Ausführungs Kontroller
+
+ diese Version nimmt nur i2c nachrichten an und gibt sie direkt
+ aus. Es wird nur dieser input verarbeitet, keine taster oder aehnliches
+ ferner gibt es ein paar neue schalter, die das licht im vortragsraum
+ zeilenweise (flipper, schraenke, beamer,tafel) ein und ausschalten
+ lassen
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <inttypes.h>
 
 #include "twi_slave/twi_slave.h"
 
-
 #include "PowerCommander.h"
-#include "pc_init.i"
-#include "switch.i"
-#include "bright.i"
 
-/*
-TODO
-irgendwo in der queue ist ein bug. er setzt beamer und schraenke der lampen auf komische werte beim start
-ferner werden keine tastercommands angenommen
-*/
+#include "pc_init.h"
 
-struct t_busdata busdata = { { 0, 0, 0}, 0,HASNDATA};
-
-// aktuller zustand
-struct t_state_vortrag vortrag_cur = { MAXHELL , MAXHELL , MAXHELL , MAXHELL , MACHDUNKEL, A_SW_ON }; // init sonst working
-struct t_state_lounge lounge_cur = { MAXHELL , MAXHELL , MACHDUNKEL, A_SW_ON }; // init sonst working
-
-// volle belaeuchtung - dimmrichtung dunkler
-struct t_state_vortrag vortrag_default = { MAXHELL , MAXHELL , MAXHELL , MAXHELL, MACHDUNKEL,A_SW_ON };
-struct t_state_lounge lounge_default = { MAXHELL , MAXHELL , MACHDUNKEL, A_SW_ON};
-
-// tafel hell rest dunkel
-struct t_state_vortrag vortrag_vortrag1 = { MAXHELL , MAXDUNKEL , MAXDUNKEL , MAXDUNKEL , MACHHELL,A_SW_ON };
-// flipper hell rest dunkel
-struct t_state_vortrag vortrag_vortrag2 = { MAXDUNKEL , MAXDUNKEL , MAXDUNKEL , MAXHELL , MACHHELL,A_SW_ON };
-
-struct t_state_vortrag freak_cur = { MAXHELL , MAXHELL , MAXHELL , MAXHELL , MACHDUNKEL,A_SW_ON }; // init sonst working
-
-
-struct t_queue_object poweroff = { QUEUE_A1, 
-																	 { BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-																	 { A_SW_OFF, // kuechenlicht
-																		 A_SW_OFF, // beamer
-																		 A_SW_OFF, // licht vortrag
-																		 A_SW_OFF, // licht lounge
-																		 A_SW_OFF, // tischsteckdose
-																		 A_SW_OFF, // klo
-																		 A_SW_OFF, // hauptschuetz
-																		 A_SW_OFF, // frei
-																		 A_SW_OFF, // licht+serverschrank-schuetz
-																		 A_SW_OFF  // alle steckdosen (herd) schuetz
-																	 }
-};
-
-struct t_queue_object sleepmode = { QUEUE_A1, 
-																		{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-																		{ A_SW_NOCHANGE, // kuechenlicht
-																			A_SW_OFF,      // beamer
-																			A_SW_NOCHANGE, // licht vortrag
-																			A_SW_NOCHANGE, // licht lounge
-																			A_SW_NOCHANGE, // tischsteckdose
-																			A_SW_NOCHANGE, // klo
-																			A_SW_OFF,      // hauptschuetz
-																			A_SW_NOCHANGE, // frei
-																			A_SW_OFF,      // licht+serverschrank-schuetz
-																			A_SW_NOCHANGE  // alle steckdosen (herd) schuetz
-																		}
-};
-
-struct t_queue_object poweron = { QUEUE_A1, 
-																	{ BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE, BRIGHT_NO_CHANGE }, 
-																	{ A_SW_NOCHANGE, // kuechenlicht
-																		A_SW_NOCHANGE, // beamer
-																		A_SW_NOCHANGE, // licht vortrag
-																		A_SW_NOCHANGE, // licht lounge
-																		A_SW_ON, // tischsteckdose
-																		A_SW_ON, // klo
-																		A_SW_ON, // hauptschuetz
-																		A_SW_NOCHANGE, // frei
-																		A_SW_ON, // licht+serverschrank-schuetz
-																		A_SW_ON  // alle steckdosen (herd) schuetz
-																	}
-};
-
-/*
-	XXX nehme drei fuer Ringbuffersize an ... das kann sich auch aendern!
-*/
-
-
-uint16_t tickscounter=0;
-
-#include "queue.i"
-
-inline void ringbuffer_flush(struct t_ringbuffer *rb){
-	uint8_t i=0;
-	for(;i<RINGBUFFERSIZE;i++) 
-		{
-			(*rb).clickstate[i]=0;	
-			(*rb).clickstate_free[i]=0;
-		}
-	(*rb).clickpos=0;
-}
-
-
-/*
-	interrupt service routine
- */
-
-/*
-	multipexing / ticks / Timerueberlauf
-*/
-ISR(TIMER0_OVF_vect)
-{
-	uint8_t i;
-	uint8_t taster_i;
-
-	/*
-		andere interrupts aus!
-	*/
-	cli();
-	/* 
-		 etwa 0.000512 sekunden
-		 gibt es hier leider nicht
-	*/
-/* 	if (( tickscounter & (TICKS_A0) ) == TICKS_A0 ){ */
-/* 		deque(QUEUE_A0); */
-/* 	} */
-	/* 
-		 etwa 0.0025 sekunden
-	*/
-	if (( tickscounter & (TICKS_A1) ) == TICKS_A1 ){
-		//		deque(QUEUE_A1);
-	}
-	/* 
-		 etwa 0.0050 sekunden
-	*/
-	if (( tickscounter & (TICKS_A2) ) == TICKS_A2 ){
-		// hier werden die clicks erfasst
-#include "click_events.i"
-		for(taster_i=0;taster_i<U_COUNT;taster_i++){
-			/*
-				check auf klick
-				+ starte <was_auch_immer>-klickaktion / oder auch nicht aber immer
-				+ reset des ringbuffers
-			*/
-			
-			/*
-				einmal kurz
-			*/
-			switch(taster_i) {
-			case U_VORTRAG:
-				{
-					// die events die durch den vortrags-taster ausgeloesst werden
-#include "events_vortrag.i"
-				}
-				break;
-			case U_LOUNGE:
-				{
-					// die events die durch den lounge-taster ausgeloesst werden
-#include "events_lounge.i"
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		//deque(QUEUE_A2);
-	}
-
-	/* 
-		 etwa 0.01 sekunden
-	*/
-	if (( tickscounter & (TICKS_A3) ) == TICKS_A3 ){
-		//deque(QUEUE_A3);
-	}
-	/* 
-		 etwa 0.04 sekunden
-	*/
-	if (( tickscounter & (TICKS_A4) ) == TICKS_A4 ){
-		/*
-			check ob der Hauptschalter umgelegt wurde 
-			muss ja nicht staendig passieren
-		*/
-		if ((!(PINB & _BV(PINB7))) == 1)   // Hauptschalter geht aus
-		{ // TODO Status merken?
-			//			add_queue(&poweroff);
-		}
-		else if ((PINB & _BV(PINB7))  == 0)   // Hauptschalter geht an
-		{
-			//add_queue(&poweron);
-		}
-		/*
-			if (nachtmodus == on) 
-			{
-			  add_queue(&sleepmode);
-      }
-			else if (nachtmodus == off)
-			{
-			  add_queue(&poweron);
-			}
-		*/
-		//deque(QUEUE_A4);
-	}
-
-	/* 
-		 etwa 0.125 sekunden
-	*/
-	if (( tickscounter & (TICKS_A5) ) == TICKS_A5 ){
-		deque(QUEUE_A5);
-	}
-	/* 
-		 etwa 0.25 sekunden
-	*/
-	if (( tickscounter & (TICKS_A6) ) == TICKS_A6 ){
-		//deque(QUEUE_A6);
-	}
-	/* 
-		 etwa 0.5 sekunden
-	*/
-	if (( tickscounter & (TICKS_A7) ) == TICKS_A7 ){
-		//deque(QUEUE_A7);
-	}
-	/* 
-		 etwa 1 sekunde
-	*/
-	if (( tickscounter & (TICKS_A8) ) == TICKS_A8 ){
-		//deque(QUEUE_A8);
-	}
-	/* 
-		 etwa 5 sekunde - im moment nur die sequenzen
-	*/
-	if (( tickscounter & (TICKS_A9) ) == TICKS_A9 ){
-		deque(QUEUE_A9);
-	}
-
-	/* 
-		 ueberlaeufe sind ok!
-	*/
-	tickscounter++;
-
-	sei();
-}
-/*
-	taster in der lounge
-*/
-ISR(INT0_vect)
-{
-	cli();
-	rb[U_LOUNGE].tastercounter++;
-	sei();
-}
-
-/*
-	taster im Vortragsraum
-*/
-ISR(PCINT2_vect)
-{
-	cli();
-	rb[U_VORTRAG].tastercounter++;
-	sei();
-}
+#include "fkt.h"
 
 int main(void)
 {
 	uint8_t TWIS_ResonseType;
+	struct t_i2cproto i2cslave = { C_NDEF, O_NDEF, F_NDEF, HASNDATA, HASNDATA, 0 ,0 };
 
 	init_modi();
 	init_relais();
 	init_timer();
 
-#include "matrix_init.i"
 	/*
 	** Clear any interrupt
 	*/
@@ -303,41 +61,42 @@ int main(void)
 							*/
 						case TWIS_ReadBytes:
 							{
-								busdata.in_data[0] = TWIS_ReadAck();   // byte 1 lesen
-								busdata.in_data[1] = TWIS_ReadAck();   // byte 2 lesen
-								busdata.in_data[2] = TWIS_ReadNack();  // byte 3 lesen
-								busdata.write_data = HASNDATA;
+								i2cslave.class   = TWIS_ReadAck();
+								i2cslave.object  = TWIS_ReadAck();
+								i2cslave.fkt     = TWIS_ReadAck();
+								i2cslave.in_data = TWIS_ReadNack();
+								i2cslave.has_out_data = HASNDATA;
 								TWIS_Stop();                // I2C stop
 								/*
 									die Anweisungen die ueber I2C kommen gehen an der queue vorbei
 									d.h. die queue bleibt erhalten wenn eine i2c nachricht rein kommt
 									XXX ... ist das gut oder schlecht?
 								*/
-								if (( busdata.in_data[0] >= (O_SW_FIRST)) && 
-										( busdata.in_data[0] <= (O_SW_LAST)))	{
-									switch_fkt(&(swp_matrix[busdata.in_data[0]]), &busdata);
+								switch(i2cslave.class){
+								case C_SW:
+									{
+										switch_fkt(&i2cslave);
+									}
+									break;
+								case C_PWM:
+									{
+										pwm_fkt(&i2cslave);
+									}
+									break;
+								default:
+									break;
 								}
-								if (( busdata.in_data[0] >= (O_LAMP_FIRST)) && 
-										( busdata.in_data[0] <= (O_LAMP_LAST)))	{
-									bright_fkt(lp_matrix[busdata.in_data[0]-(O_SW_COUNT)], &busdata);
-								}
-								if (( busdata.in_data[0] >= (O_TASTER_FIRST)) && 
-										( busdata.in_data[0] <= (O_TASTER_LAST)))	{
-									switch_ro_fkt(&(tp_matrix[busdata.in_data[0]-(O_SW_COUNT+O_LAMP_COUNT)]), &busdata);
-								}
-								
-								if ( busdata.in_data[1] >= O_SW_COUNT ) break;
-								
 							}
 							break;
 						case TWIS_WriteBytes:
 							{
-								if(busdata.write_data == HASDATA){
-									TWIS_Write(busdata.out_data);
+								if(i2cslave.has_out_data == HASDATA){
+									TWIS_Write(i2cslave.out_data);
 								} else {
-									TWIS_Write(0);
+									TWIS_Write(D_NDEF);
 								}
-								busdata.write_data = HASNDATA;
+								i2cslave.has_out_data = HASNDATA;
+								i2cslave.out_data = D_NDEF;
 								TWIS_Stop();
 							}
 							break;
