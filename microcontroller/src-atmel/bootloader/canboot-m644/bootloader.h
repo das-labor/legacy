5061dec0c6949902754295ed33a8109e2f39ca78
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-10-05 18:42:32 +0000

    add canboot for m644

---
+++ microcontroller/src-atmel/bootloader/canboot-m644/bootloader.h
@@ -0,0 +1,5 @@
+#include <avr/boot.h>
+
+
+
+
---
+++ microcontroller/src-atmel/bootloader/canboot-m644/can.h
@@ -0,0 +1,68 @@
+#ifndef CAN_H
+#define CAN_H
+
+#include <avr/boot.h>
+
+/*****************************************************************************
+ * Simple CAN Library
+ *
+ * #define CAN_INTERRUPT 1	//set this to enable interrupt driven 
+ *                                and buffering version
+ * #define CAN_RX_BUFFER_SIZE 2	//only used for Interrupt
+ * #define CAN_TX_BUFFER_SIZE 2	//only used for Interrupt
+ */
+
+#include "config.h"
+
+/*****************************************************************************
+ * Types
+ */
+
+typedef unsigned char can_addr;
+typedef unsigned char can_port;
+
+typedef struct
+{
+	can_addr      addr_src;
+	can_addr      addr_dst;
+	can_port      port_src;
+	can_port      port_dst;
+	unsigned char dlc;
+	unsigned char data[8];
+} can_message;
+
+
+extern can_message Rx_msg, Tx_msg;
+
+
+/*****************************************************************************
+ * MCP
+ */
+
+void mcp_write(unsigned char reg, unsigned char data);
+
+/*****************************************************************************
+ * Management
+ */
+
+void can_init();
+
+#define BFPCTRL 0x0C
+
+
+/*****************************************************************************
+ * Sending
+ */
+
+can_message * can_buffer_get();
+void can_transmit();
+
+
+/*****************************************************************************
+ * Receiving
+ */
+
+unsigned char can_get_nb();
+
+
+#endif
