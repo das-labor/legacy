a4ec3e7920e9b2987afb981ddebd5fe09f681171
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-10-10 01:21:23 +0000

    format

--- microcontroller/src-atmel/playground/PowerCommander/branche/rewrite_PowerCommander2/avr_controll/fkt.c
+++ microcontroller/src-atmel/playground/PowerCommander/branche/rewrite_PowerCommander2/avr_controll/fkt.c
@@ -10,27 +10,26 @@ void switch_fkt(struct t_i2cproto* i2cproto)
 {
 	switch (i2cproto->fkt)
 	{
-	case F_SW_OFF:
+		case F_SW_OFF:
 		{
 			switch_off(sw_matrix[i2cproto->object].port, 
 								 sw_matrix[i2cproto->object].pin);
 		}
 		break;
-	case F_SW_ON:
+		case F_SW_ON:
 		{
 			switch_on(sw_matrix[i2cproto->object].port, 
 								sw_matrix[i2cproto->object].pin);
 		}
 		break;
-	case F_SW_STATUS:
+		case F_SW_STATUS:
 		{
 			switch_status(sw_matrix[i2cproto->object].port, 
-										sw_matrix[i2cproto->object].pin, 
-										&(i2cproto->out_data));
+										sw_matrix[i2cproto->object].pin, &(i2cproto->out_data));
 			i2cproto->has_out_data = HASDATA;
 		}
 		break;
-	default:
+		default:
 		break;
 	}
 }
@@ -39,19 +38,18 @@ void pwm_fkt(struct t_i2cproto* i2cproto)
 {
 	switch (i2cproto->fkt)
 	{
-	case F_PWM_SET:
+		case F_PWM_SET:
 		{
 			pwm_set(pwm_matrix[i2cproto->object].port, i2cproto->in_data);
 		}
 		break;
-	case F_PWM_GET:
+		case F_PWM_GET:
 		{
-			pwm_get(pwm_matrix[i2cproto->object].port, 
-							&(i2cproto->out_data));
+			pwm_get(pwm_matrix[i2cproto->object].port, &(i2cproto->out_data));
 			i2cproto->has_out_data = HASDATA;
 		}
 		break;
-	default:
+		default:
 		break;
 	}
 }
@@ -60,16 +58,16 @@ void virt_fkt(struct t_i2cproto* i2cproto)
 {
 	switch (i2cproto->object)
 	{
-	case VIRT_POWER:
-		virt_power(i2cproto);
+		case VIRT_POWER:
+			virt_power(i2cproto);
 		break;
-	case VIRT_VORTRAG:
-		virt_vortrag(i2cproto);
+		case VIRT_VORTRAG:
+			virt_vortrag(i2cproto);
 		break;
-	case VIRT_VORTRAG_PWM:
-		virt_vortrag_pwm_set(i2cproto);
+		case VIRT_VORTRAG_PWM:
+			virt_vortrag_pwm_set(i2cproto);
 		break;
-	default:
+		default:
 		break;
 	}
 }
@@ -78,17 +76,17 @@ void virt_power(struct t_i2cproto* i2cproto)
 {
 	switch (i2cproto->fkt)
 	{
-	case F_SW_ON:
+		case F_SW_ON:
 		{
 			virt_power_on();
 		}
 		break;
-	case F_SW_OFF:
+		case F_SW_OFF:
 		{
 			virt_power_off();
 		}
 		break;
-	default:
+		default:
 		break;
 	}
 }
@@ -97,13 +95,13 @@ void virt_vortrag(struct t_i2cproto* i2cproto)
 {
 	switch (i2cproto->fkt)
 	{
-	case F_SW_ON:
-		virt_vortrag_on();
+		case F_SW_ON:
+			virt_vortrag_on();
 		break;
-	case F_SW_OFF:
-		virt_vortrag_off();
+		case F_SW_OFF:
+			virt_vortrag_off();
 		break;
-	default:
+		default:
 		break;
 	}
 	
@@ -160,27 +158,27 @@ void virt_vortrag_off()
 	switch_off(sw_matrix[SWL_VORTRAG].port, sw_matrix[SWL_VORTRAG].pin);
 }
 
-void switch_on(volatile uint8_t *port,uint8_t pin)
+void switch_on(volatile uint8_t *port, uint8_t pin)
 {
 	(*port) |= _BV(pin);
 }
 
-void switch_off(volatile uint8_t *port,uint8_t pin)
+void switch_off(volatile uint8_t *port, uint8_t pin)
 {
 	(*port) &= ~_BV(pin);
 }
 
-void switch_status(volatile uint8_t *port,uint8_t pin, uint8_t *result)
+void switch_status(volatile uint8_t *port, uint8_t pin, uint8_t *result)
 {
-	(*result) = ((*port) >> _BV(pin)) & 1;
+	(*result) = (((*port) >> _BV(pin)) & 0x01);
 }
 
-void pwm_set(volatile uint8_t *port,uint8_t value)
+void pwm_set(volatile uint8_t *port, uint8_t value)
 {
 	(*port) = value;
 }
 
-void pwm_get(volatile uint8_t *port,uint8_t *result)
+void pwm_get(volatile uint8_t *port, uint8_t *result)
 {
 	(*result) = (*port);
 }
