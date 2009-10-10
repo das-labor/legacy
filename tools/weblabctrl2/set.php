b891de421df796bbaa2b91f47edca9ae5a6eac5a
ex <ex@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-10-10 13:32:38 +0000

    fix possible bugs with status.txt

--- tools/weblabctrl2/set.php
+++ tools/weblabctrl2/set.php
@@ -14,18 +14,14 @@ if($cmd=="PWM")
 {
 	if(preg_match("/[!0-9]/",$_GET[value]) && $_GET[value]<=255 && $_GET[value]>=0) $value=dechex($_GET[value]);
 	else unset($_GET[value]);
-	if($id=="VORTRAG_ALL")
-		foreach(array("TAFEL","BEAMER","SCHRANK","FLIPPER") as $id)
-		{
-			exec("powercommander.lapcontrol powercommander $cmd $id SET 0x$value");
-		}
-	else exec("powercommander.lapcontrol powercommander $cmd $id SET 0x$value");
+	exec("powercommander.lapcontrol powercommander $cmd $id SET 0x$value");
 	echo "powercommander.lapcontrol powercommander $cmd $id SET 0x$value";
 
 	include "status.txt";
 	$first=false;
 	$file_content = "<? \$status = array( ";
-	foreach($status as $status_key => $status_value)
+	#foreach($status as $status_key => $status_value)
+	foreach($pwm_ids as $status_key)
 	{
 		if($first==true) $file_content .= ", ";
 		if($status_key==$id)
@@ -34,7 +30,7 @@ if($cmd=="PWM")
 			$script .= "slider_".$status_key."_control.setValue('".hexdec($value)."');\n";
 		}
 		else
-			$file_content .= "\"$status_key\" => \"$status_value\""; 
+			$file_content .= "\"$status_key\" => \"".$status[$status_key]."\""; 
 		$first=true;
 	}
 	$file_content .= "); ?>";
