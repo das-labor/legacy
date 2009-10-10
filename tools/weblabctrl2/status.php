df8680ab1b6f8f6dd01889375c6a8ea0a5aa8101
ex <ex@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-10-10 13:14:27 +0000

    fix bug with "'"

--- tools/weblabctrl2/status.php
+++ tools/weblabctrl2/status.php
@@ -14,7 +14,7 @@ foreach($rooms as $room => $port)
 	exec("export MPD_PORT=$port; mpc",$status);
 	$script .= "for (var i = 0; i < document.getElementsByClassName('".$room."_button').length; i++) {document.getElementsByClassName('".$room."_button')[i].style.backgroundColor='';}
 ";
-	if(strpos($status[0],"volume")===false) $script .= "document.getElementById('".$room."_status').innerHTML='".substr(htmlspecialchars($status[0]),0,24)."';\n";;
+	if(strpos($status[0],"volume")===false) $script .= "document.getElementById('".$room."_status').innerHTML='".preg_replace("/'/","&#39;",substr(htmlspecialchars($status[0]),0,24))."';\n";;
 	if(strpos($status[1],"playing")!==false) $script .= "document.getElementById('".$room."_play').style.backgroundColor='#aaaaaa';\n";;
 	if(strpos($status[1],"paused")!==false) $script .= "document.getElementById('".$room."_pause').style.backgroundColor='#aaaaaa';\n";;
 }
