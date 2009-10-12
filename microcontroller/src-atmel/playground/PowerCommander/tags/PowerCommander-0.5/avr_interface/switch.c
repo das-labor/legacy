c6e4898aa5ef40f5d9e86def9b856afec3263329
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-10-12 00:45:38 +0000

    hardware change

--- microcontroller/src-atmel/playground/PowerCommander/tags/PowerCommander-0.5/avr_interface/switch.c
+++ microcontroller/src-atmel/playground/PowerCommander/tags/PowerCommander-0.5/avr_interface/switch.c
@@ -22,114 +22,111 @@ AVRX_GCC_TASKDEF(switch_vortrag, 60, 7)
 
 	while (1)
 	{
-		if ((!(PINA & _BV(PA2))) && (stat_vortrag == 1))
+		if ((!(PINB & _BV(PB2))) && (stat_vortrag == 1))
 		{
-					i2c_outdata.outdata[0]=C_VIRT;
-					i2c_outdata.outdata[1]=VIRT_VORTRAG;
-					i2c_outdata.outdata[2]=F_SW_OFF;
-					i2c_outdata.outdata[3]=0x00;
+			i2c_outdata.outdata[0]=C_VIRT;
+			i2c_outdata.outdata[1]=VIRT_VORTRAG;
+			i2c_outdata.outdata[2]=F_SW_OFF;
+			i2c_outdata.outdata[3]=0x00;
 
-					AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
-					AvrXWaitMessageAck(&i2c_outdata.mcb);
+			AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
+			AvrXWaitMessageAck(&i2c_outdata.mcb);
 
-					AvrXDelay(&switch_timer_vortrag, 3000);
-					stat_vortrag=0;
-				}
-			if ((!(PINA & _BV(PA2))) && stat_vortrag == 0)
-				{
-					i2c_outdata.outdata[0]=C_VIRT;
-					i2c_outdata.outdata[1]=VIRT_VORTRAG;
-					i2c_outdata.outdata[2]=F_SW_ON;
-					i2c_outdata.outdata[3]=0x00;
-
-					AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
-					AvrXWaitMessageAck(&i2c_outdata.mcb);
-
-					AvrXDelay(&switch_timer_vortrag, 3000);
-					stat_vortrag=1;
-				}
-				AvrXDelay(&switch_timer_vortrag, 1);
+			AvrXDelay(&switch_timer_vortrag, 3000);
+			stat_vortrag = 0;
 		}
+		if ((!(PINB & _BV(PB2))) && stat_vortrag == 0)
+		{
+			i2c_outdata.outdata[0]=C_VIRT;
+			i2c_outdata.outdata[1]=VIRT_VORTRAG;
+			i2c_outdata.outdata[2]=F_SW_ON;
+			i2c_outdata.outdata[3]=0x00;
 
+			AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
+			AvrXWaitMessageAck(&i2c_outdata.mcb);
+
+			AvrXDelay(&switch_timer_vortrag, 3000);
+			stat_vortrag = 1;
+		}
+		AvrXDelay(&switch_timer_vortrag, 1);
+	}
 }
 
 AVRX_GCC_TASKDEF(switch_lounge, 60, 7)
 {
-	uint8_t stat_lounge=0; // nur an oder aus 
+	uint8_t stat_lounge = 0; // nur an oder aus 
 	static t_i2cMessage_out i2c_outdata;
-	while(1)
-		{
-			if ((!(PINB & _BV(PB0))) && stat_lounge == 1)
-				{
-					i2c_outdata.outdata[0]=C_SW;
-					i2c_outdata.outdata[1]=SWL_LOUNGE;
-					i2c_outdata.outdata[2]=F_SW_OFF;
-					i2c_outdata.outdata[3]=0x00;
-
-					AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
-					AvrXWaitMessageAck(&i2c_outdata.mcb);
-
-					AvrXDelay(&switch_timer_lounge, 3000);
-					stat_lounge=0;
-				}
-			if ((!(PINB & _BV(PB0))) && stat_lounge == 0)
-				{
-					i2c_outdata.outdata[0]=C_SW;
-					i2c_outdata.outdata[1]=SWL_LOUNGE;
-					i2c_outdata.outdata[2]=F_SW_ON;
-					i2c_outdata.outdata[3]=0x00;
-
-					AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
-					AvrXWaitMessageAck(&i2c_outdata.mcb);
-
-					AvrXDelay(&switch_timer_lounge, 3000);
-					
-					stat_lounge=1;
-				}
-				AvrXDelay(&switch_timer_lounge, 1);
-		}
 	
+	while (1)
+	{
+		if ((!(PIND & _BV(PD3))) && stat_lounge == 1)
+		{
+			i2c_outdata.outdata[0]=C_SW;
+			i2c_outdata.outdata[1]=SWL_LOUNGE;
+			i2c_outdata.outdata[2]=F_SW_OFF;
+			i2c_outdata.outdata[3]=0x00;
+
+			AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
+			AvrXWaitMessageAck(&i2c_outdata.mcb);
+
+			AvrXDelay(&switch_timer_lounge, 3000);
+			stat_lounge = 0;
+		}
+		if ((!(PIND & _BV(PD3))) && stat_lounge == 0)
+		{
+			i2c_outdata.outdata[0]=C_SW;
+			i2c_outdata.outdata[1]=SWL_LOUNGE;
+			i2c_outdata.outdata[2]=F_SW_ON;
+			i2c_outdata.outdata[3]=0x00;
+
+			AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
+			AvrXWaitMessageAck(&i2c_outdata.mcb);
+
+			AvrXDelay(&switch_timer_lounge, 3000);
+				
+			stat_lounge = 1;
+		}
+		AvrXDelay(&switch_timer_lounge, 1);
+	}
 }
-
-
 
 AVRX_GCC_TASKDEF(switchtask, 60, 7)
 {
 	static t_i2cMessage_out i2c_outdata;
-
-
 	uint8_t stat_haupt = 0;
 
 	while (1)
-  	{
-			if (!(PINA & _BV(PA0)) && stat_haupt == 1)
-				{
-					
-					i2c_outdata.outdata[0]=C_VIRT;
-					i2c_outdata.outdata[1]=VIRT_POWER;
-					i2c_outdata.outdata[2]=F_SW_OFF;
-					i2c_outdata.outdata[3]=0x00;
+  {
+		if (!(PINA & _BV(PA0)) && stat_haupt == 1)
+		{
+			i2c_outdata.outdata[0]=C_VIRT;
+			i2c_outdata.outdata[1]=VIRT_POWER;
+			i2c_outdata.outdata[2]=F_SW_OFF;
+			i2c_outdata.outdata[3]=0x00;
 
-					stat_haupt = 0;
-					AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
-					AvrXWaitMessageAck(&i2c_outdata.mcb);
+			stat_haupt = 0;
+			AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
+			AvrXWaitMessageAck(&i2c_outdata.mcb);
 
-					AvrXDelay(&switchtimer, 1000);
-				}
-			if ((PINA & _BV(PA0)) && stat_haupt == 0)
-				{
-					i2c_outdata.outdata[0]=C_VIRT;
-					i2c_outdata.outdata[1]=VIRT_POWER;
-					i2c_outdata.outdata[2]=F_SW_ON;
-					i2c_outdata.outdata[3]=0x00;
+			AvrXDelay(&switchtimer, 1000);
+			PORTA |= _BV(PA3); // red
+			PORTA &= ~_BV(PA2); // green
+		}
+		if ((PINA & _BV(PA0)) && stat_haupt == 0)
+		{
+			i2c_outdata.outdata[0]=C_VIRT;
+			i2c_outdata.outdata[1]=VIRT_POWER;
+			i2c_outdata.outdata[2]=F_SW_ON;
+			i2c_outdata.outdata[3]=0x00;
 
-					stat_haupt = 1;
-					AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
-					AvrXWaitMessageAck(&i2c_outdata.mcb);
+			stat_haupt = 1;
+			AvrXSendMessage(&i2cQueue_out, &i2c_outdata.mcb);
+			AvrXWaitMessageAck(&i2c_outdata.mcb);
 
-					AvrXDelay(&switchtimer, 1000);
+			AvrXDelay(&switchtimer, 1000);
 
-				}
-			AvrXDelay(&switchtimer, 1);
-  	}
+		}
+		AvrXDelay(&switchtimer, 1);
+  }
 }
+
