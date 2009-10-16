cdff02c52a3ac0833cfec12d67ec7c0b830cd28f
ex <ex@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-10-16 18:52:11 +0000

    look last commit message

--- tools/weblabctrl2/config.php
+++ tools/weblabctrl2/config.php
@@ -3,7 +3,7 @@ session_id("nakka");
 session_start();
 #light
 $pwm_ids=array("TAFEL","BEAMER","SCHRANK","FLIPPER","LOUNGE","KUECHE");
-$sw_ids=array("LAMP_TAFEL","LAMP_BEAMER","LAMP_SCHRANK","LAMP_FLIPPER","LAMP_LOUNGE","LAMP_KUECHE","LAMP_VORTRAG");
+$sw_ids=array("LAMP_TAFEL","LAMP_BEAMER","LAMP_SCHRANK","LAMP_FLIPPER","LAMP_LOUNGE","LAMP_KUECHE");
 #mpd
 $rooms=array("VORTRAG" => "6603","LOUNGE" => "6600","BASTEL" => "6602");
 $mpd_cmd=array("pause","play","volume","next","prev");
