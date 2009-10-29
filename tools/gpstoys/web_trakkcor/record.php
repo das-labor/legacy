a9dffbadb93edcd786f4ae183eced76d4df24acb
til <til@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-10-29 17:20:29 +0000

    added server-side component for generic log format, moved nmea to lat/lon conversion from map display code to log write code where it belongs

--- tools/gpstoys/web_trakkcor/record.php
+++ tools/gpstoys/web_trakkcor/record.php
@@ -6,12 +6,38 @@
 $id=htmlentities($_GET["deviceID"]);
 $pos=htmlentities($_GET["position"]);
 
-echo $id.", ".$pos;
+$input=$id.",".$pos;
+
+$data = explode(",", $input, 1000);
+		$id = $data[0];	// deviceID
+        $ts = $data[2];
+        $lat = $data[3];
+        $lon = $data[5];
+        $ns = $data[4];
+        $ew = $data[6];
+		$alt = $data[10];
+		$altunit = $data[11];
+
+function nmea2deg($nmea, $hem){
+    $deg = (int)($nmea/100) + ($nmea - (int)($nmea / 100) * 100) / 60;
+    if ( $hem == "S" || $hem == "W" )
+        $deg = 0-$deg;
+    return $deg;
+}
+
+$time = (int)$ts;
+$lat = nmea2deg($lat, $ns);
+$lon = nmea2deg($lon, $ew);
+$alt = $alt.strtolower($altunit);
+$hea="none";
+$spd="none";
+
+
 
 // write to file
 	$posFile = "view/position.csv";
 	$fh = fopen($posFile, 'w+') or die("can't open file");
-	$stringData = $id.",".$pos;
+	$stringData = $id.",".$time.",".$lat.",".$lon.",".$alt.",".$hea.",".$spd;
 	fwrite($fh, $stringData);
 	fclose($fh);
 ?>
