5cceb5c58813ba97ae5898c8f16e1a5bcd1adeec
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-11-10 19:18:33 +0000

    first working version - need to check in the os as such - this is only the workerthread and the configuration

---
+++ microcontroller/src-atmel/playground/treppen_blink/branch/femtoOS-test/include/config_application.h
@@ -0,0 +1,343 @@
+/*
+ * Femto OS v 0.91 - Copyright (C) 2008-2009 Ruud Vlaming
+ *
+ * This file is part of the Femto OS distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Please note that, due to the GPLv3 license, for application of this
+ * work and/or combined work in embedded systems special obligations apply.
+ * If these are not to you liking, please know the Femto OS is dual
+ * licensed. A commercial license and support are available.
+ * See http://www.femtoos.org/ for details.
+ */
+
+#ifndef CONFIG_TESTHELLOWORLD_H_
+#define CONFIG_TESTHELLOWORLD_H_
+
+/* Comments are in the config default file */
+
+/* ========================================================================= */
+/* SYSTEM CONFIGURATION ==================================================== */
+/* ========================================================================= */
+
+#define  cfgSysOptimized                         cfgFalse
+#define  cfgSysDebug                             cfgFalse
+#define  cfgSysInline                            cfgFalse
+#define  cfgSysFramePointerCounterMeasures       cfgFalse
+#define  cfgSysReduceProEpilogue                 cfgBikini
+#define  cfgSysRegisterCheckByte                 0xA5
+#define  cfgSysStackGrowthUp                     cfgFalse
+#define  cfgSysZeroPageStack                     cfgFalse
+#define  cfgSysClearUnusedR1                     cfgTrue
+#define  cfgSysSqueezeState                      defSysOptimized
+#define  cfgSysReuseOsStack                      cfgTrue
+#define  cfgSysClockDivider                      1
+#define  cfgSysSubTicksPerFullTick               32
+#define  cfgSysSubTickDivider                    256
+#define  cfgSysGCCstartup                        cfgOverride
+#define  cfgSysFsStorageSpace                    0
+
+
+/* ========================================================================= */
+/* INTERRUPT HANDLING ====================================================== */
+/* ========================================================================= */
+
+#define  cfgIntGlobalOnly                        cfgTrue
+#define  cfgIntUserDefined                       cfgFalse
+#define  cfgIntSwitchUsesOSstack                 cfgFalse
+#define  cfgIntOsProtected                       cfgTrue
+#define  cfgIntTickTrack                         cfgFalse
+#define  cfgIntManualTicks                       cfgFalse
+
+
+/* ========================================================================= */
+/* QUALITY ASSURANCE ======================================================= */
+/* ========================================================================= */
+
+#define  cfgCheckSystem                          cfgTrue
+#define  cfgCheckReset                           cfgTrue
+#define  cfgCheckTrace                           cfgFalse
+#define  cfgCheckAlwaysFatal                     cfgFalse
+#define  cfgCheckWatermarks                      cfgFalse
+#define  cfgCheckOsStack                         cfgTrue
+#define  cfgCheckTaskStack                       cfgTrue
+#define  cfgCheckIsrStack                        cfgFalse
+#define  cfgCheckRegisters                       cfgTrue
+#define  cfgCheckTiming                          cfgFalse
+#define  cfgCheckQueuFilling                     cfgFalse
+#define  cfgCheckMethodUse                       cfgTrue
+#define  cfgCheckApplication                     cfgFalse
+
+
+/* ========================================================================= */
+/* COMMON CAPABILITIES ===================================================== */
+/* ========================================================================= */
+
+#define  cfgUseEquidistantTicks                  cfgTrue
+#define  cfgUseDelay                             cfgTrue
+#define  cfgUseSynchronization                   cfgSyncNon
+#define  cfgUseHierarchicalRoundRobin            cfgFalse
+#define  cfgUseNestedCriticals                   cfgFalse
+#define  cfgUsePrioritizedRelease                cfgFalse
+#define  cfgUsePriorityLifting                   cfgFalse
+#define  cfgUseTaskWatchdog                      cfgFalse
+#define  cfgUseCorrectWakeupTimes                cfgFalse
+#define  cfgUseTimeout                           cfgFalse
+#define  cfgUseTasknames                         cfgFalse
+#define  cfgUseLowPowerSleep                     cfgFalse
+#define  cfgUseLowPowerOnDelay                   cfgFalse
+#define  cfgUseLowPowerDelayRelease              cfgFalse
+#define  cfgUseLoadMonitor                       cfgFalse
+#define  cfgUseFileSystem                        cfgFalse
+#define  cfgUseFileSystemConcurrentRead          cfgFalse
+#define  cfgUseFileSystemEconomyMode             cfgFalse
+#define  cfgUseFileSystemMaintainFAT             cfgFalse
+#define  cfgUseEvents                            cfgFalse
+#define  cfgUseEventsOnVariables                 cfgFalse
+
+
+/* ========================================================================= */
+/* CODE PARAMETERS ========================================================= */
+/* ========================================================================= */
+
+#define  cfgNumWatchdogDiv                       0
+#define  cfgNumMonitorDiv                        0
+#define  cfgNumSleepPeriod                       0
+#define  cfgNumSleepThreshold                    0
+#define  cfgNumGlobWidth                         0
+#define  cfgNumSwitchWidth                       0
+#define  cfgNumTickWidth                         0
+
+
+/* ========================================================================= */
+/* EXTERNAL CALLS ========================================================== */
+/* ========================================================================= */
+
+#define  callAppTick00                           cfgFalse
+#define  callAppTick08                           cfgFalse
+#define  callAppTick16                           cfgFalse
+#define  callAppTickSleep                        cfgFalse
+#define  callAppEnterIdle                        cfgFalse
+#define  callAppEnterSleep                       cfgFalse
+#define  callAppExitSleep                        cfgFalse
+#define  callAppBoot                             cfgTrue
+#define  callAppInit                             cfgFalse
+#define  callAppBark                             cfgFalse
+
+
+/* ========================================================================= */
+/* API INCLUSIONS ========================================================== */
+/* ========================================================================= */
+
+/* Standard functions ------------------------------------------------------ */
+#define  includeTaskYield                        cfgFalse
+#define  includeTaskDelayFromNow                 cfgTrue
+#define  includeTaskDelayFromWake                cfgFalse
+#define  includeGenSuspend                       cfgFalse
+#define  includeTaskSuspend                      cfgFalse
+#define  includeGenResume                        cfgFalse
+#define  includeTaskSleep                        cfgFalse
+#define  includeTaskSleepAll                     cfgFalse
+#define  includeGenSetPriority                   cfgFalse
+#define  includeGenGetPriority                   cfgFalse
+#define  includeGenGetTickCount                  cfgFalse
+#define  includeGenAddtoTickCount                cfgFalse
+#define  includeGenGetLastWakeTime               cfgFalse
+#define  includeTaskFeedWatchdog                 cfgFalse
+#define  includeTaskKillWatchdog                 cfgFalse
+#define  includeGenGetTaskname                   cfgFalse
+#define  includeTaskTerminate                    cfgFalse
+#define  includeGenReboot                        cfgFalse
+#define  includeTaskRecreate                     cfgFalse
+#define  includeTaskRestart                      cfgFalse
+#define  includeGenLogTask                       cfgFalse
+#define  includeGenLogOs                         cfgFalse
+#define  includeGenTrace                         cfgFalse
+#define  includeGenPipeInt16                     cfgFalse
+#define  includeGenPassFlashString               cfgFalse
+
+/* Event functions --------------------------------------------------------- */
+#define  includeTaskWaitForEvents                cfgFalse
+#define  includeIsrFireEvent                     cfgFalse
+#define  includeGenFireEvent                     cfgFalse
+#define  includeGenFireEventSet                  cfgFalse
+#define  includeGenCountEventBlocks              cfgFalse
+
+/* Synchronization functions ----------------------------------------------- */
+#define  includeTaskWaitForTasks                 cfgFalse
+#define  includeGenWaitRelease                   cfgFalse
+#define  includeTaskMutex                        cfgFalse
+#define  includeTaskQueu                         cfgFalse
+
+/* Queu functions --------------------------------------------------------- */
+#define  includeGenQueuWrite                     cfgFalse
+#define  includeGenQueuRead                      cfgFalse
+#define  includeGenQueuClear                     cfgFalse
+#define  includeGenQueuPeek                      cfgFalse
+#define  includeGenQueuReadable                  cfgFalse
+#define  includeGenQueuWriteable                 cfgFalse
+#define  includeGenQueuFull                      cfgFalse
+#define  includeGenQueuEmpty                     cfgFalse
+
+/* File System functions --------------------------------------------------- */
+#define  includeTaskFileAccess                   cfgFalse
+#define  includeTaskFileFormat                   cfgFalse
+#define  includeTaskFileGetSize                  cfgFalse
+#define  includeTaskFileSetSize                  cfgFalse
+#define  includeTaskFileReadByte                 cfgFalse
+#define  includeTaskFileWriteByte                cfgFalse
+#define  includeTaskFileAppendByte               cfgFalse
+#define  includeTaskFileReadPipe                 cfgFalse
+#define  includeTaskFileWritePipe                cfgFalse
+#define  includeTaskFileReadBuffer               cfgFalse
+#define  includeTaskFileWriteBuffer              cfgFalse
+
+/* Protection functions ---------------------------------------------------- */
+#define  includeTaskProtectGlobalInterrupts      cfgFalse
+#define  includeTaskProtectSwitchTasks           cfgFalse
+#define  includeTaskProtectTickInterrupts        cfgFalse
+#define  includeTaskProtectGlobalCritical        cfgTrue
+#define  includeTaskProtectSwitchCritical        cfgFalse
+#define  includeTaskProtectTickCritical          cfgFalse
+
+/* ISR header/footer functions --------------------------------------------- */
+#define  includeIsrEnter                         cfgFalse
+#define  includeIsrExit                          cfgFalse
+#define  includeIsrBegin                         cfgFalse
+#define  includeIsrEndReturn                     cfgFalse
+#define  includeIsrEndYield                      cfgFalse
+#define  includeIsrStartLoad                     cfgFalse
+#define  includeIsrStopLoad                      cfgFalse
+
+/* Quality assurance functions --------------------------------------------- */
+#define  includeTaskStackCheck                   cfgFalse
+#define  includeIsrStackCheck                    cfgFalse
+
+
+/* ========================================================================= */
+/* TASK NAMES ============================================================== */
+/* ========================================================================= */
+
+#define  CN_00                                   rundown
+//#define  CN_01                                   blueout
+//#define  CN_02                                   update
+
+
+/* ========================================================================= */
+/* INCLUDE TASKS =========================================================== */
+/* ========================================================================= */
+
+#define  TaskInclude_rundown                     cfgStartRunning
+//#define  TaskInclude_blueout                    cfgStartRunning
+//#define  TaskInclude_update                     cfgStartRunning
+
+
+/* ========================================================================= */
+/* TASK CAPABILITIES ======================================================= */
+/* ========================================================================= */
+
+#define  CapabilitiesOverride                    cfgCapAll
+
+
+/* ========================================================================= */
+/* STACK SIZES ============================================================= */
+/* ========================================================================= */
+
+#define  StackSafety                             4
+#define  StackSizeOS                             24
+#define  StackSizeISR                            0
+#define  StackSizeShared                         0
+#define  StackSizeOverride                       46
+
+
+/* ========================================================================= */
+/* TIMES SLICES ============================================================ */
+/* ========================================================================= */
+
+#define  TimeSliceIdleTime                       cfgSysSubTicksPerFullTick
+
+
+/* ========================================================================= */
+/* RUNTIME TASK NAMES ====================================================== */
+/* ========================================================================= */
+
+
+/* ========================================================================= */
+/* TASK PRIORITIES ========================================================= */
+/* ========================================================================= */
+
+#define  PriorityOverride                        0
+
+
+/* ========================================================================= */
+/* REGISTER USE ============================================================ */
+/* ========================================================================= */
+
+#define  RegisterUse_rundown                      registersAll
+//#define  RegisterUse_blueout                     registersAll
+//#define  RegisterUse_update                      registersAll
+
+
+/* ========================================================================= */
+/* REGISTER CHECK ========================================================== */
+/* ========================================================================= */
+
+#define  RegisterCheckOverride                   registersAll
+
+
+/* ========================================================================= */
+/* INTERRUPT SETTING ======================================================= */
+/* ========================================================================= */
+
+#define  InterruptStartOverride                  cfgGlobSet | cfgTickSet
+
+
+/* ========================================================================= */
+/* EVENT NAMES ============================================================= */
+/* ========================================================================= */
+
+
+/* ========================================================================= */
+/* SLOT NAMES ============================================================== */
+/* ========================================================================= */
+
+
+/* ========================================================================= */
+/* SLOT SIZES ============================================================== */
+/* ========================================================================= */
+
+
+/* ========================================================================= */
+/* SLOT USAGE ============================================================== */
+/* ========================================================================= */
+
+
+/* ========================================================================= */
+/* QUEU SIZES ============================================================== */
+/* ========================================================================= */
+
+
+/* ========================================================================= */
+/* FILE NAMES ============================================================== */
+/* ========================================================================= */
+
+
+/* ========================================================================= */
+/* FILE SIZES ============================================================== */
+/* ========================================================================= */
+
+#define  FileSpaceStandard                       0
+
+
+#endif /*CONFIG_TESTHELLOWORLD_H_*/
