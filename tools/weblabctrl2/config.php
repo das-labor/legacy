70e798705bc7e9257da9713e3c675155a31975e9
ex <ex@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-11-15 11:00:16 +0000

    added VORTRAG_PWM for all lights in VORTRAG

--- tools/weblabctrl2/config.php
+++ tools/weblabctrl2/config.php
@@ -2,8 +2,8 @@
 session_id("nakka");
 session_start();
 #light
-$pwm_ids=array("TAFEL","BEAMER","SCHRANK","FLIPPER","LOUNGE","KUECHE");
-$sw_ids=array("LAMP_TAFEL","LAMP_BEAMER","LAMP_SCHRANK","LAMP_FLIPPER","LAMP_LOUNGE","LAMP_KUECHE");
+$pwm_ids=array("VORTRAG_PWM","TAFEL","BEAMER","SCHRANK","FLIPPER","LOUNGE","KUECHE");
+$sw_ids=array("LAMP_VORTRAG_PWM","LAMP_TAFEL","LAMP_BEAMER","LAMP_SCHRANK","LAMP_FLIPPER","LAMP_LOUNGE","LAMP_KUECHE");
 #mpd
 $rooms=array("VORTRAG" => "6603","LOUNGE" => "6600","BASTEL" => "6602","KELLER" => 6601);
 $mpd_cmd=array("pause","play","volume","next","prev");
