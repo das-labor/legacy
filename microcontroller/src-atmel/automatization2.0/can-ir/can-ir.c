67c29be44bd35ec4b43fa2a1e751e254d6df95ee
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-12-01 13:34:41 +0000

    can't work, protocol use pulse distance encoding - last pulse of 32bit code will always be 1 or what ever because last pulse is missing. len + 4 for 2 entrys in the array is to mutch add pulse to the end to show last bit is one or zero

--- microcontroller/src-atmel/automatization2.0/can-ir/can-ir.c
+++ microcontroller/src-atmel/automatization2.0/can-ir/can-ir.c
@@ -53,7 +53,7 @@
 
 //macro for generating  extended nec encodings
 //x is the destination array, y is the input code, z is the bit count
-#define IR_GEN_NECEXT(x, y, z) (ir_genCode((uint16_t *)(x+4), PNEC_ON, PNEC_OFF, y, z) + 4); x[0] = PNEC_AGC_ON; x[1] = PNEC_AGC_OFF
+#define IR_GEN_NECEXT(x, y, z) (ir_genCode((uint16_t *)(x+4), PNEC_ON, PNEC_OFF, y, z) + 3); x[0] = PNEC_AGC_ON; x[1] = PNEC_AGC_OFF; x[(z * 2) + 3] = (560 / IR_TICK_US)
 
 
 /*
@@ -293,7 +293,7 @@ int main(void)
 	
 	//NECLen = ir_genCode((NECCode + 2), PNEC_ON, PNEC_OFF, 0b0001000011001000, 15) + 2;
 
-	/*NECLen = 3;
+	/*NECLen = 4;
 	NECCode[0] = PNEC_AGC_ON;
 	NECCode[1] = PNEC_AGC_OFF;
 	NECCode[2] = PNEC_AGC_ON;
