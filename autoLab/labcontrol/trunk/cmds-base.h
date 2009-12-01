56c176016fa99f9d41f9332c48b2a69c3a3a4283
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-12-01 17:06:01 +0000

    labcontrol out of nowhere

---
+++ autoLab/labcontrol/trunk/cmds-base.h
@@ -0,0 +1,16 @@
+#ifndef CMDSBASE_H
+#define CMDSBASE_H
+
+/**
+ * Available commands  
+ */
+
+void cmd_loopback(int argc, char *argv[]);
+void cmd_ping(int argc, char *argv[]);
+void cmd_reset(int argc, char *argv[]);
+void cmd_dump(int argc, char *argv[]);
+void cmd_packet(int argc, char *argv[]);
+void cmd_lamp(int argc,char *argv[]);
+
+
+#endif // CMDSBASE_H
---
+++ autoLab/labcontrol/trunk/debug.c
@@ -0,0 +1,3 @@
+
+#include "cand/debug.c"
+
