0f10b28cdcde78f2d18ae64704c17160a9519807
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-12-16 19:10:38 +0000

    created branch for implementing interface to treppenblink
    AM + treppen_blink_interface

---
+++ autoLab/labcontrol/branch/treppen_blink_interface/cmds-powercommander.h
@@ -0,0 +1,13 @@
+#ifndef CMDSPOWERCOMMANDER_H
+#define CMDSPOWERCOMMANDER_H
+
+/**
+ * Available commands  
+ */
+
+#define MAX_CAN_GET_TRY (100)
+
+
+void cmd_powercommander(int argc, char *argv[]);
+
+#endif // CMDSBASE_H
