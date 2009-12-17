1e36e18501608d0a9cde078708b82564c53e8e82
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-12-17 16:02:47 +0000

    treppenblink branche abgeschlossen

--- autoLab/labcontrol/branch/treppen_blink_interface/cmds-powercommander.h
+++ autoLab/labcontrol/branch/treppen_blink_interface/cmds-powercommander.h
@@ -6,6 +6,10 @@
  */
 
 #define MAX_CAN_GET_TRY (100)
+#define POWERCMD_IFACE_ADDR (0x02)
+#define POWERCMD_IFACE_PORT (0x01)
+#define POWERCMD_SENDER_ADDR (0x00)
+#define POWERCMD_SENDER_PORT (0x00)
 
 
 void cmd_powercommander(int argc, char *argv[]);
