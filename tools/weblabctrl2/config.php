7ec3a6f0bfbc6b99459f5ac65f44d9aedaa60465
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-12-17 16:48:21 +0000

    array fuer treppenblink angelegt

--- tools/weblabctrl2/config.php
+++ tools/weblabctrl2/config.php
@@ -4,6 +4,7 @@ session_start();
 #light
 $pwm_ids=array("VORTRAG_PWM","TAFEL","BEAMER","SCHRANK","FLIPPER","LOUNGE","KUECHE");
 $sw_ids=array("LAMP_VORTRAG_PWM","LAMP_TAFEL","LAMP_BEAMER","LAMP_SCHRANK","LAMP_FLIPPER","LAMP_LOUNGE","LAMP_KUECHE");
+$treppenblink_modes=array("blau" => "0x00", "stop" => "0x01", "weiss" => "0x02", "grau" => "0x03" , "aus" => "0x04", "gruen" => "0x05", "rot" => "0x06", "epi" => "0x07");
 #mpd
 $rooms=array("VORTRAG" => "6603","LOUNGE" => "6600","BASTEL" => "6602","KELLER" => 6601);
 $mpd_cmd=array("pause","play","volume","next","prev");
