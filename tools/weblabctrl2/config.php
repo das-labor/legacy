702c6b3de34e73626d8a1a030ac470b8cff28e7f
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-12-20 00:35:48 +0000

    es gibt kein video in

--- tools/weblabctrl2/config.php
+++ tools/weblabctrl2/config.php
@@ -6,7 +6,7 @@ $pwm_ids=array("VORTRAG_PWM","TAFEL","BEAMER","SCHRANK","FLIPPER","LOUNGE","KUEC
 $sw_ids=array("LAMP_VORTRAG_PWM","LAMP_TAFEL","LAMP_BEAMER","LAMP_SCHRANK","LAMP_FLIPPER","LAMP_LOUNGE","LAMP_KUECHE");
 $treppenblink_modes=array("blau" => "0x00", "stop" => "0x01", "weiss" => "0x02", "grau" => "0x03" , "aus" => "0x04", "gruen" => "0x05", "rot" => "0x06", "epi" => "0x07");
 $canir_beamer_a=array("suspend" => "0x00", "scan" => "0x01");
-$canir_beamer_a_channel=array("vga" => "0x02", "dvi" => "0x03" , "svideo" => "0x04", "video" => "0x05" , "component" => "0x06", "blank" => "0x07");
+$canir_beamer_a_channel=array("vga" => "0x02", "dvi" => "0x03" , "svideo" => "0x04", "component" => "0x05", "blank" => "0x06");
 $canir_teufel_a=array("leiser" => "0x00", "lauter" => "0x01", "mute" => "0x02");
 $canir_teufel_a_channel=array("main" => "0x03" , "front" => "0x04", "rear" => "0x05" , "side" => "0x06", "sub" => "0x07", "center" => "0x08");
 
