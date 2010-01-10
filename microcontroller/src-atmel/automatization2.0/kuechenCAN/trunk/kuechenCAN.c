01750cd2c20e793d98e74290896ffdc4b3f00015
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-01-10 23:39:58 +0000

    merged dimm

--- microcontroller/src-atmel/automatization2.0/kuechenCAN/trunk/kuechenCAN.c
+++ microcontroller/src-atmel/automatization2.0/kuechenCAN/trunk/kuechenCAN.c
@@ -1,3 +1,4 @@
+/* -*- Mode: C; tab-width: 2 -*- */
 /*
  * Femto OS v 0.91 - Copyright (C) 2008-2009 Ruud Vlaming
  *
@@ -44,6 +45,25 @@
 #define G_LED _BV(PC4)
 #define B_LED _BV(PC5)
 
+#define HOLD_THRESHOLD 18
+#define CLICK_THRESHOLD 0
+
+/*
+	XXX Powercommander.h
+*/
+#define C_SW  (0x00)
+#define C_PWM (0x01)
+#define O_SW00 (0x00)
+#define O_PWM05 (0x05)
+#define F_SW_TOGGLE   (0x03)
+#define F_PWM_MOD     (0x02)
+#define F_PWM_DIR     (0x03)
+
+#define PWM_KUECHE   (O_PWM05)
+#define SWL_KUECHE      (O_SW00)
+
+//0x02 0x05 0x02 0x00
+//0x02 0x00 0x03 0x00
 Tuint08 light=0;
 Tuint08 lastlight=0;
 Tuint08 rgbled_stat=0;
@@ -53,118 +73,155 @@ Tuint08 panic=0;
 
 // ALARM in hex
 can_message panic_msg = {0x23, 0x00, ALARMCANPORT, 0x00, 0x06, {0x41,0x4C,0x41,0x52,0x4D,0x00}};
-can_message light_on_msg = {0x23, 0x02, LIGHTCANPORT, 0x01, 0x04, {0x00,0x00,0x01,0x00}};
-can_message light_off_msg = {0x23, 0x02, LIGHTCANPORT, 0x01, 0x04, {0x00,0x00,0x00,0x00}};
+//can_message light_on_msg = {0x23, 0x02, LIGHTCANPORT, 0x01, 0x04, {0x00,0x00,0x01,0x00}};
+//can_message light_off_msg = {0x23, 0x02, LIGHTCANPORT, 0x01, 0x04, {0x00,0x00,0x00,0x00}};
+
 
 void appBoot(void)
 { 
-  spi_init();
-  can_init();
-  rgbled_stat=R_LED;
-  //Kuechenlicht
-  DDRC |=  _BV(PC5) | _BV(PC4) | _BV(PC3); // output
-  // led power on!
-  PORTC &= ~(_BV(PC4) | _BV(PC3));
-  PORTC |= _BV(PC5);
-  // button
-  DDRB &= ~_BV(PB1);      // in
-  PORTB |= _BV(PB1);      // pullup on
+	spi_init();
+	can_init();
+	rgbled_stat=R_LED;
+	//Kuechenlicht
+	DDRC |=  _BV(PC5) | _BV(PC4) | _BV(PC3); // output
+	// led power on!
+	PORTC &= ~(_BV(PC4) | _BV(PC3));
+	PORTC |= _BV(PC5);
+	// button
+	DDRB &= ~_BV(PB1);      // in
+	PORTB |= _BV(PB1);      // pullup on
 
 
-  // Alarmbutton
-  DDRD |= _BV(PD5) | _BV(PD6) | _BV(PD7);         // output
+	// Alarmbutton
+	DDRD |= _BV(PD5) | _BV(PD6) | _BV(PD7);         // output
 
-  // led power on!
-  PORTD &= ~(_BV(PD5) | _BV(PD6) | _BV(PD7));     // off
+	// led power on!
+	PORTD &= ~(_BV(PD5) | _BV(PD6) | _BV(PD7));     // off
 
 
-  //Button
-  DDRC &= ~_BV(PC0);      // in
-  PORTC |= _BV(PC0);      // pullup on
+	//Button
+	DDRC &= ~_BV(PC0);      // in
+	PORTC |= _BV(PC0);      // pullup on
 
 }
 
 /*
-  set as backgroundcolor
+	set as backgroundcolor
 */
 
 void can_user_cmd(can_message *rx_msg)
 {
-  if (rx_msg->port_dst == LIGHTCANPORT)
-    {
-      ;// what can we do
-    } 
-  else if (rx_msg->port_dst == ALARMCANPORT)
-    {
-      ;// what can we do
-    }
+	if (rx_msg->port_dst == LIGHTCANPORT)
+		{
+			;// what can we do
+		} 
+	else if (rx_msg->port_dst == ALARMCANPORT)
+		{
+			;// what can we do
+		}
 }
 
 void can_global_cmd(can_message *rx_msg)
 {
-  if (rx_msg->addr_dst == CANADDR)
-    {
-      ;
-    }
+	if (rx_msg->addr_dst == CANADDR)
+		{
+			;
+		}
 }
 
 #if (preTaskDefined(buttons))
 
 void appLoop_buttons(void)
 { 
-  while(true)
-    {
-      // lampe
-      if((PINB & _BV(PB1)) && (light==0))
-	{
-	  light=1;
-	}
-      if(!(PINB & _BV(PB1)) && (light==1))
-	{
-	  light=2;
-	}
-      if(light==2)
-	{
-	  // send can msg
-	  // switch color to indicate action
-	  if(lastlight == 0)
-	    {
-	      rgbled_stat=G_LED;
-	      can_transmit(&light_on_msg);
-	      lastlight=1;
-	    }
-	  else 
-	    {
-	      rgbled_stat=R_LED;
-	      can_transmit(&light_off_msg);
-	      lastlight=0;
-	    }
-	  light=0;
-	}
-      //panic
-      if((PINC & _BV(PC0)) && (panic==0))
-	{
-	  panic=1;
-	}
-      if(!(PINC & _BV(PC0)) && (panic==1))
-	{
-	  panic=2;
-	}
-      if(panic==2)
-	{
-	  can_transmit(&panic_msg);
-	  if(lastpanic == 0)
-	    {
-	      rgb_panic=1;
-	      lastpanic=1;
-	    }
-	  // you can send panic as offen as you want
-	  panic=0;
-	}
+	can_message dstpower = {0x23,0x02,LIGHTCANPORT,0x01,0x04, {0x00,0x00,0x00,0x00}};
+	Tuint08 counter_0;
+	Tuint08 clicked_0 = 0;
+	Tuint08 held_0    = 0;
+	Tuint08 last_held_0;
+
+	while(true)
+		{
+			// lampe
+			clicked_0=0;
+			held_0=0;
+			if(!(PINB & _BV(PB1))) 
+				{
+					counter_0++;
+					if(counter_0 > HOLD_THRESHOLD)
+						{
+							held_0 = 1;
+							counter_0 = HOLD_THRESHOLD;
+						}
+				} 
+			else
+				{
+					if(counter_0 > CLICK_THRESHOLD)
+						{
+							if(counter_0 < HOLD_THRESHOLD)
+								{
+									clicked_0 = 1;
+								}
+						}
+					counter_0 = 0;
+				}
+			
+			if(clicked_0 == 1)
+				{
+					dstpower.data[0] = C_SW;
+					dstpower.data[1] = SWL_KUECHE;
+					dstpower.data[2] = F_SW_TOGGLE;
+					if(rgbled_stat==R_LED)
+						{
+							rgbled_stat=G_LED;
+						}
+					else
+						{
+							rgbled_stat=R_LED;
+						}
+					can_transmit(&dstpower);
+				}
+		
+			if(held_0)
+				{
+					dstpower.data[0] = C_PWM;
+					dstpower.data[1] = PWM_KUECHE;
+					dstpower.data[2] = F_PWM_MOD;
+					can_transmit(&dstpower);
+				}
+			else if(last_held_0)
+				{
+					dstpower.data[0] = C_PWM;
+					dstpower.data[1] = PWM_KUECHE;
+					dstpower.data[2] = F_PWM_DIR;
+					can_transmit(&dstpower);
+				}
+		
+			last_held_0 = held_0;
+
+			//panic
+			if((PINC & _BV(PC0)) && (panic==0))
+				{
+					panic=1;
+				}
+			if(!(PINC & _BV(PC0)) && (panic==1))
+				{
+					panic=2;
+				}
+			if(panic==2)
+				{
+					can_transmit(&panic_msg);
+					if(lastpanic == 0)
+						{
+							rgb_panic=1;
+							lastpanic=1;
+						}
+					// you can send panic as offen as you want
+					panic=0;
+				}
 
 
-      taskDelayFromNow(100);
-    }
+			taskDelayFromNow(100);
+		}
 
 }
 
@@ -174,18 +231,18 @@ void appLoop_buttons(void)
 
 void appLoop_alarmt(void)
 { 
-  while(true)
-    {
-      if(lastpanic==1)
-	{
-	  // 10 sec blinken
-	  taskDelayFromNow(10000);
-	  rgb_panic=0;
-	  lastpanic=0;
-	}
-      taskDelayFromNow(100);
-      
-    }
+	while(true)
+		{
+			if(lastpanic==1)
+				{
+					// 10 sec blinken
+					taskDelayFromNow(10000);
+					rgb_panic=0;
+					lastpanic=0;
+				}
+			taskDelayFromNow(100);
+			
+		}
 
 }
 
@@ -196,22 +253,22 @@ void appLoop_alarmt(void)
 
 void appLoop_alarm(void)
 { 
-  while(true)
-    {
-      if(lastpanic==1)
-	{
-	  if(rgb_panic == 1)
-	    {
-	      rgb_panic=2;
-	    }
-	  taskDelayFromNow(500);
-	  if(rgb_panic == 2)
-	    {
-	      rgb_panic=1;
-	    }
-	}
-      taskDelayFromNow(100);
-    }
+	while(true)
+		{
+			if(lastpanic==1)
+				{
+					if(rgb_panic == 1)
+						{
+							rgb_panic=2;
+						}
+					taskDelayFromNow(500);
+					if(rgb_panic == 2)
+						{
+							rgb_panic=1;
+						}
+				}
+			taskDelayFromNow(100);
+		}
 
 }
 
@@ -222,48 +279,48 @@ void appLoop_alarm(void)
 
 void appLoop_leds(void)
 { 
-  while(true)
-    {
-      if((rgbled_stat & R_LED) !=0)
-	{
-	  PORTC |= R_LED;
-	}
-      else
-	{
-	  PORTC &= ~R_LED;
-	}
-      if((rgbled_stat & G_LED) !=0)
-	{
-	  PORTC |= G_LED;
-	}
-      else
-	{
-	  PORTC &= ~G_LED;
-	}
-      if( (rgbled_stat & B_LED) !=0)
-	{
-	  PORTC |= B_LED;
-	}
-      else
-	{
-	  PORTC &= ~B_LED;
-	}
+	while(true)
+		{
+			if((rgbled_stat & R_LED) !=0)
+				{
+					PORTC |= R_LED;
+				}
+			else
+				{
+					PORTC &= ~R_LED;
+				}
+			if((rgbled_stat & G_LED) !=0)
+				{
+					PORTC |= G_LED;
+				}
+			else
+				{
+					PORTC &= ~G_LED;
+				}
+			if( (rgbled_stat & B_LED) !=0)
+				{
+					PORTC |= B_LED;
+				}
+			else
+				{
+					PORTC &= ~B_LED;
+				}
 
-      if( rgb_panic == 1 )
-	{
-	  PORTD |= _BV(PD5);
-	  PORTD |= _BV(PD6);
-	  PORTD |= _BV(PD7);
-	}
-      if( rgb_panic == 2 )
-	{
-	  PORTD &= ~_BV(PD5);
-	  PORTD &= ~_BV(PD6);
-	  PORTD &= ~_BV(PD7);
-	}
-      
-      taskDelayFromNow(100);
-    }
+			if( rgb_panic == 1 )
+				{
+					PORTD |= _BV(PD5);
+					PORTD |= _BV(PD6);
+					PORTD |= _BV(PD7);
+				}
+			if( rgb_panic == 2 )
+				{
+					PORTD &= ~_BV(PD5);
+					PORTD &= ~_BV(PD6);
+					PORTD &= ~_BV(PD7);
+				}
+			
+			taskDelayFromNow(100);
+		}
 }
 
 #endif
