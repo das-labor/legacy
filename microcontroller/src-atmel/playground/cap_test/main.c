b83f91c143b72e02b647ce654deeac89310de3c5
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-01-11 06:24:59 +0000

    changed code for new illuminated sensor

--- microcontroller/src-atmel/playground/cap_test/main.c
+++ microcontroller/src-atmel/playground/cap_test/main.c
@@ -1,19 +1,23 @@
 
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 // code by tixiv
 //
 // cap 100n connected between PD5 and PD6.
 // PD 6 is additionally connected to capacitive sensor plate.
-// code charges plate by applying VCC on PD6, then charges this charge into
-// the 100n cap between PD5 and PD6. It is counted how many cycles are needed 
+// code charges plate by applying VCC on PC3, then charges this charge into
+// the 100n cap between PC2 and PC3. It is counted how many cycles are needed 
 // to charge the 100n cap. 
 //
 //  Indicator LED connected to PORTA
+//
+// LEDs in proximity to sensor are connected to PC4(Anode) and PC5(Cathode)
 
 
-#define FILTER_CONSTANT 32768ul
+#define FILTER_CONSTANT 8192ul
+//#define FILTER_CONSTANT 1024ul
 
 int main()
 {
@@ -21,44 +25,52 @@ int main()
 	DDRB |= _BV(PB0);
 
 
-	uint32_t filt = FILTER_CONSTANT * 400;
+	uint32_t filt = FILTER_CONSTANT * 600;
 	
 	uint16_t thresh;
-
+	uint8_t output = 0;
 
 	while (1)
 	{
 		uint16_t counter = 0;
 		
-		DDRC |= _BV(PC3) | _BV(PC2);
 		
-		__asm volatile ( "nop");
-		__asm volatile ( "nop");
-		__asm volatile ( "nop");
-		__asm volatile ( "nop");
-		__asm volatile ( "nop");
-		
+		DDRC |= _BV(PC3) | _BV(PC2) ; //discharge cap
+
+		_delay_us(10);		
 		
 		do
 		{
 			counter++;
-			DDRC = 0;
+			DDRC &= ~(_BV(PC2) | _BV(PC3));//discharge off from last cycle
+
+			__asm volatile ( "nop");
 			
-			PORTC = _BV(PC3);
-			DDRC = _BV(PC3);
-			DDRC = 0;
-			PORTC = 0;
+			PORTC |= _BV(PC3) ;//charge cap 
+			if(!output) PORTC |=  _BV(PC4); //activate illumination if not touched
+			DDRC |= _BV(PC3) | _BV(PC4) | _BV(PC5) ;
+			__asm volatile ( "nop");
+			__asm volatile ( "nop");
+			__asm volatile ( "nop");
+			__asm volatile ( "nop");
+			__asm volatile ( "nop");
 			
-			DDRC  = _BV(PC2);
+			PORTC &= ~ _BV(PC4);
+			
+			__asm volatile ( "nop");
+			__asm volatile ( "nop");
+			
+			DDRC &= ~(_BV(PC3) | _BV(PC4) | _BV(PC5)); //release lines
+			PORTC &= ~(_BV(PC3) | _BV(PC4));
+			
+			DDRC  |= _BV(PC2);//discharge plate to 100n cap
 			__asm volatile ( "nop");
 			__asm volatile ( "nop");
 		} while ((PINC & _BV(PC3)) == 0);
 
-		DDRC = 0;
+		DDRC &= ~_BV(PC2);//discharge off
 		
 
-
-	
 		thresh = filt/FILTER_CONSTANT;
 		
 		filt -= thresh;
@@ -67,10 +79,12 @@ int main()
 
 		if (counter > (thresh-10) )
 		{
+			output = 0;
 			PORTB &= ~_BV(PB0);
 		}
-		else if (counter < (thresh - 30) )
+		else if (counter < (thresh - 20) )
 		{
+			output = 1;
 			PORTB |= _BV(PB0);
 		}
 	
