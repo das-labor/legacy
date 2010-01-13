de02d459c93e22e0ea5921b6ee03c4d405b5c46b
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-01-13 20:59:04 +0000

    twi_master eingebaut im treppenblink wird getestet ob es geht

--- microcontroller/src-atmel/femtoos/branches/twi-master/include/twi_master.h
+++ microcontroller/src-atmel/femtoos/branches/twi-master/include/twi_master.h
@@ -1,5 +1,8 @@
+#include "femtoos_code.h"
+#ifdef TWI_MTHREAD
 /*******************************************************
  Author:					Manfred Langemann
+                                                Alexander Kasper - ported to femtoos
  mailto:					Manfred.Langemann ät t-online.de
  Begin of project:			04.01.2008
  Latest version generated:	04.01.2008
@@ -12,11 +15,33 @@
 
 #include <util/twi.h>
 
-extern uint8_t TWIM_Init(uint32_t TWI_Bitrate);
-extern uint8_t TWIM_Start(uint8_t address, uint8_t TWIM_Type);
+extern Tuint08 TWIM_Init(); // is in config_application
+extern Tuint08 TWIM_Start(Tuint08 address, Tuint08 TWIM_Type);
 extern void TWIM_Stop(void);
-extern uint8_t TWIM_Write(uint8_t byte);
-extern uint8_t TWIM_ReadAck(void);
-extern uint8_t TWIM_ReadNack(void);
+extern Tuint08 TWIM_Write(Tuint08 byte);
+extern Tuint08 TWIM_ReadAck(void);
+extern Tuint08 TWIM_ReadNack(void);
+
+
+#define PROCESSI2CNONE  (0b00000000)
+#define PROCESSI2CREAD  (0b00000001)
+#define PROCESSI2CWRITE (0b00000010)
+
+#define EI2CNONE        (0b00000000)
+#define EI2CREAD        (0b00000001)
+#define EI2CWRITE       (0b00000010)
+
+
+typedef Tuint08 i2c_addr;
+
+typedef struct {
+  i2c_addr      addr_dst;
+  Tuint08       process;
+  Tuint08       dlc;
+  Tuint08       data[8];
+
+} i2c_message;
+
+#endif
 
 #endif
