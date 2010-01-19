a4de067ec6e17fdda7c15c5381b36be9d518ba89
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-01-19 09:55:28 +0000

    done

--- microcontroller/src-atmel/borg/borgware-2d/games/breakout/playfield.h
+++ microcontroller/src-atmel/borg/borgware-2d/games/breakout/playfield.h
@@ -2,6 +2,10 @@
 #define PLAYFIELD_H
 #include "common.h"
 
+#define BOUNCE_X 0x01
+#define BOUNCE_Y 0x02
+#define BOUNCE_UNDEF 0x04
+
 /* entries for the playing field */
 enum game_field_t
 {
@@ -23,6 +27,6 @@ void playfield_set (uint8_t in_x, uint8_t in_y, enum game_field_t in_field);
 
 /* @description Checks if there is an object in the way. If so, it returns 1
  */
-uint8_t check_bounce (uint8_t in_x, uint8_t in_y);
+uint8_t check_bounce (int8_t in_x, int8_t in_y);
 
 #endif /* PLAYFIELD_H */
