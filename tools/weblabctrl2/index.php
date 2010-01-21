d1f65c5fa0def9ae139626c07a9f3d8dbdced1de
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-01-21 20:14:55 +0000

    style

--- tools/weblabctrl2/index.php
+++ tools/weblabctrl2/index.php
@@ -6,129 +6,28 @@ include "config.php";
 <script src="scriptaculous/prototype.js" type="text/javascript"></script>
 <script src="scriptaculous/scriptaculous.js" type="text/javascript"></script>
 <script src="weblabctrl.js" type="text/javascript"></script>
-<?
-if($_COOKIE['bg']) $bg=$_COOKIE['bg'];
- else $bg='dwarf';
-if($_COOKIE['toiletlamp']) $toiletlamp=$_COOKIE['toiletlamp'];
- else $toiletlamp=0;
-if($_COOKIE['pwmx']) $pwmx=$_COOKIE['pwmx'];
- else $pwmx=50;
-if($_COOKIE['pwmy']) $pwmy=$_COOKIE['pwmy'];
- else $pwmy=60;
-if($_COOKIE['mpdx']) $mpdx=$_COOKIE['mpdx'];
- else $mpdx=350;
-if($_COOKIE['mpdy']) $mpdy=$_COOKIE['mpdy'];
- else $mpdy=60;
-if($_COOKIE['miscx']) $miscx=$_COOKIE['miscx'];
- else $miscx=650;
-if($_COOKIE['miscy']) $miscy=$_COOKIE['miscy'];
- else $miscy=60;
-echo "<style type=\"text/css\">
-  * {font-family:Arial;}
-  div.slider { width:256px; margin:10px 0; background-color:#ccc; height:10px; position: relative; }
-  div.slider div.handle { width:10px; height:15px; background-color:#f00; cursor:move; position: absolute; }
-  div.pwm {position:absolute; left:".$pwmx."; top: ".$pwmy.";    background-image:url(divbg256.png); background-repeat:repeat-y; background-color: transparent; width: 256px; padding:10px;}
-  div.pwm_top {background-color:#ccccff; width: 256px; margin-bottom:5px;}
-  div.mpd {position:absolute; left:".$mpdx."; top: ".$mpdy.";    background-image:url(divbg256.png); background-repeat:repeat-y; background-color: transparent; width: 256px; padding:10px;}
-  div.mpd_top {background-color:#ccccff; width: 256px; margin-bottom:5px;}
-  div.misc {position:absolute; left:".$miscx."; top: ".$miscy."; background-image:url(divbg256.png); background-repeat:repeat-y; background-color: transparent; width: 256px; padding:10px;}
-  div.misc_top {background-color:#ccccff; width: 256px; margin-bottom:5px;}
-  body {margin: 0px; padding: 0px; background-image:url(bg/11.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.eins {background-size: auto; width: 100%; height: 100%; margin: 0px; padding: 0px; background-image:url(bg/1.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.zwei {margin: 0px; padding: 0px; background-image:url(bg/2.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.drei {margin: 0px; padding: 0px; background-image:url(bg/3.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.vier {margin: 0px; padding: 0px; background-image:url(bg/4.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.fuenf {margin: 0px; padding: 0px; background-image:url(bg/5.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.sechs {margin: 0px; padding: 0px; background-image:url(bg/6.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.sieben {margin: 0px; padding: 0px; background-image:url(bg/7.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.acht {margin: 0px; padding: 0px; background-image:url(bg/8.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.neun {margin: 0px; padding: 0px; background-image:url(bg/9.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.zehn {margin: 0px; padding: 0px; background-image:url(bg/10.jpg);background-repeat:no-repeat;background-color:#000000;}
-  body.dwarf {margin: 0px; padding: 0px; background-image:url(bg/11.jpg);background-repeat:no-repeat;background-color:#000000;}
-input.VORTRAG_button { border-size: 0px; border-style: none; background: transparent; cursor: hand; padding: 0px;}
-
-
-  input {}
-</style>";
-?>
+<link rel="stylesheet" type="text/css" href="weblabcss.php">
 </head>
 <body class="" id="body" name="body">
-
-
 <script>
 <?
   echo "set_bg('".$bg."');";
 ?>
-function movepic(img_name,img_src) {
-  document[img_name].src=img_src;
-}
+function rollelement(myelement,maxsize)
+{
+  if(document.getElementById(myelement).style.height=='0px') 
+    {
+      document.getElementById(myelement).style.height=maxsize;
+    }
+  else 
+    {
+      document.getElementById(myelement).style.height='0px';
+    }
 
-function set_value(cmd,id,value)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd='+cmd+'&id='+id+'&value='+value,{method:'get', onComplete:function() {done=true;}, evalScripts: true} );
-}
-function toilet_cmd(id,value)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=toilet&id='+id+'&value='+value,{method:'get', onComplete:function() {done=true;}, evalScripts: true} );
-}
-function treppenblink_cmd(value)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=treppenblink&value='+value,{method:'get', onComplete:function() {done=true;}} );
-}
-function canir_beamer_cmd(value)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=canir_beamer&value='+value,{method:'get', onComplete:function() {done=true;}} );
-}
-function canir_teufel_cmd(value)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=canir_teufel&value='+value,{method:'get', onComplete:function() {done=true;}} );
-}
-function mpd_cmd(room,cmd,value)
-{
-  new Ajax.Updater('ajax', 'mpd.php?cmd='+cmd+'&room='+room+'&value='+value,{method:'get', onComplete:function() {done=true;}} );
-}
-function beamer_on()
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=beamer_on',{method:'get', onComplete:function() {done=true;}} );
-}
-function coinflip()
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=coinflip',{method:'get', onComplete:function() {done=true;}} );
-}
-function dice()
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=dice',{method:'get', onComplete:function() {done=true;}} );
-}
-function w20()
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=w20',{method:'get', onComplete:function() {done=true;}} );
-}
-function text_the_borg(text)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=text_the_borg&text='+text,{method:'get', onComplete:function() {done=true;}} );
-}
-function save_pos(element)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=save_pos&x='+document.getElementById(element.id).style.left+'&y='+document.getElementById(element.id).style.top,{method:'get', onComplete:function() {done=true;}} );
-}
-function set_bg(whichImage){
-  new Ajax.Updater('ajax', 'set.php?cmd=save_bg&bg='+whichImage,{method:'get', onComplete:function() {done=true;}} );
-  document.getElementById('body').className=whichImage;
-
-}
-function blue_mode(mode)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=blue_mode&mode='+mode,{method:'get', onComplete:function() {done=true;}} );
-}
-function moodbar(r,g,b)
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=moodbar&r='+r+'&g='+g+'&b='+b,{method:'get', onComplete:function() {done=true;}} );
-}
-function moodbar_fade()
-{
-  new Ajax.Updater('ajax', 'set.php?cmd=moodbar_fade',{method:'get', onComplete:function() {done=true;}} );
 }
 </script>
+
+
 <div id="ajax">
 #debug
 </div>
@@ -162,11 +61,12 @@ foreach($pwm_ids as $id)
         </div>\n";
         if($id == 'VORTRAG_PWM'){
 echo "
-<input type=\"image\" src=\"blue_mode_off.png\" onclick=\"blue_mode('off');\">
-<input type=\"image\" src=\"blue_mode_on.png\" onclick=\"blue_mode('on');\">
-<input type=\"image\" src=\"blue_mode_video_conference.png\" onclick=\"blue_mode('video_conference');\">
-<input type=\"image\" src=\"blue_mode_speaker.png\" onclick=\"blue_mode('speaker');\">
-<input type=\"image\" src=\"blue_mode_dimm.png\" onclick=\"blue_mode('dimm');\">";
+<table width=\"100%\"><tr><td align=center>
+<input style=\"margin: 0px; padding: 0px; background-color: transparent;\" type=\"image\" src=\"blue_mode_off.png\" onclick=\"blue_mode('off');\">
+<input style=\"margin: 0px; padding: 0px; background-color: transparent;\" type=\"image\" src=\"blue_mode_on.png\" onclick=\"blue_mode('on');\">
+<input style=\"margin: 0px; padding: 0px; background-color: transparent;\" type=\"image\" src=\"blue_mode_video_conference.png\" onclick=\"blue_mode('video_conference');\">
+<input style=\"margin: 0px; padding: 0px; background-color: transparent;\" type=\"image\" src=\"blue_mode_speaker.png\" onclick=\"blue_mode('speaker');\">
+<input style=\"margin: 0px; padding: 0px; background-color: transparent;\" type=\"image\" src=\"blue_mode_dimm.png\" onclick=\"blue_mode('dimm');\"></td></tr></table>";
 	}
 
 }
@@ -198,25 +98,16 @@ foreach($rooms as $room => $port)
 	#$color="#".dechex(rand(0,255)).dechex(rand(0,255)).dechex(rand(0,255));
 	echo "$room";
 	echo "<table width=100%><tr>";
-#	echo "<td><input type=\"button\" id=\"".$room."_pause\" class=\"".$room."_button\" onclick=\"mpd_cmd('$room','pause');\" value=\"pause\"></td>";
 	echo "<td><input src=\"pause_h.png\" type=\"image\" name=\"".$room."_pause\" id=\"".$room."_pause\" class=\"".$room."_button\" style=\"background-color: transparent;\" onclick=\"mpd_cmd('$room','pause'); \" value=\"pause\"></td>";
 	echo "<td><input src=\"play_h.png\" type=\"image\" name=\"".$room."_play\" id=\"".$room."_play\" class=\"".$room."_button\" style=\"background-color: transparent;\" onclick=\"mpd_cmd('$room','play'); \" value=\"play\"></td>";
-
-
-#	echo "<td><input type=\"button\" id=\"".$room."_play\" class=\"".$room."_button\" onclick=\"mpd_cmd('$room','play');\" value=\"play\"></td>";
 	echo "<td><input src=\"rev_h.png\" type=\"image\" name=\"".$room."_prev\" id=\"".$room."_prev\" class=\"".$room."_button\" style=\"background-color: transparent;\" onclick=\"mpd_cmd('$room','prev'); \" value=\"prev\"></td>";
 	echo "<td><input src=\"ff_h.png\" type=\"image\" name=\"".$room."_next\" id=\"".$room."_next\" class=\"".$room."_button\" style=\"background-color: transparent;\" onclick=\"mpd_cmd('$room','next'); \" value=\"next\"></td>";
-
-#	echo "<td><input type=\"button\" id=\"".$room."_prev\" class=\"".$room."_button\" onclick=\"mpd_cmd('$room','prev');\" value=\"prev\"></td>";
-#	echo "<td><input type=\"button\" id=\"".$room."_next\" class=\"".$room."_button\" onclick=\"mpd_cmd('$room','next');\" value=\"next\"></td><br>";
 	echo "</tr></table>";
 	echo "<div id=\"".$room."_status\"></div>";
-	#if(strpos($status[0],"volume")===false) echo substr(htmlspecialchars($status[0]),0,24);
 	echo "<div style=\"height:15px; background-image:url(sliderbg.png); background-repeat:repeat-x; background-color: transparent;\" id=\"slider_mpd_volume_$room\" class=\"slider\"> 
             <div class=\"handle\" style=\"width:15px; background-image:url(1up.png); background-repeat:no-repeat; background-color: transparent; \"></div>
         </div>\n";
 	$color=sprintf('#%02X%02X%02X',rand(0,255),rand(0,255),rand(0,255));
-	#$color="#".dechex(rand(0,255)).dechex(rand(0,255)).dechex(rand(0,255));
 	echo "<div style=\"height:15px; background-image:url(sliderbg.png); background-repeat:repeat-x; background-color: transparent;\"  id=\"slider_mpd_process_$room\" class=\"slider\"> 
             <div class=\"handle\" style=\"width:15px; background-image:url(1up.png); background-repeat:no-repeat; background-color: transparent; \"></div>
         </div>\n";
@@ -225,8 +116,54 @@ foreach($rooms as $room => $port)
 </div>
 </div>
 
-<div id="misc" class="misc">
-<div class="misc_top">Misc</div>
+<div id="treppenblink" class="treppenblink">
+    <div class="treppenblink_top" ondblclick="rollelement('treppenblink_cont','80px')">Treppenblink</div>
+    <div id="treppenblink_cont" style="overflow: hidden;" name="treppenblink_cont">
+<?
+# button oben, text drunter
+  $foo=0;
+    echo "<table width=100%>";
+  echo "<tr>";
+  foreach ($treppenblink_modes as $tbm_key => $tbm_value){
+    $foo++;
+    echo "<td align=\"center\"><input type=\"radio\" onclick=\"treppenblink_cmd('".$tbm_value."');\" name=\"treppenblink\" value=\"".$tbm_value."\"><br>".$tbm_key."</td>";
+    if($foo == 4){
+	$foo=0;
+	echo "</tr><tr>";
+    }
+
+  }
+  if($foo != 0)  echo "</tr><tr>";
+  foreach ($treppenblink_modes as $tbm_key => $tbm_value)
+#   echo "<td>".$tbm_key."</td>";
+ echo "</tr></table>";
+# twitter
+# curl --basic --user username:password --data status="Having fun with cURL" http://twitter.com/statuses/update.xml
+?>
+</div>
+</div>
+
+
+<div id="mybackground" class="mybackground">
+<div class="mybackground_top" ondblclick="rollelement('mybackground_cont','150px')">Background</div>
+    <div id="mybackground_cont" style="overflow: hidden;" name="mybackground_cont">
+<input type="image" src="bg/1_small.jpg" onclick="set_bg('eins')">
+<input type="image" src="bg/2_small.jpg" onclick="set_bg('zwei');">
+<input type="image" src="bg/3_small.jpg" onclick="set_bg('drei');">
+<input type="image" src="bg/4_small.jpg" onclick="set_bg('vier');">
+<input type="image" src="bg/5_small.jpg" onclick="set_bg('fuenf');">
+<input type="image" src="bg/6_small.jpg" onclick="set_bg('sechs');">
+<input type="image" src="bg/7_small.jpg" onclick="set_bg('sieben');">
+<input type="image" src="bg/8_small.jpg" onclick="set_bg('acht');">
+<input type="image" src="bg/9_small.jpg" onclick="set_bg('neun');">
+<input type="image" src="bg/10_small.jpg" onclick="set_bg('zehn');">
+<input type="image" src="bg/11_small.jpg" onclick="set_bg('dwarf');">
+</div>
+</div>
+
+<div id="vortragsdevs" class="vortragsdevs">
+<div class="vortragsdevs_top" ondblclick="rollelement('vortragsdevs_cont','170px')">Hardware Vortragsraum</div>
+    <div id="vortragsdevs_cont" style="overflow: hidden;" name="vortragsdevs_cont">
 Beamer<br>
 <?
   $beamer_counter=1;
@@ -278,28 +215,13 @@ echo "source select: <select onchange=\"CheckTeufelSource()\" name=\"teufelSourc
  echo "</select>";
 echo "</form>";
 ?>
+</div>
+</div>
+
+<div id="misc" class="misc">
+<div class="misc_top">Misc</div>
 Laufschriftborg<br>
 <input type="text"  onchange="text_the_borg(this.value);this.value='';" value="">
-<br>Treppenblink<br>
-<?
-# button oben, text drunter
-  $foo=0;
-    echo "<table width=100%>";
-  echo "<tr>";
-  foreach ($treppenblink_modes as $tbm_key => $tbm_value){
-    $foo++;
-    echo "<td align=\"center\"><input type=\"radio\" onclick=\"treppenblink_cmd('".$tbm_value."');\" name=\"treppenblink\" value=\"".$tbm_value."\"><br>".$tbm_key."</td>";
-    if($foo == 4){
-	$foo=0;
-	echo "</tr><tr>";
-    }
-
-  }
-  if($foo != 0)  echo "</tr><tr>";
-  foreach ($treppenblink_modes as $tbm_key => $tbm_value)
-#   echo "<td>".$tbm_key."</td>";
- echo "</tr></table>";
-?>
 <br>ToiletLamp<br>
 <table>
   <tr>
@@ -329,18 +251,6 @@ Laufschriftborg<br>
      <td><input type="button" id="w20" onclick="w20();" value="w20"></td>
    </tr>
 </table>
-Background Image<br>
-<input type="image" src="bg/1_small.jpg" onclick="set_bg('eins')">
-<input type="image" src="bg/2_small.jpg" onclick="set_bg('zwei');">
-<input type="image" src="bg/3_small.jpg" onclick="set_bg('drei');">
-<input type="image" src="bg/4_small.jpg" onclick="set_bg('vier');">
-<input type="image" src="bg/5_small.jpg" onclick="set_bg('fuenf');">
-<input type="image" src="bg/6_small.jpg" onclick="set_bg('sechs');">
-<input type="image" src="bg/7_small.jpg" onclick="set_bg('sieben');">
-<input type="image" src="bg/8_small.jpg" onclick="set_bg('acht');">
-<input type="image" src="bg/9_small.jpg" onclick="set_bg('neun');">
-<input type="image" src="bg/10_small.jpg" onclick="set_bg('zehn');">
-<input type="image" src="bg/11_small.jpg" onclick="set_bg('dwarf');">
 
 </div>
 </form>
@@ -449,6 +359,9 @@ echo "}\nupdate_status();";
 new Draggable('pwm',{onEnd:function(element){new Ajax.Updater('ajax', 'set.php?cmd=save_pos&div=pwm&x='+document.getElementById('pwm').style.left+'&y='+document.getElementById('pwm').style.top,{method:'get', onComplete:function() {done=true;}} );}});
 new Draggable('mpd',{onEnd:function(element){new Ajax.Updater('ajax', 'set.php?cmd=save_pos&div=mpd&x='+document.getElementById('mpd').style.left+'&y='+document.getElementById('mpd').style.top,{method:'get', onComplete:function() {done=true;}} );}});
 new Draggable('misc',{onEnd:function(element){new Ajax.Updater('ajax', 'set.php?cmd=save_pos&div=misc&x='+document.getElementById('misc').style.left+'&y='+document.getElementById('misc').style.top,{method:'get', onComplete:function() {done=true;}} );}});
+new Draggable('treppenblink',{onEnd:function(element){new Ajax.Updater('ajax', 'set.php?cmd=save_pos&div=treppenblink&x='+document.getElementById('treppenblink').style.left+'&y='+document.getElementById('treppenblink').style.top,{method:'get', onComplete:function() {done=true;}} );}});
+new Draggable('mybackground',{onEnd:function(element){new Ajax.Updater('ajax', 'set.php?cmd=save_pos&div=mybackground&x='+document.getElementById('mybackground').style.left+'&y='+document.getElementById('mybackground').style.top,{method:'get', onComplete:function() {done=true;}} );}});
+new Draggable('vortragsdevs',{onEnd:function(element){new Ajax.Updater('ajax', 'set.php?cmd=save_pos&div=vortragsdevs&x='+document.getElementById('vortragsdevs').style.left+'&y='+document.getElementById('vortragsdevs').style.top,{method:'get', onComplete:function() {done=true;}} );}});
 
 var done=true;
 </script>
--- tools/weblabctrl2/weblabctrl.js
+++ tools/weblabctrl2/weblabctrl.js
@@ -1,2 +1,71 @@
+function movepic(img_name,img_src) {
+  document[img_name].src=img_src;
+}
+
+function set_value(cmd,id,value)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd='+cmd+'&id='+id+'&value='+value,{method:'get', onComplete:function() {done=true;}, evalScripts: true} );
+}
+function toilet_cmd(id,value)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=toilet&id='+id+'&value='+value,{method:'get', onComplete:function() {done=true;}, evalScripts: true} );
+}
+function treppenblink_cmd(value)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=treppenblink&value='+value,{method:'get', onComplete:function() {done=true;}} );
+}
+function canir_beamer_cmd(value)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=canir_beamer&value='+value,{method:'get', onComplete:function() {done=true;}} );
+}
+function canir_teufel_cmd(value)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=canir_teufel&value='+value,{method:'get', onComplete:function() {done=true;}} );
+}
+function mpd_cmd(room,cmd,value)
+{
+  new Ajax.Updater('ajax', 'mpd.php?cmd='+cmd+'&room='+room+'&value='+value,{method:'get', onComplete:function() {done=true;}} );
+}
+function beamer_on()
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=beamer_on',{method:'get', onComplete:function() {done=true;}} );
+}
+function coinflip()
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=coinflip',{method:'get', onComplete:function() {done=true;}} );
+}
+function dice()
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=dice',{method:'get', onComplete:function() {done=true;}} );
+}
+function w20()
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=w20',{method:'get', onComplete:function() {done=true;}} );
+}
+function text_the_borg(text)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=text_the_borg&text='+text,{method:'get', onComplete:function() {done=true;}} );
+}
+function save_pos(element)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=save_pos&x='+document.getElementById(element.id).style.left+'&y='+document.getElementById(element.id).style.top,{method:'get', onComplete:function() {done=true;}} );
+}
+function set_bg(whichImage){
+  new Ajax.Updater('ajax', 'set.php?cmd=save_bg&bg='+whichImage,{method:'get', onComplete:function() {done=true;}} );
+  document.getElementById('body').className=whichImage;
+
+}
+function blue_mode(mode)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=blue_mode&mode='+mode,{method:'get', onComplete:function() {done=true;}} );
+}
+function moodbar(r,g,b)
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=moodbar&r='+r+'&g='+g+'&b='+b,{method:'get', onComplete:function() {done=true;}} );
+}
+function moodbar_fade()
+{
+  new Ajax.Updater('ajax', 'set.php?cmd=moodbar_fade',{method:'get', onComplete:function() {done=true;}} );
+}
 
 
