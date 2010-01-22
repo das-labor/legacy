cdb11b5f1e3829036a88631edcbc708b118c6dd6
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-01-22 14:36:20 +0000

    add Can Ventil Stellantrieb Temperatur Regler

---
+++ microcontroller/src-atmel/automatization2.0/cvstatr/trunk/main.c
@@ -0,0 +1,56 @@
+/* -*- Mode: C; tab-width: 2 -*- */
+
+#include <avr/io.h>
+
+#include "config.h"
+
+#include "twi_master/twi_master.h"
+#include "can/can.h"
+#include "can_handler.h"
+#include "can/spi.h"
+#include "can/lap.h"
+#include "i2c_temp.h"
+
+
+void init(void)
+{
+	/*
+	** Initiate TWI Master Interface
+	*/
+	if (!TWIM_Init())
+	{
+		while (1);
+	}
+	
+	init_sensor();
+	
+	ACSR = _BV(ACD); // Disable Analog Comparator (power save)
+
+	DDRB &= ~_BV(PB1);
+	PORTB |= _BV(PB1);
+
+	//initialize spi port
+	spi_init();
+	
+	//initialize can communication
+	can_init();
+	read_can_addr();
+	//turn on interrupts
+  //	sei();
+}
+	 
+int main(void)
+{
+	//system initialization
+	init();
+
+	//the main loop continuously handles can messages
+	while (1)
+	{
+		can_handler();
+		switch_handler();
+	}
+	
+	return 1;
+}
+
