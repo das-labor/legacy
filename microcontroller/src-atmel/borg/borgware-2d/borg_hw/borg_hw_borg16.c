e5fa581fd78ad39b18fb012d68ee7f22e9be2139
kju <kju@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-01-29 03:05:49 +0000

    add atmega644p support for borg16 (untested)

--- microcontroller/src-atmel/borg/borgware-2d/borg_hw/borg_hw_borg16.c
+++ microcontroller/src-atmel/borg/borgware-2d/borg_hw/borg_hw_borg16.c
@@ -32,6 +32,13 @@
 #define COLDDR2  DDR(COLPORT2)
 #define ROWDDR   DDR(ROWPORT)
 
+#ifdef __AVR_ATmega644P__
+/* more ifdef magic :-( */
+#define OCR0 OCR0A
+#define SIG_OUTPUT_COMPARE0 SIG_OUTPUT_COMPARE0A
+#endif
+
+
 //Der Puffer, in dem das aktuelle Bild gespeichert wird
 unsigned char pixmap[NUMPLANE][NUM_ROWS][LINEBYTES];
 
@@ -161,7 +168,13 @@ void timer0_off(){
 	COLPORT2 = 0;
 	ROWPORT = 0;
 
+#ifdef __AVR_ATmega644P__
+        TCCR0A = 0x00;
+        TCCR0B = 0x00;
+#else
+                        
 	TCCR0 = 0x00;
+#endif
 	sei();
 }
 
@@ -178,10 +191,18 @@ void timer0_on(){
 		 1    0    1       clk/1024
 	
 */
-	TCCR0 = 0x0C;	// CTC Mode, clk/64
+#ifdef __AVR_ATmega644P__
+        TCCR0A = 0x02;  // CTC Mode
+        TCCR0B = 0x04;  // clk/256
+	TCNT0  = 0;	// reset timer
+	OCR0   = 20;	// Compare with this value
+	TIMSK0 = 0x02;	// Compare match Interrupt on
+#else
+	TCCR0 = 0x0C;	// CTC Mode, clk/256
 	TCNT0 = 0;	// reset timer
 	OCR0  = 20;	// Compare with this value
 	TIMSK = 0x02;	// Compare match Interrupt on
+#endif
 }
 
 void borg_hw_init(){
