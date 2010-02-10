318626d2c6a0c07ac374ca68e243900703290693
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-02-10 19:34:47 +0000

    branch fuer negative temperaturen von den temp-sensoren

---
+++ autoLab/labcontrol/branch/can_temp_neg/can-uart.h
@@ -0,0 +1,3 @@
+
+#include "lib/can-uart.h"
+
---
+++ autoLab/labcontrol/branch/can_temp_neg/cmds-cantemp.h
@@ -0,0 +1,10 @@
+#ifndef CMDCANTEMP_H
+#define CMDCANTEMP_H
+
+/**
+ * Available commands  
+ */
+
+void cmd_cantemp(int argc, char *argv[]);
+
+#endif // CMDCANTEMP_H
