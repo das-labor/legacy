60c720b9e1ed6897e9fa924e1f62adfc1ad2b028
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-02-12 08:25:03 +0000

    updated

--- microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/main-controller/touchscreen.h
+++ microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/main-controller/touchscreen.h
@@ -5,5 +5,17 @@ typedef struct{
 	int16_t y;
 } pixel;
 
+typedef struct{
+	uint16_t xz;
+	uint16_t xg;
+	uint16_t yz;
+	uint16_t yg;
+}calibration_values_t;
+
+extern calibration_values_t calibration_values;
+
+
+
 void handle_touchscreen();
 pixel read_touch_raw();
+pixel read_touch_screen_coordinates();
