eca8c79083e54354b4003d21713fbb98469341de
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-02-16 01:16:20 +0000

    PowerCommander v1

---
+++ microcontroller/src-atmel/automatization2.0/PowerCommander/tags/PowerCommander-1.0/avr_controll/main.c
@@ -0,0 +1,105 @@
+/* -*- Mode: C; tab-width: 2 -*- */
+/*
+ * PowerCommander - Ausführungs Kontroller
+
+ diese Version nimmt nur i2c nachrichten an und gibt sie direkt
+ aus. Es wird nur dieser input verarbeitet, keine taster oder aehnliches
+ ferner gibt es ein paar neue schalter, die das licht im vortragsraum
+ zeilenweise (flipper, schraenke, beamer,tafel) ein und ausschalten
+ lassen
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "twi_slave/twi_slave.h"
+
+#include "PowerCommander.h"
+
+#include "pc_init.h"
+
+#include "fkt.h"
+
+int main(void)
+{
+	uint8_t TWIS_ResponseType;
+	struct t_i2cproto i2cslave = { C_NDEF, O_NDEF, F_NDEF, HASNDATA, HASNDATA, D_NDEF ,D_NDEF };
+
+	init_modi();
+	init_relais();
+	init_timer();
+
+	/*
+	** Clear any interrupt
+	*/
+	cli();
+
+
+	/*
+	** Wait 0.5 second for POR
+	*/
+//	_delay_ms(500);
+
+	/*
+	** Start TWI Slave with address 15 and bitrate of 100000 Hz
+	*/
+	TWIS_Init();
+
+
+	/*
+		mainloop - die ist die kommunikation mit einem entferntem
+		host
+	*/
+	while (1) 
+	{
+		if (TWIS_ResponseRequired(&TWIS_ResponseType))
+		{
+			switch (TWIS_ResponseType)
+			{
+				/*
+				** Slave is requests to read bytes from the master.
+				*/
+				case TW_SR_SLA_ACK:
+				{
+					i2cslave.class   = TWIS_ReadAck();
+					i2cslave.object  = TWIS_ReadAck();
+					i2cslave.fkt     = TWIS_ReadAck();
+					i2cslave.in_data = TWIS_ReadNack();
+					TWIS_Stop();                // I2C stop
+					i2cslave.has_out_data = HASNDATA;
+
+					switch (i2cslave.class)
+					{
+						case C_SW:
+							switch_fkt(&i2cslave);
+							break;
+						case C_PWM:
+							pwm_fkt(&i2cslave);
+							break;
+						case C_VIRT:
+							virt_fkt(&i2cslave);
+							break;
+						default:
+							break;
+					}
+				}
+				break;
+
+				case TW_ST_SLA_ACK:
+					if (i2cslave.has_out_data == HASDATA)
+						TWIS_Write(i2cslave.out_data);
+					else
+						TWIS_Write(D_NDEF);
+					TWIS_Stop();
+					i2cslave.has_out_data = HASNDATA;
+					i2cslave.out_data = D_NDEF;
+					break;
+
+				default:
+					TWIS_Stop();
+					break;
+			}
+		}
+	}
+}
+
---
+++ microcontroller/src-atmel/automatization2.0/PowerCommander/tags/PowerCommander-1.0/avr_interface/main.c
@@ -0,0 +1,78 @@
+/* -*- Mode: C; tab-width: 2 -*- */
+
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "config.h"
+
+#include "twi_master/twi_master.h"
+#include "can/can.h"
+#include "can_handler.h"
+#include "can/spi.h"
+#include "can/lap.h"
+
+#include "../include/PowerCommander.h"
+
+#include "switch.h"
+
+void init(void)
+{
+
+//	MCUCR |= _BV(SE); // Enable "sleep" mode (low power when idle)
+
+	ACSR = _BV(ACD); // Disable Analog Comparator (power save)
+	
+	
+	DDRA |= LED_GRUEN | LED_ROT | LED_BLAU; // Status LED G R B
+	PORTA |= LED_GRUEN | LED_ROT | LED_BLAU;
+	DDRA &= ~(_BV(PA0) | _BV(PA1)); // Eingänge HS, rcd
+	DDRD &= ~(_BV(PD6) | _BV(PD7)); // Eingänge rcd licht, server
+	DDRC &= ~_BV(PC2); // Eingang power good
+		
+
+	DDRB &= ~_BV(PB2); // Eingang Lounge Taster
+	DDRD &= ~_BV(PD3); // Eingang Vortrag Taster
+	
+	//Pullups werden nichtmehr gebraucht
+	//PORTB |= _BV(PB2);
+	//PORTD |= _BV(PD3);
+
+/*
+** Initiate TWI Master Interface with bitrate of 100000 Hz
+*/
+	if (!TWIM_Init())
+	{
+		while (1);
+	}
+
+	//initialize spi port
+	spi_init();
+	
+	//initialize can communication
+	can_init();
+	
+	read_can_addr();
+
+	switch_timer_init();
+	
+	//turn on interrupts
+	sei();
+}
+	 
+int main(void)
+{
+
+	//system initialization
+	init();
+
+	//the main loop continuously handles can messages
+	while (1)
+	{
+		can_handler();
+		switch_handler();
+	}
+	return 1;
+};
