06c3246bcc7775091ff077603e354f03910e2166
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-02-18 22:16:54 +0000

    statusbot setter fixed - ping ist nicht ping

--- tools/statusbot/client/setstatus.rb
+++ tools/statusbot/client/setstatus.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 ##########################################################################
 ## Statusbot
 ##
@@ -14,44 +15,55 @@
 ###########################################################################
 
 
-
-
 require 'xmpp4r'
 require 'xmpp4r/roster/helper/roster'
 require 'xmpp4r/version/iq/version'
 require 'date'
 require 'ftools'
-require 'ping'
-require 'config.rb'
-
-status = ARGV[0];
+require '/usr/local/status-client/config.rb'
 
 def setup_connection
-	# Mit Jabber-Server verbinden
-	@client = Jabber::Client.new(Jabber::JID.new(@account))
-	@client.connect
-	@client.auth(@password)
+        # Mit Jabber-Server verbinden
+        @client = Jabber::Client.new(Jabber::JID.new(@account))
+        @client.connect
+        @client.auth(@password)
 end
 
 def connect
-	@client.send(Jabber::Presence.new.set_show(:dnd).set_status(@status))
+        @client.send(Jabber::Presence.new.set_show(:dnd).set_status(@status))
+end
+
+def pingtest
+	if @debug == 1
+	        puts("ping -c 2 "+@checkhost+" > /dev/null")
+	end
+        system("ping -c 2 "+@checkhost+" > /dev/null")
+        if $? == 0 then
+                return true
+        else
+                return false
+        end
 end
 
 def checkandsend
         loop {
-		status = Ping.pingecho(@checkhost)
-		if status then
-        		@m = Jabber::Message.new(@statusdaemon, 'status=on').set_type(:chat).set_id('1') 
-		else
-		        @m = Jabber::Message.new(@statusdaemon, 'status=off').set_type(:chat).set_id('1')
+                status = false
+                status = pingtest()
+		if @debug == 1 then
+	                puts("pingausgabe:"+ @status)
 		end
-		@client.send(@m)
-		if @debug == 1
-		        puts @m.to_s
-		end
-		
-		sleep(@interval)
-	}
+                if status then
+                        @m = Jabber::Message.new(@statusdaemon, 'status=on').set_type(:chat).set_id('1') 
+                else
+                        @m = Jabber::Message.new(@statusdaemon, 'status=off').set_type(:chat).set_id('1')
+                end
+                @client.send(@m)
+                if @debug == 1
+                        puts @m.to_s
+                end
+#                GC.start()
+                sleep(@interval)
+        }
 end
 
 
@@ -59,22 +71,23 @@ end
 # und alles ausführen (in der richtigen Reihenfolge)
 def startup
 
-	if @debug == 1
-		puts("Setting up the connection with the following parameters: ")
-		puts("account: " + @account)
-		puts("password: " + @password)
-		puts("statusmessage: " + @status)
-	end
-	setup_connection()
+        if @debug == 1
+                puts("Setting up the connection with the following parameters: ")
+                puts("account: " + @account)
+                puts("password: " + @password)
+                puts("statusmessage: " + @status)
+        end
+        setup_connection()
 
-	
-	if @debug == 1
-		puts("Connecting to server...")
-	end
-	connect()
+        
+        if @debug == 1
+                puts("Connecting to server...")
+        end
+        connect()
 
 end
 
 startup()
 checkandsend()
+Thread.stop
 @client.close
