9c3da6a721bf199a9adba512de0a794a0885b1ac
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-03-01 23:45:41 +0000

    mehr stimmen fuer das sprachgedrische - have phun

--- tools/weblabctrl2/trunk/act_speek.php
+++ tools/weblabctrl2/trunk/act_speek.php
@@ -7,10 +7,20 @@ if($_GET["doit"])
   {
     $myobj = new c_speek();
     $cmd = $myobj->isvalidmode($_GET["function"]);
+    $voice=$myobj->isvalidvoice($_GET["voice"]);
     if($cmd=="text")
       {
 	$tmpname = tempnam( "/tmp/", "weblab_speek");
-	exec("echo ".escapeshellcmd(substr($_GET['value'],0,120))." | text2wave -o ".$tmpname.".wav &&  paplay -p -d lounge ".$tmpname.".wav && rm ".$tmpname." ".$tmpname.".wav");
+	$input = "\"
+(Parameter.set 'Audio_Command \\\"cp \\\$FILE ".$tmpname.".wav\\\")
+(Parameter.set 'Audio_Method 'Audio_Command)
+(Parameter.set 'Audio_Required_Format 'snd)
+(voice_".$voice.")
+(SayText \\\"".escapeshellcmd(substr($_GET['value'],0,120))."\\\")\"";
+
+
+#	exec("echo ".escapeshellcmd(substr($_GET['value'],0,120))." | text2wave -eval voice_init/festvial.scm -o ".$tmpname.".wav &&  );
+	exec("echo ".$input." | festival &&  paplay -p -d lounge ".$tmpname.".wav && rm ".$tmpname." ".$tmpname.".wav");
       }
 
     file_put_contents($file, serialize( $localstate )); 
