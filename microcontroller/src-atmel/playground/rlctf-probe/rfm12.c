3a2f26e7e767a945c99d3dfd11c3876c80b2fade
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-06-26 18:37:12 +0000

    stuff

--- microcontroller/src-atmel/playground/rlctf-probe/rfm12.c
+++ microcontroller/src-atmel/playground/rlctf-probe/rfm12.c
@@ -1,1 +1,6 @@
+#ifndef RFM12_C_
+#define RFM12_C_
+//#include "rfm12.h"
+#include "rfm12_config.h"
 #include "rfm12/src/rfm12.c"
+#endif
--- microcontroller/src-atmel/playground/rlctf-probe/rfm12.h
+++ microcontroller/src-atmel/playground/rlctf-probe/rfm12.h
@@ -1,2 +1,5 @@
+#ifndef RFM12_H__
+#define RFM12_H__
 #include "rfm12_config.h"
 #include "rfm12/src/rfm12.h"
+#endif
