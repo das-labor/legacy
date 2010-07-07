53603fb44314034b0f934e95aaabd50b240aa7d9
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-07-07 14:58:16 +0000

    forgot to check in the new protocol definitions...

--- alarmsystem/common/protocol.h
+++ alarmsystem/common/protocol.h
@@ -1,15 +1,17 @@
-#define CMD_RESET	0x7A
-#define CMD_PING	0x01
-#define CMD_PONG	0x81
+#define CMD_RESET		0x7A
+#define CMD_PING		0x01
+#define CMD_PONG		0x81
 #define CMD_ALIVE		0x42
 #define CMD_STATE		0x19
 #define CMD_STATE_SET   	0x21
 #define CMD_MOTION		0x22
 #define CMD_MOTION_TRESH_SET	0x25
-#define CMD_PWRFAIL	0x60
-#define CMD_ERROR	0xEE
-#define PROTO_TYPE0	0x09
-#define PROTO_TYPE1	0x12
+#define CMD_MAIL_SENSOR		0x28
+#define CMD_PWRFAIL		0x60
+#define CMD_LOW_POWER		0x61 /* NOTE: the low power indicator is non-fatal, while the PWRFAIL cmd may trigger an alarm */
+#define CMD_ERROR		0xEE
+#define PROTO_TYPE0		0x09
+#define PROTO_TYPE1		0x12
 
 #define RFM12_PROTO_TYPE 0xA0
 
