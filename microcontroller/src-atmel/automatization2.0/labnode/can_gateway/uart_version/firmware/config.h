65e43fd04ee70e06d18bf1814589a522585e50be
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-07-25 22:14:22 +0000

    branched

---
+++ microcontroller/src-atmel/automatization2.0/labnode/can_gateway/uart_version/firmware/config.h
@@ -0,0 +1,42 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// util.[ch] defines
+// #define F_CPU 16000000         // Oszillator-Frequenz in Hz
+
+// uart.[ch] defines
+#define UART_INTERRUPT
+#define UART_BAUD_RATE 57600
+#define UART_RXBUFSIZE 50
+#define UART_TXBUFSIZE 50
+//#define UART_LEDS
+
+
+#define MC_MOSI PB5
+#define MC_CLK  PB7
+
+// can.[ch] defines
+#define CAN_RAW
+
+#define F_MCP F_CPU
+
+#define MCP_CMD_PORT PORTB
+#define MCP_CS       PB4
+
+#define SPI_REG_PIN_MCP_INT  PINB
+#define SPI_PIN_MCP_INT      PB2
+
+
+//#define CAN_INTERRUPT		//set this to enable interrupt driven and buffering version
+#define CAN_RX_BUFFER_SIZE 8	//only used for Interrupt
+#define CAN_TX_BUFFER_SIZE 8	//only used for Interrupt
+
+#define ENABLE_CAN_INT()   GICR |= (1<<INT2)
+#define MCP_INT_VEC        INT2_vect
+
+
+
+// lap.[ch] defines
+
+
+#endif
