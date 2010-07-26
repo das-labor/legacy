71ecda43c4ded8b5c956dcec64ec462ed56e51f5
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-07-26 00:19:29 +0000

    is working now

--- microcontroller/src-atmel/automatization2.0/can_gw/trunk/config.h
+++ microcontroller/src-atmel/automatization2.0/can_gw/trunk/config.h
@@ -12,27 +12,33 @@
 //#define UART_LEDS
 
 
-#define MC_MOSI PB5
-#define MC_CLK  PB7
 
 // can.[ch] defines
 #define CAN_RAW
+//#define CAN_INTERRUPT
 
-#define F_MCP F_CPU
+#define F_MCP 16000000
 
 #define MCP_CMD_PORT PORTB
-#define MCP_CS       PB4
+#define MCP_CS       PB2
 
-#define SPI_REG_PIN_MCP_INT  PINB
-#define SPI_PIN_MCP_INT      PB2
+#define MC_MOSI PB3
+#define MC_CLK  PB5
+
+
+#define SPI_REG_PIN_MCP_INT  PIND
+#define SPI_PIN_MCP_INT      PD2
 
 
 //#define CAN_INTERRUPT		//set this to enable interrupt driven and buffering version
 #define CAN_RX_BUFFER_SIZE 8	//only used for Interrupt
 #define CAN_TX_BUFFER_SIZE 8	//only used for Interrupt
 
-#define ENABLE_CAN_INT()   GICR |= (1<<INT2)
-#define MCP_INT_VEC        INT2_vect
+#define ENABLE_CAN_INT()   GICR |= (1<<INT0); \
+        MCUCR |=  (1<<ISC01); \
+        GIMSK |= (1<<INT0)
+
+#define MCP_INT_VEC        INT0_vect
 
 
 
