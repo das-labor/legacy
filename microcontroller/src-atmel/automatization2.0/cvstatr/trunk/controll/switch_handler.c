c37658d9d746eb7e568a5abc85649ea915399e77
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-08-02 22:51:59 +0000

    interrup zeugs

--- microcontroller/src-atmel/automatization2.0/cvstatr/trunk/controll/switch_handler.c
+++ microcontroller/src-atmel/automatization2.0/cvstatr/trunk/controll/switch_handler.c
@@ -9,23 +9,27 @@
 #include "can_handler.h"
 #include "can/lap.h"
 
-#define KLINGEL	(PD6)
-#define STANDBY	(PD5)
-#define TUER	(PD7)
-#define SCHLOSS (PD0) //XXX
+#define KLINGEL	_BV(PD6)
+#define STANDBY	_BV(PD5)
+#define TUER	_BV(PD7)
+#define SCHLOSS _BV(PD0) //XXX
 
 #define SIGNAL_PIN	(&(PIND))
 
-
+/*
 union {
+
 	struct {
 		uint8_t klingel:1;
 		uint8_t standby:1;
 		uint8_t tuerkontakt:1;
 		uint8_t schloss:1;
 	} switches;
-	uint8_t stat_sw;
 } stat_switches;
+
+*/
+
+	uint8_t stat_sw;
 
 static struct t_pin_parameter {
 	volatile uint8_t *pin;
@@ -33,31 +37,32 @@ static struct t_pin_parameter {
 } pin_matrix[] = {
 	{ SIGNAL_PIN , KLINGEL},
 	{ SIGNAL_PIN , STANDBY},
-	{ SIGNAL_PIN , TUER},
-	{ SIGNAL_PIN , SCHLOSS}
+	{ SIGNAL_PIN , TUER}
 };
 
-void send_stat() {
-	static can_message msg = {0x04, 0x00, 0x00, 0x01, 1, {0}};
-	msg.data[0] = stat_switches.stat_sw;
+void send_stat(uint8_t bla) {
+	static can_message msg = {0x04, 0x00, 0x00, 0x01, 2, {0}};
+	msg.data[0] = stat_sw;
+	msg.data[1] = bla;
+	
 	// send changed bit oder beim ziel mit tab vergleichen
 	//msg.addr_src = myaddr;
 	can_transmit(&msg);
 }
 
 void get_switches() {
-	uint8_t i;
+	uint8_t i, msk = 0x01;
 	
 	for (i = 0; i < 3; i++) {
-		if (((*pin_matrix[i].pin) & _BV(pin_matrix[i].bit)) && (((stat_switches.stat_sw >> i) & 1) == 0)) {
-			stat_switches.stat_sw |= (1 << i);
-			send_stat();
+		if (((*pin_matrix[i].pin) & pin_matrix[i].bit) && (stat_sw & msk) == 0) {
+			stat_sw |= msk;
+			send_stat(i);
 		}
-		
-		if (!((*pin_matrix[i].pin) & _BV(pin_matrix[i].bit)) && ((stat_switches.stat_sw >> i) & 1)) {
-			stat_switches.stat_sw &= ~(1 << i);
-			send_stat();
+		if ( !((*pin_matrix[i].pin) & pin_matrix[i].bit) && (stat_sw & msk)) {
+			stat_sw &= ~msk;
+			send_stat(i);
 		}
+		msk <<= 1;
 	}
 }
 
