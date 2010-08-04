0a2dd0a3395c31b54b2cdd3881705d71be57857d
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-08-04 11:10:58 +0000

    fix stuff, rm debug

--- microcontroller/src-atmel/automatization2.0/bastelraum/trunk/io.c
+++ microcontroller/src-atmel/automatization2.0/bastelraum/trunk/io.c
@@ -1,42 +1,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include "io.h"
 
-#define SREG_PORT	PORTC
-#define SREG_CK		_BV(PC6)
-#define SREG_DATA	_BV(PC7)
-#define SREG_STROBE	_BV(PA5)
 
-// ports - addressen
-#define O_SW00_PORT (&(PORTC))
-#define O_SW01_PORT (&(PORTC))
-#define O_SW02_PORT (&(PORTC))
-#define O_SW03_PORT (&(PORTC))
-#define O_SW04_PORT (&(PORTB))
-
-// PINs - uin8_t
-#define O_SW00_PIN (PC3)
-#define O_SW01_PIN (PC2)
-#define O_SW02_PIN (PC1)
-#define O_SW03_PIN (PC0)
-#define O_SW04_PIN (PB5)
-
-#define O_PWM00_PORT (&(OCR0A))
-#define O_PWM01_PORT (&(OCR0B))
-#define O_PWM02_PORT (&(OCR1AL))
-#define O_PWM03_PORT (&(OCR1BL))
-
+void change_shift_reg(uint8_t sreg);
 
 void init_io() {
 	DDRA |= SREG_STROBE;
 	PORTA |= SREG_STROBE;
-	DDRC |= SREG_CK;
+	DDRC |= SREG_CK | SREG_DATA;
 	SREG_PORT |= SREG_CK;
-
+	
+	change_shift_reg(0);
+	DDRD |= _BV(PD4) | _BV(PD5) | _BV(PD6) | _BV(PD7); // Pins mit pwm als Ausgänge
+	
 	TCCR2A |= _BV(WGM21) | _BV(WGM20) | _BV(COM2A1) | _BV(COM2B1);	// FastPWM, Set OC2X on Compare Match, clear OC2X at BOTTOM, (non inverting mode).
-	TCCR2B |= _BV(CS22);										// clk/64
+	TCCR2B |= _BV(CS22);						// clk/64
 
 	TCCR1A |= _BV(WGM10) | _BV(COM1A1) | _BV(COM1B1);	// FastPWM, Set OC1X on Compare Match, clear OC1X at BOTTOM, (non inverting mode).
-	TCCR1B |= _BV(WGM12) | _BV(CS11) | _BV(CS10);			// FastPWM bit 2, clk/64
+	TCCR1B |= _BV(WGM12) | _BV(CS11) | _BV(CS10);		// FastPWM bit 2, clk/64
 
 	//PWM
 
@@ -48,12 +30,10 @@ void init_io() {
 	OCR1A = 255;   // pwm timer compare target
 	OCR1B = 255;   // pwm timer compare target
 
+	DDRA &= ~(_BV(PA4)); // Eingänge Türkontakt
+	PORTA |= _BV(PA4);	// PULLUP
+
 }
-
-
-
-
-
 
 void change_shift_reg(uint8_t sreg) {
 	for (int i = 0; i < 8; i++) {
@@ -71,25 +51,7 @@ void change_shift_reg(uint8_t sreg) {
 }
 
 void switch_handler() {
-
-}
-
-
-void switch_on(volatile uint8_t *port, uint8_t pin)
-{
-	(*port) |= _BV(pin);
-}
-
-
-void switch_off(volatile uint8_t *port, uint8_t pin)
-{
-	(*port) &= ~_BV(pin);
-}
-
-
-void switch_status(volatile uint8_t *port, uint8_t pin, uint8_t *result)
-{
-	(*result) = ((*port) >> pin) & 1;
+	
 }
 
 
@@ -103,3 +65,4 @@ void pwm_get(volatile uint8_t *port, uint8_t *result)
 {
 	(*result) = (*port);
 }
+
--- microcontroller/src-atmel/automatization2.0/bastelraum/trunk/io.h
+++ microcontroller/src-atmel/automatization2.0/bastelraum/trunk/io.h
@@ -2,35 +2,17 @@
 #define _IO_H
 
 
-// ports - addressen
-#define O_SW00_PORT (&(PORTC))
-#define O_SW01_PORT (&(PORTC))
-#define O_SW02_PORT (&(PORTC))
-#define O_SW03_PORT (&(PORTC))
-#define O_SW04_PORT (&(PORTB))
+#define SREG_PORT	PORTC
+#define SREG_CK		_BV(PC4)
+#define SREG_DATA	_BV(PC5)
+#define SREG_STROBE	_BV(PA5)
 
-// PINs - uin8_t
-#define O_SW00_PIN (PC3)
-#define O_SW01_PIN (PC2)
-#define O_SW02_PIN (PC1)
-#define O_SW03_PIN (PC0)
-#define O_SW04_PIN (PB5)
 
-#define O_PWM00_PORT (&(OCR0A))
-#define O_PWM01_PORT (&(OCR0B))
+#define O_PWM00_PORT (&(OCR2A))
+#define O_PWM01_PORT (&(OCR2B))
 #define O_PWM02_PORT (&(OCR1AL))
 #define O_PWM03_PORT (&(OCR1BL))
 
-static struct t_switch_parameter {
-	volatile uint8_t *port;
-	uint8_t pin;
-} sw_matrix[] = {
-	{ O_SW00_PORT , O_SW00_PIN},
-	{ O_SW01_PORT , O_SW01_PIN},
-	{ O_SW02_PORT , O_SW02_PIN},
-	{ O_SW03_PORT , O_SW03_PIN},
-	{ O_SW04_PORT , O_SW04_PIN}
-};
 
 // das gleiche fuer die PWMs
 static struct t_pwm_parameter {
@@ -47,6 +29,8 @@ void switch_handler();
 void state_to_output();
 void change_shift_reg(uint8_t sreg);
 void pwm_set(volatile uint8_t *port, uint8_t value);
+
+void init_io();
 
 #endif // _IO
 
