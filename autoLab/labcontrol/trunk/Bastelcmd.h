c7363b59e528202255cc60ac9be57ccf9ed669b9
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-08-05 20:06:10 +0000

    missing h

---
+++ autoLab/labcontrol/trunk/Bastelcmd.h
@@ -0,0 +1,22 @@
+#define C_SW 0
+#define C_PWM	1
+
+#define F_REG_DRUCKER_1 1
+#define F_REG_DRUCKER_2 2
+#define F_REG_HELMER_1 3
+#define F_REG_HELMER_2 4
+#define F_REG_FENSTER 5
+#define F_REG_BANNER    6
+#define F_REG_ORGATISCH 7
+
+
+#define F_PWM_FENSTER 0
+#define F_PWM_BANNER  1
+#define F_PWM_ORGATISCH 3
+
+
+/* 
+   Aktionen als defines
+*/
+#define F_SW_OFF      (0x00)
+#define F_SW_ON       (0x01)
