67f0ad0daf6fe6894a9de52a2e3f8eb5eb75da63
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-08-15 20:03:52 +0000

    some testing stuff

--- microcontroller/src-atmel/playground/nightlight/config.h
+++ microcontroller/src-atmel/playground/nightlight/config.h
@@ -3,8 +3,7 @@
 
 
 // SPI
-#define MC_MOSI    PB3
-#define MC_CLK     PB5
+
 
 // LS20 config
 
@@ -24,24 +23,6 @@
 //#define CAN_INTERRUPT
 #define F_MCP F_CPU
 
-
-#define MEGA8
-
-#ifdef MEGA8
-#	define SPI_PORT PORTB
-#	define SPI_PIN_CS PB0
-#	define SPI_HARDWARE
-#	define SPI_DDR DDRB
-#	define SPI_PIN_SCK PB5
-#	define SPI_PIN_MOSI PB3
-#else
-#	define SPI_PORT PORTB
-#	define SPI_PIN_CS PB4
-#	define SPI_HARDWARE
-#	define SPI_DDR DDRB
-#	define SPI_PIN_SCK PB7
-#	define SPI_PIN_MOSI PB5
-#endif
 
 
 /* ATMega32 / 8*/
--- microcontroller/src-atmel/playground/nightlight/main.c
+++ microcontroller/src-atmel/playground/nightlight/main.c
@@ -3,6 +3,7 @@
 
 #include "can/can.h"
 #include "can/spi.h"
+#include "can_handler.h"
 
 #include "config.h"
 #include "ls020/ls020.h"
@@ -10,6 +11,12 @@
 
 //#include "switch.h"
 //#include "menu.h"
+
+const char pms_klein[] PROGMEM = "klein"; 
+const char pms_gross[] PROGMEM = "gross";
+const char pms_beide[] PROGMEM = "beide";
+
+const PGM_P pgm_strings[] PROGMEM = {pms_klein, pms_gross, pms_beide};
 
 
 int main(void)
@@ -33,14 +40,15 @@ int main(void)
 	spi_init(); // Setup SPI
 	
 	can_init();
+	read_can_addr();
 	
 	ls020_init_lcd(); // Init LS020 LCD
 
 	
 	ls020_8bit_mode(0);	// Set 8bit LCD host mode
-	ls020_rectangle8(0, 0, 175, 131, 0x30);	// Fill Screen with dark red color
+	ls020_rectangle8(0, 0, 175, 131, 0x0f);	// Fill Screen with dark red color
 	
-	uint8_t stat = 0;
+	uint8_t stat = 0, dir = 0, rollo = 0;
 	while (1)
 	{
 		if (!(PIND & _BV(PD4)))
@@ -57,5 +65,47 @@ int main(void)
 			}
 		_delay_ms(300);
 		}
+		if (!(PIND & _BV(PD5)))
+		{
+			uint8_t data[2] = {0, 0};
+			if (dir == 1)
+			{
+				data[1] = 0;
+				OCR1B = 255;
+				dir = 0;
+			}
+			else
+			{
+				data[1] = 1;
+				OCR1B = 0;
+				dir = 1;
+			}
+			data[0] = rollo;
+			if (rollo == 2) {
+				data[0] = 0;
+				can_send(0x02, data);
+				_delay_ms(1);
+				data[0] = 1;
+			}
+			can_send(0x02, data);
+			_delay_ms(300);
+		}
+		if (!(PIND & _BV(PD6)))
+		{
+			uint8_t data[2] = {0, 0};
+			data[1] = 2;
+			can_send(0x02, data);
+			_delay_ms(300);
+		}
+		if (!(PIND & _BV(PD7))) {
+			rollo++;
+			if (rollo == 3) {
+				rollo = 0;
+			}
+			ls020_put_string8(5, 20, (char *) pgm_read_word(pgm_strings + rollo), 0x00, 0x0f);
+			_delay_ms(300);			
+		}
+		can_handler();
 	}
 }
+
