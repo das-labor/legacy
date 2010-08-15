4a9ddf88b5460774ac44322b7b27580eb8eac1a0
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-08-15 20:19:36 +0000

    text to speech fixed

--- tools/weblabctrl2/trunk/act_speek.php
+++ tools/weblabctrl2/trunk/act_speek.php
@@ -11,16 +11,24 @@ if($_GET["doit"])
     if($cmd=="text")
       {
 	$tmpname = tempnam( "/tmp/", "weblab_speek");
-	$input = "\"
-(Parameter.set 'Audio_Command \\\"cp \\\$FILE ".$tmpname.".wav\\\")
+	$input = "
+(Parameter.set 'Audio_Command \"paplay -d lounge \$FILE \")
 (Parameter.set 'Audio_Method 'Audio_Command)
 (Parameter.set 'Audio_Required_Format 'snd)
 (voice_".$voice.")
-(SayText \\\"".escapeshellcmd(substr($_GET['value'],0,120))."\\\")\"";
-
+(SayText \"".escapeshellcmd(substr($_GET['value'],0,120))."\")";
+echo $input;
 
 #	exec("echo ".escapeshellcmd(substr($_GET['value'],0,120))." | text2wave -eval voice_init/festvial.scm -o ".$tmpname.".wav &&  );
-	exec("echo ".$input." | festival &&  paplay -p -d lounge ".$tmpname.".wav && rm ".$tmpname." ".$tmpname.".wav");
+#	exec("echo ".$input." | festival &&  paplay -p -d lounge ".$tmpname.".wav && rm ".$tmpname." ".$tmpname.".wav");
+	$cmd="/usr/bin/festival";
+	if ( ($fh = popen($cmd, 'w')) === false )
+        	die("Open failed: ${php_errormsg}\n");
+
+    	fwrite($fh, $input);
+
+	pclose($fh); 
+#	exec("cat ".$input." | festival");
       }
 
     file_put_contents($file, serialize( $localstate )); 
