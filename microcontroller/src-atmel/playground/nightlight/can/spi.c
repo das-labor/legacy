b69fba24c380b24487c99b7976581629c65dec7d
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-08-15 20:03:52 +0000

    some testing stuff

--- microcontroller/src-atmel/playground/nightlight/can/spi.c
+++ microcontroller/src-atmel/playground/nightlight/can/spi.c
@@ -3,11 +3,28 @@
 #include "spi.h"
 #include "../config.h"
 
-extern void spi_init()
+
+#if defined(__AVR_ATmega48__) | defined(__AVR_ATmega168__) | defined(__AVR_ATmega8__)
+	#define AVR_SS_BIT PB2
+	#define AVR_MOSI_BIT PB3
+	#define AVR_MISO_BIT PB4	
+	#define AVR_SCK_BIT PB5
+#elif defined(__AVR_ATmega32__) | defined(__AVR_ATmega644__)
+	#define AVR_SS_BIT PB4
+	#define AVR_MOSI_BIT PB5
+	#define AVR_MISO_BIT PB6	
+	#define AVR_SCK_BIT PB7
+
+#else
+	#error The AVR_SS_BIT is not defined for your AVR. Please add your AVR here.
+#endif
+
+
+void spi_init()
 {
 
 	/* configure MOSI, SCK, lines as outputs */
-	DDRB |= _BV(MC_MOSI) | _BV(MC_CLK); // mosi, sck output
+	DDRB |= _BV(AVR_MOSI_BIT) | _BV(AVR_SCK_BIT) | _BV(AVR_SS_BIT); // mosi, sck, avr-ss to output
 
 	SPCR = _BV(MSTR) | _BV(SPE); // Master Mode,  Enable SPI
 	SPSR = _BV(SPI2X); // Double speed on
@@ -15,7 +32,7 @@ extern void spi_init()
 
 
 
-extern uint8_t spi_send(uint8_t data)
+uint8_t spi_send(uint8_t data)
 {
 	SPDR = data;
 	while (!(SPSR & _BV(SPIF)));
