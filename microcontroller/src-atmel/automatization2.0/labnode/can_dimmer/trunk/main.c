4433a9e9a2bdad1bf526c6782d3d382ece8fef9e
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-08-24 23:13:59 +0000

    added

---
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/main.c
@@ -0,0 +1,55 @@
+/* -*- Mode: C; tab-width: 2 -*- */
+
+
+#include <avr/io.h>
+//#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "config.h"
+
+#include "can/can.h"
+#include "can_handler.h"
+#include "can/spi.h"
+#include "can/lap.h"
+
+void init(void)
+{
+	DDRB |= _BV(PB0); // LED out
+		
+	//initialize spi port
+	spi_init();
+	
+	//initialize can communication
+	can_init();
+	
+	read_can_addr();
+}
+	 
+int main(void)
+{
+	volatile uint16_t foo = 0;
+	//system initialization
+	init();
+
+	//the main loop continuously handles can messages
+	while (1)
+	{
+		can_handler();
+		
+		/*
+
+		//periodically send test message
+				
+		if(foo == 0){
+			static can_message msg = {1, 2, 3, 4, 2, {0x55, 0xaa}};
+			msg.addr_src = myaddr;
+			can_transmit(&msg);
+		}
+		foo++;
+		
+		*/
+
+	}
+
+}
