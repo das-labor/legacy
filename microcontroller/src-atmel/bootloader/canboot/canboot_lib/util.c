e74164070d258287286f3d10ade32da22ffea99b
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-09-10 11:39:04 +0000

    adapted to 128k flash device

--- microcontroller/src-atmel/bootloader/canboot/canboot_lib/util.c
+++ microcontroller/src-atmel/bootloader/canboot/canboot_lib/util.c
@@ -3,14 +3,29 @@
 
 #include "util.h"
 
-void my_memcpy_P (unsigned char size, void * dest, PGM_VOID_P source){
-		asm volatile(
-		"\nmy_memcpy_lp:\n\t"
-		"lpm r25,Z+\n\t"
-		"st X+,r25\n\t"
-		"dec %0\n\t"
-		"brne my_memcpy_lp\n\t"
-		:: "r"(size), "x"(dest), "z"(source)
-		:"r25"
-		);
-}
+
+#ifdef pgm_read_byte_far
+	void my_memcpy_P (unsigned char size, void * dest, PGM_VOID_P source){
+		uint8_t * d = dest;
+		while(size--){
+			*d++ = pgm_read_byte_far(0x10000l | (uint16_t)(source++));
+		}
+	
+	}
+#else
+
+
+
+	void my_memcpy_P (unsigned char size, void * dest, PGM_VOID_P source){
+			asm volatile(
+			"\nmy_memcpy_lp:\n\t"
+			"lpm r25,Z+\n\t"
+			"st X+,r25\n\t"
+			"dec %0\n\t"
+			"brne my_memcpy_lp\n\t"
+			:: "r"(size), "x"(dest), "z"(source)
+			:"r25"
+			);
+	}
+
+#endif
