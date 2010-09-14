bcef2e7a3f5b6583a3693badfcd532d042792748
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-09-14 20:37:18 +0000

    canboot: delete old canbootloaders

--- microcontroller/src-atmel/bootloader/canboot-m8/branches/size/bootloader.h
+++
@@ -1,5 +0,0 @@
-#include <avr/boot.h>
-
-extern unsigned char Station_id;
-
-int bootloader(void) BOOTLOADER_SECTION __attribute__ ((naked)); 
--- microcontroller/src-atmel/bootloader/canboot-m8/branches/size/main.c
+++
@@ -1,8 +0,0 @@
-#include <avr/io.h>
-int bootloader(void);
-
-int main(){
-	//DDRC = 0xFF;
-	//PORTC = 0x01;
-	bootloader();
-}
