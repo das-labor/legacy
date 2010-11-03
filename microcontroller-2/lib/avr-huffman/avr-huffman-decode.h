0c7e99d521a4b72af7195b830fc395738ecd2350
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-11-03 18:13:10 +0000

    further reduced codesize to 486 byte

--- microcontroller-2/lib/avr-huffman/avr-huffman-decode.h
+++ microcontroller-2/lib/avr-huffman/avr-huffman-decode.h
@@ -21,30 +21,17 @@
 
 #include <stdint.h>
 
-#define HUFFMAN_USE_ADDR_16 1
 
 typedef struct {
 	void* tree;
 	uint8_t  rbuffer;
 	uint8_t  rbuffer_index;
-#if HUFFMAN_USE_ADDR_16
 	uint16_t(*read_byte)(uint16_t addr);
 	uint16_t addr;
-#else
-	uint16_t(*read_byte)(uint32_t addr);
-	uint32_t addr;
-#endif
 } huffman_dec_ctx_t;
 
-#if HUFFMAN_USE_ADDR_16
 void huffman_dec_init(huffman_dec_ctx_t* ctx, uint16_t(*rb_func)(uint16_t));
 void huffman_dec_set_addr(huffman_dec_ctx_t* ctx,uint16_t addr);
-#else
-void huffman_dec_init(huffman_dec_ctx_t* ctx, uint16_t(*rb_func)(uint32_t));
-void huffman_dec_set_addr(huffman_dec_ctx_t* ctx,uint32_t addr);
-#endif
-
-
 uint16_t huffman_dec_byte(huffman_dec_ctx_t* ctx);
 
 #endif /* AVR_HUFFMAN_DECODE_H_ */
