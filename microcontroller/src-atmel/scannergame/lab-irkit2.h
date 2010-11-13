5240418905b136a87a59e7f29bc6f67e2d20a65e
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-11-13 17:01:35 +0000

    more bugfixes, more documentation

--- microcontroller/src-atmel/scannergame/lab-irkit2.h
+++ microcontroller/src-atmel/scannergame/lab-irkit2.h
@@ -50,12 +50,15 @@
 //define output PORT
 #define IRPORT PORTD
 #define IRDDR DDRD
-#define IRUSEDPORTS 0x2 
+#define IRUSEDPORTS 0x3 
 
 //define how long a carrier pulse last 0..1
 //select 0.5 for normal LED mode or
 //less values if you overcurrent the IR LEDs 
 #define IR_pulse_width 0.1f
+
+//max SRAM usage
+#define IR_MAX_SRAM 512
 
 #define IR_PhilipsRC5_Carrier IR_FREQ36K
 #define IR_PhilipsRC5_pulselength 889
@@ -120,38 +123,28 @@
 #define IR_JVC_header 0b111111111111111100000000
 #define IR_JVC_headerlength 24
 
+/*
+Code example:
+#include <stdio.h>
+#include <stdlib.h>
 
-//teufel signal definitions
-//see datasheet of PT2248 chip
-//all calculations are done in µS
-// a is the base time unit
-// a = (1/fOSC) * 192 -> fOSC typically is 455KHz
-//#define PT2248_A 431 //431 fits best, empirically
-// bit 0 = a on, 3*a off
-//#define PT_OFF (PT2248_A / IR_TICK_US), ((3*PT2248_A) / IR_TICK_US)
-// bit 1 = 3*a on, a off
-//#define PT_ON ((3*PT2248_A) / IR_TICK_US), (PT2248_A / IR_TICK_US)
+#include "lab-irkit2.h"
 
+int main(void)
+{
+	//initialize ir subsystem
+        ir_init(IR_NEC_Carrier, IR_NEC_pulselength);
+	
+	uint8_t codelength=0;
+	ir_genHeader(0, IR_NEC_header, IR_NEC_headerlength);  //generate header
+	codelength=ir_genCode(IR_NEC_headerlength,0,IR_NEC_oneCode,IR_NEC_oneCodelength,IR_NEC_zeroCode,IR_NEC_zeroCodelength,0b00010000110010001110000100011110, 32);  //generate code
+	while (1)
+	{	
+		ir_sendCode(codelength);  //transmit code
+	}
+}
 
-// Extended NEC Protocol
-// Pulse distance encoding
-// one additional bit for distance
-
-// AGC Burst 9ms + 4,5ms pause
-//#define PNEC_AGC_ON  (9000 / IR_TICK_US)
-//#define PNEC_AGC_OFF (4500 / IR_TICK_US)
-//#define PNEC_AGC_BURST (PNEC_AGC_ON), (PNEC_AGC_OFF)
-
-// bit 0 560µs on + 560µs off
-//#define PNEC_OFF (560 / IR_TICK_US), (560 / IR_TICK_US)
-
-// bit 1 560µs on + 1,69ms off
-//#define PNEC_ON (560 / IR_TICK_US), (1690 / IR_TICK_US)
-
-//macro for generating  extended nec encodings
-//x is the destination array, y is the input code, z is the bit count
-//#define IR_GEN_NECEXT(x, y, z) (ir_genCode((uint16_t *)(x+4), PNEC_ON, PNEC_OFF, y, z) + 4); x[0] = PNEC_AGC_ON; x[1] = PNEC_AGC_OFF
-
+*/
 
 // stop TIMER1, disable IRPORT
 #define FREQGEN_OFF() TCCR1B = 0; IRPORT&=IRDDR
