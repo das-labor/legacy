1836a525c78b8e28dbdd4255422a4f52cb32fbda
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-11-16 23:47:40 +0000

    canlib: add m128 spi def

--- microcontroller/src-atmel/lib/canlib/spi.c
+++ microcontroller/src-atmel/lib/canlib/spi.c
@@ -7,14 +7,18 @@
 #if defined(__AVR_ATmega48__) | defined(__AVR_ATmega168__) | defined(__AVR_ATmega8__) | defined(__AVR_ATmega88__)
 	#define AVR_SS_BIT PB2
 	#define AVR_MOSI_BIT PB3
-	#define AVR_MISO_BIT PB4	
+	#define AVR_MISO_BIT PB4
 	#define AVR_SCK_BIT PB5
 #elif defined(__AVR_ATmega32__) | defined(__AVR_ATmega644__)
 	#define AVR_SS_BIT PB4
 	#define AVR_MOSI_BIT PB5
-	#define AVR_MISO_BIT PB6	
+	#define AVR_MISO_BIT PB6
 	#define AVR_SCK_BIT PB7
-
+#elif defined(__AVR_ATmega128__) | defined(__AVR_ATmega102__)
+	#define AVR_SS_BIT PB0
+	#define AVR_MOSI_BIT PB2
+	#define AVR_MISO_BIT PB3
+	#define AVR_SCK_BIT PB1
 #else
 	#error The AVR_SS_BIT is not defined for your AVR. Please add your AVR here.
 #endif
