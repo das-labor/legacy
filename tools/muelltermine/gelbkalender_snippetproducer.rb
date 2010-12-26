8a790f015f2b92c55be6a3c429c626fd99fc58f9
thomas <thomas@b1d182e4-1ff8-0310-901f-bddb46175740>
2010-12-26 11:19:07 +0000

    tabs to newlines

--- tools/muelltermine/gelbkalender_snippetproducer.rb
+++ tools/muelltermine/gelbkalender_snippetproducer.rb
@@ -21,7 +21,7 @@ die("Falsche Anzahl an Parametern", true) unless $*.length == 1
 YEAR = $*[0].to_i
 
 def dumpline(line)
-	$stdout << ICS::fold_line(line)
+  $stdout << ICS::fold_line(line)
 end
 
 data = $stdin.read
@@ -29,17 +29,17 @@ data.gsub!("<br>", "<br/>") #hope this is sufficient to produce valid XML
 
 doc = REXML::Document.new data
 doc.elements.each("div[@class='tx-3sdsdyellowbags-pi2']/table/tbody/tr[position() = 2]/td[position() = 1]/table/tbody/tr/") do |el|
-	el.elements.each("td[position() = 3]") do |el|
-		el.texts.each do |text|
-			if (%r{(#{NUM}{2}).(#{NUM}{2}).}.match(text.to_s)) then
-				datestr = (Date.new(YEAR, $2.to_i, $1.to_i) - 1).to_icsdate
-				
-				dumpline("BEGIN:VEVENT")
-				dumpline("DTSTART;VALUE=DATE:#{datestr}")
-				dumpline("DTEND;VALUE=DATE:#{datestr}")
-				dumpline("SUMMARY:#{GELBMUELL_SUMMARY}")
-				dumpline("END:VEVENT")
-			end
-		end
-	end
+  el.elements.each("td[position() = 3]") do |el|
+    el.texts.each do |text|
+      if (%r{(#{NUM}{2}).(#{NUM}{2}).}.match(text.to_s)) then
+        datestr = (Date.new(YEAR, $2.to_i, $1.to_i) - 1).to_icsdate
+        
+        dumpline("BEGIN:VEVENT")
+        dumpline("DTSTART;VALUE=DATE:#{datestr}")
+        dumpline("DTEND;VALUE=DATE:#{datestr}")
+        dumpline("SUMMARY:#{GELBMUELL_SUMMARY}")
+        dumpline("END:VEVENT")
+      end
+    end
+  end
 end
--- tools/muelltermine/icsconverter.rb
+++ tools/muelltermine/icsconverter.rb
@@ -21,88 +21,88 @@ die("Falsche Anzahl an Parametern", true) unless $*.length == 3
 RESTMUELL_ICS, BLAUMUELL_ICS, GELBMUELL_ICS_SNIPPET = $* #store arguments
 
 def dumpline(line)
-	$stdout << ICS::fold_line(line)
+  $stdout << ICS::fold_line(line)
 end
 
 #wrap a Date object to allow repetition (currently, only weekly recurrence rule supported)
 class RepeatedDate
-	attr_reader :date
-	
-	def initialize(date, numweeks)
-		@date = date
-		@daily_increment = 7 * numweeks
-	end
+  attr_reader :date
+  
+  def initialize(date, numweeks)
+    @date = date
+    @daily_increment = 7 * numweeks
+  end
 
-	def succ
-		RepeatedDate.new(@date + @daily_increment, @daily_increment / 7)
-	end
+  def succ
+    RepeatedDate.new(@date + @daily_increment, @daily_increment / 7)
+  end
 
-	def <=>(other)
-		return @date <=> other.date
-	end
+  def <=>(other)
+    return @date <=> other.date
+  end
 end
 
 def dumppartialcalender(fname, summary)
-	parser = ICS::FileParser.new(fname)
-	pr = parser.parse
+  parser = ICS::FileParser.new(fname)
+  pr = parser.parse
 
-	(pr.select { |o| o.name == "VEVENT" }).each do |vev|
-		event = {}
-		
-		vev.each do |node| #this code might as well go into common.rb
-			case node
-			when ICS::ContentLine
-				case node.name
-				when "DTSTART"
-					#we ignore the TZID param
-					event[:dtstart] = Date.from_ics_datetime(node.values[0])
-				when "DTEND"
-					#we ignore the TZID param
-					event[:dtend] = Date.from_ics_datetime(node.values[0])
-				when "RRULE"
-					hsh = ICS::parse_rrule(node.values[0])
-					die("Unsupported repetition") unless
-							hsh[:freq] == ICS::FREQUENCIES.index("WEEKLY")
-					event[:rrule] = hsh
-				when "RDATE", "EXDATE"
-					#we ignore all parameters (should only be timezone):
-					event[node.name.downcase.to_sym] = node.values.collect do |val|
-						Date.from_ics_datetime(val)
-					end
-				else
-					#ignored
-				end
-			when ICS::Component
-				#ignore components inside a VEVENT
-			end
-		end
+  (pr.select { |o| o.name == "VEVENT" }).each do |vev|
+    event = {}
+    
+    vev.each do |node| #this code might as well go into common.rb
+      case node
+      when ICS::ContentLine
+        case node.name
+        when "DTSTART"
+          #we ignore the TZID param
+          event[:dtstart] = Date.from_ics_datetime(node.values[0])
+        when "DTEND"
+          #we ignore the TZID param
+          event[:dtend] = Date.from_ics_datetime(node.values[0])
+        when "RRULE"
+          hsh = ICS::parse_rrule(node.values[0])
+          die("Unsupported repetition") unless
+              hsh[:freq] == ICS::FREQUENCIES.index("WEEKLY")
+          event[:rrule] = hsh
+        when "RDATE", "EXDATE"
+          #we ignore all parameters (should only be timezone):
+          event[node.name.downcase.to_sym] = node.values.collect do |val|
+            Date.from_ics_datetime(val)
+          end
+        else
+          #ignored
+        end
+      when ICS::Component
+        #ignore components inside a VEVENT
+      end
+    end
 
-		die("Repetition error: first occurence not aligned to pattern") unless
-				event[:dtstart].wday == event[:rrule][:byday]
-		
-		event[:rdate].each do |date|
-			datestr = (date - 1).to_icsdate
-			dumpline("BEGIN:VEVENT")
-			dumpline("DTSTART;VALUE=DATE:#{datestr}")
-			dumpline("DTEND;VALUE=DATE:#{datestr}")
-			dumpline("SUMMARY:#{summary}")
-			dumpline("END:VEVENT")
-		end
+    die("Repetition error: first occurence not aligned to pattern") unless
+        event[:dtstart].wday == event[:rrule][:byday]
+    
+    event[:rdate].each do |date|
+      datestr = (date - 1).to_icsdate
+      dumpline("BEGIN:VEVENT")
+      dumpline("DTSTART;VALUE=DATE:#{datestr}")
+      dumpline("DTEND;VALUE=DATE:#{datestr}")
+      dumpline("SUMMARY:#{summary}")
+      dumpline("END:VEVENT")
+    end
 
-		event[:exdate].map! { |d| d - 1 } #shift all exdates one day back
-		rng = Range.new(RepeatedDate.new(event[:dtstart] - 1, event[:rrule][:interval]),
-		                RepeatedDate.new(event[:rrule][:until] - 1, event[:rrule][:interval]))
-		rng.each do |wdate|
-			if event[:exdate].index(wdate.date).nil? then
-				datestr = wdate.date.to_icsdate
-				dumpline("BEGIN:VEVENT")
-				dumpline("DTSTART;VALUE=DATE:#{datestr}")
-				dumpline("DTEND;VALUE=DATE:#{datestr}")
-				dumpline("SUMMARY:#{summary}")
-				dumpline("END:VEVENT")
-			end
-		end
-	end
+    event[:exdate].map! { |d| d - 1 } #shift all exdates one day back
+    rng = Range.new(RepeatedDate.new(event[:dtstart] - 1, event[:rrule][:interval]),
+                    RepeatedDate.new(event[:rrule][:until] - 1, event[:rrule][:interval]))
+    rng.each do |wdate|
+      if event[:exdate].index(wdate.date).nil? then
+        datestr = wdate.date.to_icsdate
+        dumpline("BEGIN:VEVENT")
+        dumpline("DTSTART;VALUE=DATE:#{datestr}")
+        dumpline("DTEND;VALUE=DATE:#{datestr}")
+        dumpline("SUMMARY:#{summary}")
+        dumpline("END:VEVENT")
+      end
+    end
+  end
 end
 
 dumpline("BEGIN:VCALENDAR")
