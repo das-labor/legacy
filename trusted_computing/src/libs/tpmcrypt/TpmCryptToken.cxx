61c2c577acce58d48e6054f354410fe4c3b167d8
zaolin <zaolin@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-02-11 19:08:01 +0000

    migrate....

---
+++ trusted_computing/src/libs/tpmcrypt/TpmCryptToken.cxx
@@ -0,0 +1,31 @@
+// Copyright (C) 2008, 2009 by Sirrix AG security technologies
+// Copyright (C) 2010 by Philipp Deppenwiese
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+/**
+ * @file        TpmCryptSubject.cxx
+ * @brief       Implementation of TpmCrypts subject class.
+ * @version     $Revision: 764 $
+ * @date        Last modification at $Date: 2009-07-29 15:33:10 +0200 (Wed, 29 Jul 2009) $ by $Author: selhorst $
+ */
+
+#include <TpmCryptToken.hxx>
+#include <TpmCryptTPM.hxx>
+
+
+using namespace std;
+using namespace utils;
