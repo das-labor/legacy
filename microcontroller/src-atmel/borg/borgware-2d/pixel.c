e92749283fcede7f2893c2c7eb5f3c13f9d0ca78
chris <chris@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-02-23 10:34:41 +0000

    optimized some functions, saving ~100 byte

--- microcontroller/src-atmel/borg/borgware-2d/pixel.c
+++ microcontroller/src-atmel/borg/borgware-2d/pixel.c
@@ -113,6 +113,7 @@ for (y=p1.y;y<(p1.y+h);y++){
 
 #endif /* ANIMATION_HERWEG */
 
+
 //shifts pixmap left. It is really shifted right, but because col0 is left in the Display it's left.
 void shift_pixmap_l(){
 	unsigned char plane, row, byte;
@@ -136,72 +137,4 @@ unsigned char get_pixel(pixel p){
 	}else{
 		return 0!= (pixmap[0][p.y][p.x/8] & shl_table[p.x%8]);
 	}
-}
-
-
-unsigned char get_next_pixel(pixel p, direction dir){
-	pixel tmp;
-	switch (dir){
-		case right:
-			tmp = (pixel){p.x-1, p.y};
-			break;
-		case left:
-			tmp = (pixel){p.x+1, p.y};
-			break;	
-		case down:
-			tmp = (pixel){p.x, p.y+1};
-			break;
-		case up:
-			tmp = (pixel){p.x, p.y-1};
-			break;
-		default:
-			tmp = p;
-			break;
-	}
-	return get_pixel(tmp);
-}
-
-direction direction_r(direction dir){
-	switch (dir){
-			case right:
-				return(down);
-			case down:
-				return(left);
-			case left:
-				return(up);
-			case up:
-				return (right);	
-		}
-	return(0);
-}
-
-void set_cursor(cursor* cur, pixel p){
-	cur->pos = p;
-	switch (cur->mode){
-		case clear:
-			clearpixel(p);
-			break;
-		case set:
-			setpixel(p,3);
-			break;
-	}
-}
-
-pixel next_pixel(pixel pix, direction dir){
-	switch (dir){
-			case right:
-				return((pixel){pix.x-1, pix.y});
-				break;
-			case left:
-				return((pixel){pix.x+1, pix.y});
-				break;
-			case down:
-				return((pixel){pix.x, pix.y+1});
-				break;
-			case up:
-				return((pixel){pix.x, pix.y-1});
-				break;
-	
-			}
-	return (pixel){0,0};
 }
