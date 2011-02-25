46553861913f09d14e7d849e8ec9377449d1f474
chris <chris@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-02-25 22:57:49 +0000

    small cleanups

--- microcontroller/src-atmel/borg/borgware-2d/animations/programm.h
+++ microcontroller/src-atmel/borg/borgware-2d/animations/programm.h
@@ -1,41 +1,24 @@
 #ifndef PROGRAMM_H_
 #define PROGRAMM_H_
 
-void off();
-void spirale(unsigned int delay);
-void joern1();
-void joern2();
-void draw_line( unsigned char yabs, signed char delta);
-void schachbrett(unsigned char times);
-void test1();
-void test_level1();
-void test_level2();
-void test_level3();
-void test_levels();
+void test_level(unsigned char level);
 void test_palette();
 void test_palette2();
 
-// randon dots appear and stay
-void dots1();
+#ifdef ANIMATION_OFF
+inline static void off()
+{
+	clear_screen(0);
 
-// rectangle-pattern
-void rectangle1();
+	while(1)
+		wait(100);
 
-// random rectangles
-void rectangles();
+}
+#endif
 
-// lines show and disappear quickly
-void lines1();
-
-//  8x8 rectangle with a cross inside 
-void checkbox();
-
-// a scanner effect with moving h & v lines
-void movinglines();
-
-void snake();
-void matrix();
-void fadein();
+void spirale(unsigned int delay);
+void joern1();
+void schachbrett(unsigned char times);
 void feuer();
 void random_bright(unsigned cycles);
 
