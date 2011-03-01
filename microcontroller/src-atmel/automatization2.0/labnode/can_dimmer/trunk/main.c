f2a4d1b5890d6c53f6664d831ec3d6662b7a5b19
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-03-01 17:28:54 +0000

    rm testcode, set pwm val

--- microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/main.c
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/main.c
@@ -30,17 +30,21 @@ void init(void)
 	 
 int main(void)
 {
-	//volatile uint16_t foo = 0;
 	//system initialization
 	init();
 
-	/*
+/*	
 	DDRA |= (1<<PA4)|(1<<PA5);
 	DDRC |= (1<<PC4)|(1<<PC5);
 	PORTA |= (1<<PA4)|(1<<PA5);
 	PORTC |= (1<<PC4)|(1<<PC5);
 	*/
 	dimmer_init();
+	
+	set_dimmer(0, 255);
+	set_dimmer(1, 255);
+	set_dimmer(2, 255);
+	set_dimmer(3, 255);
 
 	sei();
 
@@ -48,21 +52,6 @@ int main(void)
 	while (1)
 	{
 		can_handler();
-		
-		
-		/*
-
-		//periodically send test message
-				
-		if(foo == 0){
-			static can_message msg = {1, 2, 3, 4, 2, {0x55, 0xaa}};
-			msg.addr_src = myaddr;
-			can_transmit(&msg);
-		}
-		foo++;
-		
-		*/
-
 	}
 
 }
