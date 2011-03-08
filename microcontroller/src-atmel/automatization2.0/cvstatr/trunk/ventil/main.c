5b33c0fd9e45f7bea824fd4a7f2c0d3728bbbc42
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-03-08 17:39:19 +0000

    ventil steuerung: in progress

--- microcontroller/src-atmel/automatization2.0/cvstatr/trunk/ventil/main.c
+++ microcontroller/src-atmel/automatization2.0/cvstatr/trunk/ventil/main.c
@@ -4,39 +4,121 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
-//#include <uart.h>
 #include <stdlib.h>
 
 #include "config.h"
 #include "twi_slave/twi_slave.h"
 
+#define IL1 _BV(PC1)
+#define IL2 _BV(PC2)
+#define IH1 _BV(PC0)
+#define IH2 _BV(PC4)
 
-#define V_OEFFNEN()    PORTC &= ~(_BV(PC2) | _BV(PC3)); PORTC |= _BV(PC0) | _BV(PC1)
-#define V_SCHLIESSEN() PORTC &= ~(_BV(PC0) | _BV(PC1)); PORTC |= _BV(PC2) | _BV(PC3)
+
 #define FREILAUF()	   PORTC &= ~(_BV(PC1) | _BV(PC2)); PORTC |= _BV(PC0) | _BV(PC3)
 
+#define LED _BV(PD6)
 
 #define SCHRITTE 420
 
 #define VENTIL_STEP  0
 #define VENTIL_RESET 1
 
+//XXX Todo
+// truth table h-br
+// pos tracking
+// disable int pos
+// atmega48 version?
 
+volatile uint8_t mask = 0;
+volatile int16_t pos16 = 0;
+int32_t pos = 0;
+volatile uint8_t bit2ms = 0;
+
+
+// abschalten bei erreichen eines Wertes
 ISR(TIMER1_COMPA_vect) {
 	FREILAUF();
-	TCNT1H = 0;
-	TCNT1L = 0;
-	PORTD &= ~_BV(PD6);
+	TCNT1 = 0;
 }
 
+// Timer0 Motor PWM off
+ISR(TIMER2_COMPA_vect) {
+  PORTC &= ~mask;
+}
+
+// Timer0 Motor PWM on
+ISR(TIMER2_OVF_vect) {
+  PORTC |= mask;
+  bit2ms = 1;
+}
+
+// Setzt PWM für Motor
+// val intger 16 bit
+// positive Werte bis 255 vorwärtes
+// negative Werte bis 255 rückwärts
+// 0 stopp
+void setPwm(int16_t val) {
+  if (val > 0) {
+    mask = IH1;
+    PORTC |= IL2;
+    PORTC &= ~(IL1|IH2);
+    PORTD &= LED;
+    OCR2 = val;
+  }
+  else if (val < 0) {
+    mask = IH2;
+    PORTC |= IL1;
+    PORTC &= ~(IL2|IH1);
+    PORTD |= LED;
+    OCR2 = -val;
+  } else {
+    mask = 0;
+    PORTC &= ~(IH1|IH2|IL1|IL2);
+    PORTD &= ~(LED);
+  }
+}
+
+//Positions Regler
+int32_t pos_reg_soll;    // wohin will ich?
+int32_t pos_soll;    //
+uint8_t pos_regeln;  // soll ich da jetzt hinfahren oder was?
+uint8_t ramp_state = 0;
+
+void go_pos(int32_t soll) {
+	pos_soll = soll;
+	ramp_state = 1;
+}
+
+#define PWM_MAX 250
+#define PWM_MIN 14
+
+void posRegler() {
+  int32_t regler_out;
+  int32_t pos_abw; // Positionsabweichung
+
+  pos_abw = pos - pos_reg_soll; // Regeldifferenz
+
+  regler_out = pos_abw / 64; // P Anteil
+
+  if (regler_out > PWM_MAX)
+    regler_out = PWM_MAX;
+  else if(regler_out < (-PWM_MAX))
+    regler_out = -PWM_MAX;
+    
+  if (regler_out < PWM_MIN && regler_out > 0)
+    regler_out = PWM_MIN;
+  else if(regler_out > (-PWM_MIN)  && regler_out < 0)
+    regler_out = -PWM_MIN;
+
+  setPwm(regler_out); 
+}
+//
 
 void fahre_ventiel_zu() {
 	uint16_t ct_last = TCNT1;
 	uint16_t ct_act = 0;
 	
-//	PORTD |= _BV(PD6);
-	V_SCHLIESSEN();
-	_delay_ms(45);
 
 	while (1) {
 		ct_act = TCNT1;
@@ -47,14 +129,13 @@ void fahre_ventiel_zu() {
 		_delay_ms(25);
 	}
 	FREILAUF();
-//	PORTD &= ~_BV(PD6);
 }
 
 
 
 void init(void) {
 	// Output for h-bridge
-	DDRC  |= _BV(PC0) | _BV(PC1) | _BV(PC2) | _BV(PC3);
+	DDRC  |= IL1 | IL2 | IH1 | IH2;
 	// Freilauf
 	FREILAUF();
 
@@ -64,17 +145,23 @@ void init(void) {
 	// Output Compare Interrupt Timer 1 OC REG A
 	TIMSK |= _BV(OCF1A);
 
+  // Init Timer2
+  
+  TCCR2 = _BV(CS22); // 8MHz / 64
+  
+  TIMSK |= _BV(OCF2) | _BV(TOV2);
 	// LED Output
-	DDRD |= _BV(PD6);
+	DDRD |= LED;
 	
 	ACSR = _BV(ACD); // Disable Analog Comparator (power save)
 
-	fahre_ventiel_zu();
 
 	TWIS_Init();
 
 	//turn on interrupts
   sei();
+  
+  fahre_ventiel_zu();
 }
 
 int main(void) {
@@ -98,23 +185,6 @@ int main(void) {
 
 					switch (data[0]) {
 						case VENTIL_STEP:
-							if (data[1] < 6)
-								ziel_pos = data[1];
-							OCR1A = abs(ziel_pos - ist_pos) * SCHRITTE;
-
-							if (ist_pos < ziel_pos)
-							{
-								V_OEFFNEN();
-								PORTD |= _BV(PD6);
-								ist_pos = ziel_pos;
-							}
-							if (ist_pos > ziel_pos)
-							{
-								V_SCHLIESSEN();
-								PORTD |= _BV(PD6);
-								ist_pos = ziel_pos;
-							}
-							_delay_ms(50);
 							break;
 						case VENTIL_RESET:
 							//TCCR2 = 0;
@@ -132,15 +202,32 @@ int main(void) {
 					break;
 			}
 		}
-	// prüfen ob motor läuft - bei ende pos speichern
-		ct_act = TCNT1;
-		if (ct_last == ct_act) {
-			FREILAUF();
-			PORTD &= ~_BV(PD6);
-		}
-		ct_last = TCNT1;
-		_delay_ms(30); //XXX durch timer ersetzen
+		cli();
+    tmppos16 = pos16;
+    pos16 = 0;
+    sei();
+    pos += tmppos16;
 
+		if (bit2ms) { // 1ms Routine
+		  tim++;
+		  bit2ms = 0;
+		  if (tim == 10)
+		    rampe();
+		  if (tim == 20) {
+		    tim = 0;
+		    posRegler();
+		  }
+/*		  if (delay == 0) {
+		      toggle ^=1;
+		      delay = 8000;
+		      if (toggle) {
+		        go_pos(418000);
+          } else {
+            go_pos(0);
+          }
+		  } else {
+		    delay--;
+		  }*/
 	}
 
 	return 1;
