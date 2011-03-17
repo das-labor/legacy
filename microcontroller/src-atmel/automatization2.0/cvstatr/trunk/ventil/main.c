8f6ec61a5115c993108176fad808f5d2385a0f48
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-03-17 23:32:06 +0000

    ventil: fix whitespace, motorbreak

--- microcontroller/src-atmel/automatization2.0/cvstatr/trunk/ventil/main.c
+++ microcontroller/src-atmel/automatization2.0/cvstatr/trunk/ventil/main.c
@@ -19,29 +19,38 @@
 
 #define LED _BV(PD6)
 
-#define SCHRITTE 420
-
 #define VENTIL_STEP  0
 #define VENTIL_RESET 1
 
 //XXX Todo
+// sensor signal messen
+// endpos bestimmen
 // i2c befehle
 // atmega48 version?
 
 volatile uint8_t mask = 0;
+volatile int16_t pos16 = 0;
 int32_t pos = 0;
-volatile uint8_t bit2ms = 0;
+volatile uint8_t tick = 0;
 
+// INT 0 count position
+ISR(TIMER1_COMPA_vect) {
+	if (mask == IH2)
+		pos16++;
+	else
+		pos16--;
+	TCNT1 = 0;
+}
 
 // Timer0 Motor PWM off
 ISR(TIMER2_COMP_vect) {
-  PORTC |= mask;
+	PORTC |= mask;
 }
 
 // Timer0 Motor PWM on
 ISR(TIMER2_OVF_vect) {
-  PORTC &= ~mask;
-  bit2ms = 1;
+	PORTC &= ~mask;
+	tick = 1;
 }
 
 // Setzt PWM für Motor
@@ -50,25 +59,25 @@ ISR(TIMER2_OVF_vect) {
 // negative Werte bis 255 rückwärts
 // 0 stopp
 void setPwm(int16_t val) {
-  if (val > 0) {
-    mask = IH1;
-    PORTC |= IL2 | IH2;
-    PORTC &= ~(IL1);
-    PORTD |= LED;
-    OCR2 = val;
-  }
-  else if (val < 0) {
-    mask = IH2;
-    PORTC |= IL1 | IH1;
-    PORTC &= ~(IL2);
-    PORTD |= LED;
-    OCR2 = -val;
-  } else {
-    mask = 0;
-    PORTC &= ~(IL1 | IL2);
-    PORTC |= IH1 | IH2;
-    PORTD &= ~(LED);
-  }
+	if (val > 0) {
+		mask = IH1;
+		PORTC |= IL2 | IH2;
+		PORTC &= ~(IL1);
+		PORTD |= LED;
+		OCR2 = val;
+	}
+	else if (val < 0) {
+		mask = IH2;
+		PORTC |= IL1 | IH1;
+		PORTC &= ~(IL2);
+		PORTD |= LED;
+		OCR2 = -val;
+	} else {
+		mask = 0;
+		PORTC |= (IL1 | IL2);
+		PORTC |= IH1 | IH2;
+		PORTD &= ~(LED);
+	}
 }
 
 //Positions Regler
@@ -82,33 +91,33 @@ void go_pos(int32_t soll) {
 #define PWM_MIN 60
 
 void posRegler() {
-  int32_t regler_out;
-  int32_t pos_abw; // Positionsabweichung
+	int32_t regler_out;
+	int32_t pos_abw; // Positionsabweichung
 
-  pos_abw = pos - pos_reg_soll; // Regeldifferenz
+	pos_abw = pos - pos_reg_soll; // Regeldifferenz
 
-  regler_out = pos_abw * 2; // P Anteil
+	regler_out = pos_abw * 4; // P Anteil
 
-  if (regler_out > PWM_MAX)
-    regler_out = PWM_MAX;
-  else if(regler_out < (-PWM_MAX))
-    regler_out = -PWM_MAX;
-    
-  if (regler_out < PWM_MIN && regler_out > 0)
-    regler_out = PWM_MIN;
-  else if(regler_out > (-PWM_MIN) && regler_out < 0)
-    regler_out = -PWM_MIN;
+	if (regler_out > PWM_MAX)
+		regler_out = PWM_MAX;
+	else if(regler_out < (-PWM_MAX))
+		regler_out = -PWM_MAX;
 
-  setPwm(regler_out); 
+	if (regler_out < PWM_MIN && regler_out > 0)
+		regler_out = PWM_MIN;
+	else if(regler_out > (-PWM_MIN) && regler_out < 0)
+		regler_out = -PWM_MIN;
+
+	setPwm(regler_out); 
 }
 //
 
 void fahre_ventiel_zu() {
 	uint16_t ct_last = TCNT1;
 	uint16_t ct_act = 0;
-	
-  setPwm(150);
-  _delay_ms(100);
+
+	setPwm(150);
+	_delay_ms(100);
 	while (1) {
 		ct_act = TCNT1;
 		if (ct_last == ct_act)
@@ -118,7 +127,7 @@ void fahre_ventiel_zu() {
 		_delay_ms(40);
 	}
 	setPwm(0);
-  _delay_ms(1000);
+	_delay_ms(1000);
 	TCNT1 = 0;
 	pos = 0;
 }
@@ -131,36 +140,42 @@ void init(void) {
 	// Freilauf
 	FREILAUF();
 
-  // Timer 1 Zähler für Position
-	// CTC Modus OCR1A Reg 16 bit, ext clock rising edge
-	TCCR1B |= (WGM12) | _BV(CS12) | _BV(CS11) | _BV(CS10);
+	// Timer 1 Zähler für Position
+	// Normal mode, ext clock rising edge
+	TCCR1B = _BV(CS12) | _BV(CS11) | _BV(CS10);
 
-  // Init Timer2 Motor PWM und Regler Zeitschlitz
-  TCCR2 = _BV(CS22); // 8MHz / 64
-  TIMSK |= _BV(OCF2) | _BV(TOV2); // Comparematch & Overflow
-  
+
+	// Init Timer2 Motor PWM und Regler Zeitschlitz
+	TCCR2 = _BV(CS22); // 8MHz / 64
+	TIMSK |= _BV(OCF2) | _BV(TOV2); // Comparematch & Overflow
+
 	// LED Output
 	DDRD |= LED;
-	
+
 	ACSR = _BV(ACD); // Disable Analog Comparator (power save)
 
-//	TWIS_Init();
+	TWIS_Init();
 
 	//turn on interrupts
-  sei();
-  
-  fahre_ventiel_zu();
+	sei();
+
+	fahre_ventiel_zu();
+
+	TCCR1B = _BV(WGM12) | _BV(CS12) | _BV(CS11) | _BV(CS10);
+	OCR1A = 1;
+	TIMSK |= _BV(OCF1A);
 }
 
 int main(void) {
 	uint8_t TWIS_ResponseType;
 	int8_t data[2];
-  uint8_t tim = 0;
+	uint8_t tim = 0;
+	int16_t tmppos16;
 	//system initialization
 	init();
 
-  uint16_t delay = 0;
-  uint8_t toggle = 0;
+	uint16_t delay = 0;
+	uint8_t toggle = 0;
 	
 	//the main loop continuously handles can messages
 	while (1) {
@@ -176,7 +191,6 @@ int main(void) {
 						case VENTIL_STEP:
 							break;
 						case VENTIL_RESET:
-							//TCCR2 = 0;
 							wdt_enable(0);
 							while (1);
 							break;
@@ -191,32 +205,35 @@ int main(void) {
 					break;
 			}
 		}
+		cli();
+		pos += pos16;
+		pos16 = 0;
+		sei();
 
+		if (tick) { // 1ms Routine
+/*	    	if (mask == IH1)
+				pos -= TCNT1;
+			else if (mask == IH2)
+				pos += TCNT1;
+			TCNT1 = 0;*/
 
-		if (bit2ms) { // 1ms Routine
-	    if (mask == IH1)
-        pos -= TCNT1;
-      else if (mask == IH2)
-        pos += TCNT1;
-      TCNT1 = 0;
-
-		  tim++;
-		  bit2ms = 0;
-		  if (tim == 10) {
-		    tim = 0;
-		    posRegler();
-		  }
-		  if (delay == 0) {
-		      toggle ^=1;
-		      delay = 6000;
-		      if (toggle) {
-		        go_pos(300);
-          } else {
-            go_pos(0);
-          }
-		  } else {
-		    delay--;
-		  }
+			tim++;
+			tick = 0;
+			if (tim == 2) {
+				tim = 0;
+				posRegler();
+			}
+			if (delay == 0) {
+				toggle ^=1;
+				delay = 15000;
+				if (toggle) {
+					go_pos(500);
+				} else {
+					go_pos(0);
+				}
+			} else {
+				delay--;
+			}
 		}
 	}
 
