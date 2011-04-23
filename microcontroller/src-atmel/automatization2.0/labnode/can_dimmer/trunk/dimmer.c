5afb53a8f9d2d1f9aa54db83fba89d0b45a07d44
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-04-23 00:08:08 +0000

    can_dimmer: codestyle, dimmerchannel fuer evg

--- microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/dimmer.c
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/dimmer.c
@@ -6,8 +6,8 @@
 #define MAX_VAL 550
 
 //synchronize to zero cross
-ISR(TIMER1_CAPT_vect){
-	TCNT1 = 620;	
+ISR(TIMER1_CAPT_vect) {
+	TCNT1 = 620;
 }
 
 volatile uint8_t update_in_progress;
@@ -20,10 +20,10 @@ uint8_t channels_sorted[NUM_CHANNELS];
 ISR(TIMER1_COMPB_vect) {
 	static uint8_t state;
 	static uint8_t next;
-	
+
 	static uint8_t channels[NUM_CHANNELS];
 	static uint16_t dim_vals[NUM_CHANNELS];
-	
+
 	if (state == 0) {
 		if (!update_in_progress) {
 			uint8_t x;
@@ -33,11 +33,11 @@ ISR(TIMER1_COMPB_vect) {
 			}
 		}
 		next = 0;
-		if (dim_max[0]) PORTA |= (1<<PA4); else PORTA &= ~(1<<PA4);
-		if (dim_max[1]) PORTA |= (1<<PA5); else PORTA &= ~(1<<PA5);
-		if (dim_max[2]) PORTC |= (1<<PC4); else PORTC &= ~(1<<PC4);
-		if (dim_max[3]) PORTC |= (1<<PC5); else PORTC &= ~(1<<PC5);
-		
+		if (dim_max[0]) PORTA |= _BV(PA4); else PORTA &= ~_BV(PA4);
+		if (dim_max[1]) PORTA |= _BV(PA5); else PORTA &= ~_BV(PA5);
+		if (dim_max[2]) PORTC |= _BV(PC4); else PORTC &= ~_BV(PC4);
+		if (dim_max[3]) PORTC |= _BV(PC5); else PORTC &= ~_BV(PC5);
+
 		OCR1B = dim_vals[0];
 		if (dim_vals[0] != MAX_VAL) { //exept case where all lamps are off
 			state = 1;
@@ -45,26 +45,25 @@ ISR(TIMER1_COMPB_vect) {
 	} else {
 		handle_next:
 		switch (channels[next]) {
-			case 0: PORTA |= (1<<PA4); break;
-			case 1: PORTA |= (1<<PA5); break;
-			case 2: PORTC |= (1<<PC4); break;
-			case 3: PORTC |= (1<<PC5); break;	
+			case 0: PORTA |= _BV(PA4); break;
+			case 1: PORTA |= _BV(PA5); break;
+			case 2: PORTC |= _BV(PC4); break;
+			case 3: PORTC |= _BV(PC5); break;
 		}
 		next++;
 		if ((next != NUM_CHANNELS) && (dim_vals[next] != MAX_VAL)) {
 			OCR1B = dim_vals[next];
 			if (TCNT1 >= OCR1B){   //if allready time for next
-				TIFR = (1<<OCF1B); //reset flag in case it was set allready
+				TIFR = _BV(OCF1B); //reset flag in case it was set allready
 				goto handle_next;   //and handle now
 			}
 		} else {
 			//all channels handled
 			OCR1B = MAX_VAL;
-			state = 0;		
+			state = 0;
 		}
 	}
 }
-
 
 
 void dimmer_init() {
@@ -74,15 +73,15 @@ void dimmer_init() {
 		dim_vals_sorted[x] = MAX_VAL;
 	}
 
-	DDRA |= (1<<PA4) | (1<<PA5);
-	DDRC |= (1<<PC4) | (1<<PC5);
+	DDRA |= _BV(PA4) | _BV(PA5);
+	DDRC |= _BV(PC4) | _BV(PC5);
 
-	TCCR1B |= (1<<ICNC1) | (1<<WGM12) | (1<<CS12); //CTC (TOP = OCR1A), clk/256
+	TCCR1B |= _BV(ICNC1) | _BV(WGM12) | _BV(CS12); //CTC (TOP = OCR1A), clk/256
 	OCR1A = 625;
 
 	OCR1B = MAX_VAL;
 
-	TIMSK |= (1<<TICIE1) | (1<<OCIE1B);//Input capture int on, output compare 1B on
+	TIMSK |= _BV(TICIE1) | _BV(OCIE1B);//Input capture int on, output compare 1B on
 }
 
 
@@ -93,8 +92,6 @@ void set_dimmer(uint8_t channel, uint8_t bright) {
 	uint16_t dimval = 512 - bright * 2;
 	if (bright == 0) dimval = MAX_VAL;
 	if (bright == 255) dim_max[channel] = 1; else dim_max[channel] = 0;
-
-
 
 	update_in_progress = 1;
 
@@ -109,9 +106,9 @@ void set_dimmer(uint8_t channel, uint8_t bright) {
 			break;
 		}
 	}
-	
+
 	dim_vals_sorted[NUM_CHANNELS-1] = MAX_VAL+1;
-	
+
 	//insert channel into table
 	for (x = 0; x < NUM_CHANNELS; x++) { 
 		if (dimval < dim_vals_sorted[x]) {
@@ -125,8 +122,6 @@ void set_dimmer(uint8_t channel, uint8_t bright) {
 			break;
 		}
 	}
-
 	update_in_progress = 0;
-
 }
 
