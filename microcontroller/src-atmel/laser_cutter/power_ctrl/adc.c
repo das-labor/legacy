f10978a79db3b0bf205b4518d18b26696a8b8b07
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-04-25 15:59:17 +0000

    added

---
+++ microcontroller/src-atmel/laser_cutter/power_ctrl/adc.c
@@ -0,0 +1,51 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "adc.h"
+
+#define ADC_CHANNEL_U 5
+#define ADC_CHANNEL_I 4
+
+volatile uint16_t adc_u;
+volatile uint16_t adc_i;
+
+
+ISR(ADC_vect){
+	static uint8_t state;
+	if(state == 0){
+		state = 1;
+		adc_u = ADC;
+		//select AVCC reference and u channel again for next conversion (i conversion running allready)
+		ADMUX = (1<<REFS1) | (1<<REFS0) | ADC_CHANNEL_U;
+	}else{
+		state = 0;
+		adc_i = ADC;
+		//select AVCC reference and i channel again for next conversion (u conversion running allready)
+		ADMUX = (1<<REFS1) | (1<<REFS0) | ADC_CHANNEL_I;
+
+	}
+}
+
+void init_adc(){
+	//REFS1 REFS0 ADLAR MUX4 MUX3 MUX2 MUX1 MUX0      ADMUX
+	//REFS1 REFS0 Voltage Reference Selection
+	//0 0 AREF, Internal Vref turned off
+	//0 1 AVCC with external capacitor at AREF pin
+	//1 0 Reserved
+	//1 1 Internal 2.56V Voltage Reference with external capacitor at AREF pin
+
+	ADMUX = (1<<REFS1) | (1<<REFS0);
+
+	//ADEN ADSC ADFR ADIF ADIE ADPS2 ADPS1 ADPS0      ADCSRA
+	//ADPS2 ADPS1 ADPS0 Division Factor
+	//0 0 0 2
+	//0 0 1 2
+	//0 1 0 4
+	//0 1 1 8
+	//1 0 0 16
+	//1 0 1 32
+	//1 1 0 64
+	//1 1 1 128
+	ADCSRA = (1<<ADEN) | (1<<ADSC) | (1<<ADFR) | (1<<ADIE) | 7;
+	
+}
