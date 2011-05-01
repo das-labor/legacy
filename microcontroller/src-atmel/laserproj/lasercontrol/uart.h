04267b52f62efe50293b147863f5357aff999f1b
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-05-01 18:08:40 +0000

    new version modified uart.c & uart.h

--- microcontroller/src-atmel/laserproj/lasercontrol/uart.h
+++ microcontroller/src-atmel/laserproj/lasercontrol/uart.h
@@ -1,17 +1,9 @@
 #ifndef UART_H
 #define UART_H
 
- 
-  #define F_CPU 16000000         // Oszillator-Frequenz in Hz
-  #define UART_INTERRUPT 1
-  #define UART_BAUD_RATE 115200UL
-  #define UART_RXBUFSIZE 16
-  #define UART_TXBUFSIZE 16
-  #define UART_LINE_BUFFER_SIZE 40
-  #define UART_LEDS             // LED1 and LED2 toggle on tx and rx interrupt
-
 #include <inttypes.h>
 #include <avr/pgmspace.h>
+#include "config.h"
 
 void uart_init();
 
@@ -27,5 +19,12 @@ char uart_getc_nb(char *c);		// returns 1 on success
 //echo charakters back on Uart
 //returns buffer with zero terminated line on success, 0 pointer otherwise
 char * uart_getline_nb();
+
+char uart_rxbuffer_notempty(void);	//returns 1 if queue is not empty, otherwise 0
+#if UART_RXBUFSIZE < 255
+uint8_t uart_getrxqueuelength(void); //returns queue length
+#else
+uint16_t uart_getrxqueuelength(void); //returns queue length
+#endif
 
 #endif
