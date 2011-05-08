3cc6c455d3f6ffa41042038e0db7eb605cfadb52
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-05-08 17:59:26 +0000

    changes: swaped ButtonDown / ButtonUp menu_draw(uint8_t cls) can now erase and redraw the full screen or just redraw all numbers menu_init_entry entry can be marked as read-only read-only entries can't be changed by user read-only entries are marked as - instead of > fixed cursor out of range

--- microcontroller/src-atmel/lib/lcdmenu/menu.h
+++ microcontroller/src-atmel/lib/lcdmenu/menu.h
@@ -18,10 +18,14 @@
 
 #include "config.h"
 
-#define MENU_MAXENTRIES 10
+#define MENU_MAXENTRIES 12
 
 #ifndef MENU_H
 #define MENU_H
+
+#define MENU_READANDWRITE 1
+#define MENU_READONLY 0
+
 volatile static uint8_t menu_ycursor=0;  //y cursor
 volatile static uint8_t menu_LCD_MAXLINES=1;
 volatile static uint8_t menu_entry_cnt=0;
@@ -40,9 +44,9 @@ static pointer_size_t menu_val[MENU_MAXENTRIES];
 
 //function prototypes
 void menu_init(void);
-static void menu_draw(void);
+static void menu_draw(uint8_t cls);
 void menu_update(void);
-void menu_init_entry(void *p,uint8_t size, const char * menu_str);
+void menu_init_entry(void *p,uint8_t size, const char * menu_str, uint8_t iswriteable);
 
 /* example programm:
 	in file config.h:
