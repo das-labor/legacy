4e098b37bf84f19f0cc3a5e0f3fc22e638e9bc94
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-05-12 19:56:19 +0000

    fertig.

--- microcontroller/src-atmel/laserExposer/main_avr/pinout.h
+++ microcontroller/src-atmel/laserExposer/main_avr/pinout.h
@@ -20,3 +20,16 @@
 
 #define MOTOR_READY_PORT  D
 #define MOTOR_READY_BIT   3
+
+
+//Laser (OC1A)
+#define LASER_PORT        D
+#define LASER_BIT         5
+
+//Laser power setting voltage (OC0)
+#define LASER_PWM_PORT    B
+#define LASER_PWM_BIT     3
+
+//Door Switch (needs pullup, low means door closed)
+#define DOOR_SWITCH_PORT  C
+#define DOOR_SWITCH_BIT   0
--- microcontroller/src-atmel/laserExposer/main_avr/stepper.h
+++ microcontroller/src-atmel/laserExposer/main_avr/stepper.h
@@ -10,7 +10,6 @@ extern uint8_t stepper_command;
 #define CMD_HOME    3
 #define CMD_GOTO    4
 
-extern uint8_t stepper_run;
 //1= forward, 0= reverse
 extern uint8_t stepper_dir;
 
