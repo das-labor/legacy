846b402b1ebd1d7dc2d12c8eee289e5a3133410a
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-05-12 20:17:10 +0000

    real animation not just configurations ;)

---
+++ microcontroller/src-atmel/borg/borgware-2d/smallani/colwalk.h
@@ -0,0 +1,6 @@
+#ifndef COLWALK_H_
+#define COLWALK_H_
+
+void colwalk(uint8_t times,uint8_t speed);
+
+#endif /* COLWALK_H_ */
---
+++ microcontroller/src-atmel/borg/borgware-2d/smallani/rowwalk.h
@@ -0,0 +1,6 @@
+#ifndef ROWWALK_H_
+#define ROWWALK_H_
+
+void rowwalk(uint8_t times,uint8_t speed);
+
+#endif /* ROWWALK_H_ */
