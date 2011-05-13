40c72be26181dc4b221b122c0fcc5e15b39d6927
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-05-13 14:54:06 +0000

    i don't like this ethersex-menuconfig - no time left to fix rowbounce :/

---
+++ microcontroller/src-atmel/borg/borgware-2d/smallani/rowbounce.c
@@ -0,0 +1,32 @@
+#include <inttypes.h>
+#include "../config.h"
+#include "../pixel.h"
+#include "../util.h"
+#include "rowbounce.h"
+
+void rowbounce(uint8_t times,uint8_t speed)
+{
+	uint8_t  i, j,k,h;
+	
+	for(k=0;k<times;k++){
+	  clear_screen(0);
+	  for (h=0;h<NUM_ROWS;h++){
+	    for (i=0;i<NUM_COLS;i++){
+	      for (j=0;j<NUM_ROWS;j++){
+		setpixel( (pixel){i,j},(h==i) ? 1:0);
+	      }
+	    }
+	    wait(speed*10);
+	  }
+	  for (h=NUM_ROW-1;h>1;h--){
+	    for (i=0;i<NUM_COLS;i++){
+	      for (j=0;j<NUM_ROWS;j++){
+		setpixel( (pixel){i,j},((h-1)==i) ? 1:0);
+	      }
+	    }
+	    wait(speed*10);
+	  }
+
+	}
+
+}
