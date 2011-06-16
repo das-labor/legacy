07a473e8d5d953872007ef1395018d36b14420a8
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-06-16 19:55:44 +0000

    updated

--- microcontroller/src-atmel/laserproj/lasercontrol/main.c
+++ microcontroller/src-atmel/laserproj/lasercontrol/main.c
@@ -3,67 +3,25 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <inttypes.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "./uart.h"   //rs232 
-#include "./config.h"
-#include "ICcom.h"
-#include "kb.h"  //external PS/2 keyboard
-
-#include <avr/pgmspace.h>
-
-#include "ks0108.h" //include display header file
-#include "arial8.h" //include font header file
+#include "uart.h"   //rs232 
+#include "config.h"
+#include "iccom.h"
+//#include "kb.h"  //external PS/2 keyboard
+//#include "menu.h"
+#include "videoengine.h"
+#include "eeprom.h"
 
 /*
 	lasercontrol
 	made by siro 2011
 
-	Pin configuration:
-	PORTA		LCD DataPort
-  
-	PB4   free to use
-	PB3   lasersafty
-	PB2		// GLCD CS1
-	PB1		// GLCD CS2
-	PB0   free to use
-	
-	PC7   crystal
-  PC6   crystal
-  PC5		// GLCD EN
-	PC4		// GLCD R/W
-	PC3		// GLCD D/I
-	PC2   frequency for H-motor
-  PC1   I2c
-  PC0   I2c
-
-	PD0   free to use
-  PD1   free to use
-	PD2   keyboard CLK
-			black +DATA
-			red	GND
-			white +5V (300mA)
-			green CLK
-
-	PD3 	free to use
-	PD4 	keyboard DATA
-  PD5   Power supply grounding
-  PD6   power switch
-  PD7   Status LED
 */
 
-volatile unsigned char device_on = 0; //main energy off on reset
-volatile unsigned char laser_on = 0;
-volatile unsigned int hmotor_speed = 1;
-volatile unsigned char hmotor_on = 0;
-
-volatile unsigned int hmirror_reflections = 0;
-unsigned int hmirror_reflections_buf = 0;
-volatile unsigned char drawLCD = 0;
-volatile unsigned int virtTimerA=0;
-volatile unsigned int virtTimerA_counter = 0;
+volatile uint16_t mode;
+volatile uint16_t testvar=0;
 
 /* prototypes */
 
@@ -73,6 +31,8 @@ void stop_timer1(void);
 void start_timer1(void);
 void stop_timer2(void);
 void start_timer2(void);
+void save_eeprom_values(void);
+void load_eeprom_values(void);
 
 void init(void)
 {
@@ -90,8 +50,8 @@ void init(void)
 	PORTC = 0;//Internal Pullups all off
 
 	//******************
-	DDRD=_BV(PD6); //PD6 PIN_POWER
-	PORTD =0;	//Internal Pullups all off
+	DDRD=_BV(PD5); //PD5 BLINK_LED
+	PORTD =_BV(PD6);	//Internal Pullups all off,except power button
 
 
 //	MCUCR |= _BV(SE); // Enable "sleep" mode (low power when idle)
@@ -104,43 +64,117 @@ void init(void)
 int main (void)
 {
 	uint8_t main_delay=0;
-	init();		//init ports
-	uart_init(); //init uart
+	uint8_t eeprom_wait_write=0;	//counter up to 10 seconds eeprom write delay
+	void* menu_val_changed;
 		
-	while((PORTD & _BV(PIN_POWER)))	 //wait for Powerbutton to be pressed
-		__asm("nop");
-		
-	start_timer2();	//init timer2
-	start_timer1();	//init timer1
-	
-	sei();  //global enable interrupts 
+		init();		//init ports
 
+		PORTD|=_BV(PIN_BLINKLED); //toggle PIN_BLINKLED
+
+		uart_init(); //init uart
+		enable_safty(); //laser off
+	//	init_kb();	//init ps2 keyboard
+		load_eeprom_values(); //load all values from eeprom and place in correct vars
+	
+		/*	
+		menu_init_entry((void *)&mode,sizeof(mode),PSTR("mode"),MENU_READANDWRITE);
+		menu_init_entry((void *)&ve.h_sync_wait_perc,sizeof(ve.h_sync_wait_perc),PSTR("h_sync_wait %"),MENU_READANDWRITE);
+		menu_init_entry((void *)&ve.v_sync_wait_perc,sizeof(ve.v_sync_wait_perc),PSTR("v_sync_wait %"),MENU_READANDWRITE);
+		menu_init_entry((void *)&ve.h_pixel_perc,sizeof(ve.h_pixel_perc),PSTR("h_viewfield %"),MENU_READANDWRITE);
+		menu_init_entry((void *)&ve.v_pixel_perc,sizeof(ve.v_pixel_perc),PSTR("v_viewfield %"),MENU_READANDWRITE);
+		menu_init_entry((void *)&ve.h_pixel_end_val,sizeof(ve.h_pixel_end_val),PSTR("h_pixel"),MENU_READANDWRITE);
+		menu_init_entry((void *)&ve.v_pixel_end_val,sizeof(ve.v_pixel_end_val),PSTR("v_pixel"),MENU_READANDWRITE);
+		menu_init_entry((void *)&ve.h_line_cnt_avr,sizeof(ve.h_line_cnt_avr),PSTR("h lines ps"),MENU_READONLY);
+		menu_init_entry((void *)&ve.frame_cnt_avr,sizeof(ve.frame_cnt_avr),PSTR("frames ps"),MENU_READONLY);
+		menu_init_entry((void *)&testvar,sizeof(testvar),PSTR("testvar"),MENU_READANDWRITE);
+		menu_init_entry((void *)&OCR1A,sizeof(OCR1A),PSTR("Timer 1 V"),MENU_READANDWRITE);
+		menu_init_entry((void *)&OCR2,sizeof(OCR2),PSTR("Timer2 H"),MENU_READANDWRITE);
+				                  
+		menu_init();	//init the GLCD and menu
+		*/
+		icc_init((void *)&mode,sizeof(mode), ICC_DIRECTION_BOTH);	 //link var to icc
+		icc_init((void *)&ve.h_sync_wait_perc,sizeof(ve.h_sync_wait_perc), ICC_DIRECTION_BOTH); //link var to icc
+		icc_init((void *)&ve.v_sync_wait_perc,sizeof(ve.v_sync_wait_perc), ICC_DIRECTION_BOTH); //link var to icc
+		icc_init((void *)&ve.h_pixel_perc,sizeof(ve.h_pixel_perc), ICC_DIRECTION_BOTH); //link var to icc
+		icc_init((void *)&ve.v_pixel_perc,sizeof(ve.v_pixel_perc), ICC_DIRECTION_BOTH); //link var to icc
+		icc_init((void *)&ve.h_pixel_end_val,sizeof(ve.h_pixel_end_val), ICC_DIRECTION_BOTH); //link var to icc
+		icc_init((void *)&ve.v_pixel_end_val,sizeof(ve.v_pixel_end_val), ICC_DIRECTION_BOTH); //link var to icc
+//		icc_init((void *)&ve.h_line_cnt_avr,sizeof(ve.h_line_cnt_avr), ICC_DIRECTION_WRITEONLY); //link var to icc
+//		icc_init((void *)&ve.frame_cnt_avr,sizeof(ve.frame_cnt_avr), ICC_DIRECTION_WRITEONLY); //link var to icc
+//		icc_init((void *)&testvar,sizeof(testvar),ICC_DIRECTION_READONLY); //link var to icc
+		PORTD&=~_BV(PIN_BLINKLED); //toggle PIN_BLINKLED
+		sei();  //global enable interrupts 
+		
+		icc_send((void *)&mode);
+		icc_send((void *)&ve.h_sync_wait_perc);
+		icc_send((void *)&ve.v_sync_wait_perc);
+		icc_send((void *)&ve.h_pixel_perc);
+		icc_send((void *)&ve.v_pixel_perc);
+		icc_send((void *)&ve.h_pixel_end_val);
+		icc_send((void *)&ve.v_pixel_end_val);
+
+	
 	while (1) {
 		main_delay++;	 //internal counter
-
-		if(uart_rxbuffer_notempty())		//check for byte arrived on uart_init
-				icc_handle_uart();		//handle it
-			
-	
+				
+			if(uart_rxbuffer_notempty()){		//check for byte arrived on uart_init
+				if(icc_handle_uart()){		//handle it
+					eeprom_wait_write=1;//menu_val_changed=1;//menu_draw(0);	//redraw menu_init
+					PORTD|=_BV(PIN_BLINKLED); //toggle PIN_BLINKLED
+				}
+			}
+			//menu_val_changed= menu_update();	//update menu
+			//if(menu_val_changed != NULL)
+			//{
+			//	icc_send(menu_val_changed);//send if something was updated
+			//	eeprom_wait_write=1;
+			//}
 				if(!(main_delay%10)){
-					if(!(PORTD & _BV(PIN_POWER)))	 //check if powerbutton is pressed
+
+					if(POWER_BUTTON_PRESSED)	 //check if powerbutton is pressed
 					{
-							while(!(PORTD & _BV(PIN_POWER)))	//loop while powerbutton is pressed
-									__asm("nop");
-						stop_timer2();	//stop timer 2
-						stop_timer1();  //stop timer 1
-					}
+							while(POWER_BUTTON_PRESSED)	//loop while powerbutton is pressed
+							{
+								if(uart_rxbuffer_notempty())		//check for byte arrived on uart_init
+									if(icc_handle_uart())		//handle it
+										eeprom_wait_write=1;
+							}
+
+						if(	PORTC & _BV(PC5)) //is on ?
+						{
+							enable_safty(); //laser off
+							stop_timer2();	//stop timer 2
+							stop_timer1();  //stop timer 1
+							PORTD&=~_BV(PIN_BLINKLED); //turn off PIN_BLINKLED
+						}
+							else
+						{
+							start_timer1();
+							start_timer2();
+							_delay_ms(5000);
+							disable_safty();
+						}
 			
-				}
-				if(!(main_delay%50)){	 //simple led blink every 500ms
-					PORTD^=_BV(PIN_BLINKLED); //toggle PIN_BLINKLED
+					}
 				}
 
-				if(main_delay >= 100){
+				if(main_delay >= 250){
 					main_delay=0;
+					if(	PORTC & _BV(PC5)) //is on ?
+						PORTD^=_BV(PIN_BLINKLED); //toggle PIN_BLINKLED
+					else
+							PORTD&=~_BV(PIN_BLINKLED); //off PIN_BLINKLED
+					if(eeprom_wait_write){
+						eeprom_wait_write++;
+						if(eeprom_wait_write > 40)	//10 seconds
+						{
+								save_eeprom_values();
+								eeprom_wait_write=0;
+						}
+					}
 				}
 
-			_delay_ms(10);
+			_delay_ms(1);
 	}
 
 
@@ -148,37 +182,41 @@ int main (void)
 
 void start_timer2(void)
 {
-	TCCR2=_BV(COM20)|_BV(WGM21)|_BV(CS22)|_BV(CS21)|_BV(CS20);		//toggle on OCR match, Clear on OCR match, prescaler 1:1024
-	OCR2=19;	//400 Hz
-	TCNT2=0;
+	/*	16.000.000/256 = 62500
+	*   OCR2 = 62500 / 800 = 78
+	*/
+	DDRD|=_BV(PD7); //make OC2 output
+	TCCR2=_BV(COM20)|_BV(WGM21)|_BV(CS22)|_BV(CS21);		//toggle on OCR match, Clear on OCR match, prescaler 1:256
+	TCNT2=0;	//clear timer2
+	OCR2= 78; //set to 800 hz toggle
 }
 
 void stop_timer2(void)
 {
+	DDRD&=~_BV(PD7);
 	TCCR2=0;	//reset
-	OCR2=0;	//reset
 	TCNT2=0; //reset
 }
 
 void start_timer1(void)
 {
-	TCCR1A=_BV(COM1A1)|_BV(WGM11)|_BV(WGM10);		//set pin on 0,clear at top, Fast PWM 10 bit
+	DDRD|=_BV(PD4); //make OC1B output
+	TCCR1A=_BV(COM1B1)|_BV(WGM11)|_BV(WGM10);		//set pin on 0,clear at top, Fast PWM 10 bit
 	TCCR1B=_BV(WGM12)|_BV(CS11)|_BV(CS10); //1:64 prescaler
-	OCR1A=0;		//of no PWM
 	TCNT1=0;		//reset
 }
 
 void stop_timer1(void)
 {
+	DDRD&=~_BV(PD4);
 	TCCR1A=0;		//reset
 	TCCR1B=0; //reset
-	OCR1A=0;		//reset
 	TCNT1=0;		//reset
 }
 
 void enable_safty(void)//disallow laser on
 {
-				PORTC|=_BV(PC5);
+				PORTC&=~_BV(PC5);
 }
 
 void disable_safty(void)		//allow laser on
@@ -186,4 +224,38 @@ void disable_safty(void)		//allow laser on
 				PORTC|=_BV(PC5);
 }
 
+void load_eeprom_values(void)
+{
+		eeprom_t predefvalues;
+		eeprom_read(&predefvalues);
+		mode=predefvalues.mode;
+		ve.h_sync_wait_perc=predefvalues.h_sync_wait_perc;
+		ve.v_sync_wait_perc=predefvalues.v_sync_wait_perc;
+		ve.h_pixel_perc=predefvalues.h_pixel_perc;
+		ve.v_pixel_perc=predefvalues.v_pixel_perc;
+		ve.h_pixel_end_val=predefvalues.h_pixel_end_val;
+		ve.v_pixel_end_val=predefvalues.v_pixel_end_val;
+		OCR1A=predefvalues.timer1_OCR1A;
+	//	OCR2=predefvalues.timer2_OCR2;
+}
 
+void save_eeprom_values(void)
+{
+		uint8_t i=0;
+		for(i=0;i<10;i++){
+			PORTD^=_BV(PIN_BLINKLED); //toggle PIN_BLINKLED
+			_delay_ms(50);
+		}
+		
+		eeprom_t predefvalues;
+		predefvalues.mode=mode;
+		predefvalues.h_sync_wait_perc=ve.h_sync_wait_perc;
+		predefvalues.v_sync_wait_perc=ve.v_sync_wait_perc;
+	  predefvalues.h_pixel_perc=ve.h_pixel_perc;
+	  predefvalues.v_pixel_perc=ve.v_pixel_perc;
+		predefvalues.h_pixel_end_val=ve.h_pixel_end_val;
+		predefvalues.v_pixel_end_val=ve.v_pixel_end_val;
+		predefvalues.timer1_OCR1A=OCR1A;
+	//	predefvalues.timer2_OCR2=OCR2;
+		eeprom_write(&predefvalues);
+}
--- microcontroller/src-atmel/laserproj/lasercontrol/uart.h
+++ microcontroller/src-atmel/laserproj/lasercontrol/uart.h
@@ -1,30 +1,50 @@
 #ifndef UART_H
 #define UART_H
 
-#include <inttypes.h>
-#include <avr/pgmspace.h>
+/**
+ * UART Library
+ *
+ * #define F_CPU 16000000         // Oszillator-Frequenz in Hz
+ * #define UART_INTERRUPT 1
+ * #define UART_BAUD_RATE 19200
+ * #define UART_RXBUFSIZE 16
+ * #define UART_TXBUFSIZE 16
+ * #define UART_LINE_BUFFER_SIZE 40
+ * #define UART_LEDS             // PC0 and PC1 toggle on tx and rx interrupt
+ *
+ */
+
+//read config
 #include "config.h"
 
+#include <inttypes.h>
+#include <avr/pgmspace.h>
+
+//prototypes
 void uart_init();
 
-void uart_putc(unsigned char c);
+void uart_putc(char c);
 void uart_putstr(char * str);
 void uart_putstr_P(PGM_P str);
-void uart_hexdump(unsigned char *buf, int len);
 
 char uart_getc();
-char uart_getc_nb(char *c);		// returns 1 on success
+char uart_getc_nb(char *c);		// non blocking, returns 1 on success
 
-//get one Cariage return terminated line
-//echo charakters back on Uart
-//returns buffer with zero terminated line on success, 0 pointer otherwise
-char * uart_getline_nb();
+#ifdef UART_HEXDUMP
+	//hexdump utility
+	void uart_hexdump(uint8_t *buf, int len);
+#endif
 
+#ifdef UART_GETLINE
+	//get one Cariage return terminated line
+	//echo charakters back on Uart
+	//returns buffer with zero terminated line on success, 0 pointer otherwise
+	char * uart_getline();
+#endif
 char uart_rxbuffer_notempty(void);	//returns 1 if queue is not empty, otherwise 0
 #if UART_RXBUFSIZE < 255
 uint8_t uart_getrxqueuelength(void); //returns queue length
 #else
 uint16_t uart_getrxqueuelength(void); //returns queue length
 #endif
-
 #endif
