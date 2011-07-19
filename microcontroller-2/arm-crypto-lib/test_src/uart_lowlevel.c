683c8ed73bd3a4b9a3ac4abd6f009fef6abd0ce6
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-07-19 23:08:48 +0000

    fixing some header comments

--- microcontroller-2/arm-crypto-lib/test_src/uart_lowlevel.c
+++ microcontroller-2/arm-crypto-lib/test_src/uart_lowlevel.c
@@ -1,6 +1,6 @@
 /* uart_lowlevel.c */
 /*
-    This file is part of the AVR-Crypto-Lib.
+    This file is part of the ARM-Crypto-Lib.
     Copyright (C) 2010 Daniel Otte (daniel.otte@rub.de)
 
     This program is free software: you can redistribute it and/or modify
