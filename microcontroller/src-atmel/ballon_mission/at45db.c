87a2b3dd9795debd2347fb0d5e14de019c3683fc
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-08-12 17:41:27 +0000

    flash is working

---
+++ microcontroller/src-atmel/ballon_mission/at45db.c
@@ -0,0 +1,87 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "spi.h"
+
+#define CLEAR_CS() PORTB &= ~(1<<PB4)
+#define SET_CS() PORTB |= (1<<PB4)
+
+
+
+uint8_t df_memory_status_read(){
+	uint8_t retval;
+	CLEAR_CS();
+	spi_data(0x57);
+	retval =  spi_data(0);
+	SET_CS();
+	return retval;
+}
+
+
+void df_busy_wait(){
+	while(!(df_memory_status_read() & 0x80));
+}
+
+
+void df_memory_page_read(uint8_t * buffer, uint16_t page){
+	df_busy_wait();
+	
+	CLEAR_CS();
+	spi_data(0x52);
+	spi_data(page >> 7);
+	spi_data((page<<1) & 0xff);
+	spi_data(0);
+	spi_data(0);
+	spi_data(0);
+	spi_data(0);
+	spi_data(0);
+	
+	int x;
+	for(x=0; x<256; x++){
+		buffer[x] = spi_data(0);
+	}
+	SET_CS();
+}
+
+
+void df_memory_page_programm(uint8_t * buffer, uint16_t page){
+	df_busy_wait();
+	
+    CLEAR_CS();
+	spi_data(0x82);
+	spi_data(page >> 7);
+	spi_data((page<<1) & 0xff);
+	spi_data(0);
+	int x;
+	for(x=0; x<256; x++){
+		spi_data(buffer[x]);
+	}
+	SET_CS();
+}
+
+
+void df_init(void){
+   cli();
+
+   spi_init();                              /* Initialize SPI */
+   /* Reset the device */
+   //RST();
+   CLEAR_CS();
+
+   /* Skip any pending bits */
+   unsigned char i;
+   for(i = 0; i < 20; i++)
+      spi_data(0x00);
+
+   #ifdef WDT_ENABLE
+      wdt_reset();                        /* Watchdog reset */
+   #endif
+
+   _delay_us(20);
+   SET_CS();
+   _delay_us(1);
+
+   sei();
+}
---
+++ microcontroller/src-atmel/ballon_mission/at45db.h
@@ -0,0 +1,5 @@
+
+void df_init(void);
+void df_memory_page_read(uint8_t * buffer, uint16_t page);
+uint8_t df_memory_status_read();
+void df_memory_page_programm(uint8_t * buffer, uint16_t page);
