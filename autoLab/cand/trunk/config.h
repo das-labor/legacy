cbbbe0f4a254d81136c138ca755e1d7e68a3fe82
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-09-21 20:13:21 +0000

    cand: boudrate 115200

--- autoLab/cand/trunk/config.h
+++ autoLab/cand/trunk/config.h
@@ -2,8 +2,8 @@
 #define CONFIG_H
 
 // uart.[ch] defines
-//#define UART_BAUD_RATE B115200
-#define UART_BAUD_RATE B57600
+#define UART_BAUD_RATE B115200
+//#define UART_BAUD_RATE B57600
 
 #define UART_HOST
 
