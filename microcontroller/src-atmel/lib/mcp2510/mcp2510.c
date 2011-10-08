8fc211c6e4e9fa974486d4bd90c5625d3a2d2b8f
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-08 06:57:14 +0000

    threw out lap message format to stay protocol independent

--- microcontroller/src-atmel/lib/mcp2510/mcp2510.c
+++ microcontroller/src-atmel/lib/mcp2510/mcp2510.c
@@ -71,17 +71,10 @@ void message_load(can_message_x * msg)
 	spi_send(WRITE);
 	spi_send(TXB0SIDH);
 
-#ifdef CAN_RAW
 	spi_send((uint8_t)((uint32_t)msg->msg.id>>21));
 	spi_send( ((uint8_t)((uint32_t)msg->msg.id>>13) & 0xE0) | (1<<EXIDE) | ((uint8_t)((uint32_t)msg->msg.id>>16)&0x03) );
 	spi_send((uint8_t)(msg->msg.id>>8));
 	spi_send((uint8_t)(msg->msg.id));
-#else
-	spi_send( ((unsigned char)(msg->msg.port_src << 2)) | (msg->msg.port_dst >> 4 ) );
-	spi_send( (unsigned char)((msg->msg.port_dst & 0x0C) << 3) | (1<<EXIDE) | (msg->msg.port_dst & 0x03) );
-	spi_send(msg->msg.addr_src);
-	spi_send(msg->msg.addr_dst);
-#endif
 
 	spi_send(msg->msg.dlc);
 	for(x=0;x<msg->msg.dlc;x++){
@@ -105,22 +98,12 @@ void message_fetch(can_message_x * msg)
 	spi_send(READ);
 	spi_send(RXB0SIDH);
 
-#ifdef CAN_RAW
 	tmp1 = spi_send(0);
 	tmp2 = spi_send(0);
 	tmp3 = spi_send(0);
 
 	msg->msg.id = ((uint32_t)tmp1<<21) | ((uint32_t)((uint8_t)tmp2&0xE0)<<13)
 			| ((uint32_t)((uint8_t)tmp2&0x03)<<16) | ((uint16_t)tmp3<<8) | spi_send(0);
-#else
-	tmp1 = spi_send(0);
-	msg->msg.port_src = tmp1 >> 2;
-	tmp2 = spi_send(0);
-	tmp3 = (unsigned char)((unsigned char)(tmp2 >> 3) & 0x0C);
-	msg->msg.port_dst = ((unsigned char)(tmp1 <<4 ) & 0x30) | tmp3 | (unsigned char)(tmp2 & 0x03);
-	msg->msg.addr_src = spi_send(0);
-	msg->msg.addr_dst = spi_send(0);
-#endif
 
 	msg->msg.dlc = spi_send(0) & 0x0F;
 	for (x = 0; x < msg->msg.dlc; x++)
--- microcontroller/src-atmel/lib/mcp2510/mcp2510.h
+++ microcontroller/src-atmel/lib/mcp2510/mcp2510.h
@@ -19,30 +19,13 @@
 
 typedef unsigned char can_addr;
 typedef unsigned char can_port;
-typedef uint16_t can_channel_t;
-typedef uint8_t can_subchannel_t;
 
-#ifdef CAN_RAW
-
-	typedef struct
-	{
-		uint32_t id;
-		uint8_t dlc;
-		uint8_t data[8];
-	} can_message;
-
-#else
-
-	typedef struct {
-		can_addr      addr_src;
-		can_addr      addr_dst;
-		can_port      port_src;
-		can_port      port_dst;
-		unsigned char dlc;
-		unsigned char data[8];
-	} can_message;
-
-#endif
+typedef struct
+{
+	uint32_t id;
+	uint8_t dlc;
+	uint8_t data[8];
+} can_message;
 
 typedef enum { normal, mode_sleep, loopback, listenonly, config } can_mode_t ;
 
