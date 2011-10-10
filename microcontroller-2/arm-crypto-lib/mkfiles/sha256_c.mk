aaa459674a0cbd61cf9265e79bc9325a9fdf1d3d
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-10 21:10:20 +0000

    moving sha256 to sha2/

--- microcontroller-2/arm-crypto-lib/mkfiles/sha256_c.mk
+++ microcontroller-2/arm-crypto-lib/mkfiles/sha256_c.mk
@@ -4,9 +4,9 @@ ALGO_NAME := SHA256_C
 # comment out the following line for removement of SHA256 from the build process
 HASHES += $(ALGO_NAME)
 
-$(ALGO_NAME)_DIR      := sha256/
+$(ALGO_NAME)_DIR      := sha2/
 $(ALGO_NAME)_INCDIR   := hfal/
-$(ALGO_NAME)_OBJ      := sha256.o
+$(ALGO_NAME)_OBJ      := sha256.o sha2_small_common.o
 $(ALGO_NAME)_TESTBIN := main-sha256-test.o $(CLI_STD) $(HFAL_STD) hfal_sha256.o 
 $(ALGO_NAME)_NESSIE_TEST      := "nessie"
 $(ALGO_NAME)_PERFORMANCE_TEST := "performance"
