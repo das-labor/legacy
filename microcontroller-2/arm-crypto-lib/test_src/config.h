c9330b2c8bb4b85475490647e4f28243904960d6
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-11 10:35:09 +0000

    forgotten files

---
+++ microcontroller-2/arm-crypto-lib/test_src/config.h
@@ -0,0 +1,27 @@
+/* config.h */
+/*
+    This file is part of the ARM-Crypto-Lib.
+    Copyright (C) 2008  Daniel Otte (daniel.otte@rub.de)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include "sysclock.h"
+
+#define SYS_FREQ SYS_FREQ_80MHZ000
+
+#endif
+
