c357da3f995cb3daedb729ffa1129cb9ef311188
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-16 21:56:58 +0000

    please use powercommande under automatization 2.0

--- autoLab/PowerCommander/tags/PowerCommander-0.1/avr_interface/progmem.h
+++
@@ -1,6 +0,0 @@
-#include <avr/pgmspace.h>
-
-#define PB(v) pgm_read_byte(&(v))
-#define PW(v) pgm_read_word(&(v))
-#define PD(v) pgm_read_dword(&(v))
-
--- autoLab/PowerCommander/tags/PowerCommander-0.1/avr_interface/xlap.h
+++
@@ -1,7 +0,0 @@
-#include "lap.h"
-
-AVRX_EXTERNTASK(laptask);
-
-extern uint8_t myaddr;
-
-void xlap_init();
