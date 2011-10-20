2a5d348d76de96501cd2133cd8049019ed6a5d8f
asklepios <asklepios@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-20 18:55:39 +0000

    narf - new tool new problems

--- microcontroller/src-atmel/automatization2.0/candpy/trunk/LocalParserPlugin.py
+++ microcontroller/src-atmel/automatization2.0/candpy/trunk/LocalParserPlugin.py
@@ -3,6 +3,9 @@
 import threading
 import Queue
 from time import sleep
+from LAPPacket import LAPPacket
+from CanPacket import CanPacket
+from utils import hexdump
 
 class LocalParserPlugin(threading.Thread):
     def __init__(self, plugins):
@@ -20,73 +23,19 @@ class LocalParserPlugin(threading.Thread):
                 print msg
                 sleep(1)
                 pass
-                
 
-class LAPPacket(object):
-    LAPPacketTypes={
-                    "LAP_RESET":0x00,
-                    "LAP_CAN_SETFILTER":0x10,
-                    "LAP_CAN_PKT":0x11,
-                    "LAP_CAN_SETMODE":0x12,
-                    "LAP_CAN_ERROR":0x13,
-                    "LAP_CAN_NOTIFY_RESET":0x14,
-                    "LAP_RS232_PING_GATEWAY":0x15} 
-
-    def __init__(self, pkttype=0, payload=bytearray(""), rawpkt=None):
-        self.dlc=len(payload)
-        self.pkttype=pkttype
-        self.payload=payload
-        if rawpkt:
-            self.from_array(rawpkt)
-            
-    def from_array(self, rawpkt):
-        if len(rawpkt) == 0:
-            return None
-
-        # Did we receive a full packet?
-        l = rawpkt[0]
-        if len(rawpkt)-2 < l:
-            return None
-
-        # what is it?
-        if buf[1] != 0x11:
-            return None
-
-        next = l+2
-            
-        pkt = buf[2:next]
-        self.buf = buf[next:]
-
-        pass
-    
-class CanPacket(object):
-    def __init__(self, sa=0, sp=0, da=0, dp=0, data=bytearray(""), rawpkt=None):
-        self.sa=sa
-        self.sp=sp
-        self.da=da
-        self.dp=dp
-        self.data=data
-        self.dlc=len(data)
-        if rawpkt:
-            self.from_array(rawpkt)
-        
-    def from_array(self, arr):
-        """ Parse packet from RAW (byte-)array """
-        self.da = arr[0]
-        self.sa = arr[1]
-        self.dp = ((arr[2] & 0x60) >> 1) + (arr[2] & 0x0f)
-        self.sp = ((arr[3] & 0x1f) << 1) + ((arr[2] & 0x80) >> 7)
-        self.data = arr[5:]
-        self.dlc = len(self.data)
-
-    def to_array(self):
-        """
-        XXX to be done
-        """
-        return None
-
-    def __str__(self):
-        return "%02x:%02x -> %02x:%02x: %s" % (self.sa, self.sp, self.da, self.dp, hexdump(self.data))
-
-
-    
+if __name__ == '__main__':
+    rL=bytearray(0)
+    rL.extend([0x0a , 0x11 , 0x00 , 0x05 , 0x06,  0x03, 0x05, 0x0b, 0x44, 0x00, 0x00, 0x00])
+    lp=LAPPacket(rawpkt=rL)
+    #print lp.getPayload()
+    cp=CanPacket(rawpkt=lp.getPayload())
+    print "Encoded LAPPacket should be"
+    print "0a 11 00 05 06 03 05  0b 44 00 00 00"
+    print "Decoded CANPaket should be "
+    print "05:06 -> 00:06    0b 44 00 00 00"
+    print cp
+    cp=CanPacket(sa=0x05, da=0x00, sp=0x06, dp=0x06)
+    cp.setData([0x0b, 0x44, 0x00, 0x00, 0x00])
+    print cp
+    print hexdump(cp.genRawPKT())
