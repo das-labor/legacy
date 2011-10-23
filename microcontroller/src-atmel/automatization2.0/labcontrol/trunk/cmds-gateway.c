2cb10b7175e9be4c716c3ab485d48396c1eb96b3
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-23 21:56:08 +0000

    implemented gateway ping request (send only) added missing files

---
+++ microcontroller/src-atmel/automatization2.0/labcontrol/trunk/cmds-gateway.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+
+#include "can-encap.h"
+#include "can.h"
+
+void cmd_gateway(int argc, char *argv[])
+{
+	rs232can_msg rmsg;
+	rmsg.cmd = RS232CAN_PING_GATEWAY;
+	rmsg.len = 0;
+
+	printf("Pinging gateway..\nReply will be visible in debug log of Cand.\n");
+	can_transmit_raw_gateway_message(&rmsg);
+}
