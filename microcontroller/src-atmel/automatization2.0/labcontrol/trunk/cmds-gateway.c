a34dd3e5aecfbff675cdc243ebc329cb73e04e33
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-24 00:22:41 +0000

    pimp my labcontrol

--- microcontroller/src-atmel/automatization2.0/labcontrol/trunk/cmds-gateway.c
+++ microcontroller/src-atmel/automatization2.0/labcontrol/trunk/cmds-gateway.c
@@ -9,10 +9,16 @@
 
 void cmd_gateway(int argc, char *argv[])
 {
-	rs232can_msg rmsg;
+	rs232can_msg rmsg, *rmsg_in;
+	unsigned int i = 0;
+
+	//create and send gateway ping request
+	printf("Pinging gateway.. ");
 	rmsg.cmd = RS232CAN_PING_GATEWAY;
 	rmsg.len = 0;
-
-	printf("Pinging gateway.. reply will be visible in debug log of Cand.\n");
 	can_transmit_raw_gateway_message(&rmsg);
+
+	//anticipate reply and timeout after 2s
+	while((rmsg_in = can_get_raw_gateway_message_nb() == NULL) && i++ < 2000) usleep(1000);
+	printf((i<2000)?"and got a reply!\n":"but timed out after 2s :-(");
 }
