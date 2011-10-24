40119ac09c209a2bbe9a91b7ed2aae7979b12b18
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-24 00:24:38 +0000

    captain we got signal! pong!

--- microcontroller/src-atmel/automatization2.0/labcontrol/trunk/cmds-gateway.c
+++ microcontroller/src-atmel/automatization2.0/labcontrol/trunk/cmds-gateway.c
@@ -20,5 +20,5 @@ void cmd_gateway(int argc, char *argv[])
 
 	//anticipate reply and timeout after 2s
 	while((rmsg_in = can_get_raw_gateway_message_nb() == NULL) && i++ < 2000) usleep(1000);
-	printf((i<2000)?"and got a reply!\n":"but timed out after 2s :-(");
+	printf((i<2000)?"and got a reply!\n":"but timed out after 2 seconds :-(\n");
 }
