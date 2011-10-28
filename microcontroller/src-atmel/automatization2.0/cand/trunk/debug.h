31b0bb1b6db6410b16a16747ccaa5fc16366cc93
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-28 16:50:23 +0000

    removing files

--- microcontroller/src-atmel/automatization2.0/cand/trunk/debug.h
+++ microcontroller/src-atmel/automatization2.0/cand/trunk/debug.h
@@ -1,14 +1,1 @@
-#ifndef DEBUG_H
-#define DEBUG_H
-
-extern int debug_level;    
-extern int debug_syslog;   /* use syslog? */
-
-void debug_init(char* debugfile);
-void debug_close();
-
-void debug( int level, char *format, ... );
-void debug_perror( int level, char *format, ... );
-void debug_assert( int test,  char *msg, ... );
-
-#endif
+#include "lib-host/debug.h"
