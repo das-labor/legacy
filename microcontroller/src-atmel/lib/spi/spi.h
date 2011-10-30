393c8a45f32588559ea6ecc8278eb4b852e01679
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-30 02:21:55 +0000

    not using config header anymore... does this break stuff?

--- microcontroller/src-atmel/lib/spi/spi.h
+++ microcontroller/src-atmel/lib/spi/spi.h
@@ -1,16 +1,14 @@
 #ifndef SPI_H
 #define SPI_H
 
-#include "config.h"
-
 /**
- * this enables the use of Hardware SPI on ATMEGA 
+ * this enables the use of Hardware SPI on ATMEGA
  *
  * #define SPI_HARDWARE 1
  * #define SPI_PORT PORTB
  * #define SPI_PIN PINB
  * #define SPI_DDR DDRB
- * 
+ *
  * #define SPI_PIN_MOSI PB5
  * #define SPI_PIN_MISO PB6
  * #define SPI_PIN_SCK PB7
