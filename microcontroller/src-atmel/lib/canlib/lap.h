6d929af5663fcee28805af7e390a2b586eca3852
hansi <hansi@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-10-30 03:07:13 +0000

    removed ambiguous (wannabe) multi-platform lib

--- microcontroller/src-atmel/lib/canlib/lap.h
+++ microcontroller/src-atmel/lib/canlib/lap.h
@@ -98,6 +98,22 @@ unsigned char *sdo_readbuf(sdo_message *first_message,
 unsigned char sdo_sendbuf(sdo_message *fst_msg, unsigned char *buf, unsigned char len);
 unsigned char sdo_sendbuf_nb(sdo_message *fst_msg, unsigned char *buf, unsigned char len);
 
+/////////////////////////////////////////////////////////////////////////////
+/* Usage
 
+while(1) {
+	lap_message msg = lap_rcvpacket();
+	switch( msg->fkt_id ) {
+	case FKT_BLA:
+		unsigned char length = data[0]
+
+		data
+
+		char *buf = lap_read(msg, length);
+		if ( !buf ) continue;
+
+		// interpret buffer 
+}
+*/
 
 #endif // _LAP_H
