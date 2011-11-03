9c0e5570c728a2b588841340fb9657d6fb10829d
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-11-03 00:06:08 +0000

    Version von den labortagen

---
+++ microcontroller/src-atmel/ballon_mission/i2c-slave.h
@@ -0,0 +1,5 @@
+
+void i2c_init();
+
+extern volatile uint8_t i2c_send_buf[];
+extern volatile uint8_t i2c_recv_buf[];
--- microcontroller/src-atmel/ballon_mission/logger.c
+++ microcontroller/src-atmel/ballon_mission/logger.c
@@ -8,6 +8,8 @@
 #include "ioport.h"
 #include "adc.h"
 
+#include "i2c-slave.h"
+
 #define COMPASS_CS_PORT   B
 #define COMPASS_CS_BIT    3
 
@@ -17,15 +19,19 @@
 #define COMPASS_DATA_PORT B
 #define COMPASS_DATA_BIT  1
 
+
+//40 byte
 typedef struct{
 	uint16_t magic;
 	uint16_t timestamp;
 	uint16_t temperature_1;
 	uint16_t temperature_2;
 	uint16_t temperature_3;
-	uint16_t pressure;
 	uint16_t voltage;
-	uint8_t compass_data[24];
+	uint16_t compass_data[4];
+
+	char gps_time[6];
+
 }log_dataset_t;
 
 uint8_t buffer_offset;
@@ -46,23 +52,8 @@ void log_dataset(log_dataset_t * data){
 }
 
 
-uint8_t get_compass_byte(){
-	uint8_t i;
-	uint8_t b = 0;
-	for(i=0;i<8;i++){
-		OUTPUT_ON(COMPASS_SCK);
-		b <<= 1;
-		_delay_us(1);
-		if(INPUT(COMPASS_DATA)){
-			b |= 1;
-		}
-		OUTPUT_OFF(COMPASS_SCK);
-		_delay_us(1);
-	}
-	return b;
-}
-
 void get_compass_data(uint8_t * data){
+	/*
 	OUTPUT_ON(COMPASS_CS);
 	_delay_us(10);
 	uint8_t i;
@@ -70,9 +61,7 @@ void get_compass_data(uint8_t * data){
 		data[i] = get_compass_byte();
 	}
 	OUTPUT_OFF(COMPASS_CS);
-
-	
-	
+	*/
 }
 
 extern uint16_t current_second;
@@ -83,11 +72,19 @@ void build_dataset(log_dataset_t * dataset){
 	dataset->temperature_1 = analogRead(0);
 	dataset->temperature_2 = analogRead(1);
 	dataset->temperature_3 = analogRead(2);
-	dataset->pressure      = analogRead(7);
 	dataset->voltage       = analogRead(6);
 	get_compass_data(dataset->compass_data);
+		
+	memcpy(dataset->gps_time, i2c_recv_buf, 6);
 }
 
+void dataset_to_i2cbuf(log_dataset_t * dataset){
+	uint16_t * ip = (uint16_t*)i2c_send_buf;
+	ip[0] = dataset->voltage;
+	ip[1] = dataset->temperature_1;
+	ip[2] = dataset->temperature_2;
+	ip[3] = dataset->temperature_3;
+}
 
 void print_temperature(uint16_t w){
 	double d = w * 5;
@@ -104,7 +101,7 @@ uint8_t undervoltage;
 
 void print_voltage(uint16_t w){
 	double d = w;
-	d *= (3.7 * 5.0 * 1.01 / 1024.0);
+	d *= (4.9 * 5.0 * 1.00 / 1024.0); // Spannungsteiler, referenz, kallibrierung
 	
 	if(d < 9.0){
 		undervoltage_counter ++;
@@ -132,28 +129,32 @@ void print_dataset(log_dataset_t * dataset){
 	print_temperature(dataset->temperature_2);
 	print_temperature(dataset->temperature_3);
 	print_voltage(dataset->voltage);	
-	printf("%d,", dataset->pressure);
+	//printf("%d,", dataset->pressure);
 	//printf("%d,", dataset->voltage);
+	
+	printf("C: ");
 	
 	uint8_t j;
 	uint8_t x=0;
-	for(j=0;j<6;j++){
-		uint8_t i;
-		for(i=0;i<3;i++){
-			printf("%02X", dataset->compass_data[x++]);
-		}
-		printf(",");
+	for(j=0;j<4;j++){
+		printf("%04X,", dataset->compass_data[x++]);	
+	}
 	
+	printf("G: ");
+	
+	for(j=0;j<6;j++){
+		printf("%c", dataset->gps_time[j]);
 	}
 	
 	printf("\r\n");
 }
 
+/*
 void print_dataset_raw(log_dataset_t * dataset){
 	printf("%d,", dataset->timestamp);
-	printf("%d,", dataset->temperature_1);
-	printf("%d,", dataset->temperature_2);
-	printf("%d,", dataset->temperature_3);
+	printf("%d,", dataset->temperature_1);//Außenfühler
+	printf("%d,", dataset->temperature_2);//Lipo
+	printf("%d,", dataset->temperature_3);//Flightcontrol
 	printf("%d,", dataset->pressure);
 	printf("%d,", dataset->voltage);
 	
@@ -170,22 +171,23 @@ void print_dataset_raw(log_dataset_t * dataset){
 	
 	printf("\r\n");
 }
+*/
 
 void logger_init(){
-	SET_DDR(COMPASS_SCK);
-	SET_DDR(COMPASS_CS);
 }
 
 void logger_tick(){
 	log_dataset_t dataset;
 	build_dataset(&dataset);
 	log_dataset(&dataset);
+	dataset_to_i2cbuf(&dataset);
 	print_dataset(&dataset);
 }
 
 void logger_show(){
 	log_dataset_t dataset;
 	build_dataset(&dataset);
+	dataset_to_i2cbuf(&dataset);
 	print_dataset(&dataset);
 }
 
@@ -196,7 +198,7 @@ void dump_log(){
 		uint8_t buffer[256];
 		df_memory_page_read(buffer, i);
 		for(j=0; j< 240; j+=40){
-			print_dataset_raw((log_dataset_t *)&buffer[j]);
+			print_dataset((log_dataset_t *)&buffer[j]);
 		}
 	}
 }
