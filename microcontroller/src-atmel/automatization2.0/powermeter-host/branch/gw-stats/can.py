0c029b8e6bdadf3543c5a5122abf3f0b182cdedc
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-11-13 16:54:27 +0000

    [powermeter] Add support for can-power

--- microcontroller/src-atmel/automatization2.0/powermeter-host/branch/gw-stats/can.py
+++ microcontroller/src-atmel/automatization2.0/powermeter-host/branch/gw-stats/can.py
@@ -10,6 +10,7 @@
 """
 
 import socket
+import struct
 
 #=============================================================================
 # Utility functions
@@ -23,10 +24,65 @@ def hexdump(data):
 
 #=============================================================================
 # Packet types / parsers
+class ParseException(Exception):
+    pass
+
+def unpack_gwpacket(arr):
+    """ Represents a packet as its transmitted over can-tcp or can-uart """
+    if len(arr) < 1:
+        raise ParseException()
+    cmd = arr[0]
+    payload = arr[1:]
+
+    if cmd == 0x11:
+        return LAPPacket(arr=payload)
+    elif cmd == 0x19:
+        pkt = ControlPacket("PACKET_STATS", arr=payload)
+
+        # Unpack content
+        if len(payload) != 16:
+            raise ParseException("Error unpacking PACKET_STATS")
+        pkt.rx_count, pkt.tx_count, pkt.rx_size, pkt.tx_size = struct.unpack(">IIII", str(payload))
+        return pkt
+    elif cmd == 0x1b:
+        pkt = ControlPacket("POWER_DRAW", arr=payload)
+
+        # Unpack content
+        if len(payload) != 8:
+            raise ParseException("Error unpacking POWER_DRAW")
+        u_adc, i_adc, ref, gnd = struct.unpack("<HHHH", str(payload))
+
+        # From ADC values to volts
+        U_REF = 5.0
+        ADC_RES = 1<<10
+        DIVIDER_R1 = 2700
+        DIVIDER_R2 = 1000
+        SHUNT_R = 0.01
+        SHUNT_AMP = 10
+        u = (u_adc * U_REF / ADC_RES) * (DIVIDER_R1 + DIVIDER_R2) / DIVIDER_R2
+        i = (i_adc * U_REF / ADC_RES) / (SHUNT_AMP * SHUNT_R)
+            
+        pkt.u = u
+        pkt.i = i
+        return pkt
+
+    print "Unknown packet from gateway: cmd=0x%02x, payload=%s" % (cmd, hexdump(payload))
+    return None
+
+
+class ControlPacket:
+    """ A control packet from or to a can gateway """
+    def __init__(self, id, arr=None):
+        self.id = id
+        self.payload = arr
+
+    def __str__(self):
+        return "ControlPacket: %s, payload: %s" % (self.id, hexdump(self.payload))
+        
 
 class LAPPacket:
     """ A single LAP Packet """
-    def __init__(self, pkt=None):
+    def __init__(self, arr=None):
         self.sa = 0
         self.sp = 0
         self.da = 0
@@ -34,8 +90,8 @@ class LAPPacket:
         self.data = bytearray("")
         self.dlc = len(self.data)
 
-        if pkt is not None:
-            self.from_array(pkt)
+        if arr is not None:
+            self.from_array(arr)
 
     def from_array(self, arr):
         """ Parse packet from RAW (byte-)array """
@@ -52,7 +108,7 @@ class LAPPacket:
         return None
 
     def __str__(self):
-        return "%02x:%02x -> %02x:%02x: %s" % (self.sa, self.sp, self.da, self.dp, hexdump(self.data))
+        return "LAPPacket: %02x:%02x -> %02x:%02x: %s" % (self.sa, self.sp, self.da, self.dp, hexdump(self.data))
 
 #=============================================================================
 # Connection to a CAN gateway (cand)
@@ -84,15 +140,14 @@ class CANSocket:
         if len(buf)-2 < l:
             return None
 
-        # Is it a CAN packet?
-        if buf[1] != 0x11:
-            return None
-
+        # Dequeue packet
         next = l+2
-
-        pkt = buf[2:next]
+        payload = buf[1:next]
         self.buf = buf[next:]
-        return pkt
+
+        # Parse und return payload
+        return unpack_gwpacket(payload)
+
 
     def get_pkt_nb(self):
         """ Receive a CAN packet; non-blocking.
@@ -117,9 +172,7 @@ class CANSocket:
         except:
             pass
 
-        pkt = self.dequeue_pkt()
-        return pkt
-
+        return self.dequeue_pkt()
 
 
     def get_pkt(self):
