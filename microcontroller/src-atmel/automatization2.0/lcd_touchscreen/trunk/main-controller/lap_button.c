b2ed291687d66086f8a02a75a8943c58021eac40
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-12-01 13:25:08 +0000

    lcd_touchscreen: main: style, fix lap_button struct and include

--- microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/main-controller/lap_button.c
+++ microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/main-controller/lap_button.c
@@ -1,45 +1,26 @@
-
 #include <stdlib.h>
+
 #include "gui_lib/gui.h"
 #include "netvar/netvar.h"
+#include "lap_button.h"
 
-typedef struct {
-	void(*draw)(gui_element_t * self, uint8_t redraw);
-	void(*set_on_screen)(gui_element_t *self, uint8_t state);
-	void(*touch_handler)(gui_element_t *self, touch_event_t t);
-	void(*update_position)(gui_element_t *self, int16_t x_diff, int16_t y_diff); //moves an element and all sub elements.
-	void(*delete)(gui_element_t *self);
-	rectangle_t box;//gui element sets height and width, container sets x and y.
-
-	char * text;
-	icon_t * icon;
-	uint8_t state;
-
-	void(*click_handler)(gui_element_t *);
-	// -------------------- inherited from gui_button_t until here -------------
-
-	netvar_desc * nv;
-}lap_button_t;
 
 void lap_button_click_handler(gui_element_t * self) {
-	//print("click");
 	lap_button_t * s = (lap_button_t*)self;
 	uint8_t new_state = s->state ^ 1;
 	netvar_write(s->nv, &new_state);
 }
 
 void lap_button_nv_handler(netvar_desc * nd, void * ref) {
-	//print("nv_handler");
 	lap_button_t * s = (lap_button_t*) ref;
 	s->state = nd->data[0];
 	//printf("nv_handler nd=%X, ref=%X\r\n", nd, ref);
-	s->draw(s, 0);
+	s->draw((gui_element_t *)s, 0);
 }
 
 void lap_button_delete (gui_element_t *self) {
-	free (self);
+	free(self);
 }
-
 
 
 //constructor
@@ -50,15 +31,17 @@ lap_button_t * new_lap_button(char * text, uint16_t idx ) {
 	b->touch_handler = gui_button_touch_handler;
 	b->update_position = gui_button_update_position;
 	b->delete = lap_button_delete;
-	b->box = (rectangle_t){0,0,32,30};
+	b->box = (rectangle_t){0, 0, 32, 30};
 	b->icon = 0;
 	b->state = 0;
+	b->frame_size = 0x80;
 	
 	b->text = text;
 	b->click_handler = lap_button_click_handler;
 
-	b->nv = netvar_register(idx,0,1);
+	b->nv = netvar_register(idx, 0, 1);
 	netvar_add_handler(b->nv, lap_button_nv_handler, b);
 
 	return b;
 }
+
--- microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/main-controller/lap_button.h
+++ microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/main-controller/lap_button.h
@@ -1,3 +1,5 @@
+#ifndef _LAP_BUTTON_H
+#define _LAP_BUTTON_H
 
 typedef struct {
 	void(*draw)(gui_element_t * self, uint8_t redraw);
@@ -7,6 +9,7 @@ typedef struct {
 	void(*delete)(gui_element_t *self);
 	rectangle_t box;//gui element sets height and width, container sets x and y.
 
+	uint8_t frame_size;//how many pixels frame? 0x80+num draws bounding box
 	char * text;
 	icon_t * icon;
 	uint8_t state;
@@ -14,10 +17,12 @@ typedef struct {
 	void(*click_handler)(gui_element_t *);
 	// -------------------- inherited from gui_button_t until here -------------
 
-	netvar_desc * nv;	
-}lap_button_t;
+	netvar_desc * nv;
+} lap_button_t;
 
 void lap_button_click_handler(gui_element_t * self);
 void lap_button_delete (gui_element_t *self);
 //constructor
-lap_button_t * new_lap_button(char * text, uint16_t idx );
+lap_button_t * new_lap_button(char * text, uint16_t idx);
+
+#endif //_LAP_BUTTON_H
