669b11d7b12a5095e592dfd548248ae0b9891306
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-12-02 22:38:29 +0000

    changed receive and transmit functions, can now receive strings & chars, warning buffer overrun at more than 128 bytes received at once

--- microcontroller/src-stm32f4xx/serialUSB/main.c
+++ microcontroller/src-stm32f4xx/serialUSB/main.c
@@ -67,7 +67,8 @@ int main(void)
   /* Initialize LEDs and User_Button on STM32F4-Discovery --------------------*/
 	
  __IO uint32_t i = 0;  
-	
+	uint8_t buf[255];
+	uint8_t len;
  USBD_Init(&USB_OTG_dev,     
             USB_OTG_FS_CORE_ID, 
             &USR_desc, 
@@ -81,9 +82,24 @@ int main(void)
  STM_EVAL_LEDInit(LED6);
 	
  VCP_send_str("USB serial DEMO\ndas-labor.org\n");
-	   
+ buf[0] = 't';
+ buf[1] = 'e';
+ buf[2] = 's';
+ buf[3] = 't';
+ buf[4] = 0;
+	
+ VCP_send_str(&buf[0]);
+
+ buf[0] = 0;
+ buf[1] = 0;
+ buf[2] = 0;
+ buf[3] = 0;
+ buf[4] = 0;
 	while(1)
 	{
+		len = VCP_get_string(&buf[0]);
+		if(len)
+		VCP_send_str(&buf[0]);
 		if (i == 0x1000000)
     	{
 			VCP_put_char('b');
