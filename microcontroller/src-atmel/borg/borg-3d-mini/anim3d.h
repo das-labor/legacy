de9d3e354aa3bd2036855e2e05253917e4c0b32b
chris <chris@b1d182e4-1ff8-0310-901f-bddb46175740>
2011-12-16 20:35:38 +0000

    dead and buried

--- microcontroller/src-atmel/borg/borg-3d-mini/anim3d.h
+++
@@ -1,7 +0,0 @@
-void test1();
-void test3();
-void cool(unsigned char waitTime, unsigned char count);
-void schachBlinking();
-void schachBlinking2();
-void snake();
-unsigned char random();
--- microcontroller/src-atmel/borg/borg-3d-mini/uart.h
+++
@@ -1,4 +0,0 @@
-void uart_init();
-void uart_putc(uint8_t c);
-uint8_t uart_getc();
-void uart_putstr(char *p);
