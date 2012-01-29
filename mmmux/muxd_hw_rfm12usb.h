d62c311d294fa73442313d6c47a4234b62554e81
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2012-01-29 21:56:38 +0000

    working ;)

--- mmmux/muxd_hw_rfm12usb.h
+++ mmmux/muxd_hw_rfm12usb.h
@@ -1,8 +1,17 @@
+#include "muxd_config.h"
+
 #if MMMUX_USE_RFM12USB == 1
 #include "muxd.h"
 #include "muxd_hw.h"
 #include <usb.h>
 #pragma once
+
+#define RFMUSB_RQ_ECHO             0x00
+#define RFMUSB_RQ_RFM12_PUT        0x01
+#define RFMUSB_RQ_RFM12_GET        0x02
+#define RFMUSB_RQ_GETNOTIFICATION  0x03
+#define DEFAULT_USB_TIMEOUT        1000
+
 
 /* additional userdata struct for the hw description */
 typedef struct
