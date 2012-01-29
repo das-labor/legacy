a8725672a4c08798364ca5ce9667d1732016e2df
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2012-01-29 21:22:13 +0000

    + rfm12usb support

--- mmmux/muxd_hw.h
+++ mmmux/muxd_hw.h
@@ -9,8 +9,8 @@ typedef void (*mmmux_hw_init_f)  (void*);
 typedef void (*mmmux_hw_close_f) (void*);
 
 /* transmit & receive functions */
-typedef int  (*mmmux_hw_tx_f)    (void*, size_t, void*);
-typedef int  (*mmmux_hw_rx_f)    (void*, size_t, void*);
+typedef ssize_t  (*mmmux_hw_tx_f)    (void*, size_t, void*);
+typedef ssize_t  (*mmmux_hw_rx_f)    (void*, size_t, void*);
 
 /* hardware description struct
  */
@@ -27,3 +27,4 @@ typedef struct
 
 int mmmux_hw_init (void);
 int mmmux_hw_add (mmmux_hw_t *in_h);
+void mmmux_hw_remove (mmmux_hw_t *in_h);
