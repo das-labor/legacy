52033c0ad1fd8d845ddd2c22a8a8464fc7829627
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2012-02-01 22:44:14 +0000

    forgot the header... ;)

---
+++ mmmux/muxd_hw_dummy.h
@@ -0,0 +1,28 @@
+/*   MMMUX - a device access multiplexing library
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *   Copyright (C) 2012 Soeren Heisrath (forename at surename dot org)
+ */
+
+#include "muxd_config.h"
+
+#include "muxd.h"
+#include "muxd_hw.h"
+
+#if MMMUX_USE_DUMMYHW == 1
+#pragma once
+int hwdummy_find (mmmux_sctx_t *in_c);
+#endif
