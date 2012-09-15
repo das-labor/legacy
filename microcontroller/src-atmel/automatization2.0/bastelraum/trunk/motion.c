fda36dd486b0a356f51a2de854b36b245a074618
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2012-09-15 14:27:57 +0000

    motion stuff complete & working

--- microcontroller/src-atmel/automatization2.0/bastelraum/trunk/motion.c
+++ microcontroller/src-atmel/automatization2.0/bastelraum/trunk/motion.c
@@ -1,5 +1,118 @@
 #include "motion.h"
+const uint8_t motion_admux[NUM_DETECTORS] =
+{
+	(0x00 | ADMUX_PREDEF),
+	(0x01 | ADMUX_PREDEF)
+};
 
+/* motion tick counter for the current 60s
+ * note: ticks from *all* motion detectors are commulated in this value.
+ */
+volatile static uint8_t motion_ticks = 0;
+
+/* counter */
+volatile static uint8_t hscount = 0;
+
+/* current motion detector channel */
+volatile static uint8_t mux_chan = 0;
+
+
+void timer0_init ()
+{
+	TCCR0B = (_BV(CS00) | _BV(CS02)); /* clk/1024 */
+	TIMSK0 = _BV(TOIE0); /* overflow int. */
+}
+
+void motion_init ()
+{
+	timer0_init();
+}
+
+void adc_disable()
+{
+	ADCSRA &= ~(_BV(ADEN));
+}
+
+
+ISR(ADC_vect)
+{
+	static uint16_t l8[NUM_DETECTORS]; /* sum of last 8 values */
+	static uint16_t c8[NUM_DETECTORS]; /* sum of current 8 values */
+	static uint8_t s = 0;              /* state counter */
+	
+	if (s == 0)
+	{
+		/* discard first measurement */
+		s++;
+		return;
+	}
+
+	if (s < 8)
+	{
+		c8[mux_chan] += ADC;
+		s++;
+		return;
+	}
+
+	/* ... after 8 samples ... */
+	adc_disable();
+
+	if (c8[mux_chan] + MOTION_TRESHOLD < l8[mux_chan] || c8[mux_chan] - MOTION_TRESHOLD > l8[mux_chan])
+	{
+	PORTB ^= _BV(PB0); /* DEBUG FOO XXX */
+		if (motion_ticks < 0xff)
+			motion_ticks++;
+	}
+
+	l8[mux_chan] = c8[mux_chan];
+	c8[mux_chan] = 0;
+	s = 0;
+}
+
+/* initialize the ADC, enable the interrupt, select mux channel
+ */
+void adc_init ()
+{
+	mux_chan++;
+	if (mux_chan >= NUM_DETECTORS)
+		mux_chan = 0;
+	ADMUX = motion_admux[mux_chan];
+	
+	ADCSRB = 0x00; /* free running mode */
+
+	/* enable, auto trigger (free running), int enable, start conv, pscaler 128 */
+	ADCSRA = (_BV(ADEN) | _BV(ADATE) | _BV(ADIE) | _BV(ADSC) | 
+		_BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0));
+}
+
+ISR(TIMER0_OVF_vect)
+{
+	static uint8_t c = 0;
+	
+	c++;
+	if (c < 38) /* ~ 0.5s (76.3 overflows per second) */
+		return;
+	
+	c = 0;
+
+	/* enable the adc, start 8 sample runs */
+	adc_init();
+	hscount++;
+}
+
+
+/* handler to be executed from main() */
+void motion_tick ()
+{
+	if (hscount < 240)
+		return;
+	
+	motion_ticks = 0;
+	hscount = 0;
+}
+
+
+#if 0
 static volatile uint8_t motion_tresh = 128;
 
 /* motion condition */
@@ -83,3 +196,4 @@ ISR (ADC_vect)
 	
 	motion_cond = 1;
 }
+#endif
