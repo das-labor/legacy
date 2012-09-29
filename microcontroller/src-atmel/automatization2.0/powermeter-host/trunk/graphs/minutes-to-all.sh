9ef9c1e3c12b50437a038e7c304f79e7df8ad4d8
joerg <joerg@b1d182e4-1ff8-0310-901f-bddb46175740>
2012-09-29 13:28:44 +0000

    [powermeterd] Various graph updates

--- microcontroller/src-atmel/automatization2.0/powermeter-host/trunk/graphs/minutes-to-all.sh
+++ microcontroller/src-atmel/automatization2.0/powermeter-host/trunk/graphs/minutes-to-all.sh
@@ -23,7 +23,7 @@ sed 's%start=-30m%start=-8d%'   < $GRAPH-minutes.html > $GRAPH-days.html
 sed 's%start=-30m%start=-60d%'  < $GRAPH-minutes.html > $GRAPH-weeks.html
 sed 's%start=-30m%start=-400d%' < $GRAPH-minutes.html > $GRAPH-months.html
 
-sed -i 's%meta http-equiv="refresh" content="30"%meta http-equiv="refresh" content="120"%'  $GRAPH-hours.html
-sed -i 's%meta http-equiv="refresh" content="30"%meta http-equiv="refresh" content="3600"%' $GRAPH-days.html
-sed -i 's%meta http-equiv="refresh" content="30"%meta http-equiv="refresh" content="3600"%' $GRAPH-weeks.html
-sed -i 's%meta http-equiv="refresh" content="30"%meta http-equiv="refresh" content="3600"%' $GRAPH-months.html
+sed -i 's%meta http-equiv="refresh" content="60"%meta http-equiv="refresh" content="600"%'  $GRAPH-hours.html
+sed -i 's%meta http-equiv="refresh" content="60"%meta http-equiv="refresh" content="3600"%' $GRAPH-days.html
+sed -i 's%meta http-equiv="refresh" content="60"%meta http-equiv="refresh" content="3600"%' $GRAPH-weeks.html
+sed -i 's%meta http-equiv="refresh" content="60"%meta http-equiv="refresh" content="3600"%' $GRAPH-months.html
