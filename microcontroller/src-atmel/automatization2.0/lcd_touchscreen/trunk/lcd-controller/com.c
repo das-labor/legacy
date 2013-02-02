0b0afa5610912a7c3692101807ce84063b3829c4
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-02 22:48:11 +0000

    implementing back channel to master controller

--- microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/lcd-controller/com.c
+++ microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/lcd-controller/com.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
 #include "gui_lib/graphics.h"
 #include "draw_pixmap.h"
@@ -18,6 +19,8 @@
 #define WAIT_ATN_LOW()     {while(PIN_HANDSHAKE & _BV(BIT_ATN));}
 #define WAIT_ATN_HIGH()    {while((PIN_HANDSHAKE & _BV(BIT_ATN))==0);}
 
+#define ATN_IS_LOW()   ((PIN_HANDSHAKE & _BV(BIT_ATN))==0)
+#define ACK_IS_LOW()   ((PIN_HANDSHAKE & _BV(BIT_ACK))==0)
 
 void init_com() {
 	DDR_HANDSHAKE  &= ~(_BV(BIT_ATN)|_BV(BIT_ACK));
@@ -25,15 +28,36 @@ void init_com() {
 }
 
 
+#define TX_DATA_SIZE 2
+
+uint8_t tx_data_idx;
+uint8_t tx_data[TX_DATA_SIZE];
+
 static uint8_t read_byte() {
 	uint8_t b;
 
-	WAIT_ATN_LOW();
-	b = PIN_DATA;
-	ACK_PULL();
-	WAIT_ATN_HIGH();
-	ACK_RELEASE();
-	return b;
+	while(1){
+		if(ATN_IS_LOW()){ //read byte from master
+			b = PIN_DATA;
+			ACK_PULL();
+			WAIT_ATN_HIGH();
+			ACK_RELEASE();
+			WAIT_ACK_HIGH();
+			return b;
+		}
+		if(ACK_IS_LOW()){//master requests byte
+			DDR_DATA = 0xff;
+			PORT_DATA = tx_data[tx_data_idx];
+			tx_data_idx  = (tx_data_idx + 1) % TX_DATA_SIZE;
+			ATN_PULL();
+			WAIT_ACK_HIGH();
+			DDR_DATA = 0;
+			PORT_DATA = 0xff;
+			ATN_RELEASE();
+			WAIT_ATN_HIGH();
+			_delay_us(1);
+		}
+	}
 }
 
 static uint16_t read_uint16_t() {
@@ -59,6 +83,11 @@ static void read_string(char * dest) {
 	} while (b);
 }
 
+static void return_int16(int16_t i){
+	tx_data[0] = i & 0xff;
+	tx_data[1] = ((uint16_t)i) >> 8;
+	tx_data_idx = 0;
+}
 
 
 void handle_com() {
@@ -143,6 +172,18 @@ void handle_com() {
 					g_draw_pixel(x,y);	
 					break;
 				}
+			case DC_GET_STRING_WIDTH:
+				{
+					read_string(buf);
+					return_int16(g_get_string_width(buf));
+					break;				
+				}
+			case DC_GET_LAST_TEXT_HEIGHT:
+				{
+					return_int16(g_get_last_text_height());
+					break;
+				}
+			
 		}
 	}
 }
