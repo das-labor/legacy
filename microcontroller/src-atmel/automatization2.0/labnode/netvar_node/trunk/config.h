7628e7cb79833c0c09ea7e30cc3ef3c9d3fd13e8
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-07 13:39:49 +0000

    drivers and libs are basically working

--- microcontroller/src-atmel/automatization2.0/labnode/netvar_node/trunk/config.h
+++ microcontroller/src-atmel/automatization2.0/labnode/netvar_node/trunk/config.h
@@ -1,14 +1,12 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-
 // SPI
 #define MC_MOSI    PB5
 #define MC_SCK     PB7
 
-// Pin PB2 muss Ausgang sein wenn nicht als SS verwendet
-#define MCP_CS        PB4
-#define MCP_CMD_PORT  PORTB
+#define MCP_CS_BIT    PB4
+#define MCP_CS_PORT   PORTB
 
 #define SPI_REG_PIN_MCP_INT  PINB
 #define SPI_PIN_MCP_INT      PB2
@@ -16,17 +14,6 @@
 
 
 #define F_MCP F_CPU
-
-
-#define LED_GRUEN   _BV(PA2)
-#define LED_ROT _BV(PA3)
-#define LED_BLAU  _BV(PA4)
-
-
-#define TWI_ADDRESS 42
-
-#define PORT_BASTEL 0x01
-
 
 #endif // ifndef CONFIG_H
 
