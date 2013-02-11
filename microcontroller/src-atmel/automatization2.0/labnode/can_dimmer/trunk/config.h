29b8ca51419d32635584a229546b9c83d7aad995
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-11 17:08:57 +0000

    can_dimmer: cleanup

--- microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/config.h
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/config.h
@@ -7,8 +7,8 @@
 #define MC_SCK     PB7
 
 // Pin PB2 muss Ausgang sein wenn nicht als SS verwendet
-#define MCP_CS_BIT		PB4
-#define MCP_CS_PORT		PORTB
+#define MCP_CS_BIT  PB4
+#define MCP_CS_PORT PORTB
 
 #define SPI_REG_PIN_MCP_INT  PINB
 #define SPI_PIN_MCP_INT      PB2
@@ -16,16 +16,6 @@
 
 
 #define F_MCP F_CPU
-
-
-#define LED_GRUEN   _BV(PA2)
-#define LED_ROT _BV(PA3)
-#define LED_BLAU  _BV(PA4)
-
-
-#define TWI_ADDRESS 42
-
-#define PORT_BASTEL 0x01
 
 #define NUM_CHANNELS 4
 
