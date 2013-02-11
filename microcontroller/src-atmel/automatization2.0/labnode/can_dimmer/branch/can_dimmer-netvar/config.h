18a0fdf4736bf90eb9c2a562e41bd48991265f75
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-11 17:13:12 +0000

    can_dimmer: add netvar branch

---
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/branch/can_dimmer-netvar/config.h
@@ -0,0 +1,23 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+
+// SPI
+#define MC_MOSI    PB5
+#define MC_SCK     PB7
+
+// Pin PB2 muss Ausgang sein wenn nicht als SS verwendet
+#define MCP_CS_BIT  PB4
+#define MCP_CS_PORT PORTB
+
+#define SPI_REG_PIN_MCP_INT  PINB
+#define SPI_PIN_MCP_INT      PB2
+//#define CAN_INTERRUPT
+
+
+#define F_MCP F_CPU
+
+#define NUM_CHANNELS 4
+
+#endif // ifndef CONFIG_H
+
