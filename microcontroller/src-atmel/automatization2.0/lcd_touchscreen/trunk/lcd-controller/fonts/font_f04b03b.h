da823fed223964672383f7fe4ee87cf8adfe6f4a
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-12 01:58:23 +0000

    use fonts from gui_lib/fonts

--- microcontroller/src-atmel/automatization2.0/lcd_touchscreen/trunk/lcd-controller/fonts/font_f04b03b.h
+++
@@ -1,5 +0,0 @@
-// This file is generated through fontConverter see 
-// svn co https://roulette.das-labor.org/svn/microcontroller/tools/fontConverter
-#include "font.h"
-
-extern font font_f04b03b;
