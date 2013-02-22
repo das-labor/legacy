442b441368fbd323bfa0af45cf595ab2fc194380
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-22 20:22:27 +0000

    netvar_io: whitespace

--- microcontroller/src-atmel/automatization2.0/lib/netvar/netvar_io.c
+++ microcontroller/src-atmel/automatization2.0/lib/netvar/netvar_io.c
@@ -1,48 +1,48 @@
-
 #include <stdint.h>
 #include <stdlib.h>
+
 #include "netvar.h"
 #include "netvar_io.h"
 
 static void nv_output_8_handler(netvar_desc *nd, void *ref) {
-	 netvar_output_8_t * nl = (netvar_output_8_t *) ref;
-	 nl->set_output_func(nl->set_output_ref, nd->data[0]);
+	netvar_output_8_t *nl = (netvar_output_8_t *) ref;
+	nl->set_output_func(nl->set_output_ref, nd->data[0]);
 }
 
-netvar_output_8_t * new_netvar_output_8(uint16_t           idx, 
-                                        uint8_t            sidx, 
-								        set_output_func_t  set_output_func,
-								        void *             set_output_ref     )
+netvar_output_8_t *new_netvar_output_8(uint16_t           idx, 
+                                       uint8_t            sidx, 
+                                       set_output_func_t  set_output_func, 
+                                       void              *set_output_ref)
 {
-	netvar_output_8_t * nl = malloc(sizeof(netvar_output_8_t));
+	netvar_output_8_t *nl = malloc(sizeof(netvar_output_8_t));
 	nl->nd = netvar_register(idx, sidx, 1);
 	nl->set_output_func = set_output_func;
 	nl->set_output_ref  = set_output_ref;
-	
+
 	netvar_add_handler(nl->nd, nv_output_8_handler, nl);
-	
+
 	return nl;
 }
 
 
 static void nv_output_1_handler(netvar_desc *nd, void *ref) {
-	 netvar_output_8_t * nl = (netvar_output_8_t *) ref;
-	 uint8_t val = nd->data[0] ? 0xff : 0x00; //convert to 0 / 0xff
-	 nl->set_output_func(nl->set_output_ref, val);
+	netvar_output_8_t *nl = (netvar_output_8_t *) ref;
+	uint8_t val = nd->data[0] ? 0xff : 0x00; //convert to 0 / 0xff
+	nl->set_output_func(nl->set_output_ref, val);
 }
 
-netvar_output_1_t * new_netvar_output_1(uint16_t           idx, 
-                                        uint8_t            sidx, 
-								        set_output_func_t  set_output_func,
-								        void *             set_output_ref     )
+netvar_output_1_t *new_netvar_output_1(uint16_t           idx, 
+                                       uint8_t            sidx, 
+                                       set_output_func_t  set_output_func, 
+                                       void              *set_output_ref)
 {
-	netvar_output_1_t * nl = malloc(sizeof(netvar_output_1_t));
+	netvar_output_1_t *nl = malloc(sizeof(netvar_output_1_t));
 	nl->nd = netvar_register(idx, sidx, 1);
 	nl->set_output_func = set_output_func;
 	nl->set_output_ref  = set_output_ref;
-	
+
 	netvar_add_handler(nl->nd, nv_output_1_handler, nl);
-	
+
 	return nl;
 }
 
--- microcontroller/src-atmel/automatization2.0/lib/netvar/netvar_io.h
+++ microcontroller/src-atmel/automatization2.0/lib/netvar/netvar_io.h
@@ -1,25 +1,26 @@
 
 
-typedef void (*set_output_func_t)(void * ref, uint8_t val)  ; 
+typedef void (*set_output_func_t) (void *ref, uint8_t val);
 
-typedef struct{
-	set_output_func_t  set_output_func; //handler that is called to switch/dim the phsical lamp
-	void *             set_output_ref;  //user supplied reference which is given to the handler (e.g. the number representing the io-port, or pointer to structure which describes physical lamp)
-	netvar_desc *      nd;            //the netvar which is regsitered during netvar_lamp_new registered
+typedef struct {
+	set_output_func_t set_output_func; //handler that is called to switch/dim the phsical lamp
+	void             *set_output_ref;  //user supplied reference which is given to the handler (e.g. the number representing the io-port, or pointer to structure which describes physical lamp)
+	netvar_desc      *nd;              //the netvar which is regsitered during netvar_lamp_new registered
 } netvar_output_8_t;
 
-netvar_output_8_t * new_netvar_output_8(uint16_t           idx, 
-                                        uint8_t            sidx, 
-								        set_output_func_t  set_output_func,
-								        void *             set_output_ref     );
+netvar_output_8_t *new_netvar_output_8(uint16_t           idx, 
+                                       uint8_t            sidx, 
+                                       set_output_func_t  set_output_func, 
+                                       void              *set_output_ref);
 
-typedef struct{
-	set_output_func_t  set_output_func; //handler that is called to switch/dim the phsical lamp
-	void *             set_output_ref;  //user supplied reference which is given to the handler (e.g. the number representing the io-port, or pointer to structure which describes physical lamp)
-	netvar_desc *      nd;            //the netvar which is regsitered during netvar_lamp_new registered
+typedef struct {
+	set_output_func_t set_output_func; //handler that is called to switch/dim the phsical lamp
+	void             *set_output_ref;  //user supplied reference which is given to the handler (e.g. the number representing the io-port, or pointer to structure which describes physical lamp)
+	netvar_desc      *nd;              //the netvar which is regsitered during netvar_lamp_new registered
 } netvar_output_1_t;
 
-netvar_output_1_t * new_netvar_output_1(uint16_t           idx, 
-                                        uint8_t            sidx, 
-								        set_output_func_t  set_output_func,
-								        void *             set_output_ref     );
+netvar_output_1_t *new_netvar_output_1(uint16_t           idx, 
+                                       uint8_t            sidx, 
+                                       set_output_func_t  set_output_func, 
+                                       void              *set_output_ref);
+
