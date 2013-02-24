ca0d4c0c726ca8d2e0594f73937081b4622fba1a
tixiv <tixiv@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-24 23:19:09 +0000

    cleaning up, adding message converters instead of having the conversion code everywhere

--- microcontroller/src-atmel/automatization2.0/labcontrol/trunk/can-posix.c
+++ microcontroller/src-atmel/automatization2.0/labcontrol/trunk/can-posix.c
@@ -29,16 +29,16 @@ void can_init(cann_conn_t *aconn) {
 
 void can_setmode(can_mode_t mode)
 {
-	rs232can_msg rmsg;
+	rs232can_msg rs232_msg;
 
-	rmsg.len     = 1;
-	rmsg.cmd     = RS232CAN_SETMODE;
-	rmsg.data[0] = (char)mode;
+	rs232_msg.len     = 1;
+	rs232_msg.cmd     = RS232CAN_SETMODE;
+	rs232_msg.data[0] = (char)mode;
 
 	if (conn)
-		cann_transmit(conn, &rmsg);
+		cann_transmit(conn, &rs232_msg);
 	else
-		canu_transmit(&rmsg);
+		canu_transmit(&rs232_msg);
 }
 
 void can_setfilter() {
@@ -95,7 +95,7 @@ void can_transmit_raw_gateway_message(rs232can_msg *rmsg)
 //transmit a can message
 void can_transmit_raw(can_message_raw *cmsg)
 {
-	rs232can_msg rmsg;
+	rs232can_msg rs232_msg;
 
 	if (cmsg == NULL)
 	{
@@ -103,8 +103,8 @@ void can_transmit_raw(can_message_raw *cmsg)
 		exit(EXIT_FAILURE);
 	}
 
-	rs232can_can2rs(&rmsg, cmsg);
-	can_transmit_raw_gateway_message(&rmsg);
+	rs232can_msg_from_can_message_raw(&rs232_msg, cmsg);
+	can_transmit_raw_gateway_message(&rs232_msg);
 }
 
 void can_transmit(can_message *cmsg)
@@ -114,22 +114,12 @@ void can_transmit(can_message *cmsg)
 		printf("ERROR: cmsg is NULL!\n");
 		exit(EXIT_FAILURE);
 	}
-	can_message_raw *newmsg = malloc(sizeof(can_message_raw));
-	memset(newmsg, 0, sizeof(can_message_raw));
-	if (newmsg == NULL)
-	{
-		printf("ERROR: Could not allocate newmsg buffer!\n");
-		exit(EXIT_FAILURE);
-	}
-
-	newmsg->id = ((cmsg->port_src & 0x3f) << 23) | ((cmsg->port_dst & 0x30) << 17) |
-		((cmsg->port_dst & 0x0f) << 16) | (cmsg->addr_src << 8) | (cmsg->addr_dst);
-	newmsg->dlc = cmsg->dlc;
-	memcpy(newmsg->data, cmsg->data, cmsg->dlc);
+	can_message_raw raw_msg;	
+	can_message_raw_from_can_message(&raw_msg, cmsg);
+	
 	free(cmsg);
-	can_transmit_raw(newmsg);
-
-	free(newmsg);
+	
+	can_transmit_raw(&raw_msg);
 }
 
 /****************************************************************************
@@ -149,41 +139,34 @@ rs232can_msg * can_get_raw_gateway_message_nb()
 //returns next can message
 can_message * can_get_nb()
 {
-	rs232can_msg *rmsg;
-	can_message_raw  *cmsg;
-	can_message *retmsg;
+	rs232can_msg *rs232_msg;
+	can_message_raw  raw_msg;
+	can_message *can_msg;
 
 	if (conn)
-		rmsg = cann_get(conn);
+		rs232_msg = cann_get(conn);
 	else
-		rmsg = canu_get();
+		rs232_msg = canu_get();
 
-	if (!rmsg || rmsg->cmd != RS232CAN_PKT)
+	if (!rs232_msg || rs232_msg->cmd != RS232CAN_PKT)
 	{
-		free(rmsg);
+		free(rs232_msg);
 		return 0;
 	}
 
-	retmsg = malloc(sizeof(can_message));
-	if (retmsg == NULL)
+	can_msg = malloc(sizeof(can_message));
+	if (can_msg == NULL)
 	{
 		printf("ERROR: Could not allocate retmsg buffer!\n");
 		exit(EXIT_FAILURE);
 	}
 
-	cmsg = can_buffer_get_raw();
-	rs232can_rs2can(cmsg, rmsg);
-	free(rmsg);
-	retmsg->addr_src = (uint8_t) (cmsg->id >> 8);
-	retmsg->addr_dst = (uint8_t) (cmsg->id);
-	retmsg->port_src = (uint8_t) ((cmsg->id >> 23) & 0x3f);
-	retmsg->port_dst = (uint8_t) (((cmsg->id >> 16) & 0x0f) | ((cmsg->id >> 17) & 0x30));
-	retmsg->dlc = cmsg->dlc;
-	memcpy(retmsg->data, cmsg->data, cmsg->dlc);
+	can_message_raw_from_rs232can_msg(&raw_msg, rs232_msg);
+	can_message_from_can_message_raw(can_msg, &raw_msg);
+	
+	free(rs232_msg);
 
-
-	free (cmsg);
-	return retmsg;
+	return can_msg;
 }
 
 can_message *can_get()
@@ -193,18 +176,18 @@ can_message *can_get()
 
 //returns next can message, or 0 if no Message was received.
 can_message_raw * can_get_raw_nb() {
-	rs232can_msg *rmsg;
+	rs232can_msg *rs232_msg;
 	can_message_raw  *cmsg;
 
 	if (conn)
-		rmsg = cann_get_nb(conn);
+		rs232_msg = cann_get_nb(conn);
 	else
-		rmsg = canu_get_nb();
+		rs232_msg = canu_get_nb();
 
-	if (!rmsg) return 0;
+	if (!rs232_msg) return 0;
 
 	cmsg = can_buffer_get_raw();
-	rs232can_rs2can(cmsg, rmsg);
+	can_message_raw_from_rs232can_msg(cmsg, rs232_msg);
 
 	return cmsg;
 
@@ -242,16 +225,16 @@ void can_transmit_v2(can_message_v2 *in_msg)
 
 can_message_v2 * can_get_v2_nb()
 {
-	rs232can_msg *rmsg;
+	rs232can_msg *rs232_msg;
 	can_message_raw  *cmsg;
 	can_message_v2 *retmsg;
 
 	if (conn)
-		rmsg = cann_get_nb(conn);
+		rs232_msg = cann_get_nb(conn);
 	else
-		rmsg = canu_get_nb();
+		rs232_msg = canu_get_nb();
 
-	if (!rmsg) return 0;
+	if (!rs232_msg) return 0;
 
 	retmsg = malloc(sizeof(can_message_v2));
 	if (retmsg == NULL)
@@ -261,7 +244,7 @@ can_message_v2 * can_get_v2_nb()
 	}
 
 	cmsg = can_buffer_get_raw();
-	rs232can_rs2can(cmsg, rmsg);
+	can_message_raw_from_rs232can_msg(cmsg, rs232_msg);
 
 	retmsg->addr_src = (uint8_t)  (cmsg->id >> 8);
 	retmsg->addr_dst = (uint8_t)  (cmsg->id);
@@ -273,8 +256,6 @@ can_message_v2 * can_get_v2_nb()
 	free (cmsg);
 	return retmsg;
 }
-
-
 
 void can_free(can_message *msg){
 	free(msg);
