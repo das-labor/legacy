9570f05bc782f18dc0113d1a52e36aabbd9a73bc
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-02-25 23:20:23 +0000

    powercommander: fix pong

--- microcontroller/src-atmel/automatization2.0/PowerCommander/trunk/avr_interface/can_handler.h
+++ microcontroller/src-atmel/automatization2.0/PowerCommander/trunk/avr_interface/can_handler.h
@@ -1,14 +1,12 @@
 #ifndef _CAN_HANDLER_H
 #define _CAN_HANDLER_H
 
-extern void can_handler();
-
-extern void can_send(uint8_t port, uint8_t *p);
-
-extern void can_send2(uint8_t p);
-
-extern void read_can_addr();
-
 extern uint8_t myaddr;
 
+extern void can_handler(void);
+extern void can_send(uint8_t port, uint8_t *p);
+extern void can_send2(uint8_t p);
+extern void read_can_addr(void);
+
 #endif // _CAN_HANDLER
+
