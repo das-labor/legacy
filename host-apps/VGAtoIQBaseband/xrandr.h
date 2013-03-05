f7eb3d42d47f02b91cfa7b293745310b90902f39
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-03-05 18:08:45 +0000

    working :-)

--- host-apps/VGAtoIQBaseband/xrandr.h
+++ host-apps/VGAtoIQBaseband/xrandr.h
@@ -4,7 +4,7 @@ extern "C" {
 
 int add_custom_mode (char *name, int pixelclk, int hsync, int vsync);
 int enable_output (char *output_name, char* mode_name, int x, int y);
-int find_VGA_output(char *name,int *x, int *y);
+int find_VGA_output(char *name,int *x, int *y, char* mode_active);
 int rm_mode (char* output_name, char* mode_name);
 void disable_output(char* name);
 int init_xrandr(void);
