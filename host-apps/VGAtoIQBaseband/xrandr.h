8f4e222b2f1ece25ef0f978ab2f09aa42c710e56
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-03-07 13:09:59 +0000

    make arguments const, fix compilation warnings

--- host-apps/VGAtoIQBaseband/xrandr.h
+++ host-apps/VGAtoIQBaseband/xrandr.h
@@ -2,11 +2,11 @@
 extern "C" { 
 #endif
 
-int add_custom_mode (char *name, int pixelclk, int hsync, int vsync);
-int enable_output (char *output_name, char* mode_name, int x, int y);
+int add_custom_mode (const char *name, int pixelclk, int hsync, int vsync);
+int enable_output (const char *output_name, const char* mode_name, int x, int y);
 int find_VGA_output(char *name,int *x, int *y, char* mode_active);
-int rm_mode (char* output_name, char* mode_name);
-void disable_output(char* name);
+int rm_mode (const char* output_name, const char* mode_name);
+void disable_output(const char* name);
 int init_xrandr(void);
 
 #ifdef __cplusplus
