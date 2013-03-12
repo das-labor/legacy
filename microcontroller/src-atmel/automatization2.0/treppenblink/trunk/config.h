03bafa5a86ffde2654591d19352f22d0d784aee2
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-03-12 13:23:57 +0000

    trpblk: wip

--- microcontroller/src-atmel/automatization2.0/treppenblink/trunk/config.h
+++ microcontroller/src-atmel/automatization2.0/treppenblink/trunk/config.h
@@ -5,12 +5,18 @@
 #define MCP_CS_BIT    PB2
 #define MCP_CS_PORT   PORTB
 
-
 #define SPI_REG_PIN_MCP_INT  PIND
 #define SPI_PIN_MCP_INT      PD2
 
-//#define CAN_INTERRUPT
+#define CAN_INTERRUPT
 
+#define   ENABLE_CAN_INT()    GIMSK |= _BV(INT0)
+#define   DISABLE_CAN_INT()   GIMSK &= ~_BV(INT0)
+#define   SETUP_CAN_INT()     MCUCR |= _BV(ISC01)
+#define   MCP_INT_VEC         INT0_vect
+
+#define CAN_TX_BUFFER_SIZE 4
+#define CAN_RX_BUFFER_SIZE 4
 
 #define F_MCP F_CPU
 
@@ -30,20 +36,19 @@
 #define PORT_LED PORTD
 #define DDR_LED DDRD
 
-// Eingänge
+// Taster Eingänge
 #define NUM_INPUTS 2
 #define I_PIN_0 PINB
-#define I_BV_0  _BV(PB0)
+#define I_BV_0  _BV(PB0) // 
 #define I_PIN_1 PINB
-#define I_BV_1  _BV(PB1)
+#define I_BV_1  _BV(PB1) // 
 
 /* eeprom settings */
 #define EEP_MY_ADDR 0x00
 #define EEP_MOTION_TRESH 0x01
 
-
-#define TWI_SLAVE (15)
-
+// TWI Adresse des temperatursensors
+#define I2C_ADRESSE_DS1631 0x9e
 
 #endif // ifndef CONFIG_H
 
