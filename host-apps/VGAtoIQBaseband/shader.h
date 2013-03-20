c5eb76d89e80fdc19224ab0784e8bcceef8d773e
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-03-20 18:35:04 +0000

    use custom cutof frequency for sinc filter, calculate sinc filter on startup

--- host-apps/VGAtoIQBaseband/shader.h
+++ host-apps/VGAtoIQBaseband/shader.h
@@ -1,7 +1,7 @@
 #ifndef __SHADER_H
 #define __SHADER_H
 
-void setShaders(float width, float height, bool beVerbose) ;
+void setShaders(float width, float height, bool beVerbose, float max_freq, float sample_rate);
 void printProgramInfoLog(GLuint obj, bool beVerbose);
 void printShaderInfoLog(GLuint obj, bool beVerbose);
 int textFileWrite(char *fn, char *s) ;
