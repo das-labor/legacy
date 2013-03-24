ade813dd885590aa91a8fcb4275a85990b3526f6
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-03-24 18:30:32 +0000

    pc_netvar:

--- microcontroller/src-atmel/automatization2.0/PowerCommander/branch/Powercommander_netvar/avr_master/config.h
+++ microcontroller/src-atmel/automatization2.0/PowerCommander/branch/Powercommander_netvar/avr_master/config.h
@@ -10,13 +10,16 @@
 
 #define SPI_REG_PIN_MCP_INT  PIND
 #define SPI_PIN_MCP_INT      PD2
+
 #define CAN_INTERRUPT
+
 #define CAN_RX_BUFFER_SIZE 4
 #define CAN_TX_BUFFER_SIZE 4
-#define MCP_INT_VEC INT0_vect
-#define ENABLE_CAN_INT()    GIMSK |= _BV(INT0)
-#define DISABLE_CAN_INT()   GIMSK &= ~_BV(INT0)
+
+#define ENABLE_CAN_INT()    GICR |= _BV(INT0)
+#define DISABLE_CAN_INT()   GICR &= ~_BV(INT0)
 #define SETUP_CAN_INT()     MCUCR |= _BV(ISC01)
+#define MCP_INT_VEC INT0_vect
 
 #define F_MCP F_CPU
 
@@ -52,4 +55,5 @@
 
 
 
-#endif // ifndef CONFIG_H
+#endif // ifndef _CONFIG_H
+
