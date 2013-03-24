2290647b5ec1fc800bcc862390b072ca86df910a
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-03-24 18:37:42 +0000

    util_lib: astyle

--- microcontroller/src-atmel/automatization2.0/lib/util_lib/list.c
+++ microcontroller/src-atmel/automatization2.0/lib/util_lib/list.c
@@ -1,82 +1,102 @@
 //
-// This file is part of the gui-library for microcontrollers 
+// This file is part of the gui-library for microcontrollers
 // by Peter Fuhrmann.
 //
 
 #include <stdint.h>
 #include <stdlib.h>
+
 #include "list.h"
 
 #define LIST_ALLOCATION_BLOCK_SIZE 4
 
-void init_list(list_t * l){
-	l->childs = malloc(sizeof(void*) * LIST_ALLOCATION_BLOCK_SIZE );
+void init_list(list_t *l)
+{
+	l->childs = malloc(sizeof(void *) * LIST_ALLOCATION_BLOCK_SIZE);
 	l->allocated = LIST_ALLOCATION_BLOCK_SIZE;
 	l->num_childs = 0;
 }
 
 //destructors
-void uninit_list(list_t * l){
+void uninit_list(list_t *l)
+{
 	free(l->childs);
 }
 
-void delete_list (list_t * l) {
+void delete_list(list_t *l)
+{
 	free(l->childs);
 	free(l);
 }
 
-list_t * new_list() {
-	list_t * l;
+list_t *new_list()
+{
+	list_t *l;
 	l = malloc(sizeof(list_t));
 	init_list(l);
 	return l;
 }
 
-
-void list_append(list_t * l, void * child) {
-	if(l->num_childs >= l->allocated){
+void list_append(list_t *l, void *child)
+{
+	if (l->num_childs >= l->allocated)
+	{
 		l->allocated += LIST_ALLOCATION_BLOCK_SIZE;
-		l->childs = realloc(l->childs, sizeof(void*) * l->allocated);		
+		l->childs = realloc(l->childs, sizeof(void *) * l->allocated);
 	}
+
 	l->childs[l->num_childs] = child;
 	l->num_childs++;
 }
 
 //remove element from list by index
-void list_remove_at (list_t * l, uint16_t i) {
+void list_remove_at(list_t *l, uint16_t i)
+{
 	if (i >= l->num_childs) return;//index to large
-	
+
 	l->num_childs -= 1;
-	for ( ; i < l->num_childs; i++) {
+
+	for (; i < l->num_childs; i++)
+	{
 		//move up all elements
-		l->childs[i] = l->childs[i+1];
+		l->childs[i] = l->childs[i + 1];
 	}
 }
 
 //remove element from list by value
-void list_remove (list_t * l, void * child) {
+void list_remove(list_t *l, void *child)
+{
 	uint16_t i;
-	void * e = 0;
-	for (i = 0; i < l->num_childs; i++) {
+	void *e = 0;
+
+	for (i = 0; i < l->num_childs; i++)
+	{
 		e = l->childs[i];
-		if (e == child) {
+
+		if (e == child)
+		{
 			//this is the child we want to remove
 			list_remove_at(l, i);
 		}
 	}
 }
 
-void list_foreach_begin(list_iterator_t * it, list_t * l){
+void list_foreach_begin(list_iterator_t *it, list_t *l)
+{
 	l = l; //don't warn about unused list parameter
 	*it = 0;
 }
 
-void * list_foreach(list_iterator_t * it, list_t * l){
-	void * e = 0;
-	if(*it < l->num_childs){
+void *list_foreach(list_iterator_t *it, list_t *l)
+{
+	void *e = 0;
+
+	if (*it < l->num_childs)
+	{
 		e = l->childs[*it];
 		(*it)++;
 	}
+
 	return e;
 }
 
