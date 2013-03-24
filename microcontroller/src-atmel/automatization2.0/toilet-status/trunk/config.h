bea93af441cc1f95185ed8a13406de7f6a536d46
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-03-24 19:16:45 +0000

    toilet_status: config update

--- microcontroller/src-atmel/automatization2.0/toilet-status/trunk/config.h
+++ microcontroller/src-atmel/automatization2.0/toilet-status/trunk/config.h
@@ -3,12 +3,8 @@
 
 
 // SPI
-#define MC_MOSI    PB3
-#define MC_CLK     PB5
-
-// Pin PB2 muss Ausgang sein wenn nicht als SS verwendet
-#define MCP_CS        PB2
-#define MCP_CMD_PORT  PORTB
+#define MCP_CS_BIT     PB2
+#define MCP_CS_PORT  PORTB
 
 #define SPI_REG_PIN_MCP_INT  PIND
 #define SPI_PIN_MCP_INT      PD2
