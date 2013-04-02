6578883c35e75905cef1a3b3891d6a618614e728
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-04-02 13:50:00 +0000

    sync

--- microcontroller/src-atmel/rfm12/rfm12usb/branch/newctrl_ook/firmware/main.c
+++ microcontroller/src-atmel/rfm12/rfm12usb/branch/newctrl_ook/firmware/main.c
@@ -23,19 +23,88 @@
 #include "oddebug.h"
 #include "usbdrv.h"
 
+void send_testcode ()
+{
+	uint8_t byte;
+	uint8_t tx_bytes[3] = {0x41, 0x45, 0x51};
+	uint8_t msk = 0x80;
+
+
+	rfm12_ask_tx_mode(1);
+	rfm12_tx_off();
+	//#define DLY 78
+	#define DLY 360
+
+	for (byte = 0; byte < 3;)
+	{
+		rfm12_tx_on();
+		_delay_us (240);
+		_delay_us(DLY);
+
+		if (tx_bytes[byte] & msk)
+		{
+			/* send 1 (..1..) */
+			rfm12_tx_on();
+			DEBUG_LED(1);
+		} else
+		{
+			/* send 0 (..0..) */
+			rfm12_tx_off();
+			DEBUG_LED(0);
+		}
+		_delay_us (240);
+		_delay_us (DLY);
+
+		DEBUG_LED(0);
+		rfm12_tx_off();
+		_delay_us (140);
+		_delay_us (DLY);
+
+		msk >>= 1;
+		if (msk == 0x00)
+		{
+			msk = 0x80;
+			byte++;
+		}
+	}
+	
+	rfm12_tx_on();
+	_delay_us (240);
+	_delay_us (DLY);
+	rfm12_tx_off();
+
+	for (byte = 12; byte != 0; byte--)
+	{
+		_delay_us(240);
+		_delay_us(DLY);
+	}
+	
+	rfm12_ask_tx_mode(0);
+}
+
 int main ()
 {
 	HW_INIT();
 	DEBUG_LED(1);
 	usbstuff_init ();
-	FSK_INIT();
 	rfm12_init();
+	FSK_INIT();
 	DEBUG_LED(0);
-
+	wdt_disable();
+#if 0
+	_delay_ms(200);
+	rfm12_ask_tx_mode(1);
+	_delay_ms(50);
+	rfm12_ask_tx_mode(0);
+	_delay_ms(50);
+#endif
 	sei();
 
-	while (23)
+	while (1)
 	{
+		send_testcode();
+		send_testcode();
+		send_testcode();
 		rfm12_tick();
 		usbPoll();
 		handle_rx();
