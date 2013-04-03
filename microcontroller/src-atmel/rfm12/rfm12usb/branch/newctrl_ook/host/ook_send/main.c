3587a29d3f2878710756e582979e7ee5e0c4e2af
soeren <soeren@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-04-03 17:11:14 +0000

    commandline tool to control your ...

--- microcontroller/src-atmel/rfm12/rfm12usb/branch/newctrl_ook/host/ook_send/main.c
+++ microcontroller/src-atmel/rfm12/rfm12usb/branch/newctrl_ook/host/ook_send/main.c
@@ -7,7 +7,16 @@
 
 #include "../../common/requests.h"
 
-static void ook_next (uint8_t **in_p)
+void print_help (int ac, char* av[])
+{
+	printf ("usage:\n  %s <housecode> <switch> <on|off>\n", av[0]);
+	printf ("  housecode: housecode in binary - i.e. 10011\n");
+	printf ("  switch:    switch code (A to E)\n");
+	printf ("  on/off:    accepted values: on, off, 0, 1\n\n");
+	printf ("alternate usage: %s disco\n", av[0]);
+}
+
+void ook_next (uint8_t **in_p)
 {
 	int i;
 	uint8_t *dp = *in_p;
@@ -86,13 +95,15 @@ int main (int ac, char* av[])
 	static uint8_t txdata[64];
 	static uint8_t *ook_payload;
 	static uint8_t **pp;
-	static rfmusb_ook_t *ook_header = txdata;
+	static rfmusb_ook_t *ook_header = (rfmusb_ook_t *) txdata;
+	enum mymode_e {
+		single,
+		disco
+	} mymode = single;
+	int i;
 	
 	ook_payload = txdata + sizeof(rfmusb_ook_t);
 	pp = &ook_payload;
-	
-	printf ("this program will run forever and switch on all powerplugs in the neighborhood ;-)\n");
-	printf ("press CTRL-C to abort.\n");
 
 	mc = mmmuxd_init (MDBG_NONE, NULL);
 	if (mc == NULL)
@@ -101,31 +112,148 @@ int main (int ac, char* av[])
 		return - __LINE__;
 	}
 
-	ook_header->len   = 24;         /* 24 bits */
-	ook_header->type  = OOK_2722;   /* 2722 power plug */
+	ook_header->len   = 24;                  /* 24 bits */
+	ook_header->type  = OOK_2722;            /* 2722 power plug */
 	ook_header->delay = htole16(500);        /* 500us between flanks */
-	ook_header->count = 16;         /* repeat 16 times */
+	ook_header->count = 16;                  /* repeat 16 times */
 	
-	ook_payload[0] = 0x00;
-	ook_payload[1] = 0x05;
-	ook_payload[2] = 0x51;
 
 	mmmux_ctrl (mc, mode_ook, NULL);
-	sleep (1);
-	
-	while (1)
+	usleep (100000);
+
+	if (ac < 2)
 	{
-		printf ("sending code: %02X, %02X, %02X \t",
-			ook_payload[0],
-			ook_payload[1],
-			ook_payload[2]
-			);
-		mmmux_send (mc, txdata, sizeof(rfmusb_ook_t) + 3);
-		ook_next (pp);
-		usleep (1750000);
+		print_help (ac, av);
+		return - __LINE__;
+	}
+
+	if (!strncasecmp (av[1], "disco", 5))
+	{
+		printf ("disco mode: %s\n", av[1]);
+		mymode = disco;
+	} else
+	{
+		mymode = single;
 	}
 	
-	sleep (1);
+	switch (mymode)
+	{
+		case disco:
+			ook_payload[0] = 0x00;
+			ook_payload[1] = 0x05;
+			ook_payload[2] = 0x51;
+
+			printf ("this program will run forever and switch on all powerplugs in the neighborhood ;-)\n");
+			printf ("press CTRL-C to abort.\n");
+			while (1)
+			{
+				printf ("sending code: %02X, %02X, %02X \t",
+					ook_payload[0],
+					ook_payload[1],
+					ook_payload[2]
+					);
+				mmmux_send (mc, txdata, sizeof(rfmusb_ook_t) + 3);
+				ook_next (pp);
+				usleep (1750000);
+			}
+		break;
+		case single:
+		{
+			uint8_t *txcode = txdata + sizeof (rfmusb_ook_t);
+			int hc_tmp;
+			int shift = 4;
+
+			memset (txcode + sizeof(rfmusb_ook_t), 0x00, 3);
+
+			if (ac != 4)
+			{
+				print_help (ac, av);
+				return - __LINE__;
+			}
+			
+			for (i=0; i<4; i+=2)
+			{
+				hc_tmp = (av[1][i] << 8) + av[1][i+1];
+				switch (hc_tmp)
+				{
+					case ('0'<<8) + '0':
+						txcode[0] += (0x05 << shift);
+					break;
+					case ('1'<<8) + '0':
+						txcode[0] += (0x01 << shift);
+					break;
+					case ('0'<<8) + '1':
+						txcode[0] += (0x04 << shift);
+					break;
+					case ('1'<<8) + '1':
+						/* + 0 */
+					break;
+					default:
+						printf ("can't parse txcode[0]\n");
+						return -1;
+				}
+				shift -= 4;
+			}
+
+			/* bit 5 */
+			if (av[1][4] == '0')
+			{
+				txcode[1] = 0x40;
+			} else if (av[1][4] != '1')
+			{
+				printf ("can't parse housecode (bit 5)\n");
+				return -1;
+			}
+			
+			switch (av[2][0])
+			{
+				case 'a':
+				case 'A':
+					txcode[1] += 0x05;
+					txcode[2]  = 0x50;
+				break;
+				case 'b':
+				case 'B':
+					txcode[1] += 0x11;
+					txcode[2]  = 0x50;
+				break;
+
+				case 'c':
+				case 'C':
+					txcode[1] += 0x14;
+					txcode[2]  = 0x50;
+				break;
+				case 'd':
+				case 'D':
+					txcode[1] += 0x15;
+					txcode[2]  = 0x10;
+				break;
+				case 'e':
+				case 'E':
+					txcode[1] += 0x15;
+					txcode[2]  = 0x40;
+				break;
+			}
+
+
+			if (!strncasecmp (av[3], "on", 2) || av[3][0] == '1')
+			{
+				txcode[2] += 0x01;
+			} else
+			{
+				txcode[2] += 0x04;
+			}
+			printf ("sending code: %02X, %02X, %02X\n",
+				ook_payload[0],
+				ook_payload[1],
+				ook_payload[2]);
+
+			mmmux_send (mc, txdata, sizeof(rfmusb_ook_t) + 3);
+		}
+		break;
+	}
+	
+	usleep (100000);
 	mmmux_ctrl (mc, mode_normal, NULL);
 	
 	return 0;
