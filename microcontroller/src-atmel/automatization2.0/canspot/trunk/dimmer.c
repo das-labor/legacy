6b42b96e577ae77600f1535bcc00fb9f40cb07ca
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-04-09 23:18:22 +0000

    canlamp: wip

--- microcontroller/src-atmel/automatization2.0/canspot/trunk/dimmer.c
+++ microcontroller/src-atmel/automatization2.0/canspot/trunk/dimmer.c
@@ -1,12 +1,12 @@
-
 #include <avr/io.h>
 #include <avr/signal.h>
 #include <avr/interrupt.h>
+
 #include "dimmer.h"
 
 
-unsigned char Count;
-unsigned char Bright[NUM_LAMPS];
+static volatile uint8_t Count;
+uint8_t Bright[NUM_LAMPS];
 
 dimmer_ramp Ramp;
 
@@ -17,24 +17,24 @@ SIGNAL(SIG_INTERRUPT1) {
 
 
 SIGNAL(SIG_OVERFLOW0) {
-	unsigned char x, rol;
+	uint8_t x, rol;
 	TCNT0 = 256 - 20;
-	
+
 	if (Count == 0) {
 		PORTB |= 0x0F;
 	}
-	
+
 	for (x = 0, rol = 0x08; x < NUM_LAMPS; x++) {
 		if (Bright[x] <= Count) {
 			PORTB &= ~rol;
 		}
 		rol>>=1;
 	}
-	
+
 	if (++Count == 64) {
 		Count = 0;
 	};
-	
+
 	for (x = 0; x < NUM_LAMPS; x++) {
 		if (--Ramp.delay[x] == 0) {
 			Ramp.delay[x] = Ramp.delay_rl[x];
@@ -49,15 +49,15 @@ SIGNAL(SIG_OVERFLOW0) {
 
 
 void dimmer_init() {
-	unsigned char x;
+	uint8_t x;
 	for (x = 0; x < NUM_LAMPS; x++) {
 		Bright[x] = 64;
 		Ramp.end_bright[x] = 64;
 		Ramp.delay_rl[x] = 1;
 		Ramp.delay[x] = 1;
 	}
-	
-	MCUCR |=  _BV(ISC11) | _BV(ISC10);
+
+	MCUCR |= _BV(ISC11) | _BV(ISC10);
 	GIMSK |= _BV(INT1);
 
 	/* 	CS02 C021 C020
@@ -66,10 +66,10 @@ void dimmer_init() {
 		 0    1    0       clk/8
 		 0    1    1       clk/64
 		 1    0    0       clk/256
-		 1    1    1       clk/1024	
+		 1    1    1       clk/1024
 */
 	TCCR0 = 0x03;		//clk/64 
-	
+
 	TCNT0 = 256 - 20;
 	TIFR = _BV(TOV0);
 	TIMSK = _BV(TOIE0);	//overflow int on
