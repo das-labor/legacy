4a7f97affaf45aad92ae923a070d56c09dae0cd3
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-04-15 10:43:16 +0000

    canboot: cleanup, mcp regs update

--- microcontroller/src-atmel/bootloader/canboot/canboot_lib/can.h
+++ microcontroller/src-atmel/bootloader/canboot/canboot_lib/can.h
@@ -6,11 +6,6 @@
 /*****************************************************************************
  * Simple CAN Library
  *
- * #define CAN_INTERRUPT 1	//set this to enable interrupt driven 
- *                                and buffering version
- * #define CAN_RX_BUFFER_SIZE 2	//only used for Interrupt
- * #define CAN_TX_BUFFER_SIZE 2	//only used for Interrupt
- */
 
 #include "../config.h"
 
