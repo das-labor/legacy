1be49f8a6fcce5f7c59e15289e37c2c533f7218b
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-04-15 11:24:46 +0000

    netvar_node:

--- microcontroller/src-atmel/automatization2.0/labnode/netvar_node/trunk/lamp_controller.c
+++ microcontroller/src-atmel/automatization2.0/labnode/netvar_node/trunk/lamp_controller.c
@@ -21,7 +21,7 @@
   for the lamp group that is controlled.
   
   additional master sliders for subgroups could be added later.
-    
+  
   additional functionality like labor master power or alarm blink modes
   can be added later to listen on additional netvars
   
@@ -43,64 +43,62 @@
 
 
 void lamp_controller_nv_handler(netvar_desc *nd, void *ref) {
-	lamp_controller_t *s = (lamp_controller_t*) ref;
+	lamp_controller_t *s = (lamp_controller_t *) ref;
 	uint8_t sidx = nd->sidx;
-	
+
 	if ( (sidx >= 0x40) && (sidx < (0x40 + s->num_lamps)) ) {
 		//slider inputs
 		uint8_t i = sidx - 0x40;
 		uint8_t val;
 		netvar_read(nd, &val);
-		
+
 		s->lamp_values[i] = val;
 		netvar_write(s->slider_out_netvars[i], &val); //set all sliders to display new value
-		
+
 		//calculate mean brightnes to set master slider to that value
 		uint16_t master_val = 0;
 		uint8_t x;
 		for (x = 0; x < s->num_lamps; x++) {
-			master_val += s->lamp_values[x]; 
+			master_val += s->lamp_values[x];
 		}
 		master_val /= s->num_lamps;
 		netvar_write(s->master_slider_out_netvar, &master_val); //set master slider
-		
-		
+
 	} else if (sidx == 0x4f) {
 		//master slider
 		
-	
 	}
 }
 
 //create lamp controller on idx with a umber of lamps. a maximum of 15 Lamps is allowed.
-lamp_controller_t * new_lamp_controller(uint16_t idx, uint8_t num_lamps){
-	lamp_controller_t * s = (lamp_controller_t *) malloc(sizeof(lamp_controller_t));
-	netvar_desc * tmp_nv;
+lamp_controller_t *new_lamp_controller(uint16_t idx, uint8_t num_lamps) {
+	lamp_controller_t *s = (lamp_controller_t *) malloc(sizeof(lamp_controller_t));
+	netvar_desc *tmp_nv;
 	uint8_t i;
-	
+
 	s->num_lamps = num_lamps;
-	
+
 	s->lamp_values = malloc(num_lamps);
-	
+
 	s->master_slider_out_netvar = netvar_register(idx, 0x5f, 1); //master slider out
 	tmp_nv = netvar_register(idx, 0x4f, 1); //master slider input
 	netvar_add_handler(tmp_nv, lamp_controller_nv_handler, s);
-		
+
 	s->slider_out_netvars = malloc(sizeof(netvar_desc) * num_lamps);
-	
-	for (i = 0; i < num_lamps; i++){
+
+	for (i = 0; i < num_lamps; i++) {
 		tmp_nv = netvar_register(idx, 0x40 + i, 1); //slider inputs
 		netvar_add_handler(tmp_nv, lamp_controller_nv_handler, s);
-		
+
 		s->slider_out_netvars[i] = netvar_register(idx, 0x50 + i, 1); //slider outputs
 	}
-	
+
 	return s;
 }
 
 
 
-void lamp_controller_tick(lamp_controller_t * lc){
+void lamp_controller_tick(lamp_controller_t *lc) {
 
 }
 
--- microcontroller/src-atmel/automatization2.0/labnode/netvar_node/trunk/lamp_controller.h
+++ microcontroller/src-atmel/automatization2.0/labnode/netvar_node/trunk/lamp_controller.h
@@ -1,21 +1,19 @@
 
-typedef struct{
+typedef struct {
 	uint8_t num_lamps;
-	uint8_t * lamp_values;
+	uint8_t *lamp_values;
 	uint16_t on_mask;
-	
-	netvar_desc ** slider_out_netvars;
-	
-	netvar_desc * master_slider_out_netvar;
-	
-}lamp_controller_t;
+
+	netvar_desc **slider_out_netvars;
+
+	netvar_desc *master_slider_out_netvar;
+
+} lamp_controller_t;
 
 
 //create lamp controller on idx with a umber of lamps. a maximum of 15 Lamps is allowed.
-lamp_controller_t * new_lamp_controller(uint16_t idx, uint8_t num_lamps);
+lamp_controller_t *new_lamp_controller(uint16_t idx, uint8_t num_lamps);
 
 
-void lamp_controller_tick(lamp_controller_t * lc);
-
-
+void lamp_controller_tick(lamp_controller_t *lc);
 
