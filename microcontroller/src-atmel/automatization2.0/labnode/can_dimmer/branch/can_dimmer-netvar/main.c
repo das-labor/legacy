de4e76764a753dbe6f9f5c18c3ed644f77f9150a
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-04-21 14:56:30 +0000

    can_dimmer nv: wip

--- microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/branch/can_dimmer-netvar/main.c
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/branch/can_dimmer-netvar/main.c
@@ -7,45 +7,56 @@
 #include "can/can.h"
 #include "netvar/can_handler.h"
 #include "netvar/netvar.h"
-#include "dimmer.h"
+#include "dimmer/dimmer.h"
+
+
+static volatile uint8_t tickscounter;
+
+ISR(TIMER1_COMPA_vect)
+{
+	tickscounter++;
+}
+
 
 static void init()
 {
-	DDRB |= _BV(PB0); // LED out
+	DDRB |= _BV(PB0); // Labnode LED output
 	//DDRD |= _BV(PD5); // EVG: 0-10V
 
-	//initialize spi port
+	TCCR1B |= _BV(WGM12) | _BV(CS10) | _BV(CS11); // CTC, clk/64
+	// 1000 Hz
+	OCR1A = 250; // pwm timer compare target
+	TIMSK |= _BV(OCIE1A); // Enable Timer1 Overflow Interrupt
+
+	// initialize spi port
 	spi_init();
 
-	//initialize can communication
+	// initialize can communication
 	can_init();
 
 	read_can_addr();
 	wdt_enable(WDTO_250MS); // 250 ms
 	dimmer_init();
+
+	//allow interrupts
+	sei();
 }
 
 int main(void)
 {
-	//system initialization
+	// system initialization
 	init();
-
-
-	//allow interrupts
-	sei();
 
 	//the main loop continuously handles can messages
 	while (1)
 	{
 		can_handler();
-		if (ticks_in_ms >= 10)
-		{
-			cli();
-			ticks_in_ms -= 10;
-			sei();
 
+		if (tickscounter >= 20) {
+			tickscounter = 0;
 			netvar_handle_events();
 		}
+
 		wdt_reset();
 	}
 }
