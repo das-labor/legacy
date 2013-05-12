ccf7d866672c8d7238b1ced8bbbf0a3282543d9d
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-05-12 10:01:21 +0000

    added missing files

---
+++ host-apps/VGAtoIQBaseband/fifo.h
@@ -0,0 +1,18 @@
+#ifndef __FIFO_H_
+#define __FIFO_H_
+#ifdef __cplusplus    
+extern "C" { 
+#endif
+void* 	BufferIn_Start( void );
+void 	BufferIn_Finish( void );
+void* 	BufferOut_Start( void );
+void 	BufferOut_Finish( void );
+int 	BufferFull( void );
+int 	BufferEmpty( void );
+void 	BufferClean( void );
+void 	BufferInit( int bufsize );
+void 	BufferSkip( void );
+#ifdef __cplusplus
+}
+#endif
+#endif
