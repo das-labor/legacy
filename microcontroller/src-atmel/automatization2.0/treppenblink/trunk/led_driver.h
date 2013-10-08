16a0416869ad3159e09e585b9c819ee3420cb189
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-10-08 22:08:16 +0000

    treppenblink: 12 bit LUT im flash - programme nutzen 8 bit farben

--- microcontroller/src-atmel/automatization2.0/treppenblink/trunk/led_driver.h
+++ microcontroller/src-atmel/automatization2.0/treppenblink/trunk/led_driver.h
@@ -9,11 +9,19 @@ enum {
 
 
 // Dots need BGR Format
+#ifdef LED_12BIT
 typedef struct {
 	uint16_t blue;
 	uint16_t green;
 	uint16_t red;
 } dot_t;
+#else
+typedef struct {
+	uint8_t blue;
+	uint8_t green;
+	uint8_t red;
+} dot_t;
+#endif
 
 extern void pushDot(dot_t *dot);
 extern void updateDots(void);
