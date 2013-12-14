2fc3cb445573d3230e4dd67e94e4e8a819b81f68
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-12-14 11:45:12 +0000

    PowerCommander,trunk,avr_interface: fix kuechen dim, cleanup

--- microcontroller/src-atmel/automatization2.0/PowerCommander/trunk/avr_interface/main.c
+++ microcontroller/src-atmel/automatization2.0/PowerCommander/trunk/avr_interface/main.c
@@ -46,7 +46,6 @@ static void init(void)
 	DDRD &= ~(_BV(PD6) | _BV(PD7)); // Eingänge RCD Licht, Server
 	DDRC &= ~_BV(PC2); // Eingang 24V Schützspannung ok
 
-
 	DDRB &= ~_BV(PB2); // Eingang Lounge Taster
 	DDRD &= ~_BV(PD3); // Eingang Vortragsraum Taster
 
@@ -58,32 +57,24 @@ static void init(void)
 		while (1);
 	}
 
-	// get output states
-	sync_stat_cache();
+	sync_stat_cache(); 	// get output states
 
-	//initialize spi port
-	spi_init();
-
-	//initialize can communication
-	can_init();
-
+	spi_init(); // initialize spi port
 	read_can_addr();
+	can_init(); // initialize can communication
 
-	// init lamp matrix
-	init_lamp_control();
+	init_lamp_control(); // init lamp matrix
 
-	//turn on interrupts
-	sei();
+	sei(); // turn on interrupts
 
-	wdt_enable(WDTO_250MS); // 250 ms
+	wdt_enable(WDTO_250MS);
 }
 
 int main(void)
 {
-	//system initialization
-	init();
+	init(); // system initialization
 
-	//the main loop continuously handles can messages
+	// the main loop continuously handles can messages
 	while (1)
 	{
 		can_handler();
