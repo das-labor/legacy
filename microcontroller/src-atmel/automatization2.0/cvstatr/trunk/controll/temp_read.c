d0a1d3c99a6145a0723f39ff5ce14311eb3a0eaa
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-12-22 23:14:17 +0000

    cvstatr controll: fix addr in makefile, cleanup, rote led zeigt sofort das labor aus geht, temperatur seltener senden, code aus anderen projekten, besseres ds1631 default

--- microcontroller/src-atmel/automatization2.0/cvstatr/trunk/controll/temp_read.c
+++ microcontroller/src-atmel/automatization2.0/cvstatr/trunk/controll/temp_read.c
@@ -9,7 +9,7 @@ void temp_sensor_read(void)
 {
 	uint8_t data[2] = {255, 255};
 
-	get_temp_ds1631(data, I2C_ADRESSE_DS1631);
+	ds1631_get_temp(data, I2C_ADRESSE_DS1631);
 	can_send_temp_data(data);
 }
 
