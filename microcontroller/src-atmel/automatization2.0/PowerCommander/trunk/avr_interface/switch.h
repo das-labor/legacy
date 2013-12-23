655a007d3a67120763052252fa1982544d6a025a
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2013-12-23 21:29:49 +0000

    PowerCommander,trunk,avr_interface: fix wrong struct init order, ...

--- microcontroller/src-atmel/automatization2.0/PowerCommander/trunk/avr_interface/switch.h
+++ microcontroller/src-atmel/automatization2.0/PowerCommander/trunk/avr_interface/switch.h
@@ -1,7 +1,6 @@
 #ifndef _POWERCOMMANDER_SWITCH_H
 #define _POWERCOMMANDER_SWITCH_H
 
-
 extern void switch_handler(void);
 extern void switch_timer_init(void);
 
@@ -10,6 +9,7 @@ extern void tog_dimdir_kueche(void);
 extern void dim_vortrag(void);
 extern void dim_kueche(void);
 extern void toggle_vortrag(void);
+extern void toggle_kueche(void);
 
 #endif // _POWERCOMMANDER_SWITCH_H
 
