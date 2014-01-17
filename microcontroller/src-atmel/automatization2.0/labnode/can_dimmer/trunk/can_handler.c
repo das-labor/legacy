f684b936d7e61f7a35fddc845d4d02e87eb62486
siro <siro@b1d182e4-1ff8-0310-901f-bddb46175740>
2014-01-17 16:11:13 +0000

    move gamma correction to dimmer.c, fix status packets

--- microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/can_handler.c
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/can_handler.c
@@ -1,7 +1,6 @@
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <avr/eeprom.h>
-#include <avr/pgmspace.h>
 
 #include "can/can.h"
 #include "can_handler.h"
@@ -23,30 +22,12 @@ static uint8_t virt_stat = 0;
 extern void start_counter(uint16_t countdown);
 extern uint8_t get_counter_status(void);
 
-//lookuptable for gamma corretion
-static const uint8_t exptab[256] PROGMEM =
-{
-0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,5,5,
-5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,8,9,9,9,9,9,10,10,10,
-10,10,11,11,11,11,12,12,12,12,13,13,13,14,14,14,14,15,15,15,16,16,16,17,
-17,18,18,18,19,19,20,20,21,21,21,22,22,23,23,24,24,25,25,26,27,27,28,28,
-29,30,30,31,32,32,33,34,35,35,36,37,38,39,39,40,41,42,43,44,45,46,47,48,
-49,50,51,52,53,55,56,57,58,59,61,62,63,65,66,68,69,71,72,74,76,77,79,81,
-82,84,86,88,90,92,94,96,98,100,102,105,107,109,112,114,117,119,122,124,
-127,130,133,136,139,142,145,148,151,155,158,162,165,169,172,176,180,184,
-188,192,196,201,205,210,214,219,224,229,234,239,244,250,255
-};
-
 static void virt_pwm_set_all(uint8_t val) {
-	//do gammacorrection
-
 	set_dimmer(0, val);
 	set_dimmer(1, val);
 	set_dimmer(2, val);
-	set_dimmer(3, (255 - pgm_read_byte(exptab + 255 - val)));
+	set_dimmer(3, val);
 }
-
 
 void can_handler()
 {
@@ -144,10 +125,7 @@ void can_handler()
 
 							if (rx_msg->data[1] < NUM_DIMMER_CHANNELS)
 							{
-								if (rx_msg->data[1] == 3)	//channel 3
-									set_dimmer(3, 255-rx_msg->data[2]);	//invert neon tube
-								else
-									set_dimmer(rx_msg->data[1], rx_msg->data[2]);
+                                set_dimmer(rx_msg->data[1], rx_msg->data[2]);
 								can_send_status(rx_msg->addr_src);
 							}
 							break;
@@ -206,7 +184,7 @@ void can_handler()
 							break;
 
 						case 1: // Lamp brightness
-							set_dimmer(rx_msg->data[1], rx_msg->data[2]);
+                            set_dimmer(rx_msg->data[1], rx_msg->data[2]); 
 							can_send_status(rx_msg->addr_src);
 							break;
 
@@ -223,10 +201,7 @@ void can_handler()
 							break;
 
 						case 4: // All Lamp brightness
-							set_dimmer(0, rx_msg->data[2]);
-							set_dimmer(1, rx_msg->data[2]);
-							set_dimmer(2, rx_msg->data[2]);
-							set_dimmer(3, (255 - pgm_read_byte(exptab + 255 - rx_msg->data[2]))); // gamma correction for neon tube
+                            virt_pwm_set_all(rx_msg->data[2]);
 							can_send_status(rx_msg->addr_src);
 							break;
 
@@ -261,7 +236,7 @@ void can_send_status(uint8_t addr)
 	msg->data[1] = dim_vals_8bit[0];
 	msg->data[2] = dim_vals_8bit[1];
 	msg->data[3] = dim_vals_8bit[2];
-	msg->data[4] = 255 - dim_vals_8bit[3];
+	msg->data[4] = dim_vals_8bit[3];
 	can_transmit(msg);
 }
 
