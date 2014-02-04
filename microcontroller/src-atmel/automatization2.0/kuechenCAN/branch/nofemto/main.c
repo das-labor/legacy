50c8700af06ec130840f8a2ec563efeb7e399352
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2014-02-04 23:33:16 +0000

    kuechencan: fix temp reading

--- microcontroller/src-atmel/automatization2.0/kuechenCAN/branch/nofemto/main.c
+++ microcontroller/src-atmel/automatization2.0/kuechenCAN/branch/nofemto/main.c
@@ -67,7 +67,7 @@ int main(void)
 			netvar_handle_events();
 #endif
 			send_temp_counter++;
-			if (send_temp_counter > 1000) {
+			if (send_temp_counter > 5000) {
 				temp_sensor_read();
 				send_temp_counter = 0;
 			}
