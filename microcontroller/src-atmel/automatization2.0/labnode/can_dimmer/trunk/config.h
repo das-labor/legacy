288cbbaceb725dd76d63847233ed2b996e1a36fd
pl4nkton <pl4nkton@b1d182e4-1ff8-0310-901f-bddb46175740>
2014-03-10 23:03:07 +0000

    can_dimmer: use faster can int

--- microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/config.h
+++ microcontroller/src-atmel/automatization2.0/labnode/can_dimmer/trunk/config.h
@@ -5,16 +5,16 @@
 #define MCP_CS_BIT    PB4
 #define MCP_CS_PORT   PORTB
 
-#define SPI_REG_PIN_MCP_INT  PINB
-#define SPI_PIN_MCP_INT      PB2
+#define SPI_REG_PIN_MCP_INT PINB
+#define SPI_PIN_MCP_INT     PB2
 
 #define CAN_INTERRUPT
 #define CAN_INT_NOBLOCK
 
-#define   ENABLE_CAN_INT()    GICR |= _BV(INT2)
-#define   DISABLE_CAN_INT()   GICR &= ~_BV(INT2)
-#define   SETUP_CAN_INT()     MCUCSR &= ~_BV(ISC2)
-#define   MCP_INT_VEC         INT2_vect
+#define ENABLE_CAN_INT()    GICR |= _BV(INT2)
+#define DISABLE_CAN_INT()   GICR &= ~_BV(INT2)
+#define SETUP_CAN_INT()     MCUCSR &= ~_BV(ISC2)
+#define MCP_INT_VEC         INT2_vect
 
 #define CAN_TX_BUFFER_SIZE 4
 #define CAN_RX_BUFFER_SIZE 4
@@ -23,6 +23,7 @@
 
 #define NUM_DIMMER_CHANNELS 4
 
+#define OPTIMISED_LAP
 
 #endif // ifndef CONFIG_H
 
