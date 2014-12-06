fea3c7efd13c90937e53802331d386a6f2e40374
dr4go <dr4go@b1d182e4-1ff8-0310-901f-bddb46175740>
2014-12-06 21:26:51 +0000

    can-ir: add support for serial beamer controll and i2c sound controll

---
+++ microcontroller/src-atmel/automatization2.0/can-ir/trunk/beamer_rs232.h
@@ -0,0 +1,8 @@
+#ifndef _BEAMER_RS232_H
+#define _BEAMER_RS232_H
+
+extern void rs232_receive_handler(void);
+extern void rs232_send_command(uint8_t cmd);
+extern void poll_beamer_state(void);
+
+#endif // _BEAMER_RS232_H
