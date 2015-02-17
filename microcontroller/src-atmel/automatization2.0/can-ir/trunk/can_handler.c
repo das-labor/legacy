31123f5f9947f17b56b242a8b77d9c9381dd0d42
dr4go <dr4go@b1d182e4-1ff8-0310-901f-bddb46175740>
2015-02-17 20:11:24 +0000

    can-ir: cleanup

--- microcontroller/src-atmel/automatization2.0/can-ir/trunk/can_handler.c
+++ microcontroller/src-atmel/automatization2.0/can-ir/trunk/can_handler.c
@@ -36,68 +36,61 @@ void can_handler(void)
 						break;
 							default:
 						break;
-					}
-				}
-				//handle ir commands
-				else if (rx_msg->port_dst == PORT_REMOTE) { // 0x21
-					//switch the remote device type
-					switch (rx_msg->data[0]) {
-						//0 was teufel ir
-						//this is a message for the acer beamer
-						case 1:
-							PORTD |= _BV(PD7); // Enable debug LED
-							beamer_send_command(rx_msg->data[1]);
-							break;
-						// 2 was beamer IR
-						case 3:
-							setAllChannels(rx_msg->data[1]);
-							break;
-						case 4:
-							setSingleChannel(rx_msg->data[1], rx_msg->data[2]);
-							break;
-						case 5:
-							incrementChannels(rx_msg->data[2]);
-							break;
-						case 6:
-							setMute(rx_msg->data[1]);
-							break;
-						default:
-							break;
-					}
 				}
 			}
-			/* Nach Hauptschütz an nachricht mit verzögerung default werte an teufel system senden
-			 * Wenn Beamer Strom hat lamp status (auflösung / laufzeit / src) pollen
-			 * beim labor abschalten beamer abschalten, nachlaufen lassen, strom trennen
-			 */
-			if (rx_msg->addr_src == 0x02) { // get powercommander status
-				static uint8_t status = 0;
-				switch (rx_msg->port_src) {
-					case 0x02:
-						if ((rx_msg->data[1] & 0x01) && !(status & _BV(1))) { // Hauptschütz an
-							TeufelPoweron();
-							status |= _BV(1);
-						}
-						else if (!(rx_msg->data[1] & 0x01) && status & _BV(1)) {
-							status &= ~_BV(1);
-							beamer_start_shutdown();
-						}
-						if (rx_msg->data[1] & 0x02) { // Beamer Schütz an
-							set_beamer_power(1);
-						}
-						else if (!(rx_msg->data[1] & 0x02)) { // Beamer Schütz aus
-							set_beamer_power(0);
-						}
+			//handle ir commands
+			else if (rx_msg->port_dst == PORT_REMOTE) { // 0x21
+				//switch the remote device type
+				switch (rx_msg->data[0]) {
+					//0 was teufel ir
+					//this is a message for the acer beamer
+					case 1:
+						PORTD |= _BV(PD7); // Enable debug LED
+						beamer_send_command(rx_msg->data[1]);
 						break;
-					/*case 0x03:
-						if (rx_msg->data[0] & 0x01 && rx_msg->data[1] == 0) { // Hauptschalter an
-							status |= _BV(0);
-						} else {
-							status &= ~_BV(0);
-						}
-						break;*/
+					// 2 was beamer IR
+					case 3:
+						setAllChannels(rx_msg->data[1]);
+						break;
+					case 4:
+						setSingleChannel(rx_msg->data[1], rx_msg->data[2]);
+						break;
+					case 5:
+						incrementChannels(rx_msg->data[2]);
+						break;
+					case 6:
+						setMute(rx_msg->data[1]);
+						break;
+					default:
+						break;
 				}
 			}
+		}
+		/* Nach Hauptschütz an nachricht mit verzögerung default werte an teufel system senden
+		 * Wenn Beamer Strom hat lamp status (auflösung / laufzeit / src) pollen
+		 * beim labor abschalten beamer abschalten, nachlaufen lassen, strom trennen
+		 */
+		if (rx_msg->addr_src == 0x02) { // get powercommander status
+			static uint8_t status = 0;
+			switch (rx_msg->port_src) {
+				case 0x02:
+					if ((rx_msg->data[1] & 0x01) && !(status & _BV(1))) { // Hauptschütz an
+						TeufelPoweron();
+						status |= _BV(1);
+					}
+					else if (!(rx_msg->data[1] & 0x01) && status & _BV(1)) {
+						status &= ~_BV(1);
+						beamer_start_shutdown();
+					}
+					if (rx_msg->data[1] & 0x02) { // Beamer Schütz an
+						set_beamer_power(1);
+					}
+					else if (!(rx_msg->data[1] & 0x02)) { // Beamer Schütz aus
+						set_beamer_power(0);
+					}
+					break;
+			}
+		}
 		can_free(rx_msg);
 	}
 }
@@ -150,4 +143,3 @@ void can_read_addr(void)
 {
 	myaddr = eeprom_read_byte(&EE_lap_addr);
 }
-
