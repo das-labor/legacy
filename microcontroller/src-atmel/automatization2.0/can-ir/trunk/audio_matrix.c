bfeebe295c0161527b6adb2bf43729a24f1012bf
dr4go <dr4go@b1d182e4-1ff8-0310-901f-bddb46175740>
2015-07-21 22:34:43 +0000

    can-ir: audio_matrix: fix most bugs, add can interface

--- microcontroller/src-atmel/automatization2.0/can-ir/trunk/audio_matrix.c
+++ microcontroller/src-atmel/automatization2.0/can-ir/trunk/audio_matrix.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <avr/io.h>
-
+#include "can_handler.h"
 #include "i2c_pca9555.h"
 #include "i2c_tea6420.h"
 
@@ -17,28 +17,23 @@ enum {
 	LED_IN1,
 } AUDIO_MATRIX_LEDS;
 
-#define ALL_LEDS _BV(LED_OUT1) | _BV(LED_OUT2) | _BV(LED_OUT3) | _BV(LED_OUT4) | _BV(LED_POWER) | _BV(LED_IN1) | _BV(LED_IN2) | _BV(LED_IN3) | _BV(LED_IN4) | _BV(LED_IN5)
+#define ALL_LEDS (_BV(LED_OUT1) | _BV(LED_OUT2) | _BV(LED_OUT3) | _BV(LED_OUT4) | _BV(LED_POWER) | _BV(LED_IN1) | _BV(LED_IN2) | _BV(LED_IN3) | _BV(LED_IN4) | _BV(LED_IN5))
 
-static uint8_t led_out_state[4];
 
-void set_input_led(void)
+static void set_input_led(void) // set led according to the matrix config
 {
-	uint8_t input[5];
-	uint8_t led_set = pca9555_get_output();
+	uint8_t input[5] = {0, 0, 0, 0, 0};
+	uint16_t led_set = pca9555_get_output();
 
-	for (uint8_t i = 0; i < 4; i++)
-		if ((tea6420_get_state(i) & (_BV(I2) | _BV(I1) | _BV(I0))) < MUTE) {
-			input[tea6420_get_state(i) & (_BV(I2) | _BV(I1) | _BV(I0))]++;
-			led_out_state[i] = (tea6420_get_state(i) & (_BV(I2) | _BV(I1) | _BV(I0))) + 1;
-		}
-		else
-			led_out_state[i] = 0;
+	for (uint8_t i = 0; i < 4; i++) // go throuh outputs and check for used inputs
+		if ((tea6420_get_state(i) & (_BV(I2) | _BV(I1) | _BV(I0))) < MUTE)
+			input[tea6420_get_state(i) & (_BV(I2) | _BV(I1) | _BV(I0))]++; // mark used channels
 
 	for (uint8_t i = 0; i < 5; i++)
 		if (input[i])
-			led_set &= ~(_BV(8 + 5 - i)); // offset, leds are reversed
+			led_set &= ~(_BV(8 + 4 - i)); // offset, leds are reversed
 		else
-			led_set |= (_BV(8 + 5 - i));
+			led_set |= (_BV(8 + 4 - i));
 
 	pca9555_write_word(led_set, PCA9555_OUT0); // schreibe 16 bit in output 1 & 2
 }
@@ -52,22 +47,36 @@ static void am_set_default(void)
 	set_input_led();
 }
 
+void am_set_output(uint8_t input, uint8_t output)
+{
+	tea6420_connect_i2o(input, (tea6420_get_state(output) & (_BV(G0) | _BV(G1))) >> G0, output);
+	set_input_led();
+	lap_send_matrix_status(&tea6420_get_state);
+}
+
+void am_set_gain(uint8_t gain, uint8_t output)
+{
+	tea6420_connect_i2o(tea6420_get_state(output) & (_BV(I2) | _BV(I1) | _BV(I0)), gain, output);
+	set_input_led();
+	lap_send_matrix_status(&tea6420_get_state);
+}
+
 void am_set_power_led(uint8_t state)
 {
-/*	uint8_t led_set;
+	uint16_t led_set;
 	if (state)
 		led_set = pca9555_get_output() & ~_BV(LED_POWER);
 	else
 		led_set = pca9555_get_output() | ALL_LEDS;
-	
-	pca9555_write_word(led_set, PCA9555_OUT0); // schreibe 16 bit in output 1 & 2*/
+
+	pca9555_write_word(led_set, PCA9555_OUT0); // schreibe 16 bit in output 1 & 2
 	if (state)
 		am_set_default();
 }
 
 void am_init(void)
 {
-	pca9555_write_word(ALL_LEDS, PCA9555_CFG0); // set to output ports
+	pca9555_write_word(~ALL_LEDS, PCA9555_CFG0); // set to output ports
 	am_set_power_led(1);
 }
 
@@ -76,26 +85,26 @@ void am_led_tick(void)
 {
 	static uint8_t counter;
 	static uint8_t led_counter[4];
-	uint8_t led_set = _BV(LED_OUT1) | _BV(LED_OUT2) | _BV(LED_OUT3) | _BV(LED_OUT4);
+	uint16_t led_set = pca9555_get_output();
 
-	if (counter < 30) {
+	if (counter < 12) {
 		if (counter < 10)
 			for (uint8_t i = 0; i < 4; i++) {
-				if (!counter && led_out_state[i]) {
-					led_counter[i] = led_out_state[i];
-					led_set = pca9555_get_output() & ~_BV(i);
+				if (!counter && (tea6420_get_state(i) & (_BV(I2) | _BV(I1) | _BV(I0))) + 1) {
+					led_counter[i] = (tea6420_get_state(i) & (_BV(I2) | _BV(I1) | _BV(I0))) + 1;
+					led_set &= ~_BV(i);
 				}
 				else if (counter % 2) {
 					if (led_counter[i]) {
 						led_counter[i]--;
-						led_set = pca9555_get_output() | _BV(i);
+						led_set |= _BV(i);
 					}
 				}
 				else if (led_counter[i])
-					led_set = pca9555_get_output() & ~_BV(i);
+					led_set &= ~_BV(i);
 					
-				pca9555_write_word(led_set, PCA9555_OUT0); // schreibe 16 bit in output 1 & 2
 			}
+		pca9555_write_word(led_set, PCA9555_OUT0); // schreibe 16 bit in output 1 & 2
 		counter++;
 	}
 	else
--- microcontroller/src-atmel/automatization2.0/can-ir/trunk/audio_matrix.h
+++ microcontroller/src-atmel/automatization2.0/can-ir/trunk/audio_matrix.h
@@ -3,3 +3,5 @@
 void am_init(void);
 void am_set_power_led(uint8_t state);
 void am_led_tick(void);
+void am_set_output(uint8_t input, uint8_t output);
+void am_set_gain(uint8_t gain, uint8_t output);
